f709adbbaf651b1caff78feffa3949a20fb26f3e
ddmcdonald50 <ddmcdonald50@gmail.com>
2011-09-23 14:59:56 +0000

    Wrong version of digits file

--- Sparser/code/s/grammar/rules/edges/digits.lisp
+++ Sparser/code/s/grammar/rules/edges/digits.lisp
@@ -1,9 +1,9 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1990,1991,1992,1993.1994,1995  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1990-1999  David D. McDonald  -- all rights reserved
 ;;; 
 ;;;     File:  "digits"
-;;;   Module:  "analyzers;psp:edges:"
-;;;  Version:  2.0 August 1995
+;;;   Module:  "grammar;rules:edges:"
+;;;  Version:  1.11 July 1999
 
 ;;1.1  (1/3/91 v1.6) Moved to this file the routine that's called from
 ;;     assessing unknown digit sequences.
@@ -15,7 +15,17 @@
 ;;1.6  (3/30/94) adding used-in data and real daughters.
 ;;1.7  (4/27) reordered the knitting in after that used-in calc. to fix a bug where
 ;;      the edge appeared to be used in itself
-;;2.0  (8/30/95) moved the used-in after complete
+;;     (9/23/97) Tweaked Make-edge-over-unknown-digit-sequence to gate its use
+;;      of model-based operations.
+;; 1.8 (10/13) adjusted it again to provide an integer referent when the
+;;      model isn't loaded, and to get its edge label through a global
+;; 1.9 (7/5/98) Changed the category to be referential if the model is loaded.
+;; 1.10 (6/8/99) modified Make-edge-over-digit-sequence to return the edges
+;;       that it spanned as well as the new edge. 6/22, modified it to make
+;;       a call to annotate-number.
+;; 1.11 (7/18/99) Changed the arguments that Make-edge-over-unknown-digit-sequence
+;;       passes to construct-temporary-number so we won't have forgotten the
+;;       word that we created -- modivated by nlg.
 
 (in-package :sparser)
 
@@ -30,7 +40,8 @@
   (let ((edge (next-edge-from-resource))
         (starting-vector (pos-starts-here starting-position))
         (ending-vector   (pos-ends-here   ending-position))
-        (leftmost-daughter (highest-preterminal-at starting-position)))
+        (leftmost-daughter (highest-preterminal-at starting-position))
+        daughter-edges  )
 
     (setf (edge-starts-at edge) starting-vector)
     (setf (edge-ends-at   edge) ending-vector)
@@ -38,25 +49,29 @@
     (setf (edge-left-daughter  edge) leftmost-daughter)
     (setf (edge-right-daughter edge) :digit-based-number)
 
-    (setf (edge-category edge) category::number)
+    (setf (edge-category edge) 
+          (if *include-model-facilities*
+            (find-or-make-category-object 'number :referential)
+            category::number))
+
     (setf (edge-form edge) nil)
 
+    (setq daughter-edges
+          (if (eq ending-position 
+                  (chart-position-after starting-position))
+            ;; then there's just the one daughter that we've already found
+            (set-used-by leftmost-daughter edge)
+            (set-used-by/anonymous-daughters
+             starting-position ending-position edge)))
+
     (knit-edge-into-positions edge starting-vector ending-vector)
+
+    (assess-edge-label category::number edge)
 
     (when *trace-edge-creation*
       (format t "~&~%creating ~A for a known or constructed number"
               edge))
-
-    (if (eq ending-position (chart-position-after starting-position))
-      ;; then there's just the one daughter that we've already found
-      (set-used-by leftmost-daughter edge)
-      (set-used-by/anonymous-daughters
-       starting-position ending-position edge))
-
-    (assess-edge-label category::number edge)
-
-    edge ))
-
+    (values edge daughter-edges)))
 
 
 (defun make-edge-over-number-word-sequence (starting-position
@@ -76,13 +91,12 @@
     (setf (edge-left-daughter  edge) leftmost-daughter)
     (setf (edge-right-daughter edge) :word-based-number)
 
-    (setf (edge-category edge) category::number)
+    (setf (edge-category edge) 
+          (if *include-model-facilities*
+            (find-or-make-category-object 'number :referential)
+            category::number))
     (setf (edge-form     edge) nil)
     (setf (edge-referent edge) number)
-
-    (when *trace-edge-creation*
-      (format t "~&~%creating ~A for a known or constructed number"
-              edge))
 
     (if (eq ending-position (chart-position-after starting-position))
       ;; then there's just the one daughter that we've already found
@@ -92,6 +106,9 @@
 
     (assess-edge-label category::number edge)
 
+    (when *trace-edge-creation*
+      (format t "~&~%creating ~A for a known or constructed number"
+              edge))
     edge ))
 
 
@@ -106,8 +123,10 @@
     (let ((edge (next-edge-from-resource))
           (starting-vector (pos-starts-here position))
           (ending-vector   (pos-ends-here   next-position))
-          (number (construct-temporary-number digit-string ;; word
-                                              )))
+          (number 
+           (if *include-model-facilities*
+             (construct-temporary-number word digit-string nil)
+             (parse-integer digit-string))))
 
       (setf (edge-starts-at edge) starting-vector)
       (setf (edge-ends-at   edge) ending-vector)
@@ -116,7 +135,7 @@
       (setf (edge-left-daughter  edge) word)
       (setf (edge-right-daughter edge) :single-digit-sequence)
 
-      (setf (edge-category edge) category::digit-sequence)
+      (setf (edge-category edge) *the-category-of-digit-sequences*)
       (setf (edge-form edge) nil)
       (setf (edge-referent edge) number)
 
@@ -127,5 +146,10 @@
       (when *trace-edge-creation*
         (format t "~&~%creating ~A for the unknown digit sequence ~A"
                 edge word))
+
+      (when *include-model-facilities*
+        (setq *parent-edge-getting-reference* edge)
+        (annotate-number number :unknown-digit-sequence nil))
+
       edge )))
 
