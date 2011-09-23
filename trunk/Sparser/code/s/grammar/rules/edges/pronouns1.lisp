95b6b7fd5a23515ee38aae111aed4a4f4aa3e549
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2011-09-23 15:15:42 +0000

    wrong version for pronouns

--- trunk/Sparser/code/s/grammar/rules/edges/pronouns1.lisp
+++ trunk/Sparser/code/s/grammar/rules/edges/pronouns1.lisp
@@ -1,13 +1,14 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1992,1993,1994,1995  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1991  Content Technologies Inc.
+;;; copyright (c) 1992  David D. McDonald  -- all rights reserved
 ;;;
 ;;;      File:   "pronouns"
 ;;;    Module:   "analyzers;psp:"
-;;;   Version:   2.0 August 1995
+;;;   Version:   1.1 May 1993
 
+;; initiated 1/91
 ;; 1.0 (9/7/92 v2.3) flushed out of date field references
 ;; 1.1 (5/14/93) changed to using Set-used-by as a field-setter
-;; 2.0 (8/30/95) moved the used-in after complete
 
 (in-package :sparser)
 
@@ -31,6 +32,7 @@
     (setf (edge-rule edge) :respan-over-pronoun)
     (setf (edge-left-daughter edge)  pn-edge)
     (setf (edge-right-daughter edge) :single-term)
+    (set-used-by pn-edge edge)
 
     (setf (edge-form edge)     form)
     (setf (edge-referent edge) referent)
@@ -39,10 +41,6 @@
       (format t "~&~%creating ~A from ~A~
                    ~%    rule: ~A" edge pn-edge :respan-over-pronoun)) 
     (complete edge)
-
-    (set-used-by pn-edge edge)
-
     (assess-edge-label category edge)
-
     edge ))
 
