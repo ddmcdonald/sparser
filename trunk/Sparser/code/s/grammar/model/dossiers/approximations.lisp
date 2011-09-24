d837bbc31d3c1c26f20ada41b24f4b4397309b9f
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2011-09-24 02:14:20 +0000

    Added 'just' and reorganized

--- trunk/Sparser/code/s/grammar/model/dossiers/approximations.lisp
+++ trunk/Sparser/code/s/grammar/model/dossiers/approximations.lisp
@@ -1,5 +1,5 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1993,1994 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1993,1994,2011 David D. McDonald  -- all rights reserved
 ;;;
 ;;;     File:  "approximations"
 ;;;   Module:  "model;dossiers:"
@@ -7,16 +7,17 @@
 
 ;; initiated 6/6/93 v2.3.  5/27/94 removed "several".
 ;; 0.1 (11/17) distinguished determiners from adverbs
+;;     (9/23/11) Added "just"
 
 (in-package :sparser)
 
-
+(define-approximator/determiner "almost")
 (define-approximator/determiner "about")
 (define-approximator/determiner "around")
+(define-approximator/determiner "just")
 
 (define-approximator/adverbial "fairly")
 (define-approximator/adverbial "only")
 
-;; 11/15
-(define-approximator/determiner "almost")
+
 
