6b44f2a46932903dfe540d76f8c952d776ce1d1b
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2011-09-24 02:03:34 +0000

    Made over their referents as feeding the modified method

--- trunk/Sparser/code/s/grammar/rules/tree-families/adverbs.lisp
+++ trunk/Sparser/code/s/grammar/rules/tree-families/adverbs.lisp
@@ -5,12 +5,13 @@
 ;;;
 ;;;     File:  "adverbs"
 ;;;   Module:  "grammar;rules:tree-families:"
-;;;  version:  August 2011
+;;;  version:  September 2011
 
 ;; initiated 3/9/05
 ;; 0.1 (5/20/09) Added verb+ing and verb cases to pre-verb-adverb
 ;; 0.2 (8/8/11) Finished the cases with the other form options for verbs
 ;;     and added post-verb-adverb
+;; 0.3 (9/23/11) Converting the referents to a method application.
 
 (in-package :sparser)
 
@@ -21,59 +22,70 @@
      sentence-adverb ------------ pre/post S & VP
 |#
 
+;; The cases in these tree families all produce form rules, where the
+;; adverb is the concrete category and the heads are all form categories.
+;; There are no assumptions about the structure of the head in the
+;; referents. Instead everything is mediated by the CLOS method
+;; named modified, which can be specialized by both the adverb and
+;; the head. See base cases in rules/syntax/adverbs1.lisp.
+
+;; The modified method's arguments are <modifier>, <modifiers>, so the
+;; adverb edge has to be first in the call, and the head second.
+
+
 (define-exploded-tree-family pre-verb-adverb
   :description "Creates form rules for all possibilities of the adverb in front of the verb."
-  :binding-parameters ( modifier )
+  :binding-parameters ( )
   :labels ( adverb )
   :cases
     ((:modifier (verb (adverb verb)
                   :head right-edge
-                  :binds (modifier left-edge)))
+                  :method (modified left-edge right-edge)))
      (:modifier (verb+present (adverb verb+present)
                   :head right-edge
-                  :binds (modifier left-edge)))
+                  :method (modified left-edge right-edge)))
      (:modifier (verb+ed (adverb verb+ed)
                   :head right-edge
-                  :binds (modifier left-edge)))
+                  :method (modified left-edge right-edge)))
      (:modifier (verb+ing (adverb verb+ing)
                   :head right-edge
-                  :binds (modifier left-edge)))))
+                  :method (modified left-edge right-edge)))))
 
 (define-exploded-tree-family post-verb-adverb
   :description "Creates form rules for all possibilities of the adverb following the verb."
-  :binding-parameters ( modifier )
+  :binding-parameters ( )
   :labels ( adverb )
   :cases
     ((:modifier (verb (verb adverb)
                   :head left-edge
-                  :binds (modifier right-edge)))
+                  :method (modifier right-edge left-edge)))
      (:modifier (verb+present (verb+present adverb)
                   :head left-edge
-                  :binds (modifier right-edge)))
+                  :method (modifier right-edge left-edge)))
      (:modifier (verb+ed (verb+ed adverb)
                   :head left-edge
-                  :binds (modifier right-edge)))
+                  :method (modifier right-edge left-edge)))
      (:modifier (verb+ing (verb+ing adverb)
                   :head left-edge
-                  :binds (modifier right-edge)))))
+                  :method (modifier right-edge left-edge)))))
 
 (define-exploded-tree-family sentence-adverb
   :description ""
-  :binding-parameters ( modifier )
+  :binding-parameters ( )
   :labels ( adverb )
   :cases
     ((:modifier (s (adverb s)
                   :head right-edge
-                  :binds (modifier left-edge)))
+                  :method (modifier left-edge right-edge)))
      (:modifier (vp (adverb vp)
                   :head right-edge
-                  :binds (modifier left-edge)))
+                  :method (modifier left-edge right-edge)))
      (:modifier (s (s adverb)
                   :head left-edge
-                  :binds (modifier right-edge)))
+                  :method (modifier right-edge left-edge)))
      (:modifier (vp (vp adverb)
                   :head left-edge
-                  :binds (modifier right-edge)))))
+                  :method (modifier right-edge left-edge)))))
 
 
 
