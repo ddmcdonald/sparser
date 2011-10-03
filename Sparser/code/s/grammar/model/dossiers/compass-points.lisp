5b9d704a976bb61e8fc9d17cb79fc206201bccfb
ddmcdonald50 <ddmcdonald50@gmail.com>
2011-10-03 18:44:01 +0000

    Added abbreviations

--- Sparser/code/s/grammar/model/dossiers/compass-points.lisp
+++ Sparser/code/s/grammar/model/dossiers/compass-points.lisp
@@ -1,32 +1,32 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
-;;; copyright (c) 1995  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1995,2011 David D. McDonald  -- all rights reserved
 ;;; extensions copyright (c) 2007 BBNT Solutions LLC. All Rights Reserved
 ;;;
 ;;;     File:  "compass points"
 ;;;   Module:  "model;dossiers:"
-;;;  version:  September 2007
+;;;  version:  October 2011
 
 ;; initiated in 1/9/95. Added more cases 9/5/07
 
 (in-package :sparser)
 
 ;; n.b. define-compass-point adds an adjective form with the "ern" suffix
-;; in the non-ward cases
+;; in the non-ward cases. Added abbreviations 10/3/11
 
-(define-compass-point "north")
-(define-compass-point "south")
-(define-compass-point "east")
-(define-compass-point "west")
+(define-compass-point "north" "N")
+(define-compass-point "south" "S")
+(define-compass-point "east"  "E")
+(define-compass-point "west"  "W")
 
-(define-compass-point "northeast")
-(define-compass-point "southeast")
+(define-compass-point "northeast" "NE")
+(define-compass-point "southeast" "SE")
 
-(define-compass-point "northwest")
-(define-compass-point "southwest")
+(define-compass-point "northwest" "NW")
+(define-compass-point "southwest" "SW")
 
-(define-compass-point "northward" :ward)
-(define-compass-point "southward" :ward)
-(define-compass-point "eastward" :ward)
-(define-compass-point "westward" :ward)
+(define-compass-point "northward" nil :ward)
+(define-compass-point "southward" nil :ward)
+(define-compass-point "eastward" nil :ward)
+(define-compass-point "westward" nil :ward)
 
 
