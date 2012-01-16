df98ea5505465af538b75063ab1913c46b7b357d
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2012-01-16 03:33:28 +0000

    lots of incremental improvements

--- trunk/Sparser/code/s/grammar/model/core/places/relational.lisp
+++ trunk/Sparser/code/s/grammar/model/core/places/relational.lisp
@@ -4,13 +4,13 @@
 ;;;
 ;;;     File:  "relational"
 ;;;   Module:  "model;core:places:"
-;;;  version:  September 2011
+;;;  version:  October 2011
 
 ;; Extracted from object file 7/21/11. Evicerated it 9/28 in lieu of
 ;; doing something more nuanced. Removed the category/individuals
 ;; spatial-orientation because the list in the spatial-prepositions
 ;; dossier includes many that server other functions as well such
-;; as organizing time. 
+;; as organizing time. 10/27 fixed issue with the form rules
 
 (in-package :sparser)
 
@@ -36,12 +36,14 @@
 
 (def-form-rule (spatial-preposition location)
   :form pp
+  :head :right-edge
   :referent (:instantiate-individual relative-location
              :with (place right-edge
                     functor left-edge)))
 
 (def-form-rule (spatio-temporal-preposition location)
   :form pp
+  :head :right-edge
   :referent (:instantiate-individual relative-location
              :with (place right-edge
                     functor left-edge)))
