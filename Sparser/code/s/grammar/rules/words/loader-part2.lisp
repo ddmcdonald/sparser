52c6447ac5401c164cbec9dbd0436c67d21ce5bb
ddmcdonald50 <ddmcdonald50@gmail.com>
2012-01-16 02:46:47 +0000

    prepositions moved here since they now require the code in the syntax to be loaded first

--- Sparser/code/s/grammar/rules/words/loader-part2.lisp
+++ Sparser/code/s/grammar/rules/words/loader-part2.lisp
@@ -9,10 +9,16 @@
 ;; defining function words that want categorial interpretations
 ;; like the adverbs do. Positioned in load-the-grammar after
 ;; the tree-families and upper-model just before syntax is
-;; loaded.
+;; loaded. 9/29/11 Moved in prepositions2 and repositioned this
+;; after the adjuncts since the prepositions need them.
 
 (in-package :sparser)
 
 (gate-grammar *adverbs*
   (gload "words;adverbs1"))
+
+(gate-grammar *prepositions*
+  (if *include-model-facilities*
+    (gload "words;prepositions2")
+    (gload "words;prepositions1")))
 
