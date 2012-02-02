3f2efd948d27cd865d5c1ca0d52a46b6aaaec4c4
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2012-02-02 14:21:36 +0000

    Small start on doing hurricanes seriously

---
+++ trunk/Sparser/code/s/grammar/model/sl/hurricanes/loader.lisp
@@ -0,0 +1,14 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
+;;; copyright (c) 2011  David D. McDonald  -- all rights reserved
+;;;
+;;;     File:  "loader"
+;;;   Module:  "grammar/model/sl/hurricanes/"
+;;;  version:  December 2011
+
+;; Initiated 12/16/11
+
+(in-package :sparser)
+
+(gload "hurricanes;storms")
+(gload "hurricanes;object")
+(gload "hurricanes;workspace")
