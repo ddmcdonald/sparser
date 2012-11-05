e8fdf9040d5c15350b21b11dbc110782c3660579
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2012-11-05 16:32:07 +0000

    Guarded the symbol-value lookup because of cases when introducing words via Comlex where you can get a word started but not finished right away.

--- trunk/Sparser/code/s/analyzers/tokenizer/lookup2.lisp
+++ trunk/Sparser/code/s/analyzers/tokenizer/lookup2.lisp
@@ -1,14 +1,17 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
 ;;; copyright (c) 1990  Content Technologies Inc.
-;;; copyright (c) 1992  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992,2012  David D. McDonald  -- all rights reserved
 ;;; 
 ;;;     File:  "lookup"
 ;;;   Module:  "tokenizer;"
-;;;  Version:  2.0 September 1992
+;;;  Version:  2.0 November 2012
 
 ;;  1.1 (v1.6 12/14/90) Cleans up the mess in Lookup as part of doing :ignore
 ;;      unknown words.
 ;;  2.0 (9/15/92 v2.3) utterly revamped to handle the different tokenizer
+;;      (11/5/12) Guarded the symbol-value lookup because of cases when
+;;       introducing words via Comlex where you can get a word started
+;;       but not finished right away.
 
 (in-package :sparser)
 
@@ -16,6 +19,8 @@
 (defun find-word (char-type)
   (let ((symbol (lookup-word-symbol)))
     (if symbol
-      (symbol-value symbol)
+      (if (boundp symbol)
+        (symbol-value symbol)
+        (establish-unknown-word char-type))
       (establish-unknown-word char-type))))
 
