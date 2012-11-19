a8e822c046df7a7b1b794dbc6430045d735fec5c
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2012-11-19 16:22:35 +0000

    Added either and neither

--- trunk/Sparser/code/s/grammar/rules/words/determiners.lisp
+++ trunk/Sparser/code/s/grammar/rules/words/determiners.lisp
@@ -1,11 +1,10 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
-;;; copyright (c) 1992,1993,1994,1995  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992-1995,2012  David D. McDonald  -- all rights reserved
 ;;; extensions copyright (c) 2008 BBNT Solutions LLC. All Rights Reserved
-;;; $Id$
 ;;;
 ;;;      File:   "determiners"
 ;;;    Module:   "grammar;rules:words:"
-;;;   Version:   0.6 June 2008
+;;;   Version:   0.6 November 2012
 
 ;; broken out from "fn words - cases" 12/17/92 v2.3
 ;; 0.1 (6/8) added Define-determiner
@@ -14,7 +13,7 @@
 ;; 0.4 (12/5) flushed "additional" which had been included because of LS's judgement
 ;; 0.5 (1/30/95) gave 'that' its own '[' bracket
 ;; 0.6 (6/16/08) Adding a set of brackets for the single-capitalized-letter
-;;      form of "a". 
+;;      form of "a". (11/10/12) neither, either.
 
 (in-package :sparser)
 
@@ -35,7 +34,10 @@
 (define-determiner "this"  :brackets '( ].phrase .[np ))
 
 (define-determiner "these"  :brackets '( ].phrase .[np ))
-(define-determiner "those"  :brackets '( ].phrase .[np ))
+
+(define-determiner "either"  :brackets '( ].phrase .[np ))
+(define-determiner "neither"  :brackets '( ].phrase .[np ))
+
 
 
 ;; gets its own brackets because of its functional ambiguity
