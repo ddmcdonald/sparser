229522f06f0cfaaf6bfc6d03a04bcd38f8257982
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2012-11-19 16:13:06 +0000

    Bumped annotation to 1 to free it up for potentially extensive changes while retaining a reference copy

--- trunk/Sparser/code/s/objects/model/lattice-points/loader1.lisp
+++ trunk/Sparser/code/s/objects/model/lattice-points/loader1.lisp
@@ -1,11 +1,10 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
-;;; copyright (c) 1997-2000 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1997-2000,2012 David D. McDonald  -- all rights reserved
 ;;; extensions copyright (c) 2007-2009 BBNT Solutions LLC. All Rights Reserved
-;;; $Id:$
 ;;;
 ;;;     File:  "loader"
 ;;;   Module:  "objects;model:lattice-points:"
-;;;  version:  October 2009
+;;;  version:  November 2012
 
 ;; initiated 11/29/97. 3/7/98 added annotation, psi. 7/9 added c+v.
 ;; 7/29 broke up "psi" into find and make. 9/12 added runtime. 
@@ -16,13 +15,15 @@
 ;;   [find or make at runtime]=>1 v+v-objects=>2 traverse=>1.
 ;;   7/23 operations=>1 8/6 find=>1, make=>1. 10/23 Added ad-hoc-annotation.
 ;;   10/27 moved it to model/sl/poirot/ because it references the ltml package
+;;   11/12/12 Bumped annotation to 1. Added rnodes.
 
 (in-package :sparser)
 
 (lload "lattice-points;v+v objects2")
 (lload "lattice-points;c+v objects")
+(lload "lattice-points;rnodes")
 (lload "lattice-points;printers")
-(lload "lattice-points;annotation")
+(lload "lattice-points;annotation1")
 (lload "lattice-points;make1")
 (lload "lattice-points;find1")
 (lload "lattice-points;find or make at runtime1")
