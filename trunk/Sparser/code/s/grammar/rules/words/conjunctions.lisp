226a3dc66593faa130341dd646bd43ecea31007f
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2012-11-25 21:26:25 +0000

    Added subordinate conjunctions from Comlex and packaged definition into a function

--- trunk/Sparser/code/s/grammar/rules/words/conjunctions.lisp
+++ trunk/Sparser/code/s/grammar/rules/words/conjunctions.lisp
@@ -1,43 +1,87 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
-;;; copyright (c) 1992,1993,1994  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992-1994,2012  David D. McDonald  -- all rights reserved
 ;;; extensions copyright (c) 2007 BBNT Solutions LLC. All Rights Reserved
 ;;; $Id:$
 ;;;
 ;;;      File:   "conjunctions"
 ;;;    Module:   "grammar;rules:words:"
-;;;   Version:   0.1 September 1994
+;;;   Version:   0.2 November 2012
 
 ;; broken out from "fn words - cases" 12/17/92 v2.3.  Added some 1/11/94
 ;; added conjunction.] to "and" and "or" 3/17
 ;; 0.1 (5/12) removed conjunction.] -- solved segmentation problem better.
 ;;     (9/27) added "instead"
 ;;     (4/17/10] Added "thus"
+;; 0.2 (11/22/12) reorganized the list under a simpler scheme of passing
+;;      it through one function and added in a bunch from Comlex (particulary
+;;      the multi-word ones). 
 
 (in-package :sparser)
 
+
+;;--- Package up part of define-function-word, 
+;;      allowing some options in the form category
+
+(defun define-isolated-function-word (string &key form)
+  (unless form
+    (setq form 'subordinate-conjunction))
+  (define-function-word string
+    :brackets '( ].phrase  phrase.[ )
+    :form form))
+    
+
+;;------- simple conjunctions
 
 (define-function-word "and" :brackets '( ].conjunction conjunction.[ ))
 (define-function-word "or"  :brackets '( ].conjunction conjunction.[ ))
 
 
+;;------- paired coordinators
+
+(mapcar #'define-isolated-function-word
+        '("neither" "nor"
+          "if" "then" 
+          ))
+
 ;;;------ subordinating conjunctions
 
-(define-function-word "although" :brackets '( ].phrase  phrase.[ ))
-(define-function-word "among"    :brackets '( ].phrase  phrase.[ ))
-(define-function-word "because"  :brackets '( ].phrase  phrase.[ ))
-(define-function-word "but"      :brackets '( ].phrase  phrase.[ ))
-(define-function-word "even"     :brackets '( ].phrase  phrase.[ ))
-(define-function-word "however"  :brackets '( ].phrase  phrase.[ ))
-(define-function-word "instead"  :brackets '( ].phrase  phrase.[ ))
-(define-function-word "since"    :brackets '( ].phrase  phrase.[ ))
-(define-function-word "so"       :brackets '( ].phrase  phrase.[ ))
-(define-function-word "thus"     :brackets '( ].phrase  phrase.[ ))
-(define-function-word "whenever" :brackets '( ].phrase  phrase.[ ))
-(define-function-word "while"    :brackets '( ].phrase  phrase.[ ))
-(define-function-word "unless"   :brackets '( ].phrase  phrase.[ ))
-(define-function-word "until"    :brackets '( ].phrase  phrase.[ ))
+(mapcar #'define-isolated-function-word
+        '(
+          "according as"
+          "albeit"
+          "although" 
+          "among" 
+          "as far as"
+          "as if"
+          "as soon as"
+          "as though"
+          "because"
+          "besides"
+          "but"    
+          "even"
+          "except"
+          "except that"
+          "however"
+          "instead" 
+          "inasmuch as"
+          "lest"
+          "on grounds that"
+          "on the grounds that"
+          "once"
+          "only"
+          "otherwise"
+          "prior to"
+          "since"  
+          "so"
+          "such as"
+          "such that"
+          "though"
+          "till"
+          "thus"   
+          "while"   
+          "unless"  
+          "until"   
+          "upon"
+          ))
 
-
-(define-function-word "if"    :brackets '( ].phrase  phrase.[ ))
-(define-function-word "then"  :brackets '( ].phrase  phrase.[ ))
 
