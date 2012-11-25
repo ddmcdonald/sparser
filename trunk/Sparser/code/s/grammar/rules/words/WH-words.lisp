55414ae9fa2f43ffe29a1f5ce0f0b6f6411c5bdd
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2012-11-25 21:27:04 +0000

    Added xxx-ever cases

--- trunk/Sparser/code/s/grammar/rules/words/WH-words.lisp
+++ trunk/Sparser/code/s/grammar/rules/words/WH-words.lisp
@@ -1,13 +1,14 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
-;;; copyright (c) 1992,1993,1994  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992-1994,2012 David D. McDonald  -- all rights reserved
 ;;;
 ;;;      File:   "WH words"
 ;;;    Module:   "grammar;rules:words:"
-;;;   Version:   0.1 June 1994
+;;;   Version:   0.1 November 2012
 
 ;; broken out from "fn words - cases" 12/17/92 v2.3
 ;; 0.1 (6/18/93) added  .[np  np]. brackets
 ;;     (1/11/94) added some more.  6/8 added "whether"   10/24 added "how many"
+;;     (11/23/12) "whenever", "wherever"
 
 (in-package :sparser)
 
@@ -17,7 +18,9 @@
 (define-function-word "which" :brackets '( ].phrase .[np  np].  phrase.[ ))
 (define-function-word "what"  :brackets '( ].phrase .[np  np].  phrase.[ ))
 (define-function-word "where" :brackets '( ].phrase .[np  np].  phrase.[ ))
-(define-function-word "when"  :brackets '( ].phrase .[np  np].  phrase.[ ))
+(define-function-word "wherever" :brackets '( ].phrase .[np  np].  phrase.[ ))
+(define-function-word "when"     :brackets '( ].phrase .[np  np].  phrase.[ ))
+(define-function-word "whenever" :brackets '( ].phrase .[np  np].  phrase.[ ))
 (define-function-word "whom"  :brackets '( ].phrase .[np  np].  phrase.[ ))
 (define-function-word "whose" :brackets '( ].phrase .[np  np].  phrase.[ ))
 (define-function-word "why"   :brackets '( ].phrase .[np  np].  phrase.[ ))
@@ -25,3 +28,4 @@
 (define-function-word "whether" :brackets '( ].phrase .[np  np].  phrase.[ ))
 
 (define-function-word "how many" :brackets '( ].phrase .[np  np].  phrase.[ ))
+
