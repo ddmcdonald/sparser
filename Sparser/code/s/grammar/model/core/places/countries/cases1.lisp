669d615311fbd33d0282b4e439a46c3f0cbf9b0c
ddmcdonald50 <ddmcdonald50@gmail.com>
2012-11-26 14:06:02 +0000

    Moved contents to dossier

--- Sparser/code/s/grammar/model/core/places/countries/cases1.lisp
+++ Sparser/code/s/grammar/model/core/places/countries/cases1.lisp
@@ -1,34 +1,17 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
 ;;; copyright (c) 1990,1991  Content Technologies Inc.
-;;; copyright (c) 1992,1993  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992-1993,2012  David D. McDonald  -- all rights reserved
 ;;;
 ;;;     File:  "cases"
 ;;;   Module:  "model;core:names:places:countries:"
-;;;  version:  1.0 September 1993
+;;;  version:  1.1 November 2012
 
 ;; initiated in December 1990, v1.6
 ;; 1.0 (10/12/92 v2.1) introducing new semantics
 ;;     (9/7 v2.3) revived the existing set of cases
+;; 1.1 (11/24/12) pulled in  Charlie G's dump for IL MDIS 
+;;     work circa January 2010 from state.gov
 
 (in-package :sparser)
 
-
-(define-country "Britian" :adjective "British")
-
-(define-country "Canada"  :adjective "Canadian")
-
-(define-country "Germany" :adjective "German")
-
-(define-country "India"   :adjective "Indian")
-
-(define-country "Japan"   :adjective "Japanese")
-
-(define-country "Kenya"   :adjective "Kenyian")
-
-(define-country "Sweden"  :adjective "Swedish")
-
-(define-country "United States"
-                :aliases '("US" "U.S." "U.S.A."
-                           "America"
-                            ))
-
+;;  moved to dossiers/countries.lisp
