e20b99a66bc3850d8b75870e17d7ab2fd2543c77
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2012-11-27 15:18:34 +0000

    Added an ignore to quiet compiler

--- trunk/Sparser/code/s/analyzers/psp/referent/composite-referent.lisp
+++ trunk/Sparser/code/s/analyzers/psp/referent/composite-referent.lisp
@@ -1,13 +1,14 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 2005 David D. McDonald  -- all rights reserved
+;;; copyright (c) 2005,2012 David D. McDonald  -- all rights reserved
 ;;;
 ;;;      File:   "composite referent"
 ;;;    Module:   "analyzers;psp:referent:"
-;;;   Version:   0.1 February 2005
+;;;   Version:   0.1 November 2012
 
 ;; initiated 4/12/00. Extended through 5/4.
 ;; 0.1 (2/9/05) Can't remember what this helped to do. Perhaps it's involved with
-;;   annotation. So commenting out the structure to see what breaks
+;;   annotation. So commenting out the structure to see what breaks. Added
+;;   and ignore 11/25/12 to quiet the compiler.
 
 ;;;---------
 ;;; object
@@ -42,6 +43,7 @@
 (defun all-the-categories-in-a-composite (composite)
   (unless (typep composite 'composite-referent)
     (error "Threading error: ~a isn't a composite" composite))
+  #+ignore
   (let ( categories )
     (push (category-of (cr-head composite)) categories)
     (dolist (referent (cr-others composite))
