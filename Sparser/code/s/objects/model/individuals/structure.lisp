2b19ac029ec6c7365952b3d9756d34c73bf3945d
ddmcdonald50 <ddmcdonald50@gmail.com>
2012-12-27 17:30:03 +0000

    Added 'shadow' field to hold the class instance made for the individual

--- Sparser/code/s/objects/model/individuals/structure.lisp
+++ Sparser/code/s/objects/model/individuals/structure.lisp
@@ -1,13 +1,14 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1994-1998 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1994-1998.2012 David D. McDonald  -- all rights reserved
 ;;;
 ;;;     File:  "structure"
 ;;;   Module:  "objects;model:individuals:"
-;;;  version:  June 1998
+;;;  version:  December 2012
 
 ;; broken out from [object] file 8/12/94
 ;; (6/30/98) added rnodes field to hold the realization annotations
-;; that the individual receives. 
+;; that the individual receives. (12/14/12) Addes shadow to support
+;; regular lisp methods.
 
 (in-package :sparser)
 
@@ -26,5 +27,7 @@
   binds
   bound-in
   rnodes
+
+  shadow
   )
 
