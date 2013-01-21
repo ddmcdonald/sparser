b1558c055070bf3cda374249ec16211976f06c68
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-01-21 21:49:23 +0000

    Since it's now easy to do a full (modern) treatment of function rules in one compact call, putting all the new cases called for by the as-yet-untyped cases in dossiers/modifier in this new file 'others'

--- trunk/Sparser/code/s/grammar/model/core/adjuncts/loader.lisp
+++ trunk/Sparser/code/s/grammar/model/core/adjuncts/loader.lisp
@@ -1,12 +1,12 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1994-1997  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1994-1997,2013 David D. McDonald  -- all rights reserved
 ;;;
 ;;;      File:  "loader"
 ;;;    Module:  "grammar;model:core:adjuncts:"
-;;;   version:  June 1996
+;;;   version:  January 2013
 
 ;; initiated 5/25/94 v2.3 merging files from next level down. 6/25/96 added
-;; gate around the autodef'inition
+;; gate around the autodef'inition. 1/18/13 Added [others]
 
 (in-package :sparser)
 
@@ -35,4 +35,7 @@
 
 (gate-grammar *sequencers*
   (gload "sequence;object"))
+
+(gate-grammar *standard-adjuncts*
+  (gload "adjuncts;others"))
 
