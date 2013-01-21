58886bcdf20ca3d2ad08c5e0f9923ab10e791bab
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-01-21 21:32:06 +0000

    Added two short articles to add some variation

--- trunk/Sparser/code/s/init/workspaces/dm&p.lisp
+++ trunk/Sparser/code/s/init/workspaces/dm&p.lisp
@@ -1,11 +1,12 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1995  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1995,2012  David D. McDonald  -- all rights reserved
 ;;;
 ;;;     File:  "dm&p"
 ;;;   Module:  "init;workspaces:"
-;;;  version:  January 1995
+;;;  version:  January 2013
 
-;; broken out from [init;versions:v2.3a:workspace:in progress] 1/30/95
+;; broken out from [init;versions:v2.3a:workspace:in progress] 1/30/95.
+;; Added another test case 1/19/13.
 
 (in-package :sparser)
 
@@ -73,3 +74,28 @@ the fire was under control, but not out.  It was not known
 how many people were in the building at the time of the fire.  
 The cinema was a small private club showing pornographic files,
 according to initial reports."))  ;; (Reuters)
+
+(defun medtronic ()
+  ;; from Charlie G.'s summarization set. 
+  (pp "Will Medtronic's pulse quicken?
+Medical device giant Medtronic (MDT), the leader in 
+defibrillators and pacemakers, has been a market laggard. 
+The stock slumped from 60 in January to 49.19 on May 17. 
+But some investors say it may beat analysts' consensus forecast 
+of 62 cents a share when it reports earnings for 
+its fiscal fourth quarter on May 23. Among the few bulls: 
+Investment firm Harris Nesbitt's Joanne Wuensch, 
+who continues to rate it \"outperform,\" with 
+a 12-month target of 62. Still, some worry Medtronic 
+may deliver bad news. Not only has growth slowed in 
+the cardiology market, but rival St. Jude Medical (SJM) 
+also missed its quarterly sales forecast. 
+Wuensch counters that Medtronic is \"more insulated 
+from the implantable-device market sways than its brethren.\" 
+It generates 27% of revenues from them, vs. St. Jude's 36%. 
+Wuensch sees Medtronic earning $2.09 a share on sales 
+of $11.3 billion this year, and $2.38 on $12.6 billion in 2007. 
+David Sowerby, portfolio manager at investment firm Loomis Sayles, 
+which owns shares, says the stock is \"compelling\" near 
+its 10-year low, especially as he expects Medtronic to 
+gain market share and show double-digit earnings growth in 2007."))
