f947368be89a593ce28b643e6b0e31ece26fe0e8
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-01-21 21:50:08 +0000

    Simplified it

--- Sparser/code/s/grammar/model/core/adjuncts/approx/object.lisp
+++ Sparser/code/s/grammar/model/core/adjuncts/approx/object.lisp
@@ -1,5 +1,5 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
-;;; copyright (c) 1991-1995,2011 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1991-1995,2011-2013 David D. McDonald  -- all rights reserved
 ;;;
 ;;;     File:  "object"
 ;;;   Module:  "model;core:adjuncts:approx:"
@@ -12,6 +12,10 @@
 ;;     (9/12) tweeked the autodef
 ;; 0.3 (9/23/11) Reworked adverbial to use define-adverb. Reworked
 ;;      the category a little. Reworked determiner case as quantifiers
+;; 0.3 (1/4/13) Prety drastic reworking to feed define-function-term
+;;      with parameters as an approximator rather than instantiate
+;;      one per-se as was done in the 'original' code that's commented
+;;      out. 
 
 (in-package :sparser)
 
@@ -27,14 +31,6 @@
   :realization (:word name))
 
 
-(define-autodef-data 'approximator
-  :module *approximators*
-  :display-string "approximators"
-  :not-instantiable t
-  :description "a word that says that something is not exact"
-  :examples "\"about\" \"fairly\" \"only\"" )
-
-
 ;;;----------------
 ;;; defining forms
 ;;;----------------
@@ -42,38 +38,13 @@
 #| To get the brackets right we have to distinguish determiners
    from adverbs. |#
 
-(define-category approximator/determiner)
-(define-category approximator/adverbial)
 
-
-(defun define-approximator/determiner (string &key rules)
-  (unless rules
-    (setq rules '(det)))
-  (define-quantifier string
-    :brackets '( ].phrase .[np )
-      :rules rules))
-#|  original
-  (let* ((obj (define-individual 'approximator
-                :name string))
-         (word (resolve-string-to-word string)))
-    (assign-bracket/expr word ].phrase )
-    (assign-bracket/expr word .[np )
-    (let ((cfr (car (get-tag-for :rules obj))))
-      (unless (cfr-p cfr)
-        (break "expected the rule for this approximator to be ~
-                on the plist:~%~A~%" obj))
-      (setf (cfr-form cfr) category::det)
-      obj ))  |#
-
-(define-autodef-data  'approximator/determiner
-  :display-string "determiners"
-  :form 'define-approximator/determiner
-  :dossier "dossiers;approximations"
-  :module *approximators*
-  :description "an approximator that can begin a noun phrase"
-  :examples "\"about\" \"around\"" )
-
-
+(defun define-approximator/determiner (string &key tree-families)
+  (unless tree-families
+    (setq tree-families '(generic-np-premodifier)))
+  (define-function-term string 'det
+    :super-category 'approximator
+    :tree-families tree-families))
 
 
 (defun define-approximator/adverbial (string)
@@ -81,7 +52,8 @@
     :brackets '( ].adverb .[adverb adverb.[ )
     :super-category 'approximator))
 
-#|  original
+#|  original form of define-approximator/adverbial,
+      rhe approximator/determiner was essentially the same
   (let* ((obj (define-individual 'approximator
                 :name string))
          (word (resolve-string-to-word string)))
@@ -98,6 +70,26 @@
       
       obj ))  |#
 
+
+;;--- Autodef
+
+(define-category approximator/determiner)
+(define-category approximator/adverbial)
+
+(define-autodef-data 'approximator
+  :module *approximators*
+  :display-string "approximators"
+  :not-instantiable t
+  :description "a word that says that something is not exact"
+  :examples "\"about\" \"fairly\" \"only\"" )
+
+(define-autodef-data  'approximator/determiner
+  :display-string "determiners"
+  :form 'define-approximator/determiner
+  :dossier "dossiers;approximations"
+  :module *approximators*
+  :description "an approximator that can begin a noun phrase"
+  :examples "\"about\" \"around\"" )
 
 (define-autodef-data  'approximator/adverbial
   :display-string "adverbs"
