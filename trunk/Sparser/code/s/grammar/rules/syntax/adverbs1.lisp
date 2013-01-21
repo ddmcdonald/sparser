370416b5f0a5213f87e0a3838ded1ca2cc2c0475
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-01-21 21:40:34 +0000

    Rewrote the method pretty extensively

--- trunk/Sparser/code/s/grammar/rules/syntax/adverbs1.lisp
+++ trunk/Sparser/code/s/grammar/rules/syntax/adverbs1.lisp
@@ -1,11 +1,11 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1993-1995,2011 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1993-1995,2011-2013 David D. McDonald  -- all rights reserved
 ;;; extensions copyright (c) 2008 BBNT Solutions LLC. All Rights Reserved
 ;;; $Id:$
 ;;; 
 ;;;     File:  "adverbs"
 ;;;   Module:  "grammar;rules:syntax:"
-;;;  Version:  1.0 September 2011
+;;;  Version:  1.1 January 2013
 
 ;; initiated 5/16/93 v2.3, added form rules 6/6
 ;; 0.1 (10/13/95) worded out a real analysis and moved in the def form for
@@ -13,30 +13,34 @@
 ;; 0.2 (4/24/08) moved the category and def form out to words/adverbs.
 ;; 1.0 (9/19/11) Flushed the explicit form rule in favor of generated set
 ;;      by using the tree-family. See words;adverbs1. Put in the generic
-;;      method and abstract one using revived modified category
+;;      method and abstract one using revived modified category.
+;; 1.1 (1/18/13) Moved everything else out except for the method
+;;      that is used by the adverb tree families. 
 
 (in-package :sparser)
 
+;;;-----------
+;;; Base case
+;;;-----------
 
-(define-category  modified
-  ;; analogous to quantified -- both should have same parent
-  :specializes nil
-  :instantiates :self
-  :binds ((modifier . modifier)
-          (body)))
+;; N.b. the value returned by the method becomes the referent of
+;; the edge that spans the modifier and what it modifies. Since
+;; the thing being modified is usually the head, we typically just 
+;; want to do something to that head and then return it
 
-
-(defgeneric modified (modifier modified)
+(defgeneric modified (modifier head)
   (:documentation "Motivated by adverbs like 'just' and 'almost',
  but could be used with adjectives or anything else. The default
- method just added another binding to the pair to record the
- event. Specific cases (by the category of the specific adverb)
+ method creates an instance of the modifies casegory to record
+ the relationship between the two terms, and then returns the
+ head. Specific cases (by the category of the specific adverb)
  could be more ambitious."))
 
-(defmethod modified ((adv sh::adverbial) (body t))
+(defmethod modified ((adv sh::modifier) (head t))
   (let ((real-adv (dereference-shadow-individual adv))
-        (real-body (dereference-shadow-individual body)))
-    (define-individual 'modified
+        (real-head (dereference-shadow-individual head)))
+    (define-individual 'modifies
       :modifier real-adv
-      :body real-body)))
+      :modified real-head)
+    head))
 
