13a6483a059ac9b511edad28b369f33853978d51
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-01-21 21:36:12 +0000

    Moved out the cases to dossiers/modifiers

--- trunk/Sparser/code/s/grammar/rules/words/adverbs1.lisp
+++ trunk/Sparser/code/s/grammar/rules/words/adverbs1.lisp
@@ -1,11 +1,11 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1993-1995,2011-2012 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1993-1995,2011-2013 David D. McDonald  -- all rights reserved
 ;;; extensions copyright (c) 2008-2010 BBNT Solutions LLC. All Rights Reserved
 ;;; $Id:$
 ;;; 
 ;;;     File:  "adverbs"
 ;;;   Module:  "grammar;rules:words:"
-;;;  Version:  1.0 December 2012
+;;;  Version:  1.2 January 2013
 
 ;; initiated 5/16/93 v2.3. Populated 1/11/94. Added to 1/12,1/13
 ;; 0.1 (5/26) redid the bracket label as 'adverb'
@@ -27,26 +27,22 @@
 ;; 1.2 (12/15/12) Reworked it extensively so the category for the adverb
 ;;      is used in the cfr rather than 'adverbial' and we make an instance 
 ;;      of the category to serve as the referent, along with a shadow
-;;      instance to use in the methods. 
+;;      instance to use in the methods. (1/9/13) Moving out all
+;;      the words per se to dossiers;modified so they can go through more
+;;      specific definitions rather than the old two-step dance that had
+;;      so much inconsistency.
 
 (in-package :sparser)
 
 ;;;------------------------------------------------------------------------
 ;;; 
 ;;;------------------------------------------------------------------------
-
-#+ignore ;; Do we want this? How do we do it with individual adverb rules?
-(define-additional-realization adverbial
-  (:tree-family pre-verb-adverb
-   :mapping ((adverb . :self)))
-  (:tree-family post-verb-adverb
-   :mapping ((adverb . :self)))
-  (:tree-family sentence-adverb
-   :mapping ((adverb . :self))))
-
   
 ;;  This is one of the few places where I'd be comfortable with
 ;;  form+form rules.
+
+;; N.b. Generalized this code into define-function-term
+;; So should change this over too to avoid gratuitous digression. 
 
 (defun define-adverb (string &key brackets super-category)
   ;; Create a category for the word that specialization of adverbial
@@ -86,122 +82,3 @@
                    :referent individual)))
             (push-onto-plist category rule :rule)))))
     category))
-
-;;;---------- adverb adverbs
-
-;; Since these don't start verb groups, they can't be marked 'adverb'
-
-(define-function-word "too"   :brackets '( ].adverb .[adverb ))
-(define-function-word "very"  :brackets '( ].adverb .[adverb ))
-
-
-
-;;;---------- approximators
-
-;; (A)  "I have just seven days left"
-;; (B)  "I just came back from there"
-;;
-;;  These bind to their left,
-;;  and they terminate whatever is ongoing
-
-;; Definitions moved to dossiers/approximations 9/23/11
-(define-function-word "about"   :brackets '( ].adverb ))
-(define-function-word "around"  :brackets '( ].adverb ))
-
-
-(define-function-word "fairly"  :brackets '( ].adverb ))
-
-(define-function-word "just"    :brackets '( ].adverb ))
-(define-function-word "only"    :brackets '( ].adverb ))
-
-
-
-;;;---------- comparatives
-;;
-;; All of these are determiners
-
-;; These combine to their right with adverbs
-;;
-(define-function-word "less"   :brackets '( ].adverb ))
-(define-function-word "more"   :brackets '( ].adverb ))
-(define-function-word "most"   :brackets '( ].adverb ))
-
-;; These don't combine with adverbs
-;;
-(define-function-word "fewer" :brackets '( ].adverb  ))
-
-
-;; ??
-(define-function-word "than" )
-
-
-;;;---------- frequency
-
-(define-function-word "always"     :brackets '( ].adverb ))
-(define-function-word "frequently" :brackets '( ].adverb ))
-(define-function-word "never"      :brackets '( ].adverb ))
-(define-function-word "often"      :brackets '( ].adverb ))
-(define-function-word "rarely"     :brackets '( ].adverb ))
-(define-function-word "seldom"     :brackets '( ].adverb ))
-(define-function-word "usually"    :brackets '( ].adverb ))
-
-
-;;;---------- position within a process
-; These can be positioned sentence initial or final as well
-; a pre-verb, so they need bracketing that can indicate
-; segment starts too.
-
-(define-function-word "initially"    :brackets '( ].adverb  adverb.[ ))
-(define-function-word "finally"    :brackets '( ].adverb  adverb.[ ))
-(define-function-word "eventually"    :brackets '( ].adverb  adverb.[ ))
-
-
-;;;---------- likelyhood
-
-(define-function-word "probably"   :brackets '( ].adverb ))
-
-
-
-;;;---------- sequencers, can start NPs
-
-(define-function-word "last"   :brackets '( ].adverb .[np ))
-(define-function-word "next"   :brackets '( ].adverb .[np  ))
-
-
-;;;------
-;;; time
-;;;------
-
-;;;---------- deictic, standalone
-
-(define-function-word "immediately" :brackets '( ].adverb  adverb.[ ))
-(define-function-word "soon"        :brackets '( ].adverb  adverb.[ ))
-
-
-;;;---------- deictic, complement-taking
-
-(define-function-word "earlier"  :brackets '( ].adverb  adverb.[ ))
-(define-function-word "later"  :brackets '( ].adverb  adverb.[ ))
-
-
-;;;------------
-;;; {attitude}
-;;;------------
-;  Take nominalized clauses/participials
-
-(define-function-word "despite" :brackets '( ].adverb .[phrase ))
-;; If we had a "starts clause" bracket that might help
-
-(define-function-word "in spite of" :brackets '( ].adverb .[np ))
-(define-function-word "regardless of" :brackets '( ].adverb .[np ))
-
-
-;;;------------
-;;; enablement
-;;;------------
-
-;; These take interesting complements. Refine the brackets?
-(define-function-word "in order to" :brackets '(].phrase .[phrase))
-(define-function-word "so that" :brackets '(].phrase .[phrase))
-
-
