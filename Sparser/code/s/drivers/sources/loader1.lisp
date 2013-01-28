3438be3dea3423f84247a5b1aa02e7ed7750353f
scoofry <scoofry@c2db2450-100d-c977-add4-9986513c572c>
2013-01-28 19:18:40 +0000

    Directory loader, which takes a directory string and processes all files therein in the same chart.

--- Sparser/code/s/drivers/sources/loader1.lisp
+++ Sparser/code/s/drivers/sources/loader1.lisp
@@ -1,6 +1,6 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
 ;;; copyright (c) 1991,1992,1993,1994  David D. McDonald  -- all rights reserved
-;;; 
+;;;
 ;;;     File:  "loader"
 ;;;   Module:  "drivers;sources:"
 ;;;  Version:  1.2 August 1994
@@ -15,6 +15,7 @@
 (lload "source-drivers;core1")
 (lload "source-drivers;string")
 (lload "source-drivers;file1")
+(lload "source-drivers;directory")
 (lload "source-drivers;open file")
 
 (when *orthographic-structure*
