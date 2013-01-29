eb5836a03ed5cdb23807892a59b0cc91a4040349
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-01-29 20:56:41 +0000

    Cleaned up

--- Sparser/code/s/grammar/rules/SDM&P/create-categories.lisp
+++ Sparser/code/s/grammar/rules/SDM&P/create-categories.lisp
@@ -1,9 +1,10 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER COMMON-LISP) -*-
+;;; copyright (c) 2013  David D. McDonald  -- all rights reserved
 ;;; Copyright (c) 2007-2010 BBNT Solutions LLC. All Rights Reserved
-;;; $Id:$
 
 ;;;      File: "create-categories"
 ;;;    Module: "grammar;rules:SDM&P:
-;;;   Version: 0.2 April 2010
+;;;   Version: 0.2 January 2013
 
 ;; Initiated 2/9/07. Elaborated through 8/6. Refactored the head form
 ;; elevator 2/4/08. Added cases through 4/24, then through 6/16.
@@ -12,7 +13,9 @@
 ;; settings. (2/10/10) Considered extending reify-segment-head-if-needed
 ;; for the case of literal words in rules, but it lead to coniptions with "."
 ;; 0.2 4/17/10 Uppercase category names were returning nil. Changed 
-;;  elevation of segment edge of verb cases ti vg from VP. 
+;;  elevation of segment edge of verb cases to vg from VP. 
+;;    (1/23/12) cleaned up. Trying to find duplication
+
 
 (in-package :sparser)
 
@@ -28,8 +31,8 @@
 	     category::number)) ;; "the one that"
         (category::quantifier) ;; "[just no] pleasing"
 	((or category::common-noun
-	   category::common-noun/plural
-	   category::proper-noun) ;; over a name-word 
+             category::common-noun/plural
+             category::proper-noun) ;; over a name-word 
 	 (setf (edge-form edge) (category-named 'np-head)))
 	(category::modal) ;; "can can they ...
 	(category::verb) ;; "are are"
@@ -43,27 +46,43 @@
 	   (break "New case of form category of edge over segment head: ~a"
 		(edge-form edge))))))))
 
-(defun elevate-segment-edge-form-if-needed (edge)
+
+;; CCl complains that there's a duplicate keyform in here but I don't see it
+(defun generalize-segment-edge-form-if-needed (edge)
   (let* ((form-category (edge-form edge))
 	 (symbol (when form-category (cat-symbol form-category))))
     (when symbol
       (case symbol
-	((or category::np category::proper-name category::proper-noun))
+        ;; no change cases
+	((or category::np 
+             category::proper-name
+             category::proper-noun))
+
+	(category::adjunct)
+
+        ;; cases where we want to generalize
 	((or category::n-bar
 	     category::number
 	     category::common-noun
 	     category::common-noun/plural
 	     category::np-head)
 	 (setf (edge-form edge) (category-named 'np)))
-	((or category::verb category::verb+s category::verb+ed
-	     category::verb+ing category::verb+present
+
+	((or category::verb 
+             category::verb+s 
+             category::verb+ed
+	     category::verb+ing 
+             category::verb+present
 	     category::verb+passive)
 	 (setf (edge-form edge) (category-named 'vg)))
-	(category::adjunct)
+
 	(otherwise
 	 (unless *cfg-flag*
+           (push-debug `(,edge))
 	   (break "New case of form category of edge over segment: ~a"
 		  (edge-form edge))))))))
+
+
 
 
 (defun reify-segment-head-if-needed ()
