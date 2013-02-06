5edbb937e137c0a17d9629580fa5bd83f92dbda8
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-02-06 20:55:37 +0000

    Cleanup and refactor driving the no-space scanners

--- trunk/Sparser/code/s/analyzers/psp/patterns/loader.lisp
+++ trunk/Sparser/code/s/analyzers/psp/patterns/loader.lisp
@@ -1,13 +1,18 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1995  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1995,2013  David D. McDonald  -- all rights reserved
 ;;;
 ;;;     File:  "loader"
 ;;;   Module:  "analysers;psp:patterns:"
 ;;;  version:  October 1995
 
-;; initiated 9/21/95. Moved over [take transitions] 10/5
+;; initiated 9/21/95. Moved over [take transitions] 10/5.
+;; 2/5/2013 broke out the traces and the uniform scan as their own files
 
 (in-package :sparser)
+
+(lload "scan-patterns;traces")
+
+(lload "scan-patterns;uniform-scan")
 
 (lload "scan-patterns;take transitions")
 (lload "scan-patterns;start")  ;; empty as of 10/5
