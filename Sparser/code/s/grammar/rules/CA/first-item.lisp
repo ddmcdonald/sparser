574677e8ee3e30c72b9526bca477f3f0e9c15888
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-02-06 20:44:48 +0000

    Improved the ability to detect sentences start.

--- Sparser/code/s/grammar/rules/CA/first-item.lisp
+++ Sparser/code/s/grammar/rules/CA/first-item.lisp
@@ -1,15 +1,16 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1992-1995,2011  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992-1995,2011-2013 David D. McDonald  -- all rights reserved
 ;;; 
 ;;;     File:  "first item"
 ;;;   Module:  "grammar;rules:CA:"
-;;;  Version:  0.6 January 1995
+;;;  Version:  0.6 February 2013
 
 ;; 0.5 (12/7/93 v2.3) drastically simplified and started cleaning them up.
 ;; 0.6 (1/27/94) 1st serious application
 ;;     (10/24) moved in start-of-sentence check
 ;;     (1/9/95) added *most-recent-sentence-start* facility
 ;;     (7/13/11) Fixed case of dotted edge in could-be-the-start-of-a-sentence
+;;     (2/3/13) Improved (updated?) sentence-start criteria. 
 
 (in-package :sparser)
 
@@ -80,25 +81,28 @@
           (word
            (cond ((eq tt-to-the-left *the-punctuation-period*) t)
                  ((eq tt-to-the-left *source-start*) t)
-                 ((eq tt-to-the-left *the-punctuation-open-parenthesis*)
+                 ((or (eq tt-to-the-left *the-punctuation-open-parenthesis*)
+                      (eq tt-to-the-left word::double-quote))
                   (could-be-the-start-of-a-sentence
                    (chart-position-before position)))))
           (edge
-           (let ((form (edge-form tt-to-the-left)))
+           (let ((category (edge-category tt-to-the-left))
+                 (form (edge-form tt-to-the-left)))
              (cond
-               ((eq form :dotted-intermediary)
-                ;; e.g. the start of a multi-element abbreviation where
-                ;; the question is has it covered over a period.
-                (let ((position-before (chart-position-before position)))
-                  (eq *the-punctuation-period*
-                      (pos-terminal position-before))))
-               (t
-                (if form
-                  (case (cat-symbol form)
-                    (category::section-marker t)
-                    (otherwise
-                     ;;(format t "~&>>>> edge-form before cap-seq: ~A~%" form)
-                     nil))
-                  nil))))))
-          nil))))
+              ((eq category *the-punctuation-period*) t)
+              ((eq form :dotted-intermediary)
+               ;; e.g. the start of a multi-element abbreviation where
+               ;; the question is has it covered over a period.
+               (let ((position-before (chart-position-before position)))
+                 (eq *the-punctuation-period*
+                     (pos-terminal position-before))))
+              (t
+               (if form
+                 (case (cat-symbol form)
+                   (category::section-marker t)
+                   (otherwise
+                    ;;(format t "~&>>>> edge-form before cap-seq: ~A~%" form)
+                    nil))
+                 nil))))))
+        nil))))
 
