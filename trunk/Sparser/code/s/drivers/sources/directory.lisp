a51afaf888517f63e689b4a6ff89b35d237341f7
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-02-06 20:51:39 +0000

    Added .txt to the file name so that it would work in Clozure

--- trunk/Sparser/code/s/drivers/sources/directory.lisp
+++ trunk/Sparser/code/s/drivers/sources/directory.lisp
@@ -1,10 +1,13 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
 ;;; copyright (c) 1990,1991  Content Technologies Inc.
-;;; copyright (c) 1992 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992,2013 David D. McDonald  -- all rights reserved
 ;;;
 ;;;     File:  "string"
 ;;;   Module:  "drivers;sources:"
-;;;  Version:   January 2012
+;;;  Version:   January 2013
+
+;; 1/30/13 Added .txt to the string passed to directory so it would work.
+;; Might be Clozure-specific requirement. 
 
 (in-package :sparser)
 
@@ -12,7 +15,7 @@
 
 
 (defun analyze-text-from-directory (dir)
-  (let* ((file-list (directory (format nil "~a/*" dir)))
+  (let* ((file-list (directory (format nil "~a/*.txt" dir)))
          (stream (define-document-stream (gensym "document-stream")
                      :style-name 'hand-typed/no-headers
                      :file-list file-list)))
