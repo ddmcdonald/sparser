81bc483f288e5f507580fd148bb3cc072c77c870
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-02-23 17:25:18 +0000

    Added article and section initializations for single-file operation

--- trunk/Sparser/code/s/drivers/sources/file1.lisp
+++ trunk/Sparser/code/s/drivers/sources/file1.lisp
@@ -1,11 +1,13 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
-;;; copyright (c) 1992,1993,1994 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992-1994,2013 David D. McDonald  -- all rights reserved
 ;;; 
 ;;;     File:  "file"
 ;;;   Module:  "drivers;sources:"
-;;;  Version:   February 1991
+;;;  Version:   February 2013
 
 ;; initiated 2/91, added Analyze-text-from-file/at-filepos 12/14/94
+;; 2/15/13 Folded in initializations from do-document-as-stream-of-files,
+;;  for the code in objects/doc/object.lisp
 
 (in-package :sparser)
 
@@ -17,6 +19,10 @@
     (close-character-source-file))
   (let ((pathname (decode-file-expression/pathname file)))
     (establish-character-source/file pathname))
+  (when *recognize-sections-within-articles* ;; grammar module
+    (begin-new-article :location file))
+  (when *paragraph-detection*
+     (initialize-section-resource))
   (analysis-core)
   (when *open-stream-of-source-characters*
     (close-character-source-file)))
