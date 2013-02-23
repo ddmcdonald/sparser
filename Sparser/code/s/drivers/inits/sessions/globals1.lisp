701ee4deab40fe04604f39711fdd4bff32e67cd9
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-02-23 17:27:39 +0000

    Added *accumulate-content-across-documents*

--- Sparser/code/s/drivers/inits/sessions/globals1.lisp
+++ Sparser/code/s/drivers/inits/sessions/globals1.lisp
@@ -1,11 +1,10 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1991-1997,2011-2012  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1991-1997,2011-2013  David D. McDonald  -- all rights reserved
 ;;; extensions copyright (c) 2006-2007 BBNT Solutions LLC. All Rights Reserved
-;;; $Id:$
 ;;; 
 ;;;     File:  "globals"
 ;;;   Module:  "drivers;inits:sessions:"
-;;;  Version:  November 2012
+;;;  Version:  February 2013
 
 ;;;  Flags and the code to initialize them, as pertain to the state
 ;;;  of an entire session with the analyzer.
@@ -26,7 +25,7 @@
 ;; *use-subtypes*. 10/8 added *what-to-do-on-errors*.
 ;; 2/20/11 added *make-edges-over-new-digit-sequences*. 4/8/11 added
 ;; *convert-eft-form-categories-to-form-rules*. Added *do-unanalyzed-hyphenated-
-;; sequences* 11/5/12
+;; sequences* 11/5/12. Added *accumulate-content-across-documents* 2/18/13
 
 (in-package :sparser)
 
@@ -186,6 +185,13 @@
    Right now (9/09) it's not clear what the operational story should be
    about them. Setting the flag to nil lets us write sublangauge-style
    rules so they are in place when we do want themm.")
+
+(defparameter *accumulate-content-across-documents* nil
+  "Controls whether we forget all the individuals the analysis created
+   when we start the next document, or accumlate them. Read in
+   per-article-initializations, presently bound to T by the doc-stream
+   driver do-document-as-stream-of-files but could potentiallygo on all
+   of them.")
 
 (defparameter *ignore-parentheses* nil
   "Flag examined prior to setting hooks for mark-open-paren and
