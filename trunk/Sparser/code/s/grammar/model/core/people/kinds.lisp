5c9c71699a7bb0c761cbea792d904fb622b404bb
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-03-07 01:14:44 +0000

    Made all the cases (girl) use the label location in the grammar

--- trunk/Sparser/code/s/grammar/model/core/people/kinds.lisp
+++ trunk/Sparser/code/s/grammar/model/core/people/kinds.lisp
@@ -1,32 +1,45 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER COMMON-LISP) -*-
 ;;; Copyright (c) 2007 BBNT Solutions LLC. All Rights Reserved
-;;; $Id:$
+;;; copyright (c) 2013  David D. McDonald  -- all rights reserved
 ;;;
 ;;;      File:   "kinds"
 ;;;    Module:   "model;core:people:"
-;;;   version:   December 2007
+;;;   version:   March 2013
 
-;; initiated 12/27/07.
+;; initiated 12/27/07. Relabeled 'person' for parsing 3/6/13.
 
 (in-package :sparser)
 
 (define-category  girl
   :instantiates person
   :specializes person
+  :rule-label person
   ;;/// How do we index this sort of thing?
   :realization (:common-noun "girl"))
 
 (define-category  boy
   :instantiates person
   :specializes person
+  :rule-label person
   :realization (:common-noun "boy"))
 
 (define-category  man
   :instantiates person
   :specializes person
+  :rule-label person
   :realization (:common-noun ("man" :plural "men")))
 
 (define-category  woman
   :instantiates person
   :specializes person
+  :rule-label person
   :realization (:common-noun ("woman" :plural "women")))
+
+; people person human -- need to know its underspecified
+; male female ???
+
+
+;;--- Relational terms
+
+; father mother daughter son child aunt uncle niece nephew
+;  These need ETF
