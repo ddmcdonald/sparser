a0991eace26984f6dae188a19dd5c7a9233be543
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-03-07 00:57:26 +0000

    Added the case of the edge having no label (pathological, but happens while debugging)

--- Sparser/code/s/objects/chart/edges/printers.lisp
+++ Sparser/code/s/objects/chart/edges/printers.lisp
@@ -1,16 +1,17 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
 ;;; copyright (c) 1990  Content Technologies Inc. 
-;;; copyright (c) 1992,2012  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992,2012-2013  David D. McDonald  -- all rights reserved
 ;;; 
 ;;;     File:  "printers"
 ;;;   Module:  "objects;edges:"
-;;;  Version:  March 2012
+;;;  Version:  March 2013
 
 ;; initiated 8/90
 ;; (7/21/92 v2.3) patched printer for case of label being a word
 ;; (9/2) added reference-category cases
 ;; (10/5) added polyword case
-;; (10/18) added mixin.  (3/15/12) quiet compiler
+;; (10/18) added mixin.  (3/15/12) quiet compiler (3/5/13) handle case
+;;  of no label on the edge.
 
 (in-package :sparser)
 
@@ -28,20 +29,25 @@
       (princ-edge-vector-position-number (edge-starts-at obj) stream)
       (write-string " " stream)
       (let ((label (edge-category obj)))
-        (etypecase label
+        (typecase label
           ((or category referential-category mixin-category)
            (princ-category label stream))
           (word
            (princ-word label stream))
           (polyword
-           (princ-polyword label stream))))
+           (princ-polyword label stream))
+          (otherwise 
+           ;; category label is probably nil somehow
+           (if (edge-category obj)
+             (format stream "[new label type: ~a]" (type-of (edge-category obj)))
+             (write-string "[empty label]" stream)))))
       (write-string " " stream)
       (princ-edge-vector-position-number (edge-ends-at obj) stream)))
   (write-string ">" stream))
 
 
 
-(defun the-Edges ( &optional (stream *standard-output*))
+(defun the-edges ( &optional (stream *standard-output*))
   (declare (special *edge-resource-is-wrapped*
                     *length-of-edge-resource*
                     *position-of-next-available-edge-in-resource*))
