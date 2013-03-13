f9c18ea4243956f8577048037963333cc1ca4d06
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-03-13 00:42:00 +0000

    Debugged the method and its use, but removed until Chomsky adjunction fixed

--- trunk/Sparser/code/s/grammar/model/core/places/regions.lisp
+++ trunk/Sparser/code/s/grammar/model/core/places/regions.lisp
@@ -3,7 +3,7 @@
 ;;;
 ;;;     File:  "other"
 ;;;   Module:  "model;core:places:"
-;;;  version:  February 2013
+;;;  version:  March 2013
 
 ;; initiated 4/4/94 v2.3.  Added string/region 10/5.  Added missing typecase
 ;; to String-for 6/22.  (9/12) tweeked the autodef
@@ -13,7 +13,10 @@
 ;;  np-common-noun/definite because that makes a new individual and regions
 ;;  are hashed on their names -- with the gensym'd individual didn't have.
 ;; (2/28/13) Putting in the ETF and function to supply a region's name
-;;  when we get it in an 'of' construction
+;;  when we get it in an 'of' construction. 3/8/13 Why does a region-type
+;;  like 'city' inherit from 'location'? Copied the relationship-to-country
+;;  method to pick that up. ///Need to review the category choices and
+;;  inheritance structure in here. 
 
 (in-package :sparser)
 
@@ -26,7 +29,7 @@
   ;; e.g. New England, real places
   :instantiates  location
   :specializes  location
-  :binds ((name :primitive word)
+  :binds ((name :primitive word) ;;(:or name :primitive word))
           (aliases  :primitive list)
           (type . region-type)
           (containing-region . location))
@@ -37,7 +40,7 @@
                            (np-head . region-type)))
                 (:tree-family  kind-of-name ;; "strait of Hormous"
                  :mapping ((np . region)
-                           (complement . name-word) ;; and what else?
+                           (complement . (name name-word)) ;; and what else?
                            (result-np . :self)))))
 
 ;; Dossier of named regions is [regions]
@@ -52,32 +55,39 @@
   ;; by making the ETF use a method rather than a function
   ;; N.b. this returns the referent of the edge, which will be
   ;; a region based on this name
-  (unless (and (itypep name 'name-word)
-               (itypep region 'city)) ;; temp until next load
-    (break "wrong types in give-kind-its-name. Fix one or the other")
-    (return-from give-kind-its-name region))
-  (let* ((word (value-of 'name name 'name-word))
-         (new-region (find-or-make/individual 'region `(:name ,word))))
-    (break "look at region")
-    ;; Is the region the right sort of thing to be it's type??
-    ;;  <what's the instruction for creating a binding ??>
-    ;; Now we make the word refer to this region
-    (remove-name-word-rule-from word)
-    (let ((rule (define-cfr category::region
-                   `(,word)
-                  :form category::proper-name
-                  ;; /// what's the schema ???
-                  :referent new-region)))
-      (push-debug `(,rule))
-      (break "look at rule set for the word")
-      new-region)))
-
+  
+  (declare (ignore name)) region)
+#|  Turning this off until the two types of values in the name field
+    is made possible (one of them being a primitive), and until the
+    Chomsky-adjunction conundrum in the rule construction is done and
+    we get dependable rules
+  (push-debug `(:kind-name ,region ,name))
+  (let ((region-name (convert-to-canonical-name-form name)))
+    (unless (indiv-typep region category::location)
+      ;; Need to be sure that the variables are there for binding
+      (break "the 'region' individual is not a location"))
+ 
+    (let ((new-region (define-or-find-individual category::region
+                        :name region-name)))
+      ;; Is the region the right sort of thing to be it's type??
+      ;; /// Loses the fact that the new-region is, e.g. a city
+      ;;  <what's the instruction for creating a binding ??>
+      ;; Now we make the word refer to this region
+      (if nil
+        (then ;; Yesterday the input was a word. Today it's a name
+         (remove-rules-from-word word)
+         (let ((rule (define-cfr category::region `(,word)
+                       :form category::proper-name
+                       ;; /// what's the schema ???
+                       :referent new-region)))))
+        new-region))))
+|#
 ;; What about subsequent runs of "city of S" after s has this rule?
 ;;  pattern becomes <region> of <region>, which is odd.
     
 
-;; move to name words or wherever fits the eventual generalizaton
-(defun remove-name-word-rule-from (word)
+;;/// move to name words or wherever fits the eventual generalizaton
+(defun remove-rules-from-word (word)
   (let* ((rs (word-rule-set word))
          (rules (rs-single-term-rewrites rs)))
     (unless rules (error "Presumption violated"))
@@ -168,25 +178,33 @@
       (values category
               rule))))
 
-
+#|  Can't use until the Chomsky adjunction issue with multiple
+    categories in an ETF mapping is sorted out
 (define-category located-in
   :instantiates self
   :specializes associated-with-country
   :binds ((country . country) ;;/// probably too narrow
-          (region . region-type))) ;; defintely too narrow
-  
+          (region . location))) ;; maybe too narrow
+  |#
 
 (defmethod relationship-to-country ((c sh::country) (r sh::region-type))
   (declare (special *parent-edge-getting-reference*))
+  (relation-to-country-region/location-core c r))
+
+(defmethod relationship-to-country ((c sh::country) (r sh::location))
+  (declare (special *parent-edge-getting-reference*))
+  (relation-to-country-region/location-core c r))
+
+(defun relation-to-country-region/location-core (c r)
   (let ((country (dereference-shadow-individual c))
-        (region (dereference-shadow-individual r)))
+        (location (dereference-shadow-individual r)))
     (setf (edge-category *parent-edge-getting-reference*)
           category::region-type)
     (setf (edge-form *parent-edge-getting-reference*)
           category::np)
     (define-or-find-individual category::located-in
-        :country country region region)))
-
+        :country country :region location)
+    location)) ;; return the referent of the right edge
 
 
 
@@ -194,7 +212,7 @@
 ;;; kinds of edges / borders
 ;;;--------------------------
 
-;; "the forest border", "the border of the forest"
+;; "the forest border", "the border of the forest", "the border with Iran"
 
 (define-category border-type
   :instantiates :self
