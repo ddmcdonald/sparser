0caf75985c63c7575e28a78bc628cb4933358f3d
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-03-29 14:35:01 +0000

    Added psi to pname-for and converted to regular typecase

--- Sparser/code/s/objects/chart/generics/pname.lisp
+++ Sparser/code/s/objects/chart/generics/pname.lisp
@@ -1,20 +1,26 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1992,1993,1994  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992-1994,2013  David D. McDonald  -- all rights reserved
 ;;;
 ;;;      File:   "pname"
 ;;;    Module:   "objects;chart:generics:"
-;;;   Version:   July 1994
+;;;   Version:   March 2013
 
 ;; (9/3/92 v2.3) added referential-category
 ;; (1/6/94) fixed accessor for polywords.  (7/11) added mixin-category
+;; (3/18/13) Added psi and converted to regular typecase. 
 
 (in-package :sparser)
 
 
 (defun pname-for (obj)
-  (etypecase obj
+  (typecase obj
     (word     (word-pname obj))
     (polyword (pw-pname obj))
     ((or category referential-category mixin-category)
-     (symbol-name (cat-symbol obj)))))
+     (symbol-name (cat-symbol obj)))
+    (individual ;; subsumes psi
+     (symbol-name (cat-symbol (itype-of obj))))
+    (otherwise
+     (push-debug ``(,obj))
+     (error "Need the pname for new type: ~a" (type-of obj)))))
 
