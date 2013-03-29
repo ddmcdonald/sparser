635e61c194b81224d1397610dd5c37f696fc9319
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-03-29 14:47:39 +0000

    Added "region"

--- trunk/Sparser/code/s/grammar/model/dossiers/location-kinds.lisp
+++ trunk/Sparser/code/s/grammar/model/dossiers/location-kinds.lisp
@@ -1,18 +1,21 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1994,2011  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1994,2011-2013  David D. McDonald  -- all rights reserved
 ;;; extensions copyright (c) 2008 BBNT Solutions LLC. All Rights Reserved
 ;;; $Id:$
 ;;;
 ;;;     File:  "location kinds"
 ;;;   Module:  "model;dossiers:"
-;;;  version:  December 2011
+;;;  version:  March 2013
 
 ;; initiated 1/17/94 v2.3
 ;; Started populating it 6/18/08. 7/18/11 Made over as regions,
 ;; added a bunch. 9/13/11 adding border types. More 12/19/11.
+;; 3/12/13 added "region" ///need to somehow indicate that a word like
+;; that has an anaphoric role in many cases: "What region is that?"
 
 (in-package :sparser)
 
+(define-region-type "region")
 (define-region-type "city")
 (define-region-type "country")
 (define-region-type "exit")
