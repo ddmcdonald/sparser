40d9eea7c8410e2db3766a4ed7339c49e44a8703
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-04-01 21:16:58 +0000

    Added another argument to specify the name of the corpus

--- trunk/Sparser/code/s/drivers/sources/file1.lisp
+++ trunk/Sparser/code/s/drivers/sources/file1.lisp
@@ -3,11 +3,12 @@
 ;;; 
 ;;;     File:  "file"
 ;;;   Module:  "drivers;sources:"
-;;;  Version:   March 2013
+;;;  Version:   April 2013
 
 ;; initiated 2/91, added Analyze-text-from-file/at-filepos 12/14/94
 ;; 2/15/13 Folded in initializations from do-document-as-stream-of-files,
-;;  for the code in objects/doc/object.lisp. 3.14.13 fixed it. 
+;;  for the code in objects/doc/object.lisp. 3.14.13 fixed it.  Fan-out
+;;  from further change 4/1/13.
 
 (in-package :sparser)
 
@@ -17,17 +18,18 @@
 (defun analyze-text-from-file (file)
   (when *open-stream-of-source-characters*
     (close-character-source-file))
-  (let ((pathname (decode-file-expression/pathname file)))
-    (establish-character-source/file pathname))
-  (when *recognize-sections-within-articles*
-    (initialize-article-resource)
-    (initialize-section-resource)
-    (begin-new-article :name file)) ;; sets *current-article*
-  (when *note-text-relations*
-    (initialize-text-relationships))
-  (analysis-core)
-  (when *open-stream-of-source-characters*
-    (close-character-source-file)))
+  (let* ((pathname (decode-file-expression/pathname file))
+         (file-name (intern (pathname-name pathname))))
+    (establish-character-source/file pathname)
+    (when *recognize-sections-within-articles*
+      (initialize-article-resource)
+      (initialize-section-resource)
+      (begin-new-article :name file-name :location pathname))
+    (when *note-text-relations*
+      (initialize-text-relationships))
+    (analysis-core)
+    (when *open-stream-of-source-characters*
+      (close-character-source-file))))
 
 
 
