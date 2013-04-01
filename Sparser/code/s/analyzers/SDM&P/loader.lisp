1842cd61158bcd1b2781fa711ae1371b035e866e
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-04-01 21:43:09 +0000

    Moved analyse-text-relations oput

--- Sparser/code/s/analyzers/SDM&P/loader.lisp
+++ Sparser/code/s/analyzers/SDM&P/loader.lisp
@@ -4,14 +4,15 @@
 
 ;;;      File: "loader"
 ;;;    Module: "analyzers;SDM&P:
-;;;   Version: March 2013
+;;;   Version: April 2013
 
 ;; Initiated 2/9/07. Bumped scan to 1 to completely revamp it 1/21/13. Added
 ;; [form-march] 1/23/13. Added [reify-individuals] 2/28/13 and also broke out
 ;; [gofers] and [heuristics] for flexibility in salvaging more of the original
 ;; code developed for JTC/TRS.  3/9/13 added [note-text-relations] and [text-relations]
 ;; Distributed text-relations to relation-class, text-relation-form, and
-;; text-relation-indexing 3/12/13. [analyze..] 3/14.
+;; text-relation-indexing 3/12/13. [analyze..] 3/14. 4/1 moved analyze-text-relations
+;; to rules
 
 (in-package :sparser)
 
@@ -26,7 +27,7 @@
 (lload "do SDM&P;text-relation-form")
 (lload "do SDM&P;text-relation-indexing")
 (lload "do SDM&P;note-text-relations")
-(lload "do SDM&P;analyze-text-relations")
+
 
 (lload "do SDM&P;traces")
 
