2aa9ebdafac2544c42436544231d611172ec2ead
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-04-01 20:48:49 +0000

    Added keyword arg for schema

--- trunk/Sparser/code/s/objects/forms/cfrs4.lisp
+++ trunk/Sparser/code/s/objects/forms/cfrs4.lisp
@@ -1,9 +1,9 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
-;;; copyright (c) 1992,1993,1994  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992-1994.3-13  David D. McDonald  -- all rights reserved
 ;;;
 ;;;      File:   "cfrs"               ; "context free rules"
 ;;;    Module:   "grammar;forms:"
-;;;   Version:   1.4 July 1992
+;;;   Version:   1.5 April 2013
 
 (in-package :sparser)
 
@@ -11,7 +11,8 @@
 ;;  1.1  Added the multi-RHS define form and the deletion routines
 ;;  1.2  Added undef-cfr, corresponding to the multi-rhs case.
 ;;  1.3  (10/90 v1.5) Dealt out all the substantial routines to the cfr directory
-;; 1.4  (7/21/92 v2.3) added stub for relation keyword
+;;  1.4  (7/21/92 v2.3) added stub for relation keyword.
+;;  1.5  (4/1/13) Added argument for schema
 
 (export '(Def-cfr
           Def-cfr/multiple-rhs
@@ -25,14 +26,16 @@
                    rhs-list-of-categories
                    &key form
                         relation
-                        referent)
+                        referent
+                        schema)
 
   (declare (ignore relation))  ;; 7/21 to be fleshed out later
 
   `(def-cfr/expr ',lhs-category
                  ',rhs-list-of-categories
                  :form ',form
-                 :referent ',referent) )
+                 :referent ',referent
+                 :schema  ',schema) )
 
           
 ;;;--------------
