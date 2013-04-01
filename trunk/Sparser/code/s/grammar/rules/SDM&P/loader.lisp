b6bdea69dcfccb743ca3c088717b7527cfa7747b
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-04-01 21:42:43 +0000

    Moved analyse-text-relations here

--- trunk/Sparser/code/s/grammar/rules/SDM&P/loader.lisp
+++ trunk/Sparser/code/s/grammar/rules/SDM&P/loader.lisp
@@ -4,17 +4,18 @@
 ;;;
 ;;;      File: "loader"
 ;;;    Module: "grammar;rules:SDM&P:
-;;;   Version: March 2013
+;;;   Version: April 2013
 
 ;; Initiated 2/9/07. Added [infer-categories] 9/4. Added [text-relations]
-;; on 3/9/12. 
+;; on 3/9/12.  4/1 moved in analyze-text-relations
 
 (in-package :sparser)
 
 (gload "SDM&P;create-categories")
 (gload "SDM&P;infer-categories")
+
 (gload "SDM&P;text-relations")
 (gload "SDM&P;reify-relations")
-
+(lload "SDM&P;analyze-text-relations")
 
 
