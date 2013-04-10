c0b9103376b63f737c77ee06560c918592075b61
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-04-10 19:24:47 +0000

    Changed super category so doesn't appear to be modifier

--- Sparser/code/s/grammar/model/sl/reports/object.lisp
+++ Sparser/code/s/grammar/model/sl/reports/object.lisp
@@ -1,16 +1,17 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1993-2005  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1993-2005,2013  David D. McDonald  -- all rights reserved
 ;;; extensions copyright (c) 2008 BBNT Solutions LLC. All Rights Reserved
-;;; $Id:$
 ;;;
 ;;;     File:  "object"
 ;;;   Module:  "grammar;model:sl:reports:"
-;;;  version:  0.2 February 2008
+;;;  version:  0.3 April 2013
 
 ;; initiated 10/22/93 v2.3 restarting from scratch. 5/3/95 changed "+" to "-"
 ;; 0.1 (1/8/96) Reorganized the conceptualization to break-out the verb.
 ;;  (3/11/05) Elaborating cases for make-a-report. 
 ;; 0.2 (2/29/08) Switching over to instantiating the category.
+;; 0.3 (4/3/13) Blocking the whole 'make-a-report' category in favor of
+;;      just the subj/verb tags. 
 
 (in-package :sparser)
 
@@ -19,7 +20,7 @@
 ;;;----------------------------
 
 (define-category report-verb
-  :specializes nil
+  :specializes event
   :instantiates self
   :binds ((name . (:primitive (:or word polyword))))
   :index (:permanent :key name))
@@ -28,7 +29,6 @@
 
 ; (define-report-verb "report")
 ; (define-report-verb '("say" :past-tense "said"))
-
 
 ;;--- def-form
 
@@ -48,18 +48,42 @@
     (make-verb-rules word/spec category obj))))
 
 
-;;;---------------
-;;; full relation
-;;;---------------
+;;;------------------
+;;; partial relation
+;;;------------------
 
+(define-category someone-reports
+  :specializes modifier  ;; hook to fold into clause that gives the report
+  :instantiates self
+  :binds ((reporter . (:or person company title)))
+  :index (:temporary :key reporter)
+  :realization ((:tree-family just-the-tags
+                 :mapping ((result . :self)
+                           (agent . reporter)
+                           (subj-verb . :self)
+                           (np . (person company title))
+                           (vg . report-verb)))))
+
+
+
+
+;;----------------------------
+;; Attempt at full relation
+
+;; This full relation lends itself to false positives since it gloms onto
+;; any event to the right of the verb, e.g.
+;;                                 "but"
+;; e0    PRONOUN/FEMALE          2 "she" 3
+;; e3    EVENTaGENT              3 "said testing" 5
+;; e4 e5                           "for" :: for, FOR
+;;  etc.
+#+ignore
 (define-category  make-a-report
   :specializes event
   :instantiates self
   :binds ((agent . (:or person company))
           (report))
-
   :index (:temporary :sequential-keys agent report)
-
   :realization
     ((:tree-family that-complement
       ;;  "Apple reports that its ..."
@@ -76,7 +100,8 @@
         ((:reversed-tag (subj-verb (vg np)
                           :head left-referent
                           :binds (agent right-referent)))
-         #+ignore
          (:final-tag (s (s "," subj-verb)
                        :head third-daughter
                        :binds (report first-daughter)))))))
+
+
