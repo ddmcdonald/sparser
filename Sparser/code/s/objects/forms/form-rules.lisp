bdb5b75fc8519246f970779fb48d71f3ec15b4e8
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-04-10 19:09:33 +0000

    Threaded schema into syntax form

--- Sparser/code/s/objects/forms/form-rules.lisp
+++ Sparser/code/s/objects/forms/form-rules.lisp
@@ -1,15 +1,16 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1992-1994,2011  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992-1994,2011-2013  David D. McDonald  -- all rights reserved
 ;;;
 ;;;      File:   "form rules"
 ;;;    Module:   "objects;forms:"
-;;;   Version:   0.3 October 2011
+;;;   Version:   0.3 April 2013
 
 ;; initiated 9/3/92 v2.3
 ;; 0.1 (10/12) changed the arguments
 ;; 0.2 (6/6/93) ditto -- added new-category
 ;;     (12/30/94) added def-syntax-rule
-;; 0.3 (10/27/11) added :head arg to form rules
+;; 0.3 (10/27/11) added :head arg to form rules. 4/9/13 added :schema
+;;      to syntax rules
 
 (in-package :sparser)
 
@@ -28,10 +29,11 @@
 
 (defmacro def-syntax-rule (;;lhs  ??use as variant??
                            rhs-labels
-                           &key head form referent)
+                           &key head form referent schema)
 
   `(def-syntax-rule/expr ',rhs-labels
      :head ',head
      :form ',form
-     :referent ',referent )  )
+     :referent ',referent
+     :schema ',schema )  )
 
