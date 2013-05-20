c5036f3561713063c47772868f0b8eb4567a61f2
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-05-20 02:37:06 +0000

    Quiet the compiler

--- Sparser/code/s/analyzers/SDM&P/document-handling.lisp
+++ Sparser/code/s/analyzers/SDM&P/document-handling.lisp
@@ -3,13 +3,14 @@
 ;;;
 ;;;     File:  "document handling"
 ;;;   Module:  "analyzers/SDM&P/"
-;;;  version:  April 2012
+;;;  version:  May 2012
 
-;; Broken out from Grok workspace and cleaned up 4/24/13.
+;; Broken out from Grok workspace and cleaned up 4/24/13. Made to take
+;; document strings 5/1/13.
 
 (in-package :sparser)
 
-(defun grok-pass-one ()
+(defun grok-pass-one (document-stream)
   "Just pull in the vocabulary all at once"
   (let ((*do-forest-level* nil)  ;; No supra-segment parsing for this pass
         (*new-segment-coverage* :none)
@@ -17,11 +18,11 @@
     (declare (special *do-forest-level* *new-segment-coverage*
                       *do-heuristic-segment-analysis*))
     (do-normal-segment-finished-options)
-    (analyze-text-from-directory "Users/ddm/sift/nlp/Grok/corpus/bird-flu" :doc-set-name 'bird-flu)))
+    (analyze-document-stream document-stream)))
 
 
 ;; N.b. will hang on "N." if UN isn't predefined
-(defun grok-pass-two ()
+(defun grok-pass-two (document-stream)
   "Do a segment-level analysis and identify text relationships"
   (let ((*do-forest-level* nil)
         (*allow-pure-syntax-rules* nil)
@@ -33,24 +34,28 @@
                       *note-text-relations*))
     (setq  *forest-level-protocol* 'parse-forest-and-do-treetops) ;; bindable?    
     (do-note-text-relations-in-segment)
-    (analyze-text-from-directory "Users/ddm/sift/nlp/Grok/corpus/bird-flu" :doc-set-name 'bird-flu)
+    (analyze-document-stream document-stream)
     (collect-relations-from-articles)
     ;; analysis and reification goes here. See the merged contents on (doc-set)
     ;; and other code in that file
     ))
 
 
-(defun grok-pass-three-setup ()
-  (setq  *forest-level-protocol* 'parse-forest-and-do-treetops) ;; semantic rules burned in
-  (setq *do-forest-level* t)
-  (setq *allow-pure-syntax-rules* t)
-  (setq *new-segment-coverage* :trivial)
-  (setq *do-heuristic-segment-analysis* t)
-  (setq *note-text-relations* nil) ;; don't overwrite the merged contents on (doc-set)
-  ;; The relations noted on this pass will be straight off bindings
-  (do-note-text-relations-in-segment)
-  (do-strong-domain-modeling)
-  (setq *profligate-creation-of-individuals* t)
-  (analyze-text-from-directory "Users/ddm/sift/nlp/Grok/corpus/bird-flu" :doc-set-name 'bird-flu)
-  ;;  -- 1st look at some reification
-  )
+(defun grok-pass-three (document-stream)
+  (let ((*forest-level-protocol* 'parse-forest-and-do-treetops) ;; semantic rules burned in
+        (*do-forest-level* t)
+        (*allow-pure-syntax-rules* t)
+        (*new-segment-coverage* :trivial)
+        (*do-heuristic-segment-analysis* t)
+        (*profligate-creation-of-individuals* t)
+        (*note-text-relations* nil)) ;; don't overwrite the merged contents on (doc-set)
+    ;; The relations noted on this pass will be straight off bindings
+    (declare (special *forest-level-protocol* *do-forest-level*
+                      *allow-pure-syntax-rules* *new-segment-coverage*
+                      *do-heuristic-segment-analysis* *note-text-relations*
+                      *profligate-creation-of-individuals*))
+    (do-note-text-relations-in-segment)
+    (do-strong-domain-modeling)
+    (analyze-document-stream document-stream)
+    ;; More analysis, now over sentential relations. Maybe more reification
+    ))
