4924a53597b1f48c85a7bb98f7818d04a46cc079
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-05-20 02:33:07 +0000

    Move the loading of the two dossier files into this load of the rest of the module

--- Sparser/code/s/grammar/model/core/numbers/loader2.lisp
+++ Sparser/code/s/grammar/model/core/numbers/loader2.lisp
@@ -1,10 +1,10 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1992-2005  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992-2005,2013  David D. McDonald  -- all rights reserved
 ;;; extensions copyright (c) 2006-2007 BBNT Solutions LLC. All Rights Reserved
 ;;; 
 ;;;     File:  "loader"
 ;;;   Module:  "model;core:numbers:"
-;;;  Version:  2.6 August 2007
+;;;  Version:  2.7 May 2013
 
 ;; 2.0  (7/16/92 v2.3) bumped [percentages] to 1 to install new regime
 ;;      7/19 bumped nearly everything else to make it over to new categories
@@ -16,7 +16,9 @@
 ;; 2.5 (7/11/00) bumped ordinals to 2 to rename the base category.
 ;; 2.6 (2/3/05) bumped ordinals to 3 to sort it out as a coherent individual
 ;;     (3/29/06) Appeared to have left out the digits fsa in the 2.6 mod.
-;;     (8/16/07) added [relation]/.
+;;     (8/16/07) added [relation].
+;; 2.7 (5/9/13) Moved the two dossier filees in to load here so we can use
+;;      them right away.
 
 (in-package :sparser)
 
@@ -31,5 +33,5 @@
 (gload "numbers;relation")
 
 ;; and in dossiers:
-;;  (gload "number-dossier;numbers")
-;;  (gload "number-dossier;ordinals")
+(gload "dossiers;numbers")
+(gload "dossiers;ordinals")
