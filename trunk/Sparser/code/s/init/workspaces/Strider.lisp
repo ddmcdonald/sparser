c53529d1a22db56bf17cc32ff000d37eb8f9daad
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-05-20 02:24:20 +0000

    Minimal workspace for Strider work

---
+++ trunk/Sparser/code/s/init/workspaces/Strider.lisp
@@ -0,0 +1,17 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
+;;; copyright (c) 2013  SIFT LLC  -- all rights reserved
+;;;
+;;;     File:  "Strider"
+;;;   Module:  "init;workspaces:"
+;;;  version:  May 2012
+
+;; Initiated 5/7/13
+
+;;  (load "/Users/ddm/sparser/load-nlp.lisp")
+
+(in-package :sparser)
+
+;; ddm's hard pathnames. Replace with a logical or computed form
+;;  (f "/Users/ddm/sift/nlp/Grok/corpus/helicopter-attack.txt")
+
+;;  (f "/Users/ddm/ws/Strider/ws/Aljazeera_9-7-12.txt")
