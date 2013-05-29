5fcf51e3ecbadf9ebb9b7630c3ead86582074fcd
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-05-29 02:08:24 +0000

    Place to put Strider-oriented modeling. Particularly obvious names for which there won't be sufficient internal evidence, but too broad a set of external contexts to realistically get that way.

---
+++ trunk/Sparser/code/s/grammar/model/sl/middle-east/loader.lisp
@@ -0,0 +1,12 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER COMMON-LISP) -*-
+;;; copyright (c) 2013  David D. McDonald  -- all rights reserved
+;;;
+;;;      File:   "loader"
+;;;    Module:   "sl;middle-east"
+;;;   version:   May 2013
+
+;; initiated 5/23/13
+
+(in-package :sparser)
+
+(gload "mideast;named-entities")
---
+++ trunk/Sparser/code/s/grammar/model/sl/middle-east/named-entities.lisp
@@ -0,0 +1,18 @@
+;;; copyright (c) 2013  David D. McDonald  -- all rights reserved
+;;;
+;;;      File:   "loader"
+;;;    Module:   "sl;middle-east"
+;;;   version:   May 2013
+
+;; initiated 5/23/13
+
+(in-package :sparser)
+
+(define-with-all-instances-permanent
+
+    (define-person "Binyamin Netanyahu") ;; :nicknames ("Bibi")
+    ;; first name also spelled "Binyamin"  
+
+    (define-company '("Israeli" "Defense" "Forces") :takes-the t)
+
+) ;; closes with all permanent
