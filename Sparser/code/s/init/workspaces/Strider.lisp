bc526b12d91bd6b025295efe8c30261bbaf49d6e
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-05-29 01:59:38 +0000

    Addded a grammar module for this project. Mostly for predefined individuals like Obama that everyone would be expected to know.

--- Sparser/code/s/init/workspaces/Strider.lisp
+++ Sparser/code/s/init/workspaces/Strider.lisp
@@ -11,7 +11,44 @@
 
 (in-package :sparser)
 
+;;--- display parameters
+
+;; (setq *tts-after-each-section* t)
+
+
+;;--- control parameters
+
+;; (setq *break-on-new-bracket-situations* nil)
+
+
+;;--- text
+
 ;; ddm's hard pathnames. Replace with a logical or computed form
 ;;  (f "/Users/ddm/sift/nlp/Grok/corpus/helicopter-attack.txt")
 
 ;;  (f "/Users/ddm/ws/Strider/ws/Aljazeera_9-7-12.txt")
+;;  (f "/Users/ddm/ws/Strider/ws/OSC Articles.txt")
+
+;; Shaul Mofaz, former Chief of Staff of the Israeli Defense Forces, head of Kadima, and leader of the opposition in the Knesset,
+
+;; "defense" is defined as a title-modifier, while "Defence" is a name-word
+;; ditto for "forces" which is a military-force
+
+;;--- setting up a grammar module for Strider-specific content
+
+(define-grammar-module *middle-east*
+  :name "The Middle East"
+  :parent *sublanguages*)
+
+(include-grammar-module *middle-east*)
+
+(def-logical-pathname "mideast;" "sl;middle-east:")
+
+(gate-grammar *middle-east*
+  (gload "mideast;loader"))
+
+
+(setq *do-domain-modeling-and-population* t) ;; ignores null referents
+
+
+
