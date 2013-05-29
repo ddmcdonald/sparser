91c89ded9e065fba1f46a8953f46e244711769c8
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-05-29 02:12:17 +0000

    Removed the general terms, added Chief of Staff just to get through that since the caps are a real problem that PCT has to be revived to fix.

--- trunk/Sparser/code/s/grammar/model/dossiers/title-modifiers.lisp
+++ trunk/Sparser/code/s/grammar/model/dossiers/title-modifiers.lisp
@@ -1,11 +1,12 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1993,1994,1995 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1993-1995,2013 David D. McDonald  -- all rights reserved
 ;;;
 ;;;     File:  "title modifiers"
 ;;;   Module:  "model;dossiers:"
 ;;;  version:  December 1995
 
-;; initiated 6/10/93 v2.3. Initially populated 12/8/95
+;; initiated 6/10/93 v2.3. Initially populated 12/8/95.
+;; Added cases for governments 5/23/14
 
 (in-package :sparser)
 
@@ -22,6 +23,10 @@
 (define-title-modifier "managing")
 (define-title-modifier "senior")
 (define-title-modifier "vice")
+
+;;--- associates with governments
+(define-title-modifier "prime")
+(define-title-modifier "defense") ;; generic !!
 
 
 ;;--- area of responsibility
--- trunk/Sparser/code/s/grammar/model/dossiers/title-qualifiers.lisp
+++ trunk/Sparser/code/s/grammar/model/dossiers/title-qualifiers.lisp
@@ -1,7 +1,6 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1993,1994 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1993-1994,2013 David D. McDonald  -- all rights reserved
 ;;; extensions copyright (c) 2009 BBNT Solutions LLC. All Rights Reserved
-;;; $Id:$
 ;;;
 ;;;     File:  "title qualifiers"
 ;;;   Module:  "model;dossiers:"
@@ -9,16 +8,22 @@
 
 ;; initiated 6/16/93 v2.3. Modified to use def-form 1/18/94
 ;; 10/12/09 Removed "formerly" when shifting the realization to :adjective
+;; 5/256/13 Introduced 'title-status' for words that predominantly
+;;  modify titles. Punting on the others. 
 
 (in-package :sparser)
 
+;; This one overwhelmingly is used just with titles.
+;; The others take on a qualifying role when used with
+;; titles, but have manny other uses. 
+(define-title-status-term "former")
+
+
+;; "ousted"  "emeritus"  (sequencer title) "the sixth director"
+;; "a" vs. "the", (country title)
 
 (define-title-qualifier "acting")
 (define-title-qualifier "current")
-(define-title-qualifier "former")
-;;(define-title-qualifier "formerly")
 (define-title-qualifier "late")    ;; "the late ..."
-
-;; 7/22
 (define-title-qualifier "retired")
 
