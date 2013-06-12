88931d24618565aadf6f2c11b6e7b77f9a9f36ed
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-06-12 06:55:25 +0000

    dossier file for defining seasons

---
+++ trunk/Sparser/code/s/grammar/model/dossiers/seasons.lisp
@@ -0,0 +1,12 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
+;;; copyright (c) 1994 David D. McDonald  -- all rights reserved
+;;; 
+;;;     File:  "seasons"
+;;;   Module:  "model;dossiers:"
+;;;  Version:  1.0 June 2013
+
+(define-season "Summer"     1 "summer")
+(define-season "Fall"       2 "fall")
+(define-season "Autumn"     2 "autumn")
+(define-season "Winter"     3 "winter")
+(define-season "Spring"     4 "spring")
