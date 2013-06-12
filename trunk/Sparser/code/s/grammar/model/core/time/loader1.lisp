b56b79310411633add242157d3e4c63e9c107928
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-06-12 21:42:32 +0000

    season1 -> season

--- trunk/Sparser/code/s/grammar/model/core/time/loader1.lisp
+++ trunk/Sparser/code/s/grammar/model/core/time/loader1.lisp
@@ -1,11 +1,11 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1992-2000,2011 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992-2000,2011-2013 David D. McDonald  -- all rights reserved
 ;;; extensions copyright (c) 2008 BBNT Solutions LLC. All Rights Reserved
 ;;; $Id:$
 ;;;
 ;;;     File:  "loader"
 ;;;   Module:  "model;core:time:"
-;;;  version:  1.5 August 2008
+;;;  version:  1.5 June 2013
 
 ;; mature recognition grammar 9/91 v1.9
 ;; 1.0 (12/7/92 v2.3) bumped everything to move it to the new semantics
@@ -17,8 +17,8 @@
 ;; 1.4 (9/27/99) Started reworking the design for psi so removed
 ;;      day-in-month and month-in-year and bumped dates to 2.
 ;; 1.5 (7/7/00) Bumped fiscal to 2 to rework them for psi. 7/8 bumped
-;;      year.
-;;     (8/27/08) Added time-of-day. (4/6/11) bumped phrases to 2.
+;;      year. (8/27/08) Added time-of-day. (4/6/11) bumped phrases to 2.
+;;    (6/12/13) added in seasons and citations.
 
 (in-package :sparser)
 
@@ -37,7 +37,9 @@
 (gload "time;anaphors1")
 (gload "time;age1")
 (gload "time;fiscal2")
-;;added in seasons 6/8/13
-(gload "time;seasons1")
+(gload "time;seasons")
+
+(gload "time;citations")
+
 
 
