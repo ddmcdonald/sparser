c8501feab0b14563319756b8c5f4b18df4e214df
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-06-13 18:53:58 +0000

    Rewrote def form to eliminate rule duplication.

--- Sparser/code/s/grammar/model/core/time/weekdays1.lisp
+++ Sparser/code/s/grammar/model/core/time/weekdays1.lisp
@@ -1,9 +1,9 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1992-2005  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992-2005,2013 David D. McDonald  -- all rights reserved
 ;;;
 ;;;     File:  "weekdays"
 ;;;   Module:  "model;core:time:"
-;;;  version:  1.3 March 2005
+;;;  version:  1.5 June 2013
 
 ;; 1.0 (9/18/93 v2.3) completely revamped for new semantics
 ;; 1.1 (1/10/94) redid ordinals.  Broke out the cases 10/20
@@ -13,7 +13,10 @@
 ;; 1.3 (3/15/05) Replaced position-in-a-sequence v/r of position-in-week with
 ;;      ordinal.
 ;; 1.4 (5/30/13) Re-labeled category weekday as time syntactically.
-;;      weekday still labeled as both weekday and time, duplicate has remained elusive
+;;      weekday still labeled as both weekday and time, duplicate has remained elusive.
+;; 1.5 (6/13/13) Found the problem. Original code needed to write the rule
+;;      "by hand" in the def form. Now that's done by the act of defining the
+;;      individual, and that version of doing it pays attention to rule-label.
 
 (in-package :sparser)
 
@@ -24,11 +27,11 @@
 (define-category  weekday
   :specializes time
   :instantiates time
-  :rule-label time  ;;this line should re-label weekday as time, but it doesn't
+  :rule-label time
   :binds ((name :primitive word)
           (abbreviation :primitive word)
           (position-in-week . ordinal))
-  :realization (:common-noun name) ;;witout this line, time does not appear as a label
+  :realization (:common-noun name)
   :index (:permanent :key name))
   
 
@@ -37,29 +40,11 @@
 ;;; form
 ;;;------
 
-(defun define-weekday (word-string
+(defun define-weekday (string
                        abbrev-string     ;; n.b. no period
                        position-integer)
-
-  (let* ((word (define-word word-string))
-         (abbreviation (define-word abbrev-string))
-         (ordinal (nth-ordinal position-integer))
-         weekday )
-
-    (unless (setq weekday (find-individual 'weekday
-                             :name word))
-      (setq weekday
-            (define-individual 'weekday
-              :name word
-              :abbreviation abbreviation
-              :position-in-week ordinal ))
-
-      (setf (unit-plist weekday)
-            `(:rules
-              (,(define-cfr category::weekday `(,word)
-                  :form category::proper-noun
-                  :referent weekday) )
-              ,@(unit-plist weekday)) )
-
-     (define-abbreviation word-string abbrev-string)
-      weekday )))
+  (let ((weekday (define-or-find-individual 'weekday :name string))
+        (ordinal (nth-ordinal position-integer)))
+    (bind-variable 'position-in-week ordinal weekday)
+    (define-abbreviation string abbrev-string)
+    weekday ))
