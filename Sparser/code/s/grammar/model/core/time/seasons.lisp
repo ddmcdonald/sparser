3a44ef98527c64783dc0b5b6860ad90554251589
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-06-13 13:59:00 +0000

    Simplified the design since capitalization isn't required. Made the constructor use more 'all in one' functions

--- Sparser/code/s/grammar/model/core/time/seasons.lisp
+++ Sparser/code/s/grammar/model/core/time/seasons.lisp
@@ -1,8 +1,16 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1994 David D. McDonald  -- all rights reserved
+;;; copyright (c) 2013 David D. McDonald  -- all rights reserved
+;;;
 ;;;     File:  "seasons"
 ;;;   Module:  "model;core:time:"
-;;;  version:  1.0 June 2013
+;;;  version:  June 2013
+
+;; Initated 6/11/13. Worked over 6/12: Added an index specification
+;; to the category, dropped the abbreviation (see dossiers/seasons),
+;; shrank the defining function down by using a define-or-find to
+;; do the heavy lifting and setting the position in year as a separate
+;; operation. Added some citation texts to help figure out where to 
+;; place these in the type-lattice
 
 ;;;------------
 ;;; the object
@@ -13,39 +21,47 @@
   :instantiates self
   :rule-label time ;;rule-label time sets the syntactic label as "time"
   :binds ((name :primitive word)
-          (abbreviation :primitive word)
+          (abbreviation :primitive word) ;; really? I'm skeptical
           (position-in-year . ordinal))
+  :index (:permanent :key name)
   :realization (:common-noun name ))
-                          
 
+;;;-----------
+;;; citations
+;;;-----------
+#|
+
+This pattern is the same one that we get for months
+and years, and would hold for periods in the day ("morning", 
+"evenng") for the most part. 
+
+"Spring"
+"spring"
+"early spring"  -- mid
+"late Spring"
+"last spring"  -- next
+
+
+Maybe these are specific to seasons (among stuff in time)
+
+"spring has started"
+-- is (almost) over
+
+
+
+|#
 
 ;;;------
 ;;; form
 ;;;------
 
-(defun define-season (string integer &optional abbrev)
-
-  (let* ((word (define-word string))
-         (abbrev-word (when abbrev (define-word abbrev)))
-         (ordinal (nth-ordinal integer))
-         season )
-
-    (unless (setq season (find-individual 'month :name word))
-      (setq season 
-            (if abbrev
-	      (let ((m (define-individual 'season
-			 :name word
-			 :abbreviation abbrev-word
-			 :position-in-year ordinal)))
-		(define-cfr category::season `(,abbrev-word)
-		  :form category::proper-noun
-		  :referent m))
-              (define-individual 'season
-                :name word
-                :position-in-year ordinal )))
-      
-      ;; 10/10/99 flush this one too 
-      (when abbrev
-        (define-abbreviation string abbrev))
-      
-      season )))
+(defun define-season (string integer)
+  ;; The define-or-find does the conversion of the string
+  ;; into a word and creates the two rules that go with
+  ;; being a common noun (i.e. singular and plural forms)
+  ;; with the season individual as their referent
+  (let ((season (define-or-find-individual
+                    'season :name string))
+        (ordinal (nth-ordinal integer)))
+    (bind-variable 'position-in-year ordinal season)       
+    season ))
