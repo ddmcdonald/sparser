536044d4d1a1de64b2a4db24000de8a58f26f65b
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-06-19 00:20:02 +0000

    slight modifcation to seasons, no ground breaking change

--- trunk/Sparser/code/s/grammar/model/core/time/dates2.lisp
+++ trunk/Sparser/code/s/grammar/model/core/time/dates2.lisp
@@ -41,12 +41,8 @@
                           (term4 . weekday))
                 :common-noun "date"))
 
-;;this cfr is for phrases like June 26 2013, added 6/6/13
-(def-cfr date (date year))
-;;this cfr is for phrases like Monday June 26 2013, added 6/6/13
-(def-cfr date (time date))
-;;this cfr is for phrases like "Summer of 2010" or "Winter 2013", added 6/8/13
-(def-cfr date (season year))
+
+
 
 
 
