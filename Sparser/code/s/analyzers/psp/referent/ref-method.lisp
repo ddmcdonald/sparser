b927d5b820812722a33e85e3d2413d3ef927c2df
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-07-02 14:24:08 +0000

    Added macro, def-k-method, to hide all the gore that comes from using CLOS shadowing Krisp data structures. Doesn't remove the gore, just pushes it under the rug so you don't see it.

--- Sparser/code/s/analyzers/psp/referent/ref-method.lisp
+++ Sparser/code/s/analyzers/psp/referent/ref-method.lisp
@@ -4,12 +4,13 @@
 ;;;
 ;;;      File:   "loader"
 ;;;    Module:   "analyzers;psp:referent:"
-;;;   Version:   January 2012
+;;;   Version:   July 2013
 
 ;; created 9/1/11. 10/3 Adapting to getting categories as arguments, e.g.
 ;; in the case of prepositions. 11/8/12 Adjusted argument order to match
 ;; order in the rule. 1/18/13 Removed the block from *grok* so it would
-;; actually run. Cleaned up.
+;; actually run. Cleaned up. 7/1/13 Added def-k-method as syntactic sugar
+;; to hide the uglyness. 
 
 (in-package :sparser)
 
@@ -56,6 +57,45 @@
                  (error "Unanticipated layout of the rule field ~
                          in a method call:~%  ~a" (cdr rule-field))))))
         referent))))
+
+
+;; Macro
+
+(defmacro def-k-method (name args &body body)
+  `(def-k-method/expr ',name ',args ',body))
+
+(defun def-k-method/expr (name args body)
+  ;;(push-debug `(,name ,args ,body))
+  (unless (every #'symbolp args)
+    (error "The arguments in a k-method must be symbols~
+          ~%At least one of them is not: ~a" args))
+  (unless (every #'category-named args)
+    (error "As least of of the args to the k-method is not a category:~
+          ~%~a" args))
+  (let* ((categories (loop for arg in args collect (category-named arg)))
+         (shadow-classes
+          (loop for c in categories collect (get-sclass c)))
+         (dummy-parameters
+          (loop with i = 0
+            collect (intern (string-append '#:d "-" (incf i))
+                            (find-package :sparser))
+            until (= i (length shadow-classes))))
+         (method-args
+          (loop for s in shadow-classes
+            as d in dummy-parameters
+            collect `(,d ,s)))
+         (let-bindings
+          (loop for d in dummy-parameters
+            as s in args
+            collect `(,s (dereference-shadow-individual ,d)) )))
+
+    ;;(push-debug `(,let-bindings ,method-args ,dummy-parameters ,shadow-classes))
+    (let ((form 
+           `(defmethod ,name ,method-args
+              (let ,let-bindings
+                ,@body))))
+      ;;(pprint form)      
+      (eval form))))
   
 
 
