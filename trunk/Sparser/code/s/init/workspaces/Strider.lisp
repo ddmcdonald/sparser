539d2035084d9818305def8bf008f112d38e7686
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-07-02 14:07:00 +0000

    Added status of latest examples and more meta-. symbols for in-progress items

--- trunk/Sparser/code/s/init/workspaces/Strider.lisp
+++ trunk/Sparser/code/s/init/workspaces/Strider.lisp
@@ -55,10 +55,11 @@
 ;; Shaul Mofaz, former Chief of Staff of the Israeli Defense Forces, head of Kadima, and leader of the opposition in the Knesset,
 #| 
 (p "Shaul Mofaz, former Chief of Staff of the Israeli Defense Forces,")
+;;  IDF comes out as name-word -- repeats ok though.
 
 2do: 6/7/14
- - Run whole articles
-     - copy the scheme for setting up a corpus from Grok
+ - Run whole articles using Comlex
+     - Debug the 
      - write that up some
      - add a break option with *tts-after-each-section*
  - Setup a little bit of general segment handling (lifted from Grok)
@@ -72,12 +73,33 @@
        - which will require a useful extension of the conceptualization
          of named-entity
 
+
+(p "Western spy agencies, collaborated by the terrorist MKO, have assassinated several Iranian scientists in the last three years.")
+;; -- quiescence pointer doewn't move when e23 added
+;; -- the kind-of-company word "agency" has no brackets
+
+(p "Computers of some Iranian nuclear sites were attacked by the Stuxnet virus, the first known computer worm discovered in 2010 to target industrial controls.")
+
+(p "In addition to viruses, enemies of Iran tried to hinder Iran's nuclear progress through assassination of the country's elites and nuclear scientists.")
+
+(p "In the fifth attack of its kind in two years, terrorists killed a 32-year-old Iranian scientist, Mostafa Ahmadi Roshan, and his driver on January 11.")
+
+(p "The assassination method used in the bombing was similar to the 2010 terrorist bomb attacks against the then university professor, Fereidoun Abbassi Davani - who is now the head of Iran's Atomic Energy Organization - and his colleague Majid Shahriari. While Abbasi Davani survived the attack, Shahriari was martyred.")
+
+(p "Another Iranian scientist, Dariush Rezaeinejad, was also assassinated through the same method on 23 July 2011.")
+
+
 Meta-dot
   *trace-reclaimation* reclaim-all-instances get-tag-for
   collection string/sequence spread-sequence-across-ordinals
   string-for/name person named-object index-person-name-to-person
   link-named-object-to-name-word names/parens-after-names.lisp
   relationship-to-country 
+  decode-index-field print-binding-structure
+  date weekday month season relative-time-adverb numeric-time approximator
+  inc-term
+  cite set-citation-file
+  unpack-primed-word
 
 |#
 ;;--- setting up a grammar module for Strider-specific content
