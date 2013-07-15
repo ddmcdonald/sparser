1160173c427e59c42bbd69c97cef04624ac01d89
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-07-15 17:02:30 +0000

    Added "Sait", "St."

--- Sparser/code/s/grammar/model/dossiers/person-prefixes.lisp
+++ Sparser/code/s/grammar/model/dossiers/person-prefixes.lisp
@@ -1,11 +1,12 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1993,1994,1995 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1993-1995,2013 David D. McDonald  -- all rights reserved
 ;;;
 ;;;     File:  "person prefixes"
 ;;;   Module:  "model;dossiers:"
-;;;  version:  February 1995
+;;;  version:  July 2013
 
 ;; initiated 6/8/93 v2.3.  Moved the form to main file 2/23/95
+;; 7/15/13 added "Saint".
 
 (in-package :sparser)
 
@@ -17,5 +18,6 @@
 (define-person-prefix :abbrev "ms")
 (define-person-prefix :abbrev "messrs")
 (define-person-prefix :abbrev "dr")
+(define-person-prefix :full "Saint" :abbrev "St")
 (define-person-prefix :full "sir")
 
