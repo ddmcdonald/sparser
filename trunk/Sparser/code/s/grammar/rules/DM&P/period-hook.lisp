5959d06064a1248b6ff2c97e1dbd9ff8753cba75
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-07-17 20:50:01 +0000

    Revived old code with an eye to explicitly representing the content of a sentence and as an even to state a conceptual analysis pass followed by a syntactic pass

--- trunk/Sparser/code/s/grammar/rules/DM&P/period-hook.lisp
+++ trunk/Sparser/code/s/grammar/rules/DM&P/period-hook.lisp
@@ -11,11 +11,24 @@
 (in-package :sparser)
 
 
+(defun period-hook-on ()
+  (define-completion-action word::period :hook 'period-hook))
+
+
 ;;--- state variable
 
 (defvar *position-before-last-period* nil
   "Records what its name suggests. Updated with each call to
  the period-hook function.")
+
+(defvar *most-recent-sentence-start* nil
+  "Bound to a position by any routine that knows that some position
+   starts a sentence (e.g. a sectionizing routine).  It is up to the
+   managing programs to be sure that any relevant analysis on an
+   ongoing sentence is finished before it updates this state variable.
+   Read by could-be-the-start-of-a-sentence,")
+
+
 
 ;;--- Basic hook function. Invoked by completion of a period
 
@@ -23,21 +36,24 @@
   ;; position-before is the one with the period on it. After picks out
   ;; the word following the period. 
   (push-debug `(,the-word-period ,position-before ,position-after))
-  ;; (setq position-before (cadr *) position-after (caddr *))
+  ;;(setq position-before (cadr *) position-after (caddr *))
+  (unless *position-before-last-period*
+    (setq *position-before-last-period* (position# 0)))
   (when (period-marks-sentence-end? position-after)
     ;; For the moment lets ignore the first sentence. The bio chapters
     ;; all start with a lot of topic lines that don't use periods,
     ;; so we should coordinate this with paragraph breaks to get the
     ;; right size 'sentence chunks'
-    (when *position-before-last-period*
-      (delimited-sentence *position-before-last-period* position-before))
-    (setq *position-before-last-period* position-before)))
+    (break "3")
 
-;  (break "got to period hook"))
+    (delimited-sentence *position-before-last-period* position-before)
+    (push-debug `(,*position-before-last-period* ,position-before))
+    (break "got to period hook~% last = ~a~% now = ~a"
+           *position-before-last-period* position-before))
+  (setq *position-before-last-period* position-before))
 
 
-(defun period-hook-on ()
-  (define-completion-action word::period 'hook #'period-hook))
+
 
 
 ;;--- Sentences
@@ -47,8 +63,9 @@
   ;; Does the amount of space between the words matter?
   ;;   (pos-preceding-whitespace position-after)
   ;; This is enough to correctly ignore "11.4" and "p. 200"
-  (eq (pos-capitalization position-after)
-      :initial-letter-capitalized))
+  (or (eq (pos-terminal position-after) *end-of-source*)
+      (eq (pos-capitalization position-after)
+          :initial-letter-capitalized)))
 
 (defun delimited-sentence (pos-previous-period pos-this-period)
   (format t "~&[  ~a  ]~%" ;; sentence length
