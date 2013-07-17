4a68748fbad0add8d8d90edd10c5203b133f7c22
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-07-17 20:44:37 +0000

    Checkpoint on capability to build type-specific recycled resources automatically

--- trunk/Sparser/code/s/tools/basics/clos-classes.lisp
+++ trunk/Sparser/code/s/tools/basics/clos-classes.lisp
@@ -10,6 +10,8 @@
 
 (in-package :sparser)
 
+;;--- has a simple name to identify it
+
 (defclass named-object ()
   ((name :type symbol :accessor name :initarg :name 
          :initform 'unknown))
@@ -19,3 +21,19 @@
 (defmethod print-object ((i named-object) stream)
   (print-unreadable-object (i stream :type t)
     (format stream "~s" (name i))))
+
+
+;;--- partonomy: parent and children
+
+(defclass has-parent ()
+  ((parent :accessor parent
+    :documentation "Point up to the larger stucture that contains
+      this one."))
+  (:documentation "The leaf structure will only have a parent"))
+
+(defclass has-children ()
+  ((children
+    :documentation "Point to one or more structures that are 
+      in some sense contained by this object."))
+  (:documentation "The topmost structure in a partonomy will
+    only have children."))
--- trunk/Sparser/code/s/tools/basics/loader.lisp
+++ trunk/Sparser/code/s/tools/basics/loader.lisp
@@ -4,14 +4,14 @@
 ;;;
 ;;;      File:  "loader"
 ;;;    Module:   "tools:basics"
-;;;   Version:   March 2013
+;;;   Version:   July 2013
 
 ;; 7/23/09 Added 'debugging' with Jake's tools. Uncommented loading of no-breaks.
 ;; 9/10 Added the SFL macro for creating CLOS classes
 ;; 8/2/10 -- adding items here in preparation for a make-over for a common
 ;;  repository of lisp utilities.
 ;; 3/9/11 Conditionalizing on :ddm-utils feature.
-;; 3/29/13 added [clos-classes]
+;; 3/29/13 added [clos-classes]. 7/15/13 added [resource]
 
 
 (in-package :sparser)
@@ -23,4 +23,5 @@
 #-ddm-utils (lload "basic tools;SFL Clos")
 (lload "basic tools;sorting")
 (lload "basic tools;clos-classes")
+(lload "basic tools;resource")
 
