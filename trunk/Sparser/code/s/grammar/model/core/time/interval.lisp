70ec54d117e0ff4dba8921e1ed8796b8974a655f
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-07-18 15:25:06 +0000

    cleared up some of the structure of the interval category, still not fleshed out fully

--- trunk/Sparser/code/s/grammar/model/core/time/interval.lisp
+++ trunk/Sparser/code/s/grammar/model/core/time/interval.lisp
@@ -14,21 +14,22 @@
 ;;; the object
 ;;;------------
 
-;;category incomplete -- really just a skeleton/frame
+;;We will use Allen's formulation of a time interval as a model for the sparser one
+;;Each interval has two endpoints (t-, t+)
+;;Minimally we should know either t- or t+ (neither have to be overt)
+;;then there is an event which takes place relative to the anchor/interval
 (define-category  interval
   :specializes time
   :instantiates self
-  :binds ((sequencer . sequencer)
-          (amount-of-time . amount-of-time)
-          (np-segment . np-segment))
-  ;;:index (:permanent :key name)
-  ;;:realization (:common-noun name )
+  :binds ((anchor . anchor) ;;doesn't have to be an anchor, just a slot for some timex
+          (event . e)) ;;how to relate intervals with multiple events?  is this really correct?
+  :realization (:common-noun "interval")
   )
 
-#|(def-cfr interval (sequencer amount-of-time) ;;this is not an actual interval, only the anchor part of it
+#|(def-cfr interval (time amount-of-time) ;;this is not an actual interval, only the anchor part of it
   :form np
   :referent(:instantiate-individual interval
-            :with (sequencer left-edge amount-of-time right-edge)))|#      
+            :with (time left-edge amount-of-time right-edge)))|#      
                           
 
 
