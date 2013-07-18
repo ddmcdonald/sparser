04df2e3234ce57a02f7fd9aee64f463f804694c9
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-07-18 14:36:34 +0000

    added in tree pattern for anchor category (very simple right now)

---
+++ Sparser/code/s/grammar/rules/tree-families/anchor.lisp
@@ -0,0 +1,59 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
+;;; copyright (c) 1999 David D. McDonald  -- all rights reserved
+;;; Copyright (c) 2007 BBNT Solutions LLC. All Rights Reserved
+;;; $Id:$
+;;;
+;;;     File:  "anchor"
+;;;   Module:  "grammar;rules:tree-families:"
+;;;  version:  March 2007
+
+;; initiated 9/27/99. 3/22/07 Found a typo in the mapping - the year was getting
+;; the label for days.
+
+(in-package :sparser)
+
+#| This is a very ad-hoc pattern, but then so are dates. |#
+
+(define-exploded-tree-family  anchor-pattern
+  :description ""
+  :binding-parameters ( type term1 ;; sequencer
+                             term2 ;; date
+                             term3 ;; amount-of-time
+                             term4 ;; season-year
+                             term5 ;; season
+                             )
+  :labels ( np n1 ;; sequencer
+               n2 ;; date
+               n3 ;; amount-of-time
+               n4 ;; season-year
+               n5 ;; season
+               )
+  :cases
+     (
+      ;; "(the) last three years"
+      (:np (np (n1 n3)
+               :head right-edge
+               :instantiate-individual type
+               :binds (term1 left-edge
+                       term3 right-edge)))
+
+      ;; "after March 1, 2013"
+      (:np (np (n1 n2)
+               :head right-edge
+               :instantiate-individual type
+               :binds (term1 left-edge
+                       term2 right-edge)))
+
+      ;; "during the winter of 1992"
+      (:np (np (n1 n4)
+               :head right-edge
+               :instantiate-individual type
+               :binds (term1 left-edge
+                       term4 right-edge)))
+
+      ;; "during the summer"
+      (:np (np (n1 n5)
+               :head right-edge
+               :instantiate-individual type
+               :binds (term1 left-edge
+                       term5 right-edge)))))
