686fb25d9eed7d583402a72c25622ea39de4e099
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-07-22 15:24:11 +0000

    Reordered the object definitions ahead of their callers. Fixes a constraint that happens in OpenMCL that wasn't present in MCL where the struct accessors are doing type-checks and don't recognize instances of their own struct. Most Odd.

--- Sparser/code/s/analyzers/DA/loader.lisp
+++ Sparser/code/s/analyzers/DA/loader.lisp
@@ -1,22 +1,24 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1993,1994,1995  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1993-1995,2013  David D. McDonald  -- all rights reserved
 ;;; 
 ;;;     File:  "loader"
 ;;;   Module:  "analyzers;DA:"
-;;;  Version:  May 1995
+;;;  Version:  July 2013
 
-;; initiated 7/8/93 v2.3, added gate 9/13. Gave it substantial content 5/5/95
+;; initiated 7/8/93 v2.3, added gate 9/13. Gave it substantial content 5/5/95.
+;; 7/21/13 Reordering arcs and vertexes early in case that's what's confusing 
+;; the accessors (don't see vertexes as such in a call to one of their fields).
 
 (in-package :sparser)
 
 (gate-grammar *da*
 
+  (lload "DA;vertexes")
+  (lload "DA;arcs")
   (lload "DA;trie object")
   (lload "DA;index")
   (lload "DA;add pattern")
   (lload "DA;delete pattern")
-  (lload "DA;vertexes")
-  (lload "DA;arcs")
   (lload "DA;knit")
   (lload "DA;match")
   (lload "DA;do action")
