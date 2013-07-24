0e5782545ac977e3e1f0e915672bb73ae3a86b98
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-07-24 16:31:28 +0000

    Added martyr, but do need more cases

--- trunk/Sparser/code/s/grammar/model/sl/middle-east/loader.lisp
+++ trunk/Sparser/code/s/grammar/model/sl/middle-east/loader.lisp
@@ -5,9 +5,10 @@
 ;;;    Module:   "sl;middle-east"
 ;;;   version:   July 2013
 
-;; initiated 5/23/13. Added titles 7/17/13
+;; initiated 5/23/13. Added [titles] 7/17/13. [martyrdom] 7/22/13
 
 (in-package :sparser)
 
 (gload "mideast;named-entities")
 (gload "mideast;titles")
+(gload "mideast;martyrdom")
