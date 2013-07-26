087b68575aaa97145c3881a6e50c37479795c871
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-07-26 13:51:20 +0000

    Made all the symbols case-insensitive and added symbols used in later state mumble load to quash compiler complaint

--- trunk/util/package.lisp
+++ trunk/util/package.lisp
@@ -9,41 +9,45 @@
 (defpackage :ddm-util
     (:use :common-lisp
           #+ccl :ccl)
-    (:export  d 
-             string-append concat
+    (:export #:d 
+             #:string-append #:concat
 
 	  ;; n.b. there are other MISC.LISP lispm-era utils not yet
 	  ;; checked out and carried forward
-             append-new
-             keys-of-association-list 
-             tail-cons deep-copy
-             flatten flattenc
-             sexp-contains-symbol split-list-on-first-keyword
-             gensymbol
-             launder-sexp-symbols-package
-             ordinal-ending
-             string->list-of-symbols string->Java-style-symbol
-             sexp->string
-             upcase-first-lettr capitalize-symbol
-             replace-periods-with-underbars replace-bad-URI-characters
-             ends-in
+             #:append-new
+             #:keys-of-association-list 
+             #:tail-cons #:deep-copy
+             #:flatten #:flattenc
+             #:sexp-contains-symbol #:split-list-on-first-keyword
+             #:gensymbol
+             #:launder-sexp-symbols-package
+             #:ordinal-ending
+             #:string->list-of-symbols #:string->Java-style-symbol
+             #:sexp->string
+             #:upcase-first-lettr #:capitalize-symbol
+             #:replace-periods-with-underbars #:replace-bad-URI-characters
+             #:ends-in
              ;; unchecked goes here
-             hashtable-to-alist
-             break-up-at
-             defobject
-             symbol-name-string-lessp
-             until 
-             dbind defsubst
-             let-with-dynamic-extent let-with-dynamic-extent-unless-bound
-             let*-with-dynamic-extent 
-             comment
-             temporarily-inhibit-fdefine-warnings 
-             cond-every
-             cond-every
-             apush add-association remove-association delete-association
-             *tabbed-format-column* tabbed-format format-fully-qualified
-             add1 sub1
-             newline
-             list-hash-table
-             mbug mbreak sorry
+             #:hashtable-to-alist
+             #:break-up-at
+             #:defobject
+             #:symbol-name-string-lessp
+             #:until 
+             #:dbind #:defsubst
+             #:let-with-dynamic-extent #:let-with-dynamic-extent-unless-bound
+             #:let*-with-dynamic-extent 
+             #:comment
+             #:temporarily-inhibit-fdefine-warnings 
+             #:cond-every
+             #:apush #:add-association #:remove-association #:delete-association
+             #:*tabbed-format-column* #:tabbed-format #:format-fully-qualified
+             #:add1 #:sub1
+             #:newline
+             #:list-hash-table
+             #:mbug #:mbreak #:sorry
+
+             ;; auto-gen
+             #:instantiate-type
+             #:*data-structure-for-auto-gen*
+             #:set-auto-gen-symbol-package
              ))
