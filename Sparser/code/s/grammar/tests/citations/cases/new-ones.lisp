0ff503ad8193901487ee127c30b72e287ed0dd0b
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-07-26 14:38:59 +0000

    added new citations for iranian timex

--- Sparser/code/s/grammar/tests/citations/cases/new-ones.lisp
+++ Sparser/code/s/grammar/tests/citations/cases/new-ones.lisp
@@ -225,3 +225,110 @@ e11   DATE                    5 "january 1 , 1999" 9
 ;;this doesn't come up as age, since "a day" is considered a time-unit
 #|(define-citation "a day ago"
   (1 AGE 4))|#
+
+;;;---------
+;;; iranian timex
+;;;---------
+
+;;;javan-online.txt
+
+;;iranian months mentioned in javan-online.txt
+(define-citation "in the months of azar and dey"
+  (                                 SOURCE-START
+e7    PREP-TIME               1 "in the months" 4
+e5 e6                            "of" :: of, OF
+                                 "azar"
+e8                               "and"
+e9    MONTH                   7 "dey" 8
+                                 END-OF-SOURCE)
+
+;;are these really instances of time-unit?
+(define-citation "each year"
+  (1 TIME-UNIT 3))
+
+(define-citation "these months"
+  (1 TIME-UNIT 3))
+
+(define-citation "these winter days"
+  (1 TIME-UNIT 4))
+
+;;how should this complex timex be parsed?
+;;really it's redundant if now is during the month of dey
+;;but otherwise it's just picking out the days of the iranian year that belong to this month
+;;archaism, however, and shouldn't be found in contemporary English
+#|(define-citation "these days in the month of dey"
+    (                                 SOURCE-START
+e1    TIME-UNIT               1 "these days" 3
+e10   PREP-TIME               3 "in the month" 6
+e7 e8                            "of" :: of, OF
+e9    MONTH                   7 "dey" 8
+                                 END-OF-SOURCE))|#
+
+;;very interesting construction, similar to the above
+;;without context, the exact meaning should refer to the 21st day after the start of winter
+;;however, it could be the case that the iranian winter does not coincide with the actual onset
+#|(define-citation "the 21st day of winter"
+    (                                 SOURCE-START
+e5    TIME-UNIT               1 "the 21 st day" 5
+e9    OF-TIME                 5 "of winter" 7
+                                 END-OF-SOURCE))|#
+
+(define-citation "in the year 90"
+  (1 PREP-TIME 5))
+
+(define-citation "a month later"
+  (1 LATER 4))
+
+(define-citation "a few days later"
+  (1 LATER 5))
+
+;;should this work like the above two, i.e. (1 AFTER 5)?
+#|(define-citation "a few days after"
+    (                                 SOURCE-START
+e4    TIME-UNIT               1 "a few days" 4
+e5 e6                            "after" :: SEQUENCER, AFTER
+                                 END-OF-SOURCE))|#
+
+;;do we care about this phrase with respect to the time grammar?
+#|(define-citation "at the beginning of his student days"
+    (                                 SOURCE-START
+e0 e1                            "at" :: at, AT
+e2                               "the"
+e3    BEGIN-EVENT             3 "beginning" 4
+e4 e5                            "of" :: of, OF
+e10   TIME-UNIT               5 "his student days" 8
+                                 END-OF-SOURCE))|#
+
+;;;iranian-commander.txt
+
+(define-citation "in two years"
+  (1 PREP-TIME 4))
+
+;;european style of writing date
+;;this should be a date, but sparser calls it an amount
+(define-citation "23 July 2011"
+  (1 DATE 4))
+
+
+;;interesting use of "late" with several timex
+#|(define-citation "Late in June"
+    (                                 SOURCE-START
+e0    PHASE-OFFSET            1 "late" 2
+e4    RELATIVE-LOCATION       2 "in june" 4
+                                 END-OF-SOURCE))|#
+
+#|(define-citation "late last month"
+    (                                 SOURCE-START
+e0    PHASE-OFFSET            1 "late" 2
+e3    TIME                    2 "last month" 4
+                                 END-OF-SOURCE))|#
+
+;;this is still parsed as a relative-location
+#|(define-citation "in the last three years"
+    (                                 SOURCE-START
+e10   RELATIVE-LOCATION       1 "in the last three years" 6
+                                 END-OF-SOURCE))|#
+
+
+
+
