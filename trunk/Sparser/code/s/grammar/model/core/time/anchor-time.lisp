b16cf251a07b2a3fa4c749ef818358912cf7a68a
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-08-01 06:11:53 +0000

    anchor category dealing with general time--this may be too general however, as its targets are days, seasons, and season-year

---
+++ trunk/Sparser/code/s/grammar/model/core/time/anchor-time.lisp
@@ -0,0 +1,26 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
+;;; copyright (c) 2013 David D. McDonald  -- all rights reserved
+;;;
+;;;     File:  "anchor-time"
+;;;   Module:  "model;core:time:"
+;;;  version:  1.0 July 2013
+
+;;the anchor category is meant to capture timex that place events/entities at specific points
+;;i.e., these timex 'anchor' the event to a particular time
+;;any sequencer followed by a timex
+;;captures "during winter", "before the summer of 1992", "after Monday"
+(in-package :sparser)
+
+;;;------------
+;;; the object
+;;;------------
+
+(define-category anchor-time
+  :specializes anchor
+  :instantiates self
+  :binds ((sequencer . sequencer)
+          (time . time))
+  :realization (:common-noun "anchor-time"))
+
+(def-cfr anchor-time (sequencer time)
+  :referent (:instantiate-individual anchor-time))
