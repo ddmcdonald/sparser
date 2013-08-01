3e6dbb6cb5aa6da6b37c57180d568b3685850e6e
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-08-01 18:34:16 +0000

    Setup traps for bugs that can be introduced by incorrectly written action routines

--- Sparser/code/s/drivers/DA/dispatch1.lisp
+++ Sparser/code/s/drivers/DA/dispatch1.lisp
@@ -1,13 +1,14 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1994,1995,2011 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1994-1995,2011-2013 David D. McDonald  -- all rights reserved
 ;;; 
 ;;;     File:  "dispatch"
 ;;;   Module:  "drivers;DA:"
-;;;  Version:  November 2011
+;;;  Version:  July 2013
 
 ;; initiated 10/27/94 v2.3.  Moved it and gave it some flesh 5/5/95
 ;; 1.0 (5/19) redid them as tail recursion and removed an interveening layer.
-;;     (11/7/11) replaced the etypecase.
+;;     (11/7/11) replaced the etypecase. 7/29/13 Figured out strange bug
+;;      caused by bad return value from successful DA function.
 
 (in-package :sparser)
 
@@ -52,12 +53,13 @@
             (tr :resuming-DA-walk-at pos-after)
             (look-for-da-patterns pos-after))))
 
-        (cons ;; trap wacko bug
-         (push-debug `(,result ,1st-vertex ,tt ,pos-before ,pos-after))
-         ;; (setq result (car *) 1st-vertex (second *) tt (third *) pos-before (fourth *) pos-after (fifth *))
-         (break "Result from checking for extensions from ~a~
-               ~%is a list of length ~d" 1st-vertex (length result)))
-        
+        (cons 
+         ;; This can happen with a stub that ends with a push-debug
+         (push-debug `(,1st-vertex ,tt ,pos-before ,pos-after))
+         (error "Look at the return value of the function in ~
+                 the DA rule ~a. It may have returned the debugging stack"
+                *matched-da-rule*))
+
         (otherwise
          (push-debug `(,result ,1st-vertex ,tt ,pos-before ,pos-after))
          (break "Unanticipated type of result: ~a" (type-of result))))
