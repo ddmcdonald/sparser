42c99a122fd8c7df72e45a36aef53fcace330ffd
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-08-01 06:09:05 +0000

    created an anchor category for anchors involving amount-of-time

---
+++ Sparser/code/s/grammar/model/core/time/anchor-amount.lisp
@@ -0,0 +1,27 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
+;;; copyright (c) 2013 David D. McDonald  -- all rights reserved
+;;;
+;;;     File:  "anchor-amount"
+;;;   Module:  "model;core:time:"
+;;;  version:  1.0 July 2013
+
+;;the anchor category is meant to capture timex that place events/entities at specific points
+;;i.e., these timex 'anchor' the event to a particular time
+;;the anchor-date captures the complex time adverbials of sequencers followed by amount-of-time
+;;should this category also include seasons/season-year ? 
+;;captures "last three years", "two months later" 
+(in-package :sparser)
+
+;;;------------
+;;; the object
+;;;------------
+
+(define-category anchor-amount
+  :specializes anchor
+  :instantiates self
+  :binds ((sequencer . sequencer)
+          (amount-of-time . amount-of-time))
+  :realization (:common-noun "anchor-amount"))
+
+(def-cfr anchor-amount (sequencer amount-of-time)
+  :referent (:instantiate-individual anchor-amount))
