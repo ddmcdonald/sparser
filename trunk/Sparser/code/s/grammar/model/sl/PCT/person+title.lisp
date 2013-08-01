e096a50cea709c9215bf80c48baaa1a54baf6bbd
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-08-01 18:25:25 +0000

    Extensive improvement in coverage. Refined use of Debris Analysis rules. Added methods for transferring content from title to person when they're in construction.

--- trunk/Sparser/code/s/grammar/model/sl/PCT/person+title.lisp
+++ trunk/Sparser/code/s/grammar/model/sl/PCT/person+title.lisp
@@ -106,6 +106,50 @@
 ;;; <title> , <person or ne> ,  
 ;;;----------------------------
 
+(define-debris-analysis-rule comma+person+comma
+  :pattern ( "," person "," )
+  :action (:function person-absorb-appos-commas-look-around second))
+
+(defun person-absorb-appos-commas-look-around (person-edge)
+  (push-debug `(,person-edge)) 
+  (break "absorb-appos-commas edge: ~a" person-edge)
+  ;; //// When applied to ex.#1,  it looks under the leading edge
+  ;; of e64 and finds a person, but ignores the remaining parts
+  person-edge  ;; have to return an edge
+  )
+
+(define-debris-analysis-rule comma+person+comma
+  :pattern ( "," named-object"," )
+  :action (:function ne-absorb-appos-commas-look-around second))
+
+(defun ne-absorb-appos-commas-look-around (ne-edge)
+  ;;(push-debug `(,ne-edge))
+  (let ((ne (edge-referent ne-edge)))
+    ;;(push-debug `(,ne)) ;; to convert to person
+    (let ((appos-edge (DA-based-appos-edge ne-edge)))
+      (let* ((start-pos (pos-edge-starts-at appos-edge))
+             (ends-there (pos-ends-here start-pos))
+             (title-edge (da/look-under-edge/scan-edges
+                          ends-there category::title)))
+        (if title-edge
+          ;; then we convert the ne to a person and
+          ;; form the combined edge
+          (let ((person (interpret-name-as-person ne))
+                (edge-over-title (edge-used-in title-edge)))
+            (setf (edge-referent appos-edge) person)
+            (let* ((title+person (find-cfr 'person '(title person)))
+                   (full-edge
+                    (make-completed-binary-edge
+                     title-edge appos-edge title+person)))
+              (tuck-new-edge-under-already-knit
+               title-edge full-edge edge-over-title)
+              full-edge))
+          ;; otherwise we just return the ne edge having
+          ;; absorbed the commas
+          appos-edge)))))
+
+
+
 (define-debris-analysis-rule title+comma+ne+comma
   :pattern ( title "," named-object "," )
   :action (:function title-ne-in-appositive-DA third))
@@ -113,15 +157,6 @@
 (define-debris-analysis-rule title+comma+person+comma
   :pattern ( title "," person "," )
   :action (:function title-person-in-appositive-DA third))
-
-(define-debris-analysis-rule comma+person+comma
-  :pattern ( "," person "," )
-  :action (:function person-absorb-appos-commas-look-around second))
-
-
-(defun person-absorb-appos-commas-look-around (person-edge)
-  (push-debug `(,person-edge)) ;;(break "absorb-appos-commas")
-  )
 
 (defun title-ne-in-appositive-DA (ne-edge)
   (let* ((named-object (edge-referent ne-edge))
@@ -159,14 +194,54 @@
         (let ((full-edge
                (make-completed-binary-edge
                 title-edge appositive-edge title+person)))
-          (break "full edge")
           full-edge)))))
+
+(defun DA-based-appos-edge (appos-edge)
+  ;;/// sort out how to factor title-person-in-appositive-DA
+  ;; to expose this.  Appos could be named-object
+  (let* ((appos-category (edge-category appos-edge))
+         (appos-referent (edge-referent appos-edge))
+         (before-leading-comma
+          (chart-position-before (pos-edge-starts-at appos-edge)))
+         (after-trailing-comma
+          (chart-position-after (pos-edge-ends-at appos-edge)))
+         (consituents `(,(right-treetop-at before-leading-comma)
+                        ,appos-edge
+                        ,(left-treetop-at after-trailing-comma))))
+    (make-edge-over-long-span
+     before-leading-comma
+     after-trailing-comma
+     appos-category
+     :rule :DA-based-appos-edge
+     :form category::appositive
+     :referent appos-referent
+     :constituents consituents)))
 
 
 (def-k-method redistribute (age+title person)
+  ;; Called for side-effects at the bottom of referent from rule
   (push-debug `(,age+title ,person)) ;;(break "right place")
-  )
-                          
+  ;; (setq age+title (car *) person (cadr *))
+  (let ((age-binding (binds age+title 'age))
+        (title-binding (binds age+title 'title))
+        (position-binding (binds person 'position)))
+    ;; /// We should change the type of the age+title to just title
+    ;; but that's overkill, that would include cleaning up its
+    ;; bindings to fit.
+    ;; For now just add the age and the title to the person.
+    (let ((age (binding-value age-binding))
+          (title (binding-value title-binding)))
+      (bind-variable 'age age person)
+      (setf (binding-value position-binding) title) ;; more indexing????
+      (push-debug `(,person ,title))
+      (call-next-method)
+      person)))
+    
+(def-k-method redistribute (title person)
+  (let ((country-binding (binds title 'locale)))
+    (when country-binding
+      (bind-variable
+       'nationality (binding-value country-binding) person))))
 
 
 
