52fab465eb964fe155276c0d4b0efb76fe9b4f34
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-08-01 06:11:06 +0000

    took out a psr that was conflicting with anchor

--- Sparser/code/s/grammar/model/core/time/relative-moments.lisp
+++ Sparser/code/s/grammar/model/core/time/relative-moments.lisp
@@ -75,6 +75,8 @@
                           (individuator . relativizer)
                           (base-category . reference-time))))
 
+(delete/cfr# 618)
+
 
 ;;--- Specific instance of time (units): "that day", "last month"
 ;;  These become particular times that can be dereferenced in context
