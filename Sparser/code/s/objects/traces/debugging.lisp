bb81c19063e734f300e7d826c1fc4dcf701b37c9
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-08-16 14:37:45 +0000

    Augmented function inadvertently redefined in a workspace

--- Sparser/code/s/objects/traces/debugging.lisp
+++ Sparser/code/s/objects/traces/debugging.lisp
@@ -1,13 +1,13 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
-;;; copyright (c) 1992,1993,1994,1995  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992-1995,2013 David D. McDonald  -- all rights reserved
 ;;; 
 ;;;     File:  "debugging"
 ;;;   Module:  "objects;traces:"
-;;;  Version:  September 1995
+;;;  Version:  August 2013
 
 ;; initiated 3/91. Added *..new-cases* 6/13/95.  Started the debugging flags
 ;; accumulator 7/25. Added to it 8/9.  Moved the two pre-existing flags into
-;; the aggregating routine 9/5
+;; the aggregating routine 9/5/95. 8/14/13 Added in the new crop of flags.
 
 (in-package :sparser)
 
@@ -48,6 +48,11 @@
         *break-on-new-categories-in-cap-seq* t
         *break-on-new-cases* t
         *break-on-unexpected-cases* t
+        ;; new ones from Grok and beyond
+        *break-on-new-name-converter-cases* t
+        *debug-reify-implicit-individuals* t
+        *debug-segment-handling* t
+        *break-on-new-bracket-situations* t
         ))
 
 (defun turn-off-debugging-flags ()
@@ -57,5 +62,10 @@
         *break-on-new-categories-in-cap-seq* nil
         *break-on-new-cases* nil
         *break-on-unexpected-cases* nil
+        ;; new
+        *break-on-new-name-converter-cases* nil
+        *debug-reify-implicit-individuals* nil
+        *debug-segment-handling* nil
+        *break-on-new-bracket-situations* nil
         ))
 
