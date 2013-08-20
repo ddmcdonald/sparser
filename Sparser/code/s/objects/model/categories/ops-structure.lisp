c7b493dedc571803a1133eaff4dfa062e7a4bd87
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-08-20 20:00:17 +0000

    Made sure this one (that -is- loaded) has all the requisite things I'd put in the wrong one

--- Sparser/code/s/objects/model/categories/ops-structure.lisp
+++ Sparser/code/s/objects/model/categories/ops-structure.lisp
@@ -1,13 +1,14 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1994-1995,2011 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1994-1995,2011-2013 David D. McDonald  -- all rights reserved
 ;;;
 ;;;     File:  "ops structure"
 ;;;   Module:  "objects;model:categories:"
-;;;  version:  September 2011
+;;;  version:  August 2013
 
 ;; broken out from [object] file 8/12/94. 12/8/95 added Category-instantiates as
 ;; an access fn. from the category-level. 9/6/11 Added rule-label field and an
-;; accessor for it. 
+;; accessor for it. 8/16/13 Added a method that logically belongs in objecs/chart/
+;; categories/lookup, but the type's not available then.
 
 (in-package :sparser)
 
@@ -52,3 +53,12 @@
     (when ops-str
       (cat-ops-rule-label ops-str))))
 
+
+;;;------------------
+;;; Other operations
+;;;------------------
+
+;; The other cases are in objecs/chart/categories/lookup,
+(defmethod category-named ((c referential-category) &optional break-if-no-category)
+  (declare (ignore break-if-no-category))
+  c)
