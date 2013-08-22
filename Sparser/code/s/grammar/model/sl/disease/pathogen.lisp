43311982c036bcfda68e3909cb989d91ed32d1fd
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-08-22 06:21:31 +0000

    added in a new category for diease-the pathogen.  did not choose to name category disease to avoid confusion with directory name

---
+++ Sparser/code/s/grammar/model/sl/disease/pathogen.lisp
@@ -0,0 +1,52 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER COMMON-LISP) -*-
+;;; Copyright (c) 2007 BBNT Solutions LLC. All Rights Reserved
+;;; copyright (c) 2013  David D. McDonald  -- all rights reserved
+;;;
+;;;      File:   "pathogen"
+;;;    Module:   "sl;disease:"
+;;;   version:   August 2013
+
+;;category to represent named dieases, like bird flu.
+;;I conspicuously decide not to name this file disease
+;;in order to avoid confusion with the sl directory name
+;;we can use this category to build up a representation for disease outbreaks in diease:spread
+
+
+(in-package :sparser)
+
+;;;------------
+;;; the object
+;;;------------
+
+;;this is just a bare-bones representation, but works for now
+(define-category pathogen
+  :specializes nil
+  :instantiates self
+  :binds ((name :primitive word))
+          ;;(vector :primitive word) e.g. avian flu is spread by infected birds 
+  :index (:permanent :key name)
+  :realization (:common-noun name)) ;;are pathogens common nouns syntactically?  
+
+;;;-----------
+;;; citations
+;;;-----------
+
+;;"the bird flu virus h 5 n 1" from 1 Aljazeera_Jan-18.txt
+
+
+;;;------
+;;; form
+;;;------
+
+(defun define-pathogen (string)
+  (let ((name (define-or-find-individual
+                  'pathogen :name string)))
+        name))
+
+;;;------------
+;;; the instances
+;;;------------
+
+(define-pathogen "h5n1")
+
+
