6921f8a2311f5c6ae054538db6ca1cce8c5a27e8
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-08-27 06:19:13 +0000

    asthetic changes

--- Sparser/code/s/grammar/model/sl/disease/pathogen-type.lisp
+++ Sparser/code/s/grammar/model/sl/disease/pathogen-type.lisp
@@ -18,7 +18,7 @@
 ;;;------------
 
 (define-category pathogen-type
-  :specializes nil
+  :specializes pathogen
   :instantiates self
   :binds ((name :primitive word))
   :index (:permanent :key name)
