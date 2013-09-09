25cdd9918276b4eae85f8bcb512f7970906d226e
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-09-09 02:21:20 +0000

    Split the larger files into a set of smaller ones. Stubbed out a smidgen of academics

---
+++ Sparser/code/s/grammar/model/sl/middle-east/academics.lisp
@@ -0,0 +1,29 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER COMMON-LISP) -*-
+;;; copyright (c) 2013  David D. McDonald  -- all rights reserved
+;;;
+;;;      File:   "months"
+;;;    Module:   "sl;middle-east"
+;;;   version:   September 2013
+
+;; initiated 9/3/13 because they were being taken to be people with very
+;; odd results. Only discovered by decoding a possessive
+
+(in-package :sparser)
+
+;;--- Types of degrees
+
+(define-category academic-degree
+  :instantiates self
+ ;; :specializes  ?? what
+  :binds ((name :primitive word))
+  :realization (:common-noun name))
+
+(defun make-academic-degree (string)
+  (define-or-find-individual category::academic-degree
+     :name string))
+
+(make-academic-degree "B.Sc.") ;; only one in javan-online
+
+#|
+only the students and professors who
+|#
--- Sparser/code/s/grammar/model/sl/middle-east/named-entities.lisp
+++ Sparser/code/s/grammar/model/sl/middle-east/named-entities.lisp
@@ -63,27 +63,3 @@ in any of the names that is not a pure "name" like Binyamin.
 
 
 ;;---- Months
-
-;;some Iranian months from javan-online -- ordering is bogus because for that
-;;  we'd have to look them up.
-;;"the months that start on the 21st November and end on 20th January"
-(define-month "Azar" 1)
-(define-month "Dey" 2)
-(define-month "Mordad" 3)
-
-;;numbers precede months in european style
-;;also used in javan-online.txt, an iranian news article
-;;this is the rule we want, but there is an ordering issue
-;;when parsing "21 Dey of the year 90"
-;;the parser first merges "Dey" and "of the year 90" as a date
-;;then the rule for amount -> number time kicks in
-#|(def-cfr date (number month)
-  :form np
-  :referent (:instantiate-individual date
-             :with (month right-edge day left-edge)))|#
-
-;;instead we'll use this rule for now until that issue is fixed
-(def-cfr date (number date)
-  :form np
-  :referent (:head right-edge
-             :bind (day . left-edge)))
