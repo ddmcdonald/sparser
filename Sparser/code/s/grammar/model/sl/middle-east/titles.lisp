4bea2db5a69caa4b114e463965ec85d78135e049
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-09-09 02:20:31 +0000

    Added a couple more. Treatment of gunman with its irregular plural pushed the refactoring in the morphology code. Worthy of later re-evaluation

--- Sparser/code/s/grammar/model/sl/middle-east/titles.lisp
+++ Sparser/code/s/grammar/model/sl/middle-east/titles.lisp
@@ -3,14 +3,17 @@
 ;;;
 ;;;      File:   "titles"
 ;;;    Module:   "sl;middle-east"
-;;;   version:   July 2013
+;;;   version:   September 2013
 
-;; initiated 7/17/13
+;; initiated 7/17/13. Adding through 9/7/13
 
 (in-package :sparser)
 
+(define-single-word-title "Ayatollah")
 (define-single-word-title "chief")
 (define-single-word-title "colleague")
 (define-single-word-title "driver")
+(define-single-word-title '(:common-noun "gunman" :plural "gunmen"))
+(define-single-word-title "terrorist")
 (define-single-word-title "terrorist")
 ;;(define-single-word-title "")
