30c8a3a745cca067b65ad55171321ae23cfcc977
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-09-16 16:51:28 +0000

    Added little helper fn. to get the category of the will-be edge from form rules

--- trunk/Sparser/code/s/objects/rules/cfr/object1.lisp
+++ trunk/Sparser/code/s/objects/rules/cfr/object1.lisp
@@ -1,15 +1,16 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
-;;; copyright (c) 1992-2005  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992-2005,2013  David D. McDonald  -- all rights reserved
 ;;;
 ;;;      File:   "object"
 ;;;    Module:   "objects;rules:cfr:"   ;; "context free rules"
-;;;   Version:   1.4 February 2005
+;;;   Version:   1.4 September 2013
 
 ;; 1.1  (v1.5) added new fields to handle the new rule regime
 ;; 1.2  (1/29 v1.8) Moved in Binary-rule?
 ;; 1.3  (7/30 v1.8.6) Added Polyword-rule?
 ;; 1.4  (2/16/98) added 'schema' field to cfrs.
 ;;      (2/7/05) added Unary-rule?
+;;      (9/16/13) added category-to-apply
 
 (in-package :sparser)
 
@@ -49,3 +50,24 @@
   (let ((plist (cfr-plist cfr)))
     (when plist
       (cadr (member tag plist)))))
+
+
+;;;----------
+;;; decoders
+;;;----------
+
+(defun category-to-apply (rule)
+  "The rule was retrieved by hand and is being used for
+   its parts. This sorts out what to use as the category
+   in the unusual cases."
+  (let ((completion-field (cfr-completion rule)))
+    (cond
+     ((null completion-field)
+      (cfr-category rule))
+     ((eq completion-field :right-edge)
+      (form-rule-head-category rule :right-edge))
+     ((eq completion-field :left-edge)
+      (form-rule-head-category rule :left-edge))
+     (t (push-debug `(,rule))
+        (break "New case - completion fiele = ~a"
+               completion-field)))))
