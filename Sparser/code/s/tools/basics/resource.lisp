9715a33db84610daf554101fc58013f920825e6a
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-09-18 13:10:24 +0000

    Added some more generally useful CLOS classes like indexed as a mixin. Debugged (finally) a useable automatic resource generator. More methods to add and variants to crete.

--- Sparser/code/s/tools/basics/resource.lisp
+++ Sparser/code/s/tools/basics/resource.lisp
@@ -3,10 +3,11 @@
 ;;;
 ;;;      File:  "resource"
 ;;;    Module:   "tools:basics"
-;;;   Version:   July 2013
+;;;   Version:   September 2013
 
 ;; initiated 7/15/13 to provide a general facility for automating
-;; the construction and management of resources.
+;; the construction and management of resources. 9/17/13 Got it to
+;; turn over and do useful tings 
 
 (defclass resource-specification (named-object)
   ((storage-type
@@ -20,22 +21,100 @@
     :documentation "The name of the class (or perhaps struct) that
       we are creating a resource for.")
 )
-  (:documentation ""))  
+  (:documentation ""))
+
 
 
 (defclass resource ()
-  ((resource-spec 
-    :type resource-specification
-    :accessor resource-spec))
-  (:documentation "Used as a mixin in conjunction with the form
-    def-resource-data-for to specify any overrides to the default
-    values given in resource-specification. Designed with the
-    defaults in mind, it provides slots for the needed storage, etc."))
+  ((class :type symbol :initarg :name :accessor resource-class-name
+    :documentation "Name of the class this is a resource for")
+   (store :type list :accessor resource-store
+    :documentation "A kcons list of all the available instances")
+   (next-instance :accessor next-instance
+    :documentation "A pointer into the store to the next instance
+     to use")
+   (counter :initform -1 :accessor instance-counter
+    :documentation "")
+   )
+  (:documentation ""))
+
+(defmethod print-object ((r resource) stream)
+  (print-unreadable-object (r stream :type t)
+    (format stream "~a" (resource-class-name r))))
 
 
-;; (defmacro def-resource-data-for (class-name &
+(defun allocate-next-instance (r) ;; make a method on subtypes of resource
+  (let* ((index (incf (instance-counter r)))
+         (i (nth index (resource-store r))))
+    ;;/// initalize ???
+    i))
 
-;;(defgeneric 
+(defun initialize-resource (r) ;; ditto
+  (loop for a in (resource-store r)
+    do (clear a))
+  (setf (instance-counter r) -1)
+  r)
+
+(defun next-indexical-name (class-keyword)
+  (let* ((r (get-resource class-keyword))
+         (n (instance-counter r))) ;; hmmm - interects with allocate
+    (let ((pname (string-append (symbol-name (resource-class-name r))
+                                "-"
+                                n)))
+      (intern pname (find-package :sparser)))))
+  
+
+
+(defvar *classname-to-resource* (make-hash-table)
+  "Holds the resource information for the class: from a keyword
+  based on the class name to an instance of the resource class.
+  Accessed by get-resource. Created by make-resource.")
+
+(defun get-resource (keyword)
+  (unless (symbolp keyword)
+    (error "Argument should be a keyword symbol for the name ~
+            of the class"))
+  (unless (keywordp keyword)
+    (setq keyword (intern (symbol-name keyword)
+                          (find-package :keyword))))
+  (let ((r (gethash keyword *classname-to-resource*)))
+    (unless r
+      (error "No resource defined for the class ~a~
+            ~%Use define-resource" keyword))
+    r))
+
+(defmacro define-resource (class-name 
+                           &key count delta)
+  `(define-resource/expr ',class-name ,count ,delta))
+
+(defun define-resource/expr (class-name count delta)
+  (unless count(setq count 100))
+  (unless delta (setq delta 50))
+  (unless (symbolp class-name)
+    (error "The first argument must be a symbol naming the class"))
+  (let ((key (intern (symbol-name class-name)
+                     (find-package :keyword)))
+        (r (make-instance 'resource :name class-name)))
+    (setf (gethash key *classname-to-resource*) r)
+    (let ((instances
+           ;; These are recycled so can use a conventional
+           ;; list otherwise look at allocate-a-rasher-of-psi
+           (loop for i from 1 to count
+             collect (make-instance class-name :index i))))
+      (setf (resource-store r) instances) 
+      ;;/// make it circular? with a token at the split?
+      (setf (next-instance r) (car instances))
+      r)))
+
+
+(defgeneric clear (object)
+  (:documentation "Reinitialize the slots of the object and
+    indicate that it is avaiable to (re-)allocate."))
+
+(defmethod clear ((anything t))
+  ;; These use call-next-method, so they need a place
+  ;; where that chain of calls will stop.
+  anything)
 
 #| 
 TThe type that we create
