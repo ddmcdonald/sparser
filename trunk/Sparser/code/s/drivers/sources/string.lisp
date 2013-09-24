1c77354bee1a22510088449197596718d42388ba
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-09-24 20:32:46 +0000

    Initialize paragraphs to position 1, not position 0

--- trunk/Sparser/code/s/drivers/sources/string.lisp
+++ trunk/Sparser/code/s/drivers/sources/string.lisp
@@ -1,15 +1,16 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
-;;; copyright (c) 1990,1991  Content Technologies Inc.
+;;; copyright (c) 1990-1991  Content Technologies Inc.
 ;;; copyright (c) 1992,2013 David D. McDonald  -- all rights reserved
 ;;; 
 ;;;     File:  "string"
 ;;;   Module:  "drivers;sources:"
-;;;  Version:   May 2013
+;;;  Version:   September 2013
 
 ;; Created February 1991 or earlier
 ;; 5/1/13 Added section and paragraph initialization that will not be
 ;;  in place in the normal use of this form via the abbreviation p.
-;;  Might be overkill, but we don't want anything inhibited
+;;  Might be overkill, but we don't want anything inhibited. 9/23/13
+;;  added paragraph initialization
 
 (in-package :sparser)
 
@@ -20,8 +21,9 @@
   (when *recognize-sections-within-articles* ;; grammar module
     (initialize-article-resource)
     (initialize-section-resource)
+    (initialize-paragraph-state)
     (begin-new-article :name 'string-run)
-    (begin-new-paragraph (position# 0)))
+    (begin-new-paragraph (position# 1)))
   (when *note-text-relations*
     (initialize-text-relationships))
   (establish-character-source/string string)
