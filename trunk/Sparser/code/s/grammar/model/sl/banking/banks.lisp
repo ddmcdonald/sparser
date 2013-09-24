9a5bfb86c0393baff0802811b63aacf87c86fa5e
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-09-24 20:25:04 +0000

    Stub files for banking

---
+++ trunk/Sparser/code/s/grammar/model/sl/banking/banks.lisp
@@ -0,0 +1,13 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
+;;; copyright (c) 2013 David D. McDonald  -- all rights reserved
+;;;
+;;;     File: "banks"
+;;;   Module:  "grammar/model/sl/banking/
+;;;  version:  September 2013
+
+;; Initiated 9/23/13. 
+
+(in-package :sparser)
+
+(define-company "Hong Kong and Shanghai Banking Corporation"
+                :aliases '("HSBC") :takes-the t)
---
+++ trunk/Sparser/code/s/grammar/model/sl/banking/money-laundering.lisp
@@ -0,0 +1,19 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
+;;; copyright (c) 2013 David D. McDonald  -- all rights reserved
+;;;
+;;;     File: "money-laundering"
+;;;   Module:  "grammar/model/sl/banking/
+;;;  version:  September 2013
+
+;; Initiated 9/23/13. 
+
+(in-package :sparser)
+
+#|
+(define-category money-laundering
+  :specializes nil
+  :instantiates :self
+  :binds
+|#
+
+(define-polyword "money laundering")
