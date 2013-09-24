7ba51907d11aa2d7fd71c242c89d3d5b968d6918
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-09-24 20:16:51 +0000

    Add a schema argument to form rules

--- trunk/Sparser/code/s/objects/forms/form-rules.lisp
+++ trunk/Sparser/code/s/objects/forms/form-rules.lisp
@@ -3,27 +3,29 @@
 ;;;
 ;;;      File:   "form rules"
 ;;;    Module:   "objects;forms:"
-;;;   Version:   0.3 April 2013
+;;;   Version:   0.3 September 2013
 
 ;; initiated 9/3/92 v2.3
 ;; 0.1 (10/12) changed the arguments
 ;; 0.2 (6/6/93) ditto -- added new-category
 ;;     (12/30/94) added def-syntax-rule
 ;; 0.3 (10/27/11) added :head arg to form rules. 4/9/13 added :schema
-;;      to syntax rules
+;;      to syntax rules. 9/18/13 added it to form rules
 
 (in-package :sparser)
 
 
 
 (defmacro def-form-rule (rhs-labels
-                         &key form head referent new-category)
+                         &key form head referent new-category
+                              schema)
 
   `(def-form-rule/expr ',rhs-labels
      :form ',form
      :head ',head
      :referent ',referent
-     :new-category ',new-category )  )
+     :new-category ',new-category
+     :schema ',schema )  )
 
 
 
