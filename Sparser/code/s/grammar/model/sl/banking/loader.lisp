9bde6a459fa0b049074994319699f039bbb45bab
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-09-24 20:25:04 +0000

    Stub files for banking

---
+++ Sparser/code/s/grammar/model/sl/banking/loader.lisp
@@ -0,0 +1,14 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
+;;; copyright (c) 2013 David D. McDonald  -- all rights reserved
+;;;
+;;;     File: "loader"
+;;;   Module:  "grammar/model/sl/banking/
+;;;  version:  September 2013
+
+;; Initiated 9/23/13. 
+
+(in-package :sparser)
+
+(gload "banking;banks")
+(gload "banking;money-laundering")
+(gload "banking;other")
---
+++ Sparser/code/s/grammar/model/sl/banking/other.lisp
@@ -0,0 +1,10 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
+;;; copyright (c) 2013 David D. McDonald  -- all rights reserved
+;;;
+;;;     File:  "other"
+;;;   Module:  "grammar/model/sl/banking/
+;;;  version:  September 2013
+
+;; Initiated 9/23/13. 
+
+(in-package :sparser)
