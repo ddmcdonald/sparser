5cfedaa1f802e8a702c760fb9874c94f94c2b99f
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-10-25 17:08:25 +0000

    Very first stub - no content yet

---
+++ Sparser/code/s/grammar/model/sl/waypoints/loader.lisp
@@ -0,0 +1,11 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
+;;; copyright (c) 2013 SIFT LLC.  -- all rights reserved
+;;; This file is part of the SIFT CANTO project
+;;;
+;;;     File:  "loader"
+;;;            grammar/model/sl/waypoints/
+;;;  version:  October 2013
+
+;; Initiated 10/22/13
+
+(in-package :sparser)
