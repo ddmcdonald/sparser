6377b90c391c3392cb5f93b110f5fb4687cb9b43
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-10-25 16:54:02 +0000

    Objects specific to C3

---
+++ Sparser/code/s/objects/situation/loader.lisp
@@ -0,0 +1,13 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
+;;; copyright (c) 2013 David D. McDonald  -- all rights reserved
+;;; This file is part of the SIFT-Brandeis C3 project
+;;;
+;;;     File:  "loader"
+;;;            objects/situation/
+;;;  version:  October 2013
+
+;; Initiated 10/9/13.
+
+(in-package :sparser)
+
+(lload "situation-objs;pegs")
