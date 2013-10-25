f50c3a15f2d4cea7fe48656781d70365ddc42301
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-10-25 17:00:08 +0000

    Continued moving things around. New workspace for Canto work.

--- trunk/Sparser/code/s/init/workspaces/C3.lisp
+++ trunk/Sparser/code/s/init/workspaces/C3.lisp
@@ -1,6 +1,6 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
 ;;; copyright (c) 2013 David D. McDonald  -- all rights reserved
-;;; This file is part of the SIFT-Brandeis C3 project <elaborate>
+;;; This file is part of the SIFT-Brandeis C3 project
 ;;;
 ;;;     File:  "C3"
 ;;;   Module:  "init;workspaces:"
@@ -11,6 +11,20 @@
 (in-package :sparser)
 
 ;; (setq cl-user::script :c3)
+;; (load "/Users/ddm/sparser/load-nlp.lisp")
+
+;;  While there's teething problems getting it all loading 
+; (lload "objects;situation:loader")
+; (gload "sit-rules;loader")
+; (gload "isr;loader")
+
+; (c3-setting)
+; (just-bracketing-setting)
+; (word-frequency-setting)
+
+;;--- Setting up restrictions
+; decode-category-parameter-list handle-variable-restrictions
+
 ;;  => load-nlp => "scripts/C3" to set *c3* parameter
 ;;  everything -- unless boundp the parameter
 ;;     grammar-config dispatch, New file c3
@@ -19,25 +33,38 @@
 ;;   => (initiate-c3-protocol) -- new file in drivers/chart/psp/
 ;;      crib from inititate-top-edges-protocol
 
+;; c3-grammar-module-choices  lambda-variable-named itype
+
+;; (c3-setting)
+
+; the-Master-loader load-the-grammar  c3-grammar-module-choices
+
+
+;;--- single word, unambiguous edge
+;; transition referent to situation in referent-from-unary-rule 
+;; at the end when the *c3* flag is up via 
+;; incorprate-into-the-situation (referent rule edge)
+;;
+;;--- composition of two edges into a third
+;; same idea, but in referent-from-rule
+
+(setq cl-user::location-of-text-corpora ;; copied from Grok
+ "Users:ddm:sift:nlp:corpus:")          ;;/// Needs one location  !!
+;; Note the pathname is given in ancient Mac syntax. 
+(def-logical-pathname "corpus;" cl-user::location-of-text-corpora)
+
+(def-logical-pathname "gboss;"  "corpus;gboss:")
+
+(defvar gboss
+  (define-document-stream '|ISR transcripts from EC10|
+    :style-name 'hand-typed/no-headers
+    :directory "gboss;"
+    :unified t))
 
 
 
 
-
-
-
-
-
-
-
-;;--- sentences
+;;--- 
 #|  to do
-(period-hook-on)
-
-Do sentence global like (doc-set)
-Write printer
-Add traces
-try it out
-Finish debugging resource  -- tools/basics/resource -- follow word-freq clear initialize
 
 |#
---
+++ trunk/Sparser/code/s/init/workspaces/Canto workspace.lisp
@@ -0,0 +1,149 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
+;;; copyright (c) 2013 SIFT LLC.  -- all rights reserved
+;;; This file is part of the SIFT CANTO project
+;;;
+;;;     File:  "Canto workspace"
+;;;            "init;workspaces:"
+;;;  version:  October 2013
+
+;; Initiated 10/22/13
+
+(in-package :sparser)
+
+; (load "/Users/ddm/sparser/load-nlp.lisp")
+
+(define-grammar-module *waypoints*
+                       :name "Waypoints and related concepts"
+                       :parent *sublanguages*)
+(include-grammar-module *waypoints*)
+(def-logical-pathname "waypoint;"  "sl;waypoints:")
+
+; (gload "waypoint;loader")
+
+
+
+#|
+(defvar *exact-comm*
+"The first waypoint is LVN. 
+It is an entry point. 
+There are no airspeed or altitude restrictions. 
+The effective radius is 2.5 miles. 
+The second waypoint is h-area. 
+It is a target. 
+The airspeed restriction is between 50 and 200 knots. 
+There is no altitude restriction. 
+The effective radius is 5 miles.")
+
+(defvar *t7m6-mod-comm*
+"LVN is our first entry point with a radius of 2. 
+After LVN proceed to H-Area as a target.  
+I'll need to be above 3000 for H-Area.  
+H-Area radius is 5.  
+H-Area speed is 50 to 200.")
+
+(defvar *scripted-comm*
+"The first waypoint is LVN. 
+It's an entry point. 
+There are no airspeed or altitude restrictions. 
+The effective radius is 2.5 miles. 
+We're within the effective radius so go to the second waypoint. 
+The second waypoint is H-AREA. 
+It's a target. 
+The airspeed restriction is between 50 and 200 knots. 
+There is no altitude restriction. 
+The effective radius is 5 miles. 
+Keep the altitude over 3000 feet for the photo! 
+I have a good photo of H-AREA.")
+
+(defvar *2-waypoint-comm*
+"The first waypoint is LVN. 
+It is an entry point. 
+There are no airspeed or altitude restrictions. T
+he effective radius is 2 miles. 
+The second waypoint is H-AREA. 
+It is a target. 
+The airspeed restriction is between 50 and 200 knots. 
+There is no altitude restriction. 
+The effective radius is 5 miles. 
+Keep the altitude over 3000 feet for the photo!")
+
+(defvar *waypoint-comm*
+"The first waypoint is LVN. 
+It is an entry point. 
+There are no airspeed or altitude restrictions. 
+The effective radius is 2.5 miles.")
+
+
+
+
+(setf *smr-wypt-msgs-list* ;; ORIGINAL LISP AGENT STYLE MESSAGES--GOOD FOR REGRESSION TESTING
+
+  '((lvn  20 
+
+     ("The next waypoint is h-area. It is a target."
+
+      "The radius is 5."
+
+      "The altitude restriction is below 1500. 
+
+      The speed restriction is from 50 to 100."))
+
+   (h-area  20 
+
+     ("The next waypoint is sel. It is a target."
+
+      "The radius is 5."
+
+      "The speed restriction is from 150 to 200. The altitude restriction is above 2500."))
+
+   (sel  20 
+
+     ("The next waypoint is f-area. It is a target."
+
+      "The effective radius is 5."
+
+      "The altitude restriction is below 1000. The speed restriction is above 250."))
+
+   (f-area  20 
+
+     ("The next waypoint is cmn. It is a target."
+
+      "The effective radius is 5."
+
+      "The altitude restriction is above 1500. The speed restriction is below 100."))
+
+   (cmn  20 
+
+     ("The next waypoint is zi. It is a target."
+
+      "The speed restriction is from 300 to 400. The altitude restriction is from 3000 to 5000."
+
+      "The effective radius is 5."))
+
+   (zi  20 
+
+     ("The next waypoint is oak. It is a exit."
+
+      "The altitude restriction is from 2500 to 3000. The speed restriction is below 150."
+
+      "The effective radius is 5."))
+
+   (oak  20 
+
+     ("The next waypoint is wp8. It is a target."
+
+      "The speed restriction is from 150 to 200. The altitude restriction is from 500 to 1000."
+
+      "The radius is 2.5."))
+
+   (wp8  20 
+
+     ("The next waypoint is prk. It is a entry."
+
+      "The altitude restriction is from 500 to 1000. The speed restriction is above 150."
+
+      "The radius is 5.")))
+
+);; end setf *smr-wypt-msgs-list*
+
+|#
