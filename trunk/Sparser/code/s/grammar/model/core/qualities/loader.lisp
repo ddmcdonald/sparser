abdbf0818236dd7bef193400974acc144581eefe
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-12-06 18:17:10 +0000

    Added shortcut for defining standard attribution patterns

--- trunk/Sparser/code/s/grammar/model/core/qualities/loader.lisp
+++ trunk/Sparser/code/s/grammar/model/core/qualities/loader.lisp
@@ -4,11 +4,13 @@
 ;;;
 ;;;     File:  "loader"
 ;;;   Module:  "grammar/core/qualities/
-;;;  version:  October 2013
+;;;  version:  November 2013
 
 ;; Initiated 10/8/13 to provide a place to put size, weight, color
 ;; and similar properties that inhere in endurants of particular sorts.
+;; 11/21/13 added attribute
 
 (in-package :sparser)
 
 (gload "qualities;color")
+(gload "qualities;attribute")
