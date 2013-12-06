8e6d0cb7bd80c63a3f592a851ffadc54800f8c7d
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-12-06 17:21:41 +0000

    1st cut at defining exports, but has to be repositioned.

--- trunk/Mumble/design.lisp
+++ trunk/Mumble/design.lisp
@@ -1,6 +1,5 @@
 ;;; -*- Mode: Lisp; Syntax: Common-lisp; -*-
-;;; $Id$
-
+;;;
 ;;;  MUMBLE-86:  design
 
 ;;; Copyright (C) 1985, 1986, 1987, 1988  David D. McDonald
@@ -8,12 +7,13 @@
 ;;;   reserved. Permission is granted to use and copy
 ;;;   this file of the Mumble-86 system for
 ;;;   non-commercial purposes.
-;;; Copyright (c) 2010 David D. McDonald
+;;; Copyright (c) 2010-2013 David D. McDonald
 
 ;; Modified, 12/15/10 (ddm) to provides a location for packages and
 ;; exports to be defined across the whole system so that they can 
 ;; be available early before anything other than utilities have
-;; been loaded.
+;; been loaded. 11/19/13 Added lots more exports, done in the correct
+;; style. 
 
 (in-package :cl-user)
 
@@ -21,7 +21,7 @@
 ;;--- Utilities
 
 (unless (find-package :ddm-util)
-  (break "Setup problem -- ddm-util should have been loaded"))
+  (error "Setup problem -- ddm-util should have been loaded"))
 
 
 ;;--- Mumble 
@@ -32,13 +32,23 @@
 
 #+openmcl (import '(ccl:memq) (find-package :mumble))
 
-(export '(mumble demos build-mumble-frame)
-	(find-package :mumble))
-
 (shadow '(labels
 	  locative
 	  number)
 	 (find-package :mumble))
+#| Move to a design2 loaded last
+(export '(#:say
+          #:mumble 
+          #:turn-on-tracker
+          #:turn-off-tracker
+          #:derivation-tree
+          #:make-derivation-tree-node
+          #:with-derivation-tree
+          #:set-derivation-tree
+          
+          
+          )
+	(find-package :mumble))  |#
 
 
 ;;--- Sparser
