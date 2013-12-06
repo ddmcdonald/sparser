3aca0332182d664f859f2798a1a652d2000751f0
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-12-06 17:50:09 +0000

    Cleaned up, reorganized to make the present approach clearer and  push aside the not-working PSI version. Pushed another case through.

--- Sparser/code/s/interface/mumble/binding-centric.lisp
+++ Sparser/code/s/interface/mumble/binding-centric.lisp
@@ -9,42 +9,7 @@
 
 (in-package :sparser)
 
-;;--- Check
-
-(defmethod mumble::has-realization? ((i individual))
-  ;; Called from mumble::realize
-  (if *do-not-use-psi*
-    (has-bindings? i)
-    (else
-     (unless *annotate-realizations*
-       (error "You have to set *annotate-realizations* to t"))
-     (or (indiv-rnodes i)
-         (mumble::has-realization? (itype-of i))))))
-
-(defun has-bindings? (i)
-  (indiv-binds i))
-
-
-;;;-----------------------------
-;;; Construct DTN (or whatever)
-;;;-----------------------------
-
-;;--- realization-for
-;; Called from mumble::realize -- has to return something
-;; that it can consume
-
-
-(defmethod mumble::realization-for ((e edge))
-  (let ((referent (edge-referent e)))
-    (if (null *do-not-use-psi*)
-      (mumble::realization-for referent) ;; psi-route
-
-      ;; new experiment 4/5/13
-      (let ((shadow (find-or-make-shadow referent)))
-        (or (tailored-realization shadow e referent)
-            (make-derivation-tree-from-bindings
-             referent e))))))
-
+;;--- categories
 
 (defmethod mumble::realization-for ((c referential-category))
   ;; The only reason to realize a category is to retrieve
@@ -53,48 +18,44 @@
     (mumble::get-mumble-word-for-sparser-word s-word)))
 
 
+;;--- words
 
-;;; tailored realizations
+(defmethod mumble::realization-for ((w word))
+  (mumble::find-or-make-word w))
 
-(defmethod tailored-realization ((shadow t) edge referent)
-  (push-debug `(,shadow ,edge ,referent))
-  nil)
+;;--- bindings
 
 
-;;; from first principles
+;; (mumble::realization-for b)
+(defmethod mumble::realization-for ((b binding))
+  (realization-for-triple 
+   (binding-body b) (binding-variable b) (binding-value b))
+  (let ((dt (mumble::derivation-tree)))
+    (setf (mumble::referent dt) b)
+    dt))
 
-(defun make-derivation-tree-from-bindings (referent edge)
-  (case (cat-symbol (edge-form edge))
-     (category::np
-      (make-np-dtn-from-bindings referent))
-    (otherwise
-     (push-debug `(,referent ,edge))
-     (break "New form: ~a"
-            (edge-form edge)))))
+(defgeneric realization-for-triple (body variable value))
 
-(defmethod make-np-dtn-from-bindings ((i individual))
-  (with-bindings (category has-determiner) i
-    (unless category (error "no category binding"))
-    (ad-hoc-1 category has-determiner i)))
+(defmethod realization-for-triple ((body individual) (v lambda-variable) (value t))
+  (push-debug `(,body ,v ,value))
+  (let* ((body-shadow (get-nominal-instance (get-sclass (itype-of body))))
+         (var-shadow (var-shadow v))
+         (*shadows-to-individuals*
+          `((,body-shadow . ,body)
+            (,var-shadow . ,v))))
+    (declare (special *shadows-to-individuals*))
+    (funcall #'realization-for-triple body-shadow var-shadow value)))
 
-(defun ad-hoc-1 (head-category determiner-value i)
-  ;; do it all by hand just to see something completely through
-  (push-debug `(,head-category ,determiner-value ,i))
-  ;; (setq head-category (car *) determiner-value (cadr *) i (caddr *))
-  (let* ((dtn (mumble::make-derivation-tree-node
-              :referent i))
-         (phrase (mumble-phrase 'common-noun))
-         ;; args = h
-         (args (mumble::parameters-to-phrase phrase)))
-
-    (setf (mumble::resource dtn)
-          phrase)
-    (mumble::make-complement-node (car args)
-                                  head-category
-                                  dtn)
-    (setf (mumble::features dtn)
-          `(,(convert-determiner-value-to-policy
-              determiner-value)))
-    dtn))
+#|  Kills load until waypoint module is integrated in regular load |#
+(def-k-method realization-for-triple ((wp waypoint) (v (variable nname)) (value t))
+  (push-debug `(,wp ,v ,value)) ;;(break "name triple")
+  ;; (setq wp (car *) v (cadr *) value (caddr *))
+  ;; "the name of waypoint is LVN"
+  ;; LVN
+  ;; Taps into the realization class for named attributes (hight, color, ...)
+  ;; But start simply
+  (mumble::predicated-attribute-value :value value :of wp))
+ 
+    
 
 
