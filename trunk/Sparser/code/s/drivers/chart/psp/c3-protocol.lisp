243220f36f3817faf3afbf611510e0ef0e4ebf24
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2013-12-06 18:43:11 +0000

    Rearranged to start with segment scan. Checkponit

--- trunk/Sparser/code/s/drivers/chart/psp/c3-protocol.lisp
+++ trunk/Sparser/code/s/drivers/chart/psp/c3-protocol.lisp
@@ -16,52 +16,87 @@
   ;; We pull in the start marker and check that we're in the
   ;; right place, then we start the actual scan.
   (let* ((p0 (scan-next-position))  ;; status = :scanned
-         (ss (pos-terminal p0)))
-    (push-debug `(,p0 ,ss))
-    ;; They get passed to the equivalent of 
-    ;; check-for-[-from-word-after to see whether ss introduces
-    ;; a bracket, which it should.
-    ;;  But for now ignore sementation in favor of just getting
-    ;; the right things set up in the situation
-    (check-if-word-starts-segment p0 ss)))
-
-(defun check-if-word-starts-segment (position-before word)
-  ;; Leading brackets introduced and pondered here
-  ;; Then closing brackets and potentially terminating
-  ;; the segment and doing the cleanup and state updating
-  ;; that entails.
-  (tr :check-if-word-starts-segment position-before word)
-  (simple-eos-check word)
-  (scan-next-word (chart-position-after position-before)))
-
-(defun scan-next-word (position)
-  ;;(break "position = ~a" position)
-  ;; analog of the regular scan-next-pos with selected parts of
-  ;; continue-scan-next-pos inlined. 
-  (unless (pos-terminal position)
-    ;; if the tail recursion is correct we don't need this check
-    (scan-next-position))
-  (let ((word (pos-terminal position)))
-    ;;   Ignoring all the usual stuff we jump ahead to
-    ;; introduce the edge(s) on the word.
-    (introduce-known-edge/s-over-word position word)))
+         (ss (pos-terminal p0)) ;; #<word SOURCE-START>
+         (p1 (scan-next-position)))
+    (scan-segment p1)))
 
 
-(defun introduce-known-edge/s-over-word (position-before word)
-  (unless (word-with-single-edge-rules? word)
-    (push-debug `(,position-before ,word))
-    (error "C3 only runs on known words. The word ~s at p~a is unknown"
-           (word-pname word) (pos-token-index position-before)))
-  (let ((position-after (chart-position-after position-before)))
-    (install-terminal-edges word position-before position-after)
-    (push-debug `(,position-before ,word ,position-after)) ; 
-    ;(break "Installed terminal edges over ~a at ~a" word position-before)
-    ;; All the good stuff now happens in the referent processing
-    (check-if-word-starts-segment position-before word)))
+(defun scan-segment (start-pos)
+  ;; Compare scan-next-segment in the regular protocol.
+  ;; This is the fixed point where resume between segments.
+
+  ;; first delimit the segment
+  (let ((last-position (read-through-segment-to-end start-pos))
+        (position start-pos))
+    ;; Then walk through it left-to-right extending
+    ;; the situation. Assume that the final word is the head.
+    (break "last-position = ~a" last-position)
+    (loop 
+      (introduce-next-word position (eq position last-position))
+      (setq position (chart-position-after position))
+      (when (eq position last-position)
+        (return)))
+    ;; pts-style inside-the-segment parsing goes here
+    (break "where are we?")))
 
 
-(defun simple-eos-check (word)
-  ;; analogous to end-of-source-check but without any checking
-  ;; to ensure we've tied up all the bits at the forest level.
-  (when (eq word *end-of-source*)
-    (terminate-chart-level-process))) ;; does a throw
+(defun introduce-next-word (position-before head?)
+  (declare (special *this-is-the-head*))
+  (let ((word (pos-terminal position-before)))
+    (unless (word-with-single-edge-rules? word)
+      (push-debug `(,position-before ,word))
+      (error "C3 only runs on known words. The word ~s at p~a is unknown"
+             (word-pname word) (pos-token-index position-before)))
+    (setq *this-is-the-head* head?)
+    (let ((position-after (chart-position-after position-before)))
+      ;; All the good stuff now happens in the referent processing
+      (install-terminal-edges word position-before position-after))))
+
+
+
+(defun read-through-segment-to-end (start-pos)
+  (let ((start-bracket (bracket-that-starts-the-segment start-pos)))
+    (interpret-open-bracket-as-segment-start start-bracket start-pos)
+    (let ((position-before start-pos)
+          word-after )
+      (loop
+        (unless (pos-terminal position-before)
+          (scan-next-position))
+        (break "position before = ~a" position-before)
+        (setq word-after (pos-terminal position-before))
+
+        (when (eq word-after *end-of-source*)
+          (if *left-segment-boundary*
+            ;; there's more processing to do
+            (break "returning the correct position on eos?"
+            (return-from read-through-segment-to-end
+              (chart-position-before position-before))
+            ;; else we're done
+            (terminate-chart-level-process)))) ;; does a throw
+
+        (introduce-leading-brackets word-after position-before) ;; brackets to the left
+        ;; cribed from check-for-]-from-word-after
+        (let ((] (]-on-position-because-of-word? position-before word-after)))
+          (set-status :]-from-word-after-checked position-before)
+          (if ]
+            (if (bracket-ends-the-segment? ] position-before)
+              (then
+               (break "segment ended on brackets of ~a" word-after)
+               ;; bracket-check set the right-boundary global
+               (return position-before))
+              (else ;; loop around
+               (setq position-before 
+                     (chart-position-after position-before))))))))))
+ 
+
+;;;----------
+;;;  gofers
+;;;----------
+
+(defun bracket-that-starts-the-segment (start-pos)
+  ;; Should look at the position and think about it
+  ;; so that the algebra works out.
+  ;;/// once we're going the lead word may have included start brackets
+  (declare (ignore start-pos))
+  phrase.[ )
+
