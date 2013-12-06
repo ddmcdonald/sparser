16079242f9535a73c7645923e9aac308a638ecc8
ddmcdonald50 <ddmcdonald50@gmail.com>
2013-12-06 18:16:26 +0000

    Explicitly defining some molecule types

---
+++ Sparser/code/s/grammar/model/sl/biology/loader.lisp
@@ -0,0 +1,13 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER COMMON-LISP) -*-
+;;; Copyright (c) 2013 SIFT LLC. All Rights Reserved
+;;;
+;;;    File: "loader"
+;;;  Module: "grammar/model/sl/biology/
+;;; version: November 2013
+
+;; Initiated 11/5/13
+
+(in-package :sparser)
+
+(gload "bio;molecules")
+
