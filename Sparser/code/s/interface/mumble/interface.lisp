3934c248e5059618a392740aaff4d29d5ffd47b9
ddmcdonald50 <ddmcdonald50@gmail.com>
2014-01-22 21:01:36 +0000

    Clarified, pushed through more cases

--- Sparser/code/s/interface/mumble/interface.lisp
+++ Sparser/code/s/interface/mumble/interface.lisp
@@ -61,12 +61,90 @@
 ;;;-----------------------------------------
 ;;; realizations for classes of individuals
 ;;;-----------------------------------------
+     
+(defgeneric tailored-individual-realization (individual)
+  (:documentation "Looks for a type-specific realization
+   'specialist' that will handle the derivation tree all
+   on its own. Initially called with an individual that
+   version of the method sets up the shadow environment
+   and then funcalls this method on the shadow for a
+   k-method that matches the type. A version that accepts
+   anything (T) returns nil to indicate that there is not
+   a specialist for this type of individual."))
+
+; This can be done with a lot of generality if we finish
+; the machinery to read schemas and their argument mappings
+; and link in phrases at the ETF level, perhaps as an
+; additional component of the ETF definition. See the code
+; in Mumble/interface/tsro/map-translations for a start.
+
+(defmethod tailored-individual-realization ((i individual))
+  (declare (special *shadows-to-individuals*))
+  (let* ((shadow (find-or-make-shadow i))
+         (*shadows-to-individuals*
+          `((,shadow . ,i))))
+    (funcall #'tailored-individual-realization shadow)))
+
+(defmethod tailored-individual-realization ((top t))
+  ;; indicate that there's no specialist. Read by the
+  ;; 'or' in realiation-for(individual) which will now
+  ;; try another method of getting a realization. 
+  nil)
+
+(def-k-method tailored-individual-realization ((i rate))
+  (with-bindings (quantity units) i
+    (let ((dtn (mumble::simple-2-arg-phrase
+                'mumble::number-np quantity units)))
+      (setf (mumble::referent dtn) i)
+      dtn)))
+
+
+
+;;--- hack in lieu of fixing all of the code that's in
+;; Mumble/grammar/numbers.lisp, which most notably is not
+;; getting an illions-distribution off the number for some reason
+
+(def-k-method tailored-individual-realization ((n number))
+  (let ((digits-word
+         (cadr (memq :digit-sequence (indiv-plist n))))
+        (actual-number (value-of 'value n)))
+    (cond
+     (digits-word
+      (mumble::get-mumble-word-for-sparser-word digits-word))
+     (actual-number
+      (let ((number-string (format nil "~a" actual-number)))
+        (mumble::find-or-make-word number-string))) ;; digit pos???
+     (t (push-debug `(,n))
+        (break "stub: need yet another way to get a word ~
+              ~%from a number individual: ~a" n)))))
+        
+
+
+;;--- names
+
+(defun realize-using-name-binding (i)
+  (let ((name (value-of 'name i)))
+    (when name
+      (if (word-p name)
+        (mumble::get-mumble-word-for-sparser-word name)
+        (else
+         (format t "Complex name value for ~a~
+                  ~%  ~a" i name))))))
+
+;; need number and unit-rate-of-change for args
+
+
 
 (defmethod mumble::realization-for ((i individual))
   ;; 1st look for a tailored realization
   ;; 2d does this have a name?
   ;; 3d make a defNP from the name of the category
-  (push-debug `(,i)) (break "stub"))
+  (or (tailored-individual-realization i) ;; 1st
+      (realize-using-name-binding i)
+      (else 
+       (push-debug `(,i))
+       (break "no tailored realization or name for ~a" i))))
+
 
 
 ;;;--------------------------------------------
@@ -77,16 +155,16 @@
   (let ((referent (edge-referent e)))
     ;; new experiment 
     (let ((shadow (find-or-make-shadow referent)))
-      (or (tailored-realization shadow e referent)
+      (or (tailored-edge-realization shadow e referent)
           (make-derivation-tree-from-bindings
              referent e)))))
 
 
 ;;; tailored realizations
 
-(defmethod tailored-realization ((shadow t) (e edge) (referent t))
+(defmethod tailored-edge-realization ((shadow t) (edge edge) (referent t))
   (push-debug `(,shadow ,edge ,referent))
-  nil)
+  (break "stub"))
 
 
 ;;; from first principles
