61a9ccc1cbea264f951bdcf8c7d585e876ec89de
ddmcdonald50 <ddmcdonald50@gmail.com>
2014-01-29 22:43:05 +0000

    Added an 'un-' case and rationalized a bit.

--- Sparser/code/s/init/versions/v4.0/workspace/display.lisp
+++ Sparser/code/s/init/versions/v4.0/workspace/display.lisp
@@ -1,12 +1,13 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 2013  David D. McDonald  -- all rights reserved
+;;; copyright (c) 2013-2014  David D. McDonald  -- all rights reserved
 ;;; 
 ;;;     File:  "display"
 ;;;   Module:  "init;versions:v2.3:workspace:"
-;;;  version:  October 2013
+;;;  version:  January 2014
 
 ;; Initiated 10/28/13 to put pointers to all the parameters 
 ;; that govern display in a single place for easy reference.
+;; Added quiet and instance of undisplay-bracketing 1/27/14.
 
 (in-package :sparser)
 
@@ -17,6 +18,12 @@
 (display-bracketing)
 ;; turns off *display-word-stream*
 ;; turns on *readout-segments-inline-with-text* and *record-bracketing-progress*
+
+;; (undisplay-bracketing) goes back to *display-word-stream*
+
+(defun quiet ()
+  (undisplay-bracketing)
+  (setq *display-word-stream* nil))
 
 ; (trace-lexicon-unpacking)
 ;; Spews each new word as it's added
