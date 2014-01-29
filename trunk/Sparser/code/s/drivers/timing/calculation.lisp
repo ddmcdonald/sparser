de961fe9f05a9b430c34cf7f9844a2197acf3ecd
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2014-01-29 22:50:34 +0000

    Lots of nits and details to get things to the right speed

--- trunk/Sparser/code/s/drivers/timing/calculation.lisp
+++ trunk/Sparser/code/s/drivers/timing/calculation.lisp
@@ -1,15 +1,22 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:sparser -*-
-;;; copyright (c) 1992,1993,1994 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992,2014 David D. McDonald  -- all rights reserved
 ;;; 
 ;;;     File:  "calculation"
 ;;;   Module:  "drivers;timing:"
-;;;  Version:   February 1991
+;;;  Version:   January 2014
 
-;; initiated 2/91
+;; initiated 2/91. Added some usage documentation 1/27/14.
 
 (in-package :sparser)
 
+#|
+These are embedded in chart-based-analysis which is the inner loop 
+of the analysis, using the symbol *time-at-chart-level* which is
+bound in drivers/timing/cases.lisp.
 
+In drivers/timing/presentation.lisp the function run-string-for-timing
+uses this information. 
+|#
 
 (defun start-timer (symbol)
   (unless (boundp symbol)
--- trunk/Sparser/code/s/drivers/timing/presentation.lisp
+++ trunk/Sparser/code/s/drivers/timing/presentation.lisp
@@ -1,16 +1,37 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1992,1993,1994,1995 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992-1995,2014 David D. McDonald  -- all rights reserved
 ;;; extensions copyright (c) 2007 BBNT Solutions LLC. All Rights Reserved
-;;; $Id:
 ;;; 
 ;;;     File:  "presentation"
 ;;;   Module:  "drivers;timing:"
-;;;  Version:  October 2007
+;;;  Version:  January 2014
 
 ;; file created 2/91. Given content 1/6/95
 ;; Added Time decoded 1/23. 10/2/07 Extended and added Allegro variation.
+;; 1/27/14 Brough openMCL details up to date. Added macro to reflect what
+;; to turn off. 
 
 (in-package :sparser)
+
+
+;;--- Wrapper to minimize other operations
+
+(defmacro with-inessentials-turned-off (&body body)
+  `(let ((*display-word-stream* nil)
+         (*recognize-sections-within-articles* nil)
+         (*newline-delimits-paragraphs* nil)
+         (*do-strong-domain-modeling* nil)  ;; Have to turn all of these
+         (*reify-implicit-individuals* nil) ;; off explicitly given how
+         (*note-text-relations* nil)  ;; after-action-on-segments is pre-set
+         (*after-action-on-segments* 'normal-segment-finished-options))
+     (declare (special *display-word-stream*
+                       *recognize-sections-within-articles*
+                       *newline-delimits-paragraphs*
+                       *do-strong-domain-modeling*
+                       *reify-implicit-individuals*
+                       *note-text-relations*
+                       *after-action-on-segments*))
+     ,@body))
 
 
 ;;;--------------------------------------------
@@ -18,8 +39,8 @@
 ;;;--------------------------------------------
 
 (defun run-string-for-timing (string)
-  (let ((*display-word-stream* nil))
-    (analyze-text-from-string string)
+  (with-inessentials-turned-off
+      (analyze-text-from-string string)
     (let ((tps (/ *time-at-chart-level*
                   *number-of-next-position*)))
       (format t "~&speed: ~4,1F tokens/msec" (float tps))
@@ -27,13 +48,13 @@
 
 
 (defun run-string-for-timing/no-forest (string)
-  (let ((*display-word-stream* nil)
-        (*do-forest-level* nil))
-    (analyze-text-from-string string)
-    (let ((tps (/ *time-at-chart-level*
-                  *number-of-next-position*)))
-      (format t "~&speed: ~4,1F tokens/msec" (float tps))
-      )))
+  (with-inessentials-turned-off
+      (let ((*do-forest-level* nil))
+        (analyze-text-from-string string)
+        (let ((tps (/ *time-at-chart-level*
+                      *number-of-next-position*)))
+          (format t "~&speed: ~4,1F tokens/msec" (float tps))
+          ))))
 
 
 ;;;-----------------------------------------------------------
@@ -41,14 +62,14 @@
 ;;;-----------------------------------------------------------
 
 (defun time-analysis (input-string)
-  (let* ((*display-word-stream* nil)
-         (trace-string (make-string-output-stream))
-         (*trace-output* trace-string))
+  (with-inessentials-turned-off
+      (let* ((trace-string (make-string-output-stream))
+             (*trace-output* trace-string))
 
-    (time (pp input-string))
+        (time (analyze-text-from-string input-string))
 
-    (analyze-and-report-timing-data
-     (get-output-stream-string trace-string))))
+        (analyze-and-report-timing-data
+         (get-output-stream-string trace-string)))))
       
 
 
@@ -68,6 +89,7 @@
 ; space allocation:
 ;  1,304,549 cons cells, 27,777,504 other bytes, 0 static bytes
   (format t "~a" time-report-string)
+  (push-debug `(,time-report-string))
   (multiple-value-bind (number units)
       (extract-ms-from-time-report-string time-report-string)
     (let* ((word-count *number-of-next-position*)
@@ -83,8 +105,11 @@
                (/ word-count number))
               (otherwise
                (break "New time unit: ~a" units)))))
-      (format t "~&speed: ~4,1F tokens/second"
-              (float tokens-per-second)))))
+      (let* ((speed-as-string 
+              (format nil "~4,1F" (float tokens-per-second)))
+             (speed-with-commas
+              (insert-commas-into-number-string speed-as-string)))
+        (format t "~&speed: ~a tokens/second" speed-with-commas)))))
 
 #+:allegro
 (defun extract-ms-from-time-report-string (s)
@@ -111,17 +136,29 @@
 ;During that period, 7,848 microseconds (0.007848 seconds) were spent in user mode
 ;                    151 microseconds (0.000151 seconds) were spent in system mode
 ; 89,184 bytes of memory allocated.
-  (let* ((pos-of-k (position #\k s))
+  (let* ((pos-of-k (position #\k s)) ;; in "took"
          (from-number-start-onwards (subseq s (+ pos-of-k 2)))
-         (pos-next-space (position #\space from-number-start-onwards))
-         (number-string (subseq from-number-start-onwards 0 pos-next-space)))
-    ;(break "number-string = '~a'" number-string)))
+         ;; There is usually just one space between "took" and the number,
+         ;; but sometimes (1/27/13 Clozure 1.8.1) there are two.
+         (trimmed-from-number-onwards
+           (remove-leading-whitespace from-number-start-onwards))
+         (pos-next-space (position #\space trimmed-from-number-onwards))
+         (number-string (subseq trimmed-from-number-onwards 0 pos-next-space)))
+    ;;(break "number-string = '~a'" number-string)
     (when (position #\, number-string)
       (setq number-string (remove-comma-from-number number-string)))
     (let ((units (extract-initial-time-unit 
-                  (subseq from-number-start-onwards (+ 1 pos-next-space)))))
+                  (subseq trimmed-from-number-onwards (+ 1 pos-next-space)))))
       (values (read-from-string number-string)
               units))))
+
+;; From Peter Clark's string code for KM
+;; Expose in util
+(defparameter *whitespace-chars* '(#\Space #\Tab #\Newline 
+                                   #\Return #\Linefeed #\Page))
+(defun remove-leading-whitespace (string) 
+  (string-left-trim *whitespace-chars* string))
+
 
 ;; move to util
 (defun remove-comma-from-number (s)
@@ -132,6 +169,27 @@
             (right (subseq s (1+ pos-comma))))
         (string-append left right))
       s)))
+
+(defun insert-commas-into-number-string (s)
+  ;;  created by "~4,1F", so it has a decimal point
+  (flet ((subdivide-into-length-3-strings (string)
+           (let ((remaining string)
+                 (remaining-length (length string))
+                 triplets )
+             (loop
+               (when (<= remaining-length 3)
+                 (push remaining triplets)
+                 (return))
+               (let ((index (- remaining-length 3)))
+                 (push (subseq remaining index) triplets)
+                 (setq remaining (subseq remaining 0 index))
+                 (setq remaining-length (length remaining))))
+             triplets )))
+  (let* ((decimal-point-pos (position #\. s))
+         (decimal-and-after (subseq s decimal-point-pos))
+         (before-point (subseq s 0 decimal-point-pos))
+         (triplets (subdivide-into-length-3-strings before-point)))
+    (values triplets decimal-and-after))))
 
 
 
