a57e12f0e39730c6f5c6c3836a502c2cb5788312
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2014-01-29 20:33:56 +0000

    transferring from a peg to a new individual

--- trunk/Sparser/code/s/objects/situation/pegs.lisp
+++ trunk/Sparser/code/s/objects/situation/pegs.lisp
@@ -1,13 +1,14 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 2013 David D. McDonald  -- all rights reserved
+;;; copyright (c) 2013-2014 David D. McDonald  -- all rights reserved
 ;;; This file is part of the SIFT-Brandeis C3 project
 ;;;
 ;;;     File:  "pegs"
 ;;;            objects/situation/
-;;;  version:  October 2013
+;;;  version:  January 2014
 
 ;; Initiated 10/9/13. 10/17 decided pegs were typed, taken right from
-;; the variable
+;; the variable. 1/23/14 Transfer it all to an individual, with type-checker
+;; and more specific accessor.
 
 (in-package :sparser)
 
@@ -16,7 +17,7 @@
     :documentation "More natural to call it 'type', but Lisp
       would object. Holds a list of categories, just  like the
       type field of an individual does.")
-   (variables :type list :initform nil :accessor vars
+   (variables :type list :initform nil :accessor peg-v+v-alist
     :documentation "draft -- make it a alist vs. v+v's")))
 
 (defvar *current-peg* nil
@@ -38,10 +39,33 @@
 
 
 
-
 (defmethod bind-variable-on-peg ((peg peg) (variable lambda-variable)
                                  (value t))
   (let ((category (var-category variable)))
     (pushnew category (peg-type-list peg))
     (push `(,variable ,value)
-          (vars peg))))
+          (peg-v+v-alist peg))))
+
+
+(defun transfer-peg-bindings-to-individual (peg i)
+  ;; Called by incorporate-phrasal-head to cache-out the peg.
+  ;; Convert the alist of v+v pairs on the peg to bindings
+  ;; on the individual. Return the list of bindings to use
+  ;; in indexing the individual.
+  (loop for pair in (peg-v+v-alist peg)
+    as variable = (car pair)
+    as value = (cadr pair)
+    collect (bind-variable/expr variable value i)))
+
+(defun check-validity-of-individual (i peg)
+  (let ( failed )
+    (dolist (c (peg-type-list peg))
+      (unless (itypep i c)
+        (push c failed)))
+    (when failed
+      (push-debug `(,i ,peg))
+      (error "The individual does not fit all the types ~
+              of the peg ~a~%~a does not fit ~a"
+             peg i failed))))
+
+
