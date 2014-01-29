5e4b9755f3b2e209e9207020b3ebd78df5b90194
ddmcdonald50 <ddmcdonald50@gmail.com>
2014-01-29 22:39:04 +0000

    Added real state info at multiple levels

--- Sparser/code/s/objects/doc/situation.lisp
+++ Sparser/code/s/objects/doc/situation.lisp
@@ -1,40 +1,67 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 2013 David D. McDonald  -- all rights reserved
+;;; copyright (c) 2013-2014 David D. McDonald  -- all rights reserved
 ;;; This file is part of the SIFT-Brandeis C3 project <elaborate>
 ;;;
 ;;;     File:  "situation"
 ;;;   Module:  "objects/doc/"
-;;;  version:  October 2013
+;;;  version:  January 2014
 
-;; Initiated 9/18/13. Fleshed out 10/9/13.
+;; Initiated 9/18/13. Fleshed out 10/9/13. 1/23/14 Added sentence state
 
 (in-package :sparser)
 
 
+;;--- Base case
+
 (defclass core-situation (container)
-  ((model :type list :initform nil :accessor model
+  ;; from container we get a bkptr slot, init arg :in that points
+  ;; to the document container we're part of. Also supplies a print-method
+
+  ((model :type list :initform nil 
+          :accessor model
     :documentation "an unordered list of the objects in the situation")
-   (state :type symbol :initform :initial-state :accessor state
+
+   (state :type state 
+          :initform (state-named :initial-state)
+          :accessor state
     :documentation "a keyword naming the current state of the analysis")
-   (variable-values :type list :initform nil :accessor variable-values
+
+   (variable-values :type list :initform nil 
+                    :accessor variable-values
     :documentation "The present value of the indexical functional variables
       that apply to this text and discourse. Draft design is just an alist."))
+
   (:documentation "These are the elements that any situation has, 
     without considering how one situation builds on another."))
 
+
+;;--- Sentences
+
 (defclass sentence-situation (core-situation)
-  ()
-  (:documentation "a hook in case we want to be more specific or record
-    more data at some point"))
+  ((sentence-state :type state 
+                   :initform (state-named :initial-sentence-state)
+                   :accessor sentence-state
+    :documentation "The state of core-situation is now effectively
+      the state of an accumulating phrase, while this state records
+      the accumulation of the elements of a sentence."))     
+  (:documentation "Additional elements needed to organize a situation
+      that spans a sentence-worth of content."))
 
 
 ;; (designate-sentence-container :situation)  ;; re-eval after any change
 (defun make-sentence-container/situation (sentence)
-  ;; Called from make-sentence-container. This happens when the sentence's
+  ;; Called from make-sentence-container when the sentence's
   ;; document element is being laid down by start-sentence, where the only
   ;; thing we actually know is the starting position.
   (make-instance 'sentence-situation :in sentence))
 
+
+;;--- Discourse 
+
+
+
+
+;;--- retrieving the situation
 
 (defun the-situation ()
   ;; ok for now when just in one sentence / a single situation
