d2080fcc3e939bd4ecbecfadf6664af4152bdf85
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2014-03-26 16:06:14 +0000

    Further rationalizing printing of individuals with names

--- trunk/Sparser/code/s/grammar/model/core/names/printing.lisp
+++ trunk/Sparser/code/s/grammar/model/core/names/printing.lisp
@@ -1,15 +1,18 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1994-1995,2013 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1994-1995,2013-2014 David D. McDonald  -- all rights reserved
 ;;;
 ;;;     File:  "printing"
 ;;;   Module:  "model;core:names:"
-;;;  version:  0.1 July 2013
+;;;  version:  0.2 February 2014
 
 ;; initiated 2/1/94 v2.3, breaking the special fns out of specific files
 ;; (4/5) stubbed a generic printer.  10/3 added string-printers
 ;; 0.1 (1/25/95) gated the routine for initials.
 ;;     (2/28) added case for uncategorized names.
 ;;     (6/7/13) Tweaking for adjustments over the years. 7/4/13 more.
+;; 0.2 (2/6/14) Found duplication in treatment of names that are individuals
+;;      in print-individual-with-name so consolidating them here in one
+;;      string-for routine. 
 
 (in-package :sparser)
 
@@ -25,20 +28,35 @@
       (word (word-pname w/pw))
       (polyword (pw-pname w/pw))
       (individual
-       (case (cat-symbol (itype-of w/pw))
-         (category::name-word (string/name-word w/pw))
-         (category::uncategorized-name
-          (string/uncategorized-name w/pw))
-         (category::sequence ;; upstream should catch this quicker
-          (string/sequence w/pw))
-         (otherwise
-          (push-debug `(,n ,w/pw))
-          (error "Unexpected type of individual for a name: ~a~%~a"
-                 (type-of w/pw) w/pw))))
+       (string-for/name/individual w/pw))
       (otherwise
        (push-debug `(,n ,w/pw))
        (format t "String-for/name -- new type: ~A~%" (itype-of w/pw))
        "" ))))
+
+
+(defun string-for/name/individual (name)
+  ;; Caller has to know this is an individual
+  (case (cat-symbol (itype-of name))
+    (category::name-word 
+     (string/name-word name))
+    (category::uncategorized-name
+     (string/uncategorized-name name))
+    (category::sequence ;; upstream should catch this quicker
+     (string/sequence name))
+             
+    ((category::person-name/first-last
+      category::person-name)
+     (let ((string (string/person-name name)))
+       (format nil "~a" string)))
+    (category::name-of-location
+     (let ((seq (value-of 'sequence name category::name-of-location)))
+       (string/sequence seq)))
+ 
+    (otherwise
+     (push-debug `(,name))
+     (format t "~&New type of name individual: ~a~%" (itype-of name))
+     "")))
 
 
 ;;--- Name Words
