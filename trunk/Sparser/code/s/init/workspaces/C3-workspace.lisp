aa3b9204c8764be223700551de8ab59098dc4dd8
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2014-03-26 15:52:53 +0000

    More meta-. and internal doc

--- trunk/Sparser/code/s/init/workspaces/C3-workspace.lisp
+++ trunk/Sparser/code/s/init/workspaces/C3-workspace.lisp
@@ -43,7 +43,46 @@
 ;;  setting -> establish-kind-of-chart-processing-to-do = :c3-protocol
 ;;   => (initiate-c3-protocol) -- new file in drivers/chart/psp/
 ;;      crib from inititate-top-edges-protocol
+#| 
+ => scan-segment is the loop
+Calls read-through-segment-to-end (same file), 
+which does the scan-next-position calls and bracket analysis
+and returns once it's found the end of the segment.
 
+Every word has  single-rewrite rule ends up in 
+
+;;--- single word, unambiguous edge
+;; transition referent to situation in referent-from-unary-rule 
+;; at the end when the *c3* flag is up via 
+;; incorprate-referent-into-the-situation (referent rule edge)
+;;
+;;--- composition of two edges into a third would be
+;; same idea, but in referent-from-rule
+
+What happens in incorporate-referent-into-the-situation (referent of single word)
+depends on the current state, which comes from calling phrasal-state.
+All in grammar/rules/situation/
+
+The phrasal-state is the value of the state field of the situation object.
+The choice of situation object, e.g. sentence-situation in objecs/doc/situation.lisp
+ is set up once by designate-sentence-container and then used by
+ make-sentence-container/situation which is called from make-sentence-container
+ when start-sentence is called which is kicked from from initialize-paragraphs and
+ on up: objects/doc/object1.lisp
+
+States are objects with transition tables (grammar/rules/situation/state.lisp)
+that may be for assembling phrases, sentences, or the whole discourse. 
+
+They go with situation classes with the same range. Though right now (3/20/14)
+it's not clear how to structure the discourse. 
+
+States standup indexicals: grammar/rules/situation/variables.lisp
+
+
+
+
+
+|#
 
 (setq cl-user::location-of-text-corpora ;; copied from Grok
  "Users:ddm:sift:nlp:corpus:")          ;;/// Needs one location  !!
@@ -73,20 +112,13 @@
 
 ; traces: trace-c3
 ; protocol: initiate-c3-protocol  bracket-that-starts-the-segment
-;   incorprate-into-the-situation  incorporate-phrasal-head
+;   incorporate-referent-into-the-situation  incorporate-phrasal-head
 ;   the-situation  current-peg  update-situation-state
 ;   add-indexical-to-situation
 
 ; ontology is all in grammar/sl/ISR/
 
 
-;;--- single word, unambiguous edge
-;; transition referent to situation in referent-from-unary-rule 
-;; at the end when the *c3* flag is up via 
-;; incorprate-into-the-situation (referent rule edge)
-;;
-;;--- composition of two edges into a third
-;; same idea, but in referent-from-rule
 
 
 ;;--- 
