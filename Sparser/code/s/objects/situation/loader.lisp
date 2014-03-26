b1ba067c27424fdeefdb066ea9524b6355e61c15
ddmcdonald50 <ddmcdonald50@gmail.com>
2014-03-26 16:17:24 +0000

    Moved class definition up to where the were used

--- Sparser/code/s/objects/situation/loader.lisp
+++ Sparser/code/s/objects/situation/loader.lisp
@@ -1,13 +1,16 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 2013 David D. McDonald  -- all rights reserved
+;;; copyright (c) 2013-2013 David D. McDonald  -- all rights reserved
 ;;; This file is part of the SIFT-Brandeis C3 project
 ;;;
 ;;;     File:  "loader"
 ;;;            objects/situation/
-;;;  version:  October 2013
+;;;  version:  March 2014
 
-;; Initiated 10/9/13.
+;; Initiated 10/9/13. Moved variables and state in from grammar/rules/
+;; situation/ 3/26/14 becaues of dependencies.
 
 (in-package :sparser)
 
+(gload "situation-objs;variables")
+(gload "situation-objs;state")
 (lload "situation-objs;pegs")
