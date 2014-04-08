5a68054fcbf34b11a3a831c82203af7c88bc81b4
ddmcdonald50 <ddmcdonald50@gmail.com>
2014-04-08 18:36:34 +0000

    Comment out stale code to quiet the compiler

--- Sparser/code/s/drivers/sinks/core.lisp
+++ Sparser/code/s/drivers/sinks/core.lisp
@@ -1,9 +1,9 @@
 ;;; -*- Mode:Lisp; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
-;;; copyright (c) 1993-1997,2013 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1993-1997,2013-2014 David D. McDonald  -- all rights reserved
 ;;;
 ;;;     File:  "core"
 ;;;   module:  "drivers;sinks:"
-;;;  Version:  0.3 April 2013
+;;;  Version:  0.4 March 2014
 
 ;; redesigned from scratch 12/28/93 v2.3. Added fixed set of final
 ;; actions 1/16/94.
@@ -13,6 +13,8 @@
 ;;     (8/17/97) sharpened that for when the delivery license doesn't even
 ;;      include that global. 
 ;; 0.4 (4/11/13) Fan-out from change to treatment of sections
+;;     (3/26/14) Finished evicerating do-the-last-things-in-an-analysis.
+;;      It has to be reworked to match the today's actual cases.
 
 (in-package :sparser)
 
@@ -25,6 +27,8 @@
   ;; terminate the analysis.  This is where standard final actions
   ;; go, especially if they are to be carried out in a specific
   ;; order.
+  (declare (ignore last-position))
+  #+ignore  ;; terminate-section was reconceptualized
   (when *recognize-sections-within-articles*
     (when *current-paragraph*
       ;; as in Scott's treatment in objects/docts/object.lisp
