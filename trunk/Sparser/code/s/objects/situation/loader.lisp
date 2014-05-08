feac4df3026600fc5f6ae88b242602b133c785c0
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2014-05-08 15:45:11 +0000

    New file for next step in implementation

--- trunk/Sparser/code/s/objects/situation/loader.lisp
+++ trunk/Sparser/code/s/objects/situation/loader.lisp
@@ -1,16 +1,17 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 2013-2013 David D. McDonald  -- all rights reserved
+;;; copyright (c) 2013-2014 David D. McDonald  -- all rights reserved
 ;;; This file is part of the SIFT-Brandeis C3 project
 ;;;
 ;;;     File:  "loader"
 ;;;            objects/situation/
-;;;  version:  March 2014
+;;;  version:  May 2014
 
 ;; Initiated 10/9/13. Moved variables and state in from grammar/rules/
-;; situation/ 3/26/14 becaues of dependencies.
+;; situation/ 3/26/14 because of dependencies. 5/8/14 added [latent-predicates]
 
 (in-package :sparser)
 
+(lload "situation-objs;latent-predicates")
 (gload "situation-objs;variables")
 (gload "situation-objs;state")
 (lload "situation-objs;pegs")
