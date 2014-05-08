91fdb6e93f45a4f56f2db77cf9ae66f56a7a50f5
ddmcdonald50 <ddmcdonald50@gmail.com>
2014-05-08 15:45:11 +0000

    New file for next step in implementation

---
+++ Sparser/code/s/objects/situation/latent-predicates.lisp
@@ -0,0 +1,15 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
+;;; copyright (c) 2014 David D. McDonald  -- all rights reserved
+;;; This file is part of the SIFT-Brandeis C3 project <elaborate>
+;;;
+;;;     File:  "atent-predicates"
+;;;   Module:  "objects/doc/"
+;;;  version:  May 2014
+
+;; Initiated 5/8/14
+
+(in-package :sparser)
+
+
+
+(defun collect-variables-from-supercs (c)
