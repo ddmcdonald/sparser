a774e426ae85605ddfc6f55a08f90890dc5a6838
ddmcdonald50 <ddmcdonald50@gmail.com>
2014-05-18 20:30:59 +0000

    Incremental improvements

--- Sparser/code/s/objects/situation/loader.lisp
+++ Sparser/code/s/objects/situation/loader.lisp
@@ -8,10 +8,14 @@
 
 ;; Initiated 10/9/13. Moved variables and state in from grammar/rules/
 ;; situation/ 3/26/14 because of dependencies. 5/8/14 added [latent-predicates]
+;; and moved in [situation] from docs. %/15/14 added [definitions}
 
 (in-package :sparser)
 
 (lload "situation-objs;latent-predicates")
-(gload "situation-objs;variables")
-(gload "situation-objs;state")
+(lload "situation-objs;variables")
+(lload "situation-objs;state")
 (lload "situation-objs;pegs")
+(lload "situation-objs;situation")
+
+(lload "situation-objs;definitions")
--- Sparser/code/s/objects/situation/situation.lisp
+++ Sparser/code/s/objects/situation/situation.lisp
@@ -1,13 +1,13 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
 ;;; copyright (c) 2013-2014 David D. McDonald  -- all rights reserved
-;;; This file is part of the SIFT-Brandeis C3 project <elaborate>
+;;; This file is part of the SIFT-Brandeis C3 project 
 ;;;
 ;;;     File:  "situation"
-;;;   Module:  "objects/doc/"
-;;;  version:  January 2014
+;;;   Module:  objects/situation/
+;;;  version:  May 2014
 
 ;; Initiated 9/18/13. Fleshed out 10/9/13. 1/23/14 Added sentence state.
-;; 4/9/14 added a tailored container.
+;; 4/9/14 added a tailored container. 5/13/14 Starting discourse level. 
 
 (in-package :sparser)
 
@@ -21,7 +21,9 @@
      Ought to parallel the discourse history. Probably want
      more than just a list, but it's a start.")
    (relations :type list :initform nil :accessor relations
-    :documentation "Predicates over entities, or something like that"))
+    :documentation "Predicates over entities, or something like that")
+   (latent-predicates :type list :initform nil :accessor latent-predicates
+    :documentation "List of latents-wrapper objects"))
   (:documentation "Provides some organization to the
     content model of a situation. Perhaps specializes by level?"))
 
@@ -56,7 +58,7 @@
 
 ;;--- Sentence level
 
-(defclass sentence-situation (core-situation)
+(defclass sentence-situation (core-situation ordered has-parent)
   ((sentence-state :type state 
                    :initform (state-named :initial-sentence-state)
                    :accessor sentence-state
@@ -68,7 +70,101 @@
 
 
 ;;--- Discourse level
-;; tbd
+
+(defclass discourse-situation (has-children)
+  ((first-situation :initarg :first :accessor first-situation
+    :documentation "Points to the first sentence situation.
+      Provides the reference point to chain forward from.")
+   (current-situation :accessor current-situation
+    :documentation "Caches the current situation. The last,
+      most recent sentence situation in the chain.")
+)
+  (:documentation
+   ""))
+
+;;;----------------
+;;; initialization
+;;;----------------
+
+;;/////////////// Give these an initialization routine. Setup resource
+(defvar *current-situation* nil
+  "Designates the currently active sentence-level situation object.")
+
+(defvar *previous-situation* nil
+  "Managed by make-sentence-container/situation.")
+
+(defvar *discourse-situation* nil
+  "There's only one. 
+   Extended by make-sentence-container/situation.")
+
+
+;; (designate-sentence-container :situation)  
+;; This is a computed function. Re-eval after any change
+;;
+(defun make-sentence-container/situation (sentence)
+  ;; Called from make-sentence-container when the sentence's
+  ;; document element is being laid down by start-sentence, where the only
+  ;; thing we actually know is the starting position.
+  ;;//////// revise to use allocation
+  (let ((s (make-a-situation sentence)))
+    (when *current-situation*
+      (unless *discourse-situation*
+        (initialize-discourse-situation *current-situation*))
+      (extend-discourse-situation s)
+      (setq *previous-situation* *current-situation*))
+    (setq *current-situation* s)))
+
+(defun make-a-situation (sentence) ;;//////// revise to use allocation
+  (let ((model (make-instance 'model-of-situation)))
+    (make-instance 'sentence-situation 
+      :in sentence
+      :model model)))
+
+(defun initialize-discourse-situation (first-sentence-situation)
+  "Create and setup the first discourse-situation object based on
+   the initial sentence-situation and the one just created."
+  (let ((ds (make-instance 'discourse-situation
+              :first first-sentence-situation)))
+    (setf (parent first-sentence-situation) ds)
+    (setf (children ds) first-sentence-situation)
+    (setf (current-situation ds) first-sentence-situation)
+    (setq *discourse-situation* ds)
+    ds))
+
+(defun extend-discourse-situation (new-sit)
+  (let ((ds *discourse-situation*))
+    (unless ds
+      (error "Threading bug: no discourse situation."))
+    (let ((last (current-situation ds)))
+      (setf (next last) new-sit)
+      (setf (previous new-sit) last)
+      (setf (current-situation ds) new-sit)
+      (make-necessary-adjustments-to-situations ds last))))
+
+(defun make-necessary-adjustments-to-situations (ds last)
+  ;; Here is where we set up functors, entities to
+  ;; reference, things like that.
+  (push-debug `(,ds ,last)))
+
+
+;;;--------------------------
+;;; retrieving the situation
+;;;--------------------------
+
+(defun the-situation ()
+  ;; ok for now when just in one sentence / a single situation
+  (let ((sentence (sentence)))
+    (unless sentence
+      (error "Threading bug: there no active sentence"))
+    (let ((contents (contents sentence)))
+      (unless contents
+        (error "The current sentence has no content model"))
+      (unless (typep contents 'sentence-situation)
+        (push-debug `(,sentence ,contents))
+        (error "Unexpected type of situation: ~a"
+               (type-of contents)))
+      contents)))
+
 
 ;;;-------------------------------------
 ;;; tracking the stuff in the situation
@@ -102,23 +198,10 @@
           (s (the-situation))) ;; who would know about the level?
       ;; that makes the associaton on the instance
       ;; Now we add it to the content as another relation
+      (tr :adding-relation-to-situation variable i value) ;; + situation?
       (integrate-into-situation b s)
       ;; return value isn't consumed
       b)))
-
-(defgeneric add-entity (item)
-  (:documentation "Incorporate the item into the ongoing situation
-    at the appropriate level, carrying out any associated inferences
-    or priming operations."))
-
-(defmethod add-entity ((item t))
-  "Just stash it in the entity field of the model just to park it.
-   Essentially a no-op."
-  (let ((model (model (the-situation)))) ;; level ?? 
-    (push-debug `(,item ,model)) (break "adding ~a" item)
-    (push item (entities model))))
-
-
 
 (defgeneric integrate-into-situation (item situation)
   (:documentation "Provides a hook for doing interesting things
@@ -131,39 +214,25 @@
     (push b (relations model) )))
 
 
+(defgeneric add-entity (item)
+  (:documentation "Incorporate the item into the ongoing situation
+    at the appropriate level, carrying out any associated inferences
+    or priming operations."))
 
-;;;----------------
-;;; initialization
-;;;----------------
+(defmethod add-entity ((item t))
+  "Just stash it in the entity field of the model just to park it.
+   Essentially a no-op."
+  ;;/// trap unexpected types here?
+  (let ((model (model (the-situation)))) ;; level ?? 
+    (push item (entities model))))
 
-;; (designate-sentence-container :situation)  
-;; This is a computed function. Re-eval after any change
-(defun make-sentence-container/situation (sentence)
-  ;; Called from make-sentence-container when the sentence's
-  ;; document element is being laid down by start-sentence, where the only
-  ;; thing we actually know is the starting position.
-  (let ((model (make-instance 'model-of-situation)))
-    (make-instance 'sentence-situation 
-      :in sentence
-      :model model)))
+(defmethod add-entity ((i individual))
+  (let ((model (model (the-situation))) ;; level ?? 
+        (wrapper (wrap-latent-predicates i)))
+    (push-debug `(,wrapper ,i ,model))
+    (push wrapper (latent-predicates model))
+    (tr :adding-entity-to-situation i)
+    (push i (entities model))))
+    
 
-
-
-;;;--------------------------
-;;; retrieving the situation
-;;;--------------------------
-
-(defun the-situation ()
-  ;; ok for now when just in one sentence / a single situation
-  (let ((sentence (sentence)))
-    (unless sentence
-      (error "Threading bug: there no active sentence"))
-    (let ((contents (contents sentence)))
-      (unless contents
-        (error "The current sentence has no content model"))
-      (unless (typep contents 'sentence-situation)
-        (push-debug `(,sentence ,contents))
-        (error "Unexpected type of situation: ~a"
-               (type-of contents)))
-      contents)))
 
