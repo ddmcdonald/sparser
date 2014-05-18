2068d0005ecb02565e2d147f3839550ad16104aa
ddmcdonald50 <ddmcdonald50@gmail.com>
2014-05-18 21:44:26 +0000

    Incremental fixes

--- Sparser/code/s/drivers/chart/psp/c3-protocol.lisp
+++ Sparser/code/s/drivers/chart/psp/c3-protocol.lisp
@@ -4,14 +4,23 @@
 ;;;
 ;;;     File:  "C3-protocol"
 ;;;   Module:  "drivers/chart/psp/"
-;;;  version:  April 2014
+;;;  version:  May 2014
 
 ;; Initiated 9/18/13 by analogy to inititate-top-edges-protocol.
 ;; 10/9/13 started putting meat on its bones. Debugging segement 
 ;; scan through 1/21/14. Debugging extension of scan into next
-;; segment through 4/7/14
+;; segment through 4/7/14. Tweaking to get all the way to end of
+;; source through 5/11/14. 
 
 (in-package :sparser)
+
+(defvar *reached-eos* nil
+  "In the regular protocol, its much easier to know what
+   remains to be done once you've scanned the the eos word.
+   Here things change as we work out how things should be
+   done. Until they are (or we've moved all this back to
+   some variation on the regular protocol) we punt and use
+   this flag that read-through-segment-to-end sets.")
 
 (defun initiate-c3-protocol ()
   ;; Called from lookup-the-kind-of-chart-processing-to-do
@@ -20,7 +29,12 @@
   (let* ((p0 (scan-next-position))  ;; status = :scanned
          (ss (pos-terminal p0)) ;; #<word SOURCE-START>
          (p1 (scan-next-position)))
+    (declare (ignore ss))
+    ;; need them to just to keep straight what the scan does
+
+    (setq *reached-eos* nil) ;; initialize
     (scan-segment p1)))
+
 
 
 (defun scan-segment (start-pos)
@@ -43,14 +57,24 @@
       (setq position (chart-position-after position))
       (when (eq position last-position)
         (return)))
+
     ;; After it gets to the head (assumed to be the last constituent
     ;; but verb groups are more interesting with adverbs and such)
     ;; are drops out of the loop, we need to span the whole
     ;; segment with an edge and update the sentential state
-    (c3-process-segment-and-update-state start-pos last-position)
+    (let ((edge (c3-process-segment-and-update-state
+                 start-pos last-position)))
 
-    ;; presumably we now just scan the next segment
-    (scan-segment last-position)))
+      ;; Now that we have analyzed the segment we need to fold in
+      ;; the edge to the left. Like moving to the forest level
+      ;; after every segment. For the ISR experiment there will
+      ;; always be an edge here
+      (roll-up-edges-to-the-left edge) ;; also updates state
+
+      ;; presumably we now just scan the next segment
+      (if *reached-eos*
+        (terminate-chart-level-process)
+        (scan-segment last-position)))))
 
 
 (defun introduce-next-word (position-before head-word?)
@@ -96,14 +120,14 @@
 
         (when (eq word-after *end-of-source*)
           (push-debug `(,start-pos ,position-before))
-          (break "EOS -- how do we tell what remains to be done?")
+          (setq *reached-eos* t)
+          (tr :read-through-scanned-eos)
           (if *left-segment-boundary*
             ;; there's more processing to do
-            (break "returning the correct position on eos?"
             (return-from read-through-segment-to-end
-              (chart-position-before position-before))
+              position-before)
             ;; else we're done
-            (terminate-chart-level-process)))) ;; does a throw
+            (terminate-chart-level-process))) ;; does a throw
 
         (unless skip-]-check
           ;; we introduced the leading brackets as 
@@ -121,6 +145,11 @@
              (push-debug `(,] ,position-before))
              (break "still not fixed: ~a" position-before))
            ;; bracket-check set the right-boundary global
+           (unless (start-bracket-on? position-before)
+             ;; Gets around stupid issue about how proper names
+             ;; are labeled, which never came up because PNF
+             ;; always got them.
+             (add-nominal-start-bracket position-before))
            (return position-before))
 
           (else ;; loop around
@@ -129,9 +158,6 @@
                  (chart-position-after position-before))
            (tr :c3-segment-advancing-to position-before)))))))
  
-
-
-
 
 (defun c3-process-segment-and-update-state (start-pos last-pos)
   ;; called by scan-segment just after it gets out of its
@@ -155,26 +181,53 @@
       (add-indexical-to-situation var referent)
       ;; Expand it here, now that it's got all it's parts
       (add-entity referent)
-      (push-debug `(,new-state ,var))
-      
-    ;; Essence from incorporate-referent-into-the-situation is
-    ;;   (indexical-for-state new-state)
-    ;;   (add-indexical-to-situation var peg)
-)))
+      edge)))
 
-;; This is a clone of parse-at-the-segment-level, which can't be
-;; used as-is because it knits back to segment-parsed1 of the
-;; standard protocol. Also we can make different assumptions
-;; because, e.g., we know there is only one edge over every
-;; word because we're going to do ambiguity in the situation,
-;; and indeed that there is an edge over every word.
+
+(defun roll-up-edges-to-the-left (edge)
+  ;; Called from scan segment with responsibility to update the state
+  (let ((right-edge edge) ;; easier to keep straight
+        (left-edge (edge-ending-at (pos-edge-starts-at edge))))
+    (when left-edge ;;///convert to if to add traces
+      (let ((*edges-from-referent-categories* t)
+            (*allow-pure-syntax-rules* t))
+        (declare (special *allow-pure-syntax-rules*
+                          *edges-from-referent-categories*))
+        (let ((spanning-edge (check-one-one left-edge right-edge)))
+          (unless spanning-edge
+            (push-debug `(,left-edge ,right-edge))
+            (error "e~a and e~a didn't compose"
+                   (edge-position-in-resource-array left-edge)
+                   (edge-position-in-resource-array right-edge)))
+          ;; presumably we should update the state, but for
+          ;; suv + enter the edge's form is sentence, which 
+          ;; I suppose would move us to the discourse level
+          ;; and I don't want to do that yet (5/12/14)
+          spanning-edge)))))
+
+
 (defun c3-segment-parse (start-position end-position)
+  ;; This is a clone of parse-at-the-segment-level, which can't be
+  ;; used as-is because it knits back to segment-parsed1 of the
+  ;; standard protocol. Also we can make different assumptions
+  ;; because, e.g., we know there is only one edge over every
+  ;; word because we're going to do ambiguity in the situation,
+  ;; and indeed that there is an edge over every word.
+  (tr :c3-segment-parse start-position end-position)
   (let* ((head-edge (edge-starting-at end-position))
          (head-starts-at (pos-edge-starts-at head-edge))
          (edge-just-to-the-left (edge-ending-at head-starts-at))
+         (*edges-from-referent-categories* t)
          (*allow-pure-syntax-rules* t))
-    (declare (special *allow-pure-syntax-rules*))
+    (declare (special *allow-pure-syntax-rules* 
+                      *edges-from-referent-categories*))
     (push-debug `(,head-edge ,head-starts-at ,edge-just-to-the-left))
+
+    (when (eq (pos-edge-starts-at head-edge)
+              start-position)
+      (tr :c3-segment-parse-value head-edge)
+      (return-from c3-segment-parse head-edge))
+              
     ;; everthing acrues to the head, ///no provision yet for
     ;; composition of pre-head constituents
     ;;(break "before the loop")
@@ -194,13 +247,14 @@
           (error "e~a and e~a didn't compose"
                  (edge-position-in-resource-array left-edge)
                  (edge-position-in-resource-array right-edge)))
+        (tr :c3-segment-edge left-edge right-edge edge)
         (if (eq (pos-edge-starts-at edge) start-position)
           (return)
           (else
            (setq right-edge edge)
            (setq left-edge (edge-ending-at (pos-edge-starts-at edge))))))
+      (tr :c3-segment-parse-value edge)
       edge)))
-
 
 
 ;;;----------
@@ -220,4 +274,20 @@
        (error "No form category listed for ~a" state)))))
 
 
+(defun start-bracket-on? (position)
+  (let* ((edge-vector (pos-starts-here position))
+         (bracket (ev-boundary edge-vector)))
+    (when bracket
+      ;; if they're laid down uniformly then this is all we need
+      ;; but for now keeping this hook
+      bracket)))
+
+(defun add-nominal-start-bracket (position)
+  (push-debug `(,position))
+  ;; This open-codes do-boundary/begins-before which wants the
+  ;; label (word or category) that's introducing the bracket so that
+  ;; it can annotate the source of the bracket, which matters when
+  ;; assessing stronger-bracket of one that might be already there.
+  (let ((vector (pos-starts-here position)))
+    (setf (ev-boundary vector) phrase.[)))
 
