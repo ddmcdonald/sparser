8b87c7811fa20edef8991967478c81feaa200bab
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2014-05-19 22:08:20 +0000

    Broke out per-sentence functions with the greek letter spelled out as workaround for some change distorting the character set and warping the character point interpretations.

--- trunk/Sparser/code/s/init/workspaces/Biology-workspace.lisp
+++ trunk/Sparser/code/s/init/workspaces/Biology-workspace.lisp
@@ -3,7 +3,7 @@
 ;;;
 ;;;     File:  "Biology-workspace"
 ;;;   Module:  "init;workspaces:"
-;;;  version:  February 2014
+;;;  version:  May 2014
 
 ;; Initiated 11/6/13 to setup experiments in reading biology texts
 ;; and constructing process models from them. 
@@ -18,26 +18,35 @@
 
 ;; 2/27/14
 ; The greek characters are in -- entry-for-out-of-band-character --
-; but with ascii value for the character that they return, 
-; Can return the greek characters by backquoting the list and
-; using code-char to get the character, but on first try of
-; that we get a hangup in define-polyword/expr because the call
-; to not-all-same-character-type doesn't know of the out of band
-; chacters. This naievity is also burned into run-token-fas which
-; picks up the first character of a newly-started token.
-; The dispatch in continue-token has to be factored out so its
-; usable in these other places, which isn't simple. The function
-; tokenizer-entry might be a place to put this since lots of
-; other code uses it.
+;; 4/19/14
+; Something has changed oddly whereby a kappa in this file is coming out
+; as the combination of #\Latin_Capital_Letter_I_With_Circumflex with
+; #\Masculine_Ordinal_Indicator when passed to the parser.
+; Eyeballing the string shows a kappa. Describing it reveils that pair
+; rather than a kappa. 
 
 ;; 2/25/14 Have to remove SGML rules, e.g. for 'p' unless it's ok w/in ns-sequences
+
 ;; From 15677466
-(defvar *pc* "The processing of the nfκb2 gene product p100 to generate p52 is a regulated event, which is important for the instrumental function of NF-κB. We previously demonstrated that this tightly controlled event is regulated positively by NF-κB-inducing kinase (NIK) and its downstream kinase, IκB kinase α (IKKα). However, the precise mechanisms by which NIK and IKKα induce p100 processing remain unclear. Here, we show that, besides activating IKKα, NIK also serves as a docking molecule recruiting IKKα to p100. This novel function of NIK requires two specific amino acid residues, serine 866 and serine 870, of p100 that are known to be essential for inducible processing of p100. We also show that, after being recruited into p100 complex, activated IKKα phosphorylates specific serines located in both N- and C-terminal regions of p100 (serines 99, 108, 115, 123, and 872). The phosphorylation of these specific serines is the prerequisite for ubiquitination and subsequent processing of p100 mediated by the β-TrCP ubiquitin ligase and 26 S proteasome, respectively. These results highlight the critical but different roles of NIK and IKKα in regulating p100 processing and shed light on the mechanisms mediating the tight control of p100 processing. These data also provide the first evidence for explaining why overexpression of IKKα or its activation by many other stimuli such as tumor necrosis factor and mitogens fails to induce p100 processing."
+(defvar *pc* "The processing of the nfκb2 gene product p100 to generate p52 is a regulated event, which is important for the instrumental function of NF-κB. We previously demonstrated that this tightly controlled event is regulated positively by NF-κB-inducing kinase (NIK) and its downstream kinase, IκB kinase α (IKKα). 
+However, the precise mechanisms by which NIK and IKKα induce p100 processing remain unclear. 
+Here, we show that, besides activating IKKα, NIK also serves as a docking molecule recruiting IKKα to p100. 
+This novel function of NIK requires two specific amino acid residues, serine 866 and serine 870, of p100 that are known to be essential for inducible processing of p100. 
+We also show that, after being recruited into p100 complex, activated IKKα phosphorylates specific serines located in both N- and C-terminal regions of p100 (serines 99, 108, 115, 123, and 872). 
+The phosphorylation of these specific serines is the prerequisite for ubiquitination and subsequent processing of p100 mediated by the β-TrCP ubiquitin ligase and 26 S proteasome, respectively. 
+These results highlight the critical but different roles of NIK and IKKα in regulating p100 processing and shed light on the mechanisms mediating the tight control of p100 processing. 
+These data also provide the first evidence for explaining why overexpression of IKKα or its activation by many other stimuli such as tumor necrosis factor and mitogens fails to induce p100 processing."
   "target paragraph for proposal")
+
+(defun kappa-1 ()
+  ;; Rewrite of *pc* without the greek letters to get around 4/19 wierdness
+  (p "The processing of the nfkappab2 gene product p100 to generate p52 is a regulated event, which is important for the instrumental function of NF-kappaB."))
+(defun kappa-2 ()
+  (p "We previously demonstrated that this tightly controlled event is regulated positively by NF-kappaB-inducing kinase (NIK) and its downstream kinase, IkappaB kinase α (IKKα)."))
 
 
 (defvar *nkf2* "Processing of NF-kappaB2 precursor protein p100 to generate p52 is tightly controlled, which is important for proper function of NF-kappaB. Accordingly, constitutive processing of p100, caused by the loss of its C-terminal processing inhibitory domain due to nfkappab2 gene rearrangements, is associated with the development of various lymphomas and leukemia. In contrast to the physiological processing of p100 triggered by NF-kappaB-inducing kinase (NIK) and its downstream kinase, IkappaB kinase alpha (IKKalpha), which requires the E3 ligase, beta-transducin repeat-containing protein (beta-TrCP), and occurs only in the cytoplasm, the constitutive processing of p100 is independent of beta-TrCP but rather is regulated by the nuclear shuttling of p100. Here, we show that constitutive processing of p100 also requires IKKalpha, but not IKKbeta (IkappaB kinase beta) or IKKgamma (IkappaB kinase gamma). It seems that NIK is also dispensable for this pathogenic processing of p100. These results demonstrate a general role of IKKalpha in p100 processing under both physiological and pathogenic conditions. Additionally, we find that IKKalpha is not required for the nuclear translocation of p100. Thus, these results also indicate that p100 nuclear translocation is not sufficient for the constitutive processing of p100."
-  "The entire abstact")
+  "The entire abstact according to the abstractor, not as in the article")
 
 ;; 2/6/14
 ; (setq *uniformly-scan-all-no-space-token-sequences* t)
@@ -51,7 +60,7 @@
 ;; When it's ok we knit it into the usual set of files
 
 (define-grammar-module *biology*
-                       :name "Waypoints and related concepts"
+                       :name "Biology for Big Mechanism"
                        :parent *sublanguages*)
 (include-grammar-module *biology*)
 (def-logical-pathname "bio;" "sl;biology:")
