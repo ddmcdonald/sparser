8699bda1505300b27a7439ba61f88bf45bc08e68
ddmcdonald50 <ddmcdonald50@gmail.com>
2014-06-02 13:16:09 +0000

    Added the number of days in each month. Ignores leap years.

--- Sparser/code/s/grammar/model/dossiers/months.lisp
+++ Sparser/code/s/grammar/model/dossiers/months.lisp
@@ -1,26 +1,27 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1994 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1994,2014 David D. McDonald  -- all rights reserved
 ;;; 
 ;;;     File:  "months"
 ;;;   Module:  "model;dossiers:"
-;;;  Version:  October 1994
+;;;  Version:  May 2014
 
-;; broken out as its own file 10/20/94 v2.3
+;; broken out as its own file 10/20/94 v2.3.
+;; 5/27/14 Added number of days. 
 
 (in-package :sparser)
 
 
-(define-month "January"   1 "Jan")
-(define-month "February"  2 "Feb")
-(define-month "March"     3 "Mar")
-(define-month "April"     4 "Apr")
-(define-month "May"       5 )
-(define-month "June"      6 "Jun")
-(define-month "July"      7 "Jul")
-(define-month "August"    8 "Aug")
-(define-month "September" 9 "Sept")
-(define-month "October"  10 "Oct")
-(define-month "November" 11 "Nov")
-(define-month "December" 12 "Dec")
+(define-month "January"   1 31 "Jan")
+(define-month "February"  2 28 "Feb") ;; hackup leap year
+(define-month "March"     3 31 "Mar")
+(define-month "April"     4 30 "Apr")
+(define-month "May"       5 31)
+(define-month "June"      6 30 "Jun")
+(define-month "July"      7 31 "Jul")
+(define-month "August"    8 31 "Aug")
+(define-month "September" 9 30 "Sept")
+(define-month "October"  10 31 "Oct")
+(define-month "November" 11 30 "Nov")
+(define-month "December" 12 31 "Dec")
 
 
