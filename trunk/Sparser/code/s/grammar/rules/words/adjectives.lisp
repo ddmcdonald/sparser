0389b414c80fc592b98798294e15204f6fbfd62d
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2014-06-05 18:14:06 +0000

    Completely rebuilt to fit modern pattern

--- trunk/Sparser/code/s/grammar/rules/words/adjectives.lisp
+++ trunk/Sparser/code/s/grammar/rules/words/adjectives.lisp
@@ -1,17 +1,19 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
-;;; copyright (c) 1995  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1995,2014  David D. McDonald  -- all rights reserved
 ;;;
 ;;;     File:  "adjectives"
 ;;;   Module:  "grammar;rules:words:"
-;;;  version:  April 1995
+;;;  version:  June 2014
 
-;; initiated in 1/9/95. Redone in terms of fn words 4/12
+;; initiated in 1/9/95. Redone in terms of fn words 4/12/95,
+;; Rewritten from scratch 6/4/14.
 
 (in-package :sparser)
 
 
 (defun define-adjective (string)
-  (define-function-word string
-    :brackets '( ].phrase .[np )
-    :form (category-named 'adjective)))
+  (define-function-term string 'adjective
+    :super-category 'modifier
+    :rule-label 'modifier
+    :tree-families '(prenominal-adjective)))
 
