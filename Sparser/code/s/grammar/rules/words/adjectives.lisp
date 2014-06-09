078f50ae69694bc6c25988849a4153dc798d6ecd
ddmcdonald50 <ddmcdonald50@gmail.com>
2014-06-09 16:30:56 +0000

    completely rebuilt define-adjective to provide the full set of fn word optoins

--- Sparser/code/s/grammar/rules/words/adjectives.lisp
+++ Sparser/code/s/grammar/rules/words/adjectives.lisp
@@ -6,14 +6,18 @@
 ;;;  version:  June 2014
 
 ;; initiated in 1/9/95. Redone in terms of fn words 4/12/95,
-;; Rewritten from scratch 6/4/14.
+;; Rewritten from scratch 6/4/14. Added the other options 6/9/14
 
 (in-package :sparser)
 
 
-(defun define-adjective (string)
+(defun define-adjective (string
+                         &key super-category rule-label discriminator)
   (define-function-term string 'adjective
-    :super-category 'modifier
-    :rule-label 'modifier
+    :super-category (or super-category 
+                        (super-category-for-POS :adjective))
+    :rule-label (or rule-label
+                    'modifier)
+    :discriminator discriminator
     :tree-families '(prenominal-adjective)))
 
