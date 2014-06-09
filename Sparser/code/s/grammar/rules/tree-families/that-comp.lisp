cf97e94de5d833b6ea3d1b437f6fb02eef11ad1a
ddmcdonald50 <ddmcdonald50@gmail.com>
2014-06-09 16:32:29 +0000

    Removed options that had gotten report verbs into trouble

--- Sparser/code/s/grammar/rules/tree-families/that-comp.lisp
+++ Sparser/code/s/grammar/rules/tree-families/that-comp.lisp
@@ -1,13 +1,16 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1993-2005,2013 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1993-2005,2013-2014 David D. McDonald  -- all rights reserved
 ;;;
 ;;;     File:  "that comp"
 ;;;   Module:  "grammar;rules:tree-families:"
-;;;  version:  April 2013
+;;;  version:  June 2014
 
 ;; initiated 10/22/93 v2.3.  Added def-rule data 3/8/95.
 ;; Converted uninteresting "+" to a "-" 5/2.  Added instantiation of
 ;; 'result' 3/11/05. 4/3/13 Added family for just the tag parts.
+;; 6/6/14 Removed the tag and late-complement options from that-complement
+;; because they didn't seem to generalize and the commented out use of
+;; this ETF in sl/reports/object.lisp said that it was too aggressive.
 
 (in-package :sparser)
 
@@ -21,18 +24,18 @@
                 constituent of the subject and verb by themselves should ~
                 the grammar not have the rules for parsing the complement."
   :binding-parameters ( agent theme result )
-  :labels (s subj-verb np vg vp s/that-comp)
+  :labels (s #|subj-verb|# np vg vp s/that-comp)
   :cases
      ((:subject (s (np vp)
                   :head right-edge
                   :binds (agent left-edge)))
 
-      (:tag (subj-verb (np vg)
+      #+ignore (:tag (subj-verb (np vg)
                :head right-edge
                  
                :binds (agent left-edge)))
 
-      (:late-complement (s (subj-verb s/that-comp)
+      #+ignore (:late-complement (s (subj-verb s/that-comp)
                            :head left-edge
                            :binds (theme right-edge)))
 
