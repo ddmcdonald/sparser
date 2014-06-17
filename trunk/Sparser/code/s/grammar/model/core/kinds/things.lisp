c7b19f873b849a73e12fbe2376d5e9093b414732
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2014-06-17 22:45:28 +0000

    Brought in the rest of what i'd done for Dolce in Stratus

--- trunk/Sparser/code/s/grammar/model/core/kinds/things.lisp
+++ trunk/Sparser/code/s/grammar/model/core/kinds/things.lisp
@@ -70,3 +70,60 @@
   :realization (:common-noun "agent")) ;; n.b. "Mossad agent"
 
 
+(define-category feature
+  :specializes physical
+  realization (:common-noun "feature"))
+  :documentation
+  "These are things like stains, holes, boundaries, or surfaces that are
+ dependent on some other PhysicalObject as the 'host'. Other examples would
+ be the region under a table (dependent on the table, moves when it moves)
+ or the front of a house.")
+
+(define-category aggregate 
+  :specializes physical
+  :documentation
+  "Aggregates have no unity. They aren't wholes. They don't have good
+  connection relations among their parts or a sensible notion of their
+  bounderies.
+    There are two kinds of aggregates we could break out as subclasses:
+  amounts of matter (e.g. water or clay that has been individuated by
+  some criterion), and arbitrary collections of things that are individually
+  wholes (i.e. feature or physical objects). My college class, the red
+  things in this room.")
+
+
+(define-category non-physical 
+  :specializes endurant
+  :documentation
+  "Non-physical objects have no location. They often dependent in some way
+ on physical objects")
+
+(define-category mental-object 
+  :specializes non-physical
+  :documentation
+  "Ideas, sensations, dreams, maybe unicorns. Mental objects are dependent
+ on something with a mind, Perhaps the conclusions drawn by a program
+ fall into this category, while the program itself is a process.")
+
+(define-category social-object 
+  :specializes non-physical
+  :documentation
+  "Social objects are dependent on a community of agents. They don't have
+ to be people. Army ants and mercats form societies as well.")
+
+(define-category social-agent 
+  :specializes social-object
+  :documentation
+  "This lets us reify social roles, like President of the United States,
+ which is dependent on a person, but is not the person but the role
+ they play. 'Dependent' in the same way that a boundary depends of the
+ thing it bounds. Other examples are corporations, super-pacs, nations,
+ the U.N., AAAI. Thse are constitued of Agents (board of directors,
+ employees, members).")
+
+(define-category social-nonagentivive 
+  :specializes social-object
+  :documentation
+  "laws, normls, shares in a corporation, peace treaties. These are
+ generically dependent on societies but not on particular agents.")
+
