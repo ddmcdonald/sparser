32575b09fbcf28e51c223d5e1c0766a9e2c68079
amandalynne.p <amandalynne.p@gmail.com>
2014-07-02 16:29:15 +0000

    keeping track of discourse phrases

---
+++ Sparser/code/s/grammar/model/sl/biology/bio-discourse.lisp
@@ -0,0 +1,48 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER COMMON-LISP) -*-
+;;; Copyright (c) 2013-2014 SIFT LLC. All Rights Reserved
+;;;
+;;;    File: "bio-discourse"
+;;;  Module: "grammar/model/sl/biology/
+;;; version: June 2014
+
+;;; Discourse phrases
+
+;;; ===PHRASES===
+
+;;; from Hallmarks of Cancer
+
+;;; "recent results"
+;;; Recent results have highlighted the importance of negative-feedback loops that 
+;;; normally operate to dampen various types of signaling and thereby ensure homeostatic 
+;;; regulation of the flux of signals coursing through the intracellular circuitry.
+
+
+;;; "early studies"
+;;; Early studies of oncogene action encouraged the notion that ever-increasing 
+;;; expression of such genes and the signals manifested in their protein products 
+;;; would result in correspondingly increased cancer cell proliferation and thus tumor growth.
+
+;;; ----------------------------------
+
+;;; from Dragging Ras Back in the Ring
+
+;;; "early efforts"
+;;; Early efforts to block Ras cancers by preventing Ras farnesylation, once thought to be an
+;;; essential posttranslational modification for Ras activity, were thwarted by the unexpected
+;;; presence of a backup system (geranylgeranyltransferase) that restored activity of K-Ras and
+;;; N-Ras after farnesyltransferase treatment. 
+
+
+;;; "new efforts"
+;;; A convergence of urgent unmet clinical needs and advances in drug discovery has energized 
+;;; new efforts to target Ras cancers within academic centers and in the biopharmaceutical industry.
+
+;;; "not yet" -- Adverby, but makes reference to earlier (failed) trials
+;;; Inhibitors of PI3K pathway have not yet fared much better in the clinic, also because of feedback
+;;; mechanisms that activate upstream signaling, as well as poor therapeutic index. 
+
+;;; "recent advances" 
+;;; "ahead"
+;;; Here, we will discuss some of these knowledge gaps, as well as recent advances and the 
+;;; challenges that lie ahead.
+
