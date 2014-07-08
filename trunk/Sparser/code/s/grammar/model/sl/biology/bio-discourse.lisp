751a1fca7f0be2c59dcb253c90a9b035d8472fd0
amandalynne.p@gmail.com <amandalynne.p@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2014-07-08 21:21:07 +0000

    another phrase found

--- trunk/Sparser/code/s/grammar/model/sl/biology/bio-discourse.lisp
+++ trunk/Sparser/code/s/grammar/model/sl/biology/bio-discourse.lisp
@@ -22,9 +22,20 @@
 ;;; expression of such genes and the signals manifested in their protein products 
 ;;; would result in correspondingly increased cancer cell proliferation and thus tumor growth.'
 
+;;; "recent research"
+;;; 'Recent research has interrelated the acquisition of CSC traits with the EMT transdifferentiation 
+;;; program discussed above (Sing and Settleman, 2010; ....)'
+
 ;;; ----------------------------------
 
 ;;; from Dragging Ras Back in the Ring
+
+;;; "first" "classic" 
+;;; 'Ras genes were the *first* oncogenes identified in human cancer cells. In a series 
+;;; of *classic* experiments, the groups of Weinberg, Cooper, Barbacid, and Wigler independently 
+;;; identified the transforming genes from T24/EJ bladder carcinoma cells as H-Ras (Der et al., 1982, ...). 
+;;; More than 30 years later, Ras genes are well established as the most frequently mutated oncogenes in 
+;;; human cancer (Table 1), though H-Ras itself is rarely one of them.' 
 
 ;;; "early efforts"
 ;;; 'Early efforts to block Ras cancers by preventing Ras farnesylation, once thought to be an
@@ -38,6 +49,9 @@
 ;;; *new efforts* to target Ras cancers within academic centers and in the biopharmaceutical industry.'
 
 ;;; "not yet" -- Adverby, but makes reference to earlier (failed) trials
+;;; 'MAP kinase kinase (MEK) inhibitors and phosphatidylinositol 3-kinase (PI3K) inhibitors have *not yet* 
+;;; shown significant clinical activity in Ras cancers, for reasons relating to feedback loops and poor 
+;;; therapeutic windows, among other issues discussed below.'
 ;;; 'Inhibitors of PI3K pathway have *not yet* fared much better in the clinic, also because of feedback
 ;;; mechanisms that activate upstream signaling, as well as poor therapeutic index.'
 
@@ -46,3 +60,6 @@
 ;;; 'Here, we will discuss some of these knowledge gaps, as well as *recent advances* and the 
 ;;; challenges that lie *ahead*.'
 
+;;; "recently"
+;;; 'Recently, Legius and colleagues (Brems et al., 2007) discovered mutations in the Sprouty-related protein, 
+;;; SPRED1, in a form of neurofibromatosis type I (NF1) in which the neurofibromin gene is wild-type.'
