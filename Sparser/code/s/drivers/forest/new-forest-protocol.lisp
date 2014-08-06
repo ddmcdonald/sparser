73b38aa0784bd307b3df35c85c06ba6d498e6d3f
ddmcdonald50 <ddmcdonald50@gmail.com>
2014-08-06 14:23:44 +0000

    Stub for new forest level

---
+++ Sparser/code/s/drivers/forest/new-forest-protocol.lisp
@@ -0,0 +1,18 @@
+;;; copyright (c) 2014  David D. McDonald  -- all rights reserved
+;;; 
+;;;     File:  "new-forest-protocol"
+;;;   Module:  "drivers;forest:"
+;;;  Version:  August 2014
+
+;; Initiated 8/4/14
+
+(in-package :sparser)
+
+
+; (what-to-do-at-the-forest-level :new-forest-protocol)
+;
+(defun new-forest-driver (rightmost-position)
+;; Just a dummy that prints the treetops at each sentence. 
+  (break "got to the forest"))
+
+
