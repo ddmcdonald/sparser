661557b8a3445537df62f3592c4bb63d8e9942a5
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2014-08-06 14:23:44 +0000

    Stub for new forest level

--- trunk/Sparser/code/s/drivers/forest/loader.lisp
+++ trunk/Sparser/code/s/drivers/forest/loader.lisp
@@ -1,13 +1,14 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1992,1993,1994,1995  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992-1995,2014  David D. McDonald  -- all rights reserved
 ;;; 
 ;;;     File:  "loader"
-;;;   Module:  "drivers;treetop:"
-;;;  Version:  0.4 April 1995
+;;;   Module:  "drivers;forest:"
+;;;  Version:  0.4 August 2014
 
 ;; 0.2 (7/16/93 v2.3) flushed [boundaries], bumped [CA] -> 3
 ;; 0.3 (8/16) gated CA to facilitate parser-only version
 ;; 0.4 (4/27/95) bumped [ca] to 4
+;;     (8/4/14) added [new-forest-protocol]
 
 (in-package :sparser)
 
@@ -16,4 +17,6 @@
 
 (lload "forest-drivers;actions1")
 (lload "forest-drivers;trap2")
+
+(lload "forest-drivers;new-forest-protocol")
 
