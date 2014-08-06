0449b75f0c6e78aa6d229b1fa5c0b2574c2fb926
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2014-08-06 14:25:21 +0000

    Integrating affixes into edges

--- trunk/Sparser/code/s/grammar/rules/syntax/affix-rules1.lisp
+++ trunk/Sparser/code/s/grammar/rules/syntax/affix-rules1.lisp
@@ -1,9 +1,9 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1993,1994,1995  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1993-1995,2014  David D. McDonald  -- all rights reserved
 ;;; 
 ;;;     File:  "affix rules"
 ;;;   Module:  "grammar;rules:syntax:"
-;;;  Version:  0.2 January 1995
+;;;  Version:  1.0 August 2014
 
 ;; moved over from preterminals code 5/11/93, v2.3
 ;; 0.1 (3/28/94) changed the 'rule' on these edges from :known-affix to
@@ -15,67 +15,14 @@
 ;;      individuals. Added vastly bigger set of morphological options.
 ;;      Assigning the brackets to the word rather than introducing them
 ;;      right now, which simplifies the action by making it just as
-;;      though the word had been predefined.
+;;      though the word had been predefined. Making the words denote categories
+;;      using same flag and code as Comlex.
 
 (in-package :sparser)
 
-;;;------------------------
-;;; morphology-based edges
-;;;------------------------
-
-(defun make-edge-based-on-morphology (word
-                                      position-scanned
-                                      next-position)
-
-  ;; the caller -- Consider-morphology-based-edges -- only makes this
-  ;; call if the word has a non-nill morphology field.  Given the
-  ;; assumptions of the caller this *must* return only one edge, not
-  ;; that I can think of a situation in which more than one would
-  ;; ever make sense.
-  (case (word-morphology word)
-    (:ends-in-s   (make-morph-edge-over-unknown-word
-                   word position-scanned next-position
-                   category::ends-in-s))
-    (:ends-in-ed  (make-morph-edge-over-unknown-word
-                   word position-scanned next-position
-                   category::verb+ed))
-    (:ends-in-ing (make-morph-edge-over-unknown-word
-                   word position-scanned next-position
-                   category::verb+ing))
-    (:ends-in-ly  (make-morph-edge-over-unknown-word
-                   word position-scanned next-position
-                   category::adverb))
-    (otherwise
-     (break/debug "Unexpected affix keyword: ~A"
-                  (word-morphology word))
-     :foo ;; keep this frame on the stack
-     )))
-
-
-
-
-(defun make-morph-edge-over-unknown-word (word
-                                          pos-before pos-after
-                                          form-category)
-
-  ;; called from Make-edge-based-on-morphology during the process
-  ;; of introducing edges over single words.
-
-  (let ((edge (make-edge-over-unknown-word
-               word pos-before pos-after
-               form-category :spelling-based-edge)))
-    
-    (setf (edge-form     edge) form-category)
-    (setf (edge-referent edge) word)
-    (setf (edge-right-daughter edge) :morphology-based-edge)
-    (setf (edge-rule edge) :word-affix-morphology)
-
-    (tr :making-edge-over-unknown-word/morph edge)
-
-    (list edge)))
-
-
-
+;;;------------------------------------------
+;;; Assigning brackets and making categories
+;;;------------------------------------------
 
 (defun assign-morph-brackets-to-unknown-word (word morph-keyword)
 
@@ -127,4 +74,68 @@
      (push-debug `(,word ,morph-keyword))
      (error "Unexpected type of morph keyword: ~a~%~a"
             (type-of morph-keyword) morph-keyword))))
+
+
+
+;;;------------------------
+;;; morphology-based edges
+;;;------------------------
+
+;; These two make-edge functions are moot with the change to 
+;; give unknown words real content (7/14). Keeping them around
+;; in case we want to adapt something like this for another
+;; purpose. 
+
+(defun make-edge-based-on-morphology (word
+                                      position-scanned
+                                      next-position)
+
+  ;; the caller -- Consider-morphology-based-edges -- only makes this
+  ;; call if the word has a non-nill morphology field.  Given the
+  ;; assumptions of the caller this *must* return only one edge, not
+  ;; that I can think of a situation in which more than one would
+  ;; ever make sense.
+  (case (word-morphology word)
+    (:ends-in-s   (make-morph-edge-over-unknown-word
+                   word position-scanned next-position
+                   category::ends-in-s))
+    (:ends-in-ed  (make-morph-edge-over-unknown-word
+                   word position-scanned next-position
+                   category::verb+ed))
+    (:ends-in-ing (make-morph-edge-over-unknown-word
+                   word position-scanned next-position
+                   category::verb+ing))
+    (:ends-in-ly  (make-morph-edge-over-unknown-word
+                   word position-scanned next-position
+                   category::adverb))
+    (otherwise
+     (push-debug `(,word ,position-scanned ,next-position))
+     (break/debug "Unexpected affix keyword: ~A"
+                  (word-morphology word))
+     :foo ;; keep this frame on the stack
+     )))
+
+
+(defun make-morph-edge-over-unknown-word (word
+                                          pos-before pos-after
+                                          form-category)
+
+  ;; called from Make-edge-based-on-morphology during the process
+  ;; of introducing edges over single words.
+
+  (let ((edge (make-edge-over-unknown-word
+               word pos-before pos-after
+               form-category :spelling-based-edge)))
+    
+    (setf (edge-form     edge) form-category)
+    (setf (edge-referent edge) word)
+    (setf (edge-right-daughter edge) :morphology-based-edge)
+    (setf (edge-rule edge) :word-affix-morphology)
+
+    (tr :making-edge-over-unknown-word/morph edge)
+
+    (list edge)))
+
+
+
 
