1837c79986007ea18c840f15804f9266e19e206f
ddmcdonald50 <ddmcdonald50@gmail.com>
2014-08-07 21:50:09 +0000

    Checkpoint

--- Sparser/code/s/drivers/forest/new-forest-protocol.lisp
+++ Sparser/code/s/drivers/forest/new-forest-protocol.lisp
@@ -9,10 +9,22 @@
 (in-package :sparser)
 
 
-; (what-to-do-at-the-forest-level :new-forest-protocol)
+; (what-to-do-at-the-forest-level :new-forest-protocol) ;; eval with each edit
 ;
 (defun new-forest-driver (rightmost-position)
-;; Just a dummy that prints the treetops at each sentence. 
-  (break "got to the forest"))
+  ;; Just a dummy that prints the treetops at each sentence. 
+  (tr :new-forest-driver rightmost-position)
+  (break "got to the forest")
+  (let ((sentence (previous-sentence)))
+    (cond
+     (sentence
+      (let ((start-pos (starts-at-pos sentence))
+            (stop-pos (ends-at-pos sentence)))
+        (format t "~&~%")
+        (print-flat-forest t start-pos stop-pos)
+        (format t "~&~%~%")
+        (scan-next-segment *where-the-last-segment-ended*)))
+     (t 
+       (scan-next-segment *where-the-last-segment-ended*)))))
 
-
+ 
---
+++ Sparser/code/s/drivers/forest/parsing-containers.lisp
@@ -0,0 +1,51 @@
+;;; copyright (c) 2014 David D. McDonald  -- all rights reserved
+;;; 
+;;;     File:  "parsing-containers"
+;;;   Module:  "drivers;forest:"
+;;;  Version:  August 2014
+
+;; Initiated 8/6/14. To hold the new class of containers to support
+;; analysis and discourse structure to go with the new forest protocol
+
+(in-package :sparser)
+
+
+;;; mixins
+
+(defclass parsing-status ()
+  ((level-completed :initarg level :accessor level-completed
+    :documentation "The parsing is tiered into successive levels
+      of analysis. Which one have we completed for this level of
+      document structure?
+      Possible values:
+        :initial  -- we're at the start of the sentence and
+                   haven't scanned any part of it.
+"))
+  (:documentation ""))
+
+
+
+;;; class
+
+(defclass sentence-content (container parsing-status ordered)
+  ()
+  (:documentation "From container we get :in to point back to the
+    sentence. From ordered we get previous and next so we can link
+    the directly without having to go to the sentence objects."))
+
+(defmethod print-object ((c sentence-content) stream)
+  (print-unreadable-object (c stream :type t)
+    (let ((sentence (bkptr c)))
+      (format stream "p~a -- "
+              (pos-token-index (starts-at-pos sentence)))
+      (if (and (slot-boundp sentence 'ends-at-pos)
+               (ends-at-pos sentence))
+        (format stream "p~a" (pos-token-index (ends-at-pos sentence)))
+        (format stream "?")))))
+
+; (designate-sentence-container :complex)  ;; run with every change
+;
+(defun make-sentence-content-container (sentence)
+  (make-instance 'sentence-content
+    :in sentence
+    :level :initial
