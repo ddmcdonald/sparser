79ae94e9e1a555e7d99cc8209bbc45e4d2455a66
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2014-08-12 19:55:28 +0000

    moved in stranded *tts-after-each-section*

--- trunk/Sparser/code/s/init/versions/v4.0/workspace/display.lisp
+++ trunk/Sparser/code/s/init/versions/v4.0/workspace/display.lisp
@@ -3,17 +3,18 @@
 ;;; 
 ;;;     File:  "display"
 ;;;   Module:  "init;versions:v2.3:workspace:"
-;;;  version:  January 2014
+;;;  version:  August 2014
 
 ;; Initiated 10/28/13 to put pointers to all the parameters 
 ;; that govern display in a single place for easy reference.
 ;; Added quiet and instance of undisplay-bracketing 1/27/14.
+;; 8/10/14 moved in stranded *tts-after-each-section*. 
 
 (in-package :sparser)
 
-
-;; Referenced in begin-new-paragraph
-;;   *tts-after-each-section*
+(defparameter *tts-after-each-section* nil
+  "Gates displaying the chart after each section.
+   Referenced in begin-new-paragraph")
 
 (display-bracketing)
 ;; turns off *display-word-stream*
@@ -30,3 +31,4 @@
 
 ;; Announces what file is being run
 (setq *verbose-document-stream* t)
+
