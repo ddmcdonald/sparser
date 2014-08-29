cf1e9d9e17e00ebed084345c43d6ab4232c3ad12
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2014-08-29 15:22:21 +0000

    More vocabulary, though a bunch of it is hacked

--- trunk/Sparser/code/s/grammar/model/sl/biology/verbs.lisp
+++ trunk/Sparser/code/s/grammar/model/sl/biology/verbs.lisp
@@ -9,13 +9,41 @@
 
 (in-package :sparser)
 
+
+(svo "act")
+
+
 ;;--- "activate"
 ;; "NIK  activates IKKalpha"
+;; "turn on Ras by activating <p>"
 
 (svo/nominal "activate" "activation"
   :subject 'bio-entity
   :theme 'bio-entity)
 
+(svo "call")
+
+;;--- "encode"
+;; <enzyme> encoded by <gene>
+(svo "encode")  ;;/// wrong -- placeholder
+
+(svo/passive/nominal "enhance" "enhancement"
+  :super-category bio-process
+  :patient bio-process  ;; inhibiton of <process>
+  :agent bio-entity)
+
+;;--- "increase"
+;; Observed as a verb: 
+;;  "Other less frequently observed mutations, such as those found in the
+;;  G4 and G5 boxes, increase the rate of nucleotide exchange, thereby
+;;  mimicking the GEFs and increasing the GTP-bound state."
+;; But the noun form is probably in there too.
+;; Note that there's a dossier of change-in-amount-verbs though it's
+;; not being loaded which goes with, e.g., define-change-in-amount-verb/up
+;; which presumes a 'standalone-direction' category which exists
+;; in places/directions but doesn't work the way it used to or was
+;; abandoned between directions and directions1
+(svo "increase")
 
 ;;--- "induce"
 ;; "which induce transcription of the p53 gene"
@@ -26,6 +54,13 @@
    :theme 'event)
 ;;/// want subtypes, want to understand the syntax of "-inducing"
 
+
+;;--- inhibit
+;; "by inhibiting <p>"
+(svo/passive/nominal "inhibit" "inhibition"
+  :super-category bio-process
+  :patient bio-process  ;; inhibiton of <process>
+  :agent bio-entity)
 
 ;;--- "mutation"
 ;; "mutated oncogenes"
@@ -41,6 +76,11 @@
              :subject 'bio-entity :theme 'bio-entity)
 
 
+(svo/passive/nominal "suggest" "suggestion"
+  :super-category bio-process
+  :patient bio-process 
+  :agent bio-entity)
+
 
 ;;--- "regulate"
 ;;
@@ -48,3 +88,14 @@
   :super-category bio-process
   :patient bio-process  ;; regulation of <process>
   :agent bio-entity)    ;; by <entity>
+
+
+
+
+
+;;--- "turn on" (off)
+;; "Growth factors can turn on Ras"
+
+(sv-prep-marked-o "turn" "on")
+
+
