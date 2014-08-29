4fdce063e1a55ecd8ded2466be605df133db4d4c
dwormans <dwormans@gmail.com>
2014-08-29 06:10:36 +0000

    added several disease related organizations to named-entities: world health organization (apparently this was missing?) and doctors without borders (will need to add French name as well).

--- Sparser/code/s/grammar/model/sl/disease/named-entities.lisp
+++ Sparser/code/s/grammar/model/sl/disease/named-entities.lisp
@@ -3,15 +3,24 @@
 ;;;
 ;;;      File:   "named-entities"
 ;;;    Module:   "sl;disease:"
-;;;   version:   May 2013
+;;;   version:   August 2014
 
 ;; initiated 5/6/13
+;; updated 28/8/14--added WHO, Doctors Without Borders
+
 
 (in-package :sparser)
 
 (define-with-all-instances-permanent
 
-    ;; "company" isn't right for most of these, but it will do
+    ;; "company" isn't right for most of these, but it will do 
+    ;; agreed, should perhaps have "organization" category?
     (define-company '("United" "Nations") :aliases '("U.N.") :takes-the t)
+    (define-company '("World" "Health" "Organization") :aliases '("W.H.O."))
+    ;;medical group operating throughout the world: Doctors Without Borders
+    ;;also French name is used in English news articles as well
+    ;;since it is actually a French based organization
+    ;;Médecins Sans Frontières--won't work yet because of special characters?
+    (define-company '("Doctors" "Without" "Borders"))
 
 ) ;; closes with all permanent
