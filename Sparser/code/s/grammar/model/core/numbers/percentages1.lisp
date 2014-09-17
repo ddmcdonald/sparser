fecf9247a2491342aa61c4ab0fb571f17b14eacb
ddmcdonald50 <ddmcdonald50@gmail.com>
2014-09-17 20:22:23 +0000

    Added n-fold

--- Sparser/code/s/grammar/model/core/numbers/percentages1.lisp
+++ Sparser/code/s/grammar/model/core/numbers/percentages1.lisp
@@ -1,14 +1,15 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
-;;; copyright (c) 1992-1998  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992-1998,2014  David D. McDonald  -- all rights reserved
 ;;; 
 ;;;     File:  "percentages"
 ;;;   Module:  "grammar;model:core:numbers:"
-;;;  Version:  1.3 July 1998
+;;;  Version:  1.3 September 2014
 
 ;; 1.1 (7/16/92 v2.3) pilot instances of the new representation regime
 ;; 1.2 (1/10/94) stubbed measurement to get around load-order paradox
 ;;     (1/9/96) added string printer
 ;; 1.3 (7/5/98) redone with a schematic realization.
+;;     (9/9/14) added 'n-fold'
 
 (in-package :sparser)
 
@@ -35,4 +36,27 @@
 
 (defun string/percent (p)
   (format nil "~A%" (string-for (value-of 'number p))))
+
+
+
+;;;--------
+;;; N-fold
+;;;--------
+;; These describe changes in amount.
+;;/// we ought to reflect that is some relation that 
+;; can be used to organize search if it's not in construction
+;; this this
+
+(define-category n-fold
+  :instantiates :self
+  :specializes  measurement
+  :rule-label measurement
+  :binds  ((number . number))
+  :index (:temporary :list)
+  :realization (:tree-family item+idiomatic-head
+                :mapping ((np . :self)
+                          (np-head . "fold")
+                          (modifier . number)
+                          (result-type . :self)
+                          (item . number))))
 
