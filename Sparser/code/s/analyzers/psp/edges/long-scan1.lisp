a6bfc8a81fc4acc8a288a76037aa0685cbee5946
ddmcdonald50 <ddmcdonald50@gmail.com>
2014-09-27 20:02:44 +0000

    Better treatment of explicitly supplied list of constituents

--- Sparser/code/s/analyzers/psp/edges/long-scan1.lisp
+++ Sparser/code/s/analyzers/psp/edges/long-scan1.lisp
@@ -1,9 +1,9 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
-;;; copyright (c) 1992-1994,2013  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992-1994,2013-2014 David D. McDonald  -- all rights reserved
 ;;;
 ;;;      File:   "long scan"
 ;;;    Module:   "analyzers;psp:edges:"
-;;;   Version:   1.2 January 2013
+;;;   Version:   1.3 September 2014
 
 ;; 1.0 (9/7/92 v2.3) flushed out of date field references
 ;; 1.1 (2/24/94) added daughter and used-in encoding
@@ -12,6 +12,10 @@
 ;;     (1/9/13) Added constituents and words keywords to let those
 ;;      fields on the edge to be populated when the caller
 ;;      has them in its hand. 
+;; 1.3 (9/25/14) switched to using successive-treetop because
+;;      of a bug found in the multiple-edge case of all-treetops,
+;;      though even that is uneven about choice among multiple edges
+;;      so added semi-redundant set-used-by when constituents are known.
 
 (in-package :sparser)
 
@@ -44,13 +48,18 @@
 
     (complete edge)
 
-    (let ((daughters (all-treetops :from starting-position
+    (let ((daughters (successive-treetops :from starting-position
                                    :to ending-position
                                    :below edge)))
       (mapcar #'(lambda (tt)
                   (when (edge-p tt)
                     (set-used-by tt edge)))
               daughters)
+      (when constituents
+        (dolist (c constituents)
+          (when (edge-p c)
+            (unless (edge-used-in c)
+              (set-used-by c edge)))))
       (setf (edge-left-daughter edge) (first daughters))
       (setf (edge-right-daughter edge) :long-span)
       (setf (edge-constituents edge)
