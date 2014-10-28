8eedf47258edc3874935e4676797bdbc1ad675a3
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2014-10-28 16:09:26 +0000

    Added a model-walking function. Could you more tuning/customizing

--- trunk/Sparser/code/s/interface/grammar/printing.lisp
+++ trunk/Sparser/code/s/interface/grammar/printing.lisp
@@ -6,7 +6,7 @@
 ;;;   version:  April 1994
 
 ;; initiated 2/10/92 v2.2, adjusted 2/20 to changes in field names
-;; (4/6/94) Added Describe-gmod
+;; (4/6/94) Added Describe-gmod.
 
 (in-package :sparser)
 
@@ -81,5 +81,89 @@
   (dolist (gm *grammar-modules-in-image*)
     (number-of-objects gm stream)
     (terpri stream)))
+
+
+;;;---------------------------
+;;; Pretty printing referents
+;;;---------------------------
+;; Could adapt this from the workbench printing routines,
+;; but they're entangled with visual elements.
+
+;; Same as pretty-print-individual but it will recurse on
+;; the individuals in bindings in-line (print-binding) 
+(defmethod pp-individual ((i individual) stream)
+  (format stream "~&")
+  (let ((category (car (indiv-type i)))
+        (id (indiv-id i)))
+    (format stream "~a ~a" (cat-symbol category) id))
+  (let ((binds (indiv-binds[ i)))
+    (when binds
+      (dolist (b binds)
+        (princ-binding b stream 4))))
+  (let ((bound-in (indiv-bound-in i)))
+    (when bound-in
+      (dolist (b bound-in)
+        (princ-bound-in b stream 4)))))
+
+(defmethod walk-model ((i individual) stream)
+  (let ((*individuals* (list i))
+         *printed*  i )
+    (declare (special *individuals* *bindings*))
+    (labels ((indiv/short-form (i)
+             (format nil "#<~a ~a>"
+                     (cat-symbol (itype-of i))
+                     (indiv-id i)))
+           (format-binding-value (v)
+             (typecase v
+               (individual 
+                (unless (memq v *individuals*)
+                  (setq *individuals*
+                        (tail-cons v *individuals*)))
+                (indiv/short-form v))
+               (otherwise
+                (format nil "~a" v))))
+           (format-binding (b type)
+             (let ((id (binding-id b))
+                   (var (binding-variable b))
+                   (value (binding-value b))
+                   (body (binding-body b)))
+               (ecase type
+                 (:bind
+                  (format nil "[b~a ~a: ~a]"
+                          id (var-name var)
+                          (format-binding-value value)))
+                 (:bound-in 
+                  (format nil "[b~a the ~a of ~a]"
+                          id (var-name var)
+                          (format-binding-value body)))))))
+      (loop
+        (when (null *individuals*)
+          (return))
+        (setq i (pop *individuals*))
+        (unless (memq i *printed*)
+          (setq *printed* (push i *printed*))
+          (let* ((type-field (indiv-type i))
+                 (binds (indiv-binds i))
+                 (bound-in (indiv-bound-in i)))
+            (format stream "~&~%~a"
+                    (indiv/short-form i))
+            
+            (format stream "~&  Type: (")
+            (dolist (c type-field) 
+              (format stream "~a " (cat-symbol c)))
+            (format stream ")~%")
+            
+            (when (or binds bound-in)
+              (format stream "~&  Relations:~%"))
+            
+            (dolist (b binds)
+              (format stream "~&    ~a"
+                      (format-binding b :bind)))
+            
+            (dolist (b bound-in)
+              (format stream "~&    ~a"
+                      (format-binding b :bound-in)))
+            
+            (format stream "~%")))))))
 
 
