4d22da9d58a5a0ba22fb0587f542a5456d11cf1d
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2014-11-02 19:40:42 +0000

    Redone as objects

--- trunk/Sparser/code/s/grammar/rules/words/determiners.lisp
+++ trunk/Sparser/code/s/grammar/rules/words/determiners.lisp
@@ -1,10 +1,10 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
-;;; copyright (c) 1992-1995,2012  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992-1995,2012-2014  David D. McDonald  -- all rights reserved
 ;;; extensions copyright (c) 2008 BBNT Solutions LLC. All Rights Reserved
 ;;;
 ;;;      File:   "determiners"
 ;;;    Module:   "grammar;rules:words:"
-;;;   Version:   0.6 December 2012
+;;;   Version:   0.7 October 2014
 
 ;; broken out from "fn words - cases" 12/17/92 v2.3
 ;; 0.1 (6/8) added Define-determiner
@@ -13,23 +13,49 @@
 ;; 0.4 (12/5) flushed "additional" which had been included because of LS's judgement
 ;; 0.5 (1/30/95) gave 'that' its own '[' bracket
 ;; 0.6 (6/16/08) Adding a set of brackets for the single-capitalized-letter
-;;      form of "a". (11/10/12) neither, either. 12/4/12 "those"
+;;      form of "a". (11/10/12) neither, either. 12/4/12 "those".
+;; 0.7 (10/29/14) Makeover sort of following the pattern in conjunctions.
 
 (in-package :sparser)
 
 
-(defun define-determiner (string  &key brackets)
+(define-category determiner
+  :binds ((word  :primitive word)))
+;; This is for functions/methods as a supercategory,
+;; as distinct from the form category 'det'
+
+
+(defun define-determiner (string &key brackets)
+  (unless brackets
+    (setq brackets '( ].phrase .[article)))
   (let ((word (define-function-word string
                 :brackets brackets
-                :form 'det)))
-    word ))
+                :form 'det))
+        (category-name (name-to-use-for-category string)))
+
+    (let* ((category-form
+            `(define-category ,category-name
+               :specializes determiner
+               :instantiates :self
+               :bindings (word ,word)))
+           (category (eval category-form)))
+
+      (let* ((object (define-individual category
+                        :word word))
+             (cfr ;; the base rule for the word
+               (def-cfr/expr category ;; lhs
+                             (list word) ;; rhs
+                :form (category-named 'det)
+                 :referent object)))
+        (add-rule-to-individual cfr object)
+        object))))
 
 
 
-(define-determiner "the"  :brackets '( ].phrase .[article ))
-(define-determiner "an"   :brackets '( ].phrase .[article ))
-(define-determiner "a"    :brackets '( ].phrase .[article ))
-(define-determiner "A"    :brackets '( ].phrase .[article ))
+(define-determiner "the")
+(define-determiner "an")
+(define-determiner "a")
+(define-determiner "A") ;;/// still needed?
 
 (define-determiner "this"  :brackets '( ].phrase .[np ))
 
