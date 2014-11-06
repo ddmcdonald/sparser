da60b0ffe79a480c5b3c22bb7764c35ba6adc1be
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2014-11-06 20:20:07 +0000

    For defaulted unknown words

--- trunk/Sparser/code/s/objects/traces/tokenizer.lisp
+++ trunk/Sparser/code/s/objects/traces/tokenizer.lisp
@@ -4,11 +4,12 @@
 ;;; 
 ;;;     File:  "tokenizer"
 ;;;   Module:  "objects;traces:"
-;;;  Version:  August 2014
+;;;  Version:  November 2014
 
 ;; Initiated 11/90 (v1.5)
 ;; 11/12/14 Adding traces for giving unknown words light content because of
 ;; their morphology and such. More 8/22/14 to cover individual cases
+;; Adding as needed through 11/6/14
 
 (in-package :sparser)
 
@@ -36,8 +37,14 @@
     (trace-msg "[Morphology] defining as a ~a" pos-term )))
 
 (deftrace :defining-lemma-as-given-morph (lemma pos-term)
-;; called from assign-morph-brackets-to-unknown-word
+  ;; called from assign-morph-brackets-to-unknown-word
   (when *trace-morphology*
     (trace-msg "[Morphology] defining lemma ~s as a ~a"
                (word-pname lemma) pos-term)))
+
+(deftrace :unknown-word-defaulted-to-noun (word)
+  ;; called from setup-unknown-word-by-default
+  (when *trace-morphology*
+    (trace-msg "Defining the unknown word ~s as a noun by default"
+               (word-pname word))))
 
