445ea7ed998cd29a2dd15748fd8c4cf79ee0d442
ddmcdonald50 <ddmcdonald50@gmail.com>
2014-11-20 21:55:44 +0000

    process-accepted-scan-pattern had never been finished. Did that.

--- Sparser/code/s/analyzers/psp/patterns/accept.lisp
+++ Sparser/code/s/analyzers/psp/patterns/accept.lisp
@@ -1,12 +1,13 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1995-1996  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1995-1996,2014 David D. McDonald  -- all rights reserved
 ;;;
 ;;;     File:  "accept"
 ;;;   Module:  "analysers;psp:patterns:"
-;;;  version:  July 1996
+;;;  version:  November 2014
 
 ;; initiated 9/21/95. Given some content 10/5. 7/11/96 That context was just a stub.
 ;; Now it's getting substance.
+;; (11/17/14) Finished the setup to call an acceptance function.
 
 (in-package :sparser)
 
@@ -31,17 +32,31 @@
 (defun process-accepted-scan-pattern (state)
   (let* ((pattern (state-is-an-accept-state? state))
          (accept-function (sp-accept-function pattern)))
-    (push-debug `(,accept-function)) (break "accepted")
+    ;;(push-debug `(,accept-function)) (break "accepted")
 
-    *last-scan-pattern-element*
+    ;; Collect the elements of the array into a list.
+    ;; All the processing that's going to happen to them
+    ;; will be in this list.
+    (let* ((arg-forms ;; e.g. (#<word "off"> . :word) 
+            (loop for i from 0 to  *last-scan-pattern-element* ;; count
+              collect (aref *instantiated-scan-pattern-elements* ;; array
+                            i)))
+           (args (loop for form in arg-forms
+                   collect (car form))))
 
-    *instantiated-scan-pattern-elements*
+      (unless (fboundp accept-function)
+        (error "The scan accept function ~a is undefined" accept-function))
 
-    ;;/// this is a stand-in until I figure out a nice way to setup the
-    ;; pattern elements to the accept function
+      (apply accept-function 
+             *position-scan-pattern-started-at* 
+             *position-reached-by-scan-pattern*
+             args))))
 
+
+;;/// this is a stand-in until I figure out a nice way to setup the
+;; pattern elements to the accept function
+#+ignore
     (let ((category (def-category/expr (sp-name pattern))))
- ;(break)
       (let ((edge
              (make-chart-edge
               :starting-position *position-scan-pattern-started-at*
@@ -58,10 +73,9 @@
 
         ;; // trace
               
-
         ;; We're the last point in the thread that started in 
         ;; Check-for/initiate-scan-patterns, and we have to return
         ;; the position where the base scan should resume its
         ;; actions. That's the position just beyond the span
         ;; that we've covered with this pattern.
-        *position-reached-by-scan-pattern* ))))
+        *position-reached-by-scan-pattern* ))
