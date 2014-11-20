d86775dfbf6bfadfacdae4ea7342d947ccf66c5b
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2014-11-20 21:53:54 +0000

    Accommodated fan-out from low-level changes

--- trunk/Sparser/code/s/drivers/chart/psp/no-brackets-protocol.lisp
+++ trunk/Sparser/code/s/drivers/chart/psp/no-brackets-protocol.lisp
@@ -3,11 +3,12 @@
 ;;;
 ;;;     File:  "no-brackets-protocol"
 ;;;   Module:  "drivers/chart/psp/"
-;;;  version:  October 2014
+;;;  version:  November 2014
 
 ;; Initiated 10/5/14, starting from the code for detecting bio-entities.
 ;; 10/29/14 added flags to turn off various steps so lower ones
-;; could be independently tested
+;; could be independently tested. 11/18/14 Reflecting the decomposition
+;; of the sweep into a succession of sweeps. 
 
 (in-package :sparser)
 
@@ -24,6 +25,12 @@
 
 ; (setq *kind-of-chart-processing-to-do* :successive-sweeps)
 ; (bio-setting)  ;; revert to 'normal'
+
+
+(defun sucessive-sweeps? ()
+  ;; syntactic sugar for a mode detector. Cf. new-forest-protocol?
+  (eq *kind-of-chart-processing-to-do* :successive-sweeps))
+
 
 ;;;--------
 ;;; Driver
@@ -55,6 +62,9 @@
         (tr :scanning-terminals-of sentence)
         (catch :end-of-sentence
           (scan-terminals-loop start-pos first-word))
+        (pattern-sweep sentence)
+        (short-conjunctions-sweep sentence) ;; precede parens
+        (sweep-to-span-parentheses sentence)
         (when *trace-island-driving* (tts))
 
         (when *chunk-sentence-into-phrases*
@@ -73,162 +83,3 @@
         ;; start scanning terminals and it will throw
         ;; beyond this point. 
         (setq sentence (next sentence))))))
-
-
-#| scan-next-position:
-Bumps the chart indexes. Does an add-terminal-to-chart if
-there is not already a word at this 'next' position. Returns 
-the position. (N.b. there's an incremental trace hook in it.) |#
-
-(defun scan-terminals-loop (position-before word)
-  (when *trace-sweep*
-    (format t "~&p~a ~s"
-            (pos-token-index position-before) (word-pname word)))
-  (simple-eos-check position-before word)
-
-  ;; Polyword check
-  (let* ((where-pw-ended (polyword-check position-before word))
-         (position-after (or where-pw-ended
-                             (chart-position-after position-before))))
-    (when where-pw-ended
-      (setq position-before where-pw-ended)
-      (unless (includes-state where-pw-ended :scanned)
-        ;; PW can complete without thinking about the
-        ;; word that follows it.
-        (scan-next-position))
-      (setq word (pos-terminal where-pw-ended)))
-
-    (unless (includes-state position-after :scanned)
-      (scan-next-position))
-
-    (when nil
-      (push-debug `(,position-before ,position-after)) 
-      (break "After PW check: before = p~a  after= p~a"
-             (pos-token-index position-before)
-             (pos-token-index position-after)))
-
-    (let ((traversal-checked position-before))
-      ;; (trace-traversal-hook) (trace-traversal-hits)
-      (word-traversal-hook word position-before position-after)
-      ;; Traversal actions are managed by a hash table from the word
-      ;; qua label (i.e. could be applied to edges as well) to a function
-      ;; that takes these same arguments. This is used for bracket pairs
-      ;; such as parentheses, double quotes, etc. Check with a call to
-      ;; (list-hash-table *traversal-routine-table*)
-      ;;    The action is always on the matching close. The open
-      ;; notes its oposition so the close knows what span to operate
-      ;; over. We check for traversal hits before the no-space check
-      ;; because the ns is greedy and moves the position, which can
-      ;; cause the open to be missed. 
-
-
-      (when (no-space-before-word? position-before)
-        ;; As with PW, if it succeeds we need to restart the loop
-        ;; where it leaves off. 
-        (let ((where-ns-ended (do-no-space-collection position-before)))
-          (when where-ns-ended ;; which will have been scanned
-            (setq position-before where-ns-ended
-                  position-after (chart-position-after position-before)
-                  word (pos-terminal position-before)))))
-      (unless (includes-state position-after :scanned)
-        (scan-next-position))
-
-
-      (unless (eq traversal-checked position-before)
-        ;;/// if we noted whether no-space had completed we could
-        ;; conditionalize this 'addional' check. But we do need it
-        ;; for the cases where the earlier traversal check picked up
-        ;; the open and the no-space has moved us ahead so that we're
-        ;; about to see the close. 
-        ;; (trace-parentheses)
-        (word-traversal-hook word
-                             position-before
-                             position-after)))
-
-
-    (complete-word/hugin word position-before position-after)
-    ;; (setq *trace-completion-hook* t)
-    ;; The function check-for-completion-actions/word looks on the
-    ;; rule-set of the word for a completion action or actions and
-    ;; runs carry-out-actions to execute (funcall) them. 
-    ;;   The significant case is the period-hook (in rules/DM&P/period-hook)
-    ;; because it is responsible for managing the succession of sentences.
-    ;; That requires some tweaking because normally the period hook signals
-    ;; the progression to the forest level and here we need to notice
-    ;; the period (in order to stop this pass and start the next), but
-    ;; be more selective in what happens. 
-    ;;    Another important case is conjunction. Both "and" and "or"
-    ;; set the *pending-conjunction* flag. 
-
-
-    (do-just-terminal-edges word position-before position-after)
-
-
-    (let ((next-word (pos-terminal position-after)))
-      (when nil
-        (format t "~&Next step: p~a ~a"
-               (pos-token-index position-after) (word-pname next-word)))
-      (scan-terminals-loop position-after next-word))))
-
-
-
-(defun do-just-terminal-edges (word position-before position-after)
-  ;; modeled on introduce-terminal-edges but returns after the
-  ;; edges are created rather than continuing in the incremental
-  ;; scan. 
-  (install-terminal-edges word position-before position-after))
-
-(defparameter *integrate-no-space-patterns* nil)
-
-(defun do-no-space-collection (position-before)
-  ; lifted from check-for-uniform-no-space-sequence
-  (tr :check-for-uniform-no-space-sequence position-before)
-  (if *integrate-no-space-patterns*
-    (break "ns-pattern stub ~a" position-before)
-    (let ((uniform-pos-reached
-           (collect-no-space-sequence-into-word position-before)))
-      uniform-pos-reached)))
-   
-
-(defun polyword-check (position-before word)
-  ;; lifted from check-for-polywords where all we want is
-  ;; the fsa to fire if there is one, and to get the position
-  ;; that it ends at. Returns either that position or nil. 
-  (tr :check-for-polywords word position-before)
-  (set-status :polywords-check position-before)
-  (let ((pw-cfr (initiates-polyword1 word position-before)))
-    ;; This "1" variant calls a "1" variant of capitalized-correspondent
-    ;; which use the position information correctly
-    (when pw-cfr
-      (tr :word-initiates-polyword word position-before)
-      (let ((position-reached
-             (do-polyword-fsa word pw-cfr position-before)))
-        (if position-reached
-          (tr :pw-was-found position-before position-reached)
-          (tr :pw-not-found word position-before))
-        (push-debug `(,position-reached))
-        ;; Check the status -- cf. adjudicate-result-of-word-fsa
-        ;(break "Polyword succeeded at ~a" position-reached)
-        position-reached))))
-
-
-
-(defun simple-eos-check (position-before word)
-  ;; Taken from end-of-source-check without worrying about the
-  ;; forest or other things to do. In the usual scan, this check
-  ;; is part of check-for-[-from-word-after before anything happens.
-  ;;    Has to be ammended in a similar way to the original because
-  ;; there will invariably be something that needs to be tied
-  ;; off
-  (tr :end-of-source-check word position-before)
-  (when (eq word *end-of-source*)
-    ;; This just does the throw up to chart-based-analysis
-    (terminate-chart-level-process)))
-
-
-
-(defun sucessive-sweeps? ()
-  ;; syntactic sugar for a mode detector. Cf. new-forest-protocol?
-  (eq *kind-of-chart-processing-to-do* :successive-sweeps))
-
-
