bb26011172860ffd37e0d0af3e44b393513704ae
ddmcdonald50 <ddmcdonald50@gmail.com>
2014-12-16 00:49:24 +0000

    Restricting interpretation of "in" as a location only when adjoining to something that inherits from physical. Commented out modifier default because presenting case was "... that in certain contexts" and the subordinate conjunction should never have been seen as a option

--- Sparser/code/s/grammar/rules/syntax/syntax-functions.lisp
+++ Sparser/code/s/grammar/rules/syntax/syntax-functions.lisp
@@ -3,7 +3,7 @@
 ;;; 
 ;;;     File:  "syntax-functions"
 ;;;   Module:  grammar/rules/syntax/
-;;;  Version:  October 2014
+;;;  Version:  December 2014
 
 ;; Initiated 10/27/14 as a place to collect the functions associated
 ;; with syntactic rules when they have no better home.
@@ -28,15 +28,16 @@
 ; parent-edge-for-referent
     
 (defun interpret-pp-adjunct-to-np (np pp)
-  (push-debug `(,np ,pp))
   (or (call-compose np pp)
       (let* ((pp-edge (right-edge-for-referent))
              (prep-edge (edge-left-daughter pp-edge))
              (prep-word (edge-left-daughter prep-edge)))
         ;;/// ought to use the referent of the prep but just doing
         ;; this one case for now
-        (if (eq prep-word (word-named "in"))
-          (bind-variable 'location pp ;; daughter rule
-                         np)
-          (bind-variable 'modifier pp np))
+        (when (eq prep-word (word-named "in"))
+          (when (itypep np 'physical)
+            ;; otherwise we don't know what to do with it.
+            (bind-variable 'location pp  np)))
+        ;; Leaving this be while waiting on subcat. fixes
+        ;;(bind-variable 'modifier pp np))
         np)))
