de7f55d03b5e14f8955d1624be9367b683956642
rjbobrow <rjbobrow@gmail.com>
2014-12-20 20:27:32 +0000

    tentative fix to allow interpret-pp-adjunct-to-np to handle bio-context

--- Sparser/code/s/grammar/rules/syntax/syntax-functions.lisp
+++ Sparser/code/s/grammar/rules/syntax/syntax-functions.lisp
@@ -7,6 +7,7 @@
 
 ;; Initiated 10/27/14 as a place to collect the functions associated
 ;; with syntactic rules when they have no better home.
+;; RJB 12/20/2014 tentative fix to allow interpret-pp-adjunct-to-np to handle bio-context
 
 (in-package :sparser)
 
@@ -36,10 +37,14 @@
         ;;/// ought to use the referent of the prep but just doing
         ;; this one case for now
         (when (eq prep-word (word-named "in"))
-          (when (and (itypep np 'physical)
-                     (itypep pp 'location))
+          (cond
+           ((and (itypep np 'physical)
+                  (itypep pp 'location))
             ;; otherwise we don't know what to do with it.
-            (bind-variable 'location pp  np)))
+            (bind-variable 'location pp  np))
+           ((and (itypep np 'bio-entity)
+                 (itypep pp 'bio-context))
+            (bind-variable 'bio-context pp np))))
         ;; Leaving this be while waiting on subcat. fixes
         ;;(bind-variable 'modifier pp np))
         np)))
