d7d8fc29e02850d24b5c422cc9dcef5e9f7fd792
ddmcdonald50 <ddmcdonald50@gmail.com>
2014-12-28 18:28:43 +0000

    Routine for noun + noun

--- Sparser/code/s/grammar/rules/syntax/syntax-functions.lisp
+++ Sparser/code/s/grammar/rules/syntax/syntax-functions.lisp
@@ -1,5 +1,5 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 2014 David D. McDonald  /Users/ddm/ws/R3/trunk/opp/ISI/amr_eng_align.pdf-- all rights reserved
+;;; copyright (c) 2014 David D. McDonald -- all rights reserved
 ;;; 
 ;;;     File:  "syntax-functions"
 ;;;   Module:  grammar/rules/syntax/
@@ -7,12 +7,59 @@
 
 ;; Initiated 10/27/14 as a place to collect the functions associated
 ;; with syntactic rules when they have no better home.
-;; RJB 12/20/2014 tentative fix to allow interpret-pp-adjunct-to-np to handle bio-context
+;; RJB 12/20/2014 tentative fix to allow interpret-pp-adjunct-to-np 
+;; to handle bio-context. 
 
 (in-package :sparser)
 
 
-;; vg+pp
+; (left-edge-for-referent)
+; (right-edge-for-referent)
+; (parent-edge-for-referent)
+
+;;;-------------------
+;;; noun premodifiers
+;;;-------------------
+
+(defun noun-noun-compound (pre-nominal head)
+  ;; goes with (common-noun common-noun) syntactic rule 
+  (when (category-p head)
+    (setq head (make-individual-for-dm&p head)))
+  ;; (push-debug `(,pre-nominal ,head)) (break "check")
+  (if (itypep head 'process) ;; poor man's standing for verb?
+    (then
+     (let ((variable (object-variable head)))
+       (if variable ;; really should check for passivizing
+         (bind-variable variable pre-nominal head)
+         ;; otherwise it's not obvious what to bind
+         (bind-variable 'modifier pre-nominal head))))
+    (else
+     ;; what's the right relationship?
+     ;; Systemics would say they are qualifiers, so perhaps
+     ;; subtype? 
+     (bind-variable 'modifier pre-nominal head))) ;; safe
+  head)
+
+
+
+
+
+;;;-----------------
+;;; VG + Complement
+;;;-----------------
+
+(defun vg-plus-adjective (vg adj)
+  (let ((var (object-variable vg)))
+    (if var
+      (bind-variable var adj vg)
+      (bind-variable 'participant adj vg))
+    vg))
+
+
+;;;---------
+;;; VG + PP
+;;;---------
+
 (defun adjoin-pp-to-vg (vg pp)
   (push-debug `(,vg ,pp))
   (cond
@@ -24,9 +71,9 @@
   vg)
      
 
-; left-edge-for-referent
-; right-edge-for-referent
-; parent-edge-for-referent
+;;;---------
+;;; NP + PP
+;;;---------
     
 (defun interpret-pp-adjunct-to-np (np pp)
   (push-debug `(,np ,pp))
@@ -36,7 +83,8 @@
              (prep-word (edge-left-daughter prep-edge)))
         ;;/// ought to use the referent of the prep but just doing
         ;; this one case for now
-        (when (eq prep-word (word-named "in"))
+        (cond
+         ((eq prep-word (word-named "in"))
           (cond
            ((and (itypep np 'physical)
                   (itypep pp 'location))
@@ -44,7 +92,10 @@
             (bind-variable 'location pp  np))
            ((and (itypep np 'bio-entity)
                  (itypep pp 'bio-context))
-            (bind-variable 'bio-context pp np))))
-        ;; Leaving this be while waiting on subcat. fixes
-        ;;(bind-variable 'modifier pp np))
+            (bind-variable 'bio-context pp np))
+           (t
+            (bind-variable 'modifier pp np))))
+         (t ;;///////// subcat should revise this!
+          ;; Look for a subcategorized preposition on the np
+          (bind-variable 'modifier pp np)))
         np)))
