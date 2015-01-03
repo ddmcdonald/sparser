75e510fa575308a875f6a6bf05013e558389369a
rjbobrow@gmail.com <rjbobrow@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2015-01-03 03:12:21 +0000

    NG chunks that start with a pronoun end with that pronoun -- this needs to by fixed to account for possesive pronouns, of course

--- trunk/Sparser/code/s/grammar/rules/syntax/categories.lisp
+++ trunk/Sparser/code/s/grammar/rules/syntax/categories.lisp
@@ -67,6 +67,7 @@
 ;; 12/20/2014 add thatcomp for nouns 
 ;; 1/1/2015 fix ng-head? to allow "that" as the head of an NG (used mostly for that relative clauses)
 ;; 1/1/2015 block inclusion of initial adjective/modifier when the previous chunk was a copula verb (just check for BE at this time)
+;; 1/2/2015 NG chunks that start with a pronoun end with that pronoun -- this needs to by fixed to account for possesive pronouns, of course
 (in-package :sparser)
 
 
@@ -313,7 +314,7 @@
     CATEGORY::SUPERLATIVE
     CATEGORY::SPATIAL-ADJECTIVE
     CATEGORY::TEMPORAL-ADJECTIVE
-    CATEGORY::VERB+ED
+    ;;CATEGORY::VERB+ED
     CATEGORY::VERB+ING
     CATEGORY::COMMON-NOUN/PLURAL
     CATEGORY::NOUN/VERB-AMBIGUOUS
@@ -353,7 +354,12 @@
 (defmethod ng-compatible? ((name symbol) chunk)
   (declare (special chunk name))
   (or
-   (memq name *ng-internal-categories*)
+   (and
+    (or
+     (memq name *ng-internal-categories*)
+     (eq name 'CATEGORY::VERB+ED)) ;;in fact nothing should follow a pronoun (except a possessive pronoun)
+    (not (and (chunk-edge-list chunk)
+              (eq category::pronoun (edge-form (car (chunk-edge-list chunk)))))))
    (and
     ;; partitive construction e.g. "all of these lines"
     (eq (edge-category (car (chunk-edge-list chunk))) category::quantifier-of)
