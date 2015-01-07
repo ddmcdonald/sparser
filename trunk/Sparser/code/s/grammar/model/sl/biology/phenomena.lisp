810d2bc1dbf03636aee9558204e3e2b69bc71610
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2015-01-07 04:13:14 +0000

    Incorporates all the protein-id pairs that Matt supplied today from Mitre. Adds signaling, N/C terminus, a dumb hack to make np-mod be known (unclear why original definition becomes unfindable)

--- trunk/Sparser/code/s/grammar/model/sl/biology/phenomena.lisp
+++ trunk/Sparser/code/s/grammar/model/sl/biology/phenomena.lisp
@@ -1,15 +1,89 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER COMMON-LISP) -*-
-;;; Copyright (c) 2014 SIFT LLC. All Rights Reserved
+;;; Copyright (c) 2014-2015 SIFT LLC. All Rights Reserved
 ;;;
 ;;;    File: "phenomena"
 ;;;  Module: "grammar/model/sl/biology/
-;;; version: December 2014
+;;; version: January 2015
 
 ;; Initiated 12/28/14 to handle moderately complicated notions
 ;; like cell line and mutation. Conformation and isoform and such
-;; would go here. Reorganizes some items in terms. 
+;; would go here. Reorganizes some items in terms. Incremental
+;; extensions through 1/6/15
 
 (in-package :sparser)
+
+(define-realization-scheme pre-mod premodifier-adds-property 
+  ;; used in quarter -- as are a number of other np patterns
+  :head :common-noun
+  :mapping ((property . modifier-slot)
+            (np-head . :self)
+            (modifier . modifier-v/r)))
+
+;;---------- signalling 
+;; "RAS signalling"
+;; a new mode of Ras activation in which signaling is sustained ...
+(noun signaling
+  :specializes bio-process
+  :obo-id "GO:0023052"  ;; reasonable stand-in
+  :binds ((agent bio-entity)) ;; what's doing the signalling
+  :index (:permanent :key agent)
+  :realization 
+    (:etf pre-mod
+     :noun "signaling"
+     :m agent))
+
+;;;-------------------
+;;; protein terminals
+;;;-------------------
+
+; "located in both N- and C-terminal regions of p100"
+; "via the ASPP2 N-terminus"
+
+; There are two terminals, N and C, and they only make
+; sense in a technical article when they're tied to 
+; a particular protein.
+; Semantically these are 'regions' in that they talk about
+; things happening in these places. "region" is defined in
+; dossiers/location-kinds.lisp as a region-type
+
+(define-category protein-terminus
+  :specializes bio-location
+  :instantiates :self
+  :lemma ((:common-noun "terminal")
+          (:common-noun "terminus")))
+
+;; not clear that we need a proper handling
+;; of the molecule configuration, etc. that
+;; differentiates N from C
+
+(define-category N-terminal ;; amino-terminus
+  :specializes protein-terminus
+  :binds ((protein protein))
+  :index (:permanent :key protein)
+  :realization
+    (:etf (pre-mod of-nominal)
+     :noun ("N-terminal" "N-terminus")
+     :o protein
+     :m protein))
+
+(define-category C-terminal ;; carboxyl-terminus
+  :specializes protein-terminus
+  :binds ((protein protein))
+  :index (:permanent :key protein)
+  :realization
+    (:etf (pre-mod of-nominal)
+     :noun ("C-terminal" "C-terminus")
+     :o protein
+     :m protein))
+
+
+#|The convention for writing peptide sequences is 
+to put the N-terminus on the left and write 
+the sequence from N- to C-terminus. When the 
+protein is translated from messenger RNA, 
+it is created from N-terminus to C-terminus.|#
+
+
 
 ;;;----------
 ;;; Pathways
@@ -23,7 +97,7 @@
 (define-category  pathway
   :specializes bio-entity
   :instantiates :self
-  :mixins (type-marker)
+  :mixins (type-marker biological)
   :binds ((protein-sequence sequence))
   :index (:permanent :key name)
   :lemma (:common-noun "pathway")
@@ -40,8 +114,38 @@
   (:noun "signaling pathway"))
 
 
-;(defmethod define-pathway ((proteins cons))
-;  (let ((sequence (create-sequence 
+(defmacro def-pathway (&rest strings-naming-proteins)
+  `(def-pathway/expr ',strings-naming-proteins))
+
+(defun def-pathway/expr (strings-naming-proteins)
+  (let ( proteins )
+    (dolist (name strings-naming-proteins)
+      (let ((protein (get-protein name)))
+        (unless protein
+          (error "Cannot identify a protein named ~s" name))
+        (push protein proteins)))
+    (setq proteins (nreverse proteins))
+    (let ((sequence (create-sequence proteins))
+          (name (create-slash-separated-string strings-naming-proteins)))
+      (let ((i (find-or-make-individual 'pathway :name name)))
+        ;;/// make a lowercase version?
+        (bind-variable 'protein-sequence sequence i)
+        i))))
+
+;;//// move to string utilities
+(defun create-slash-separated-string (strings)
+  (let ( list )
+    (do ((string (car strings) (car rest))
+         (rest (cdr strings) (cdr rest)))
+        ((null string))
+      (push string list)
+      (unless (null rest)
+        (push "/" list)))
+    (apply #'string-append (nreverse list))))
+
+;(def-pathway "RAS" "Raf" "MAPK")
+;(def-pathway "MEK" "ERK")
+
 
 
 (define-category step
@@ -52,8 +156,6 @@
   :lemma (:common-noun "step")
   :realization (:common-noun name))
 
-(def-bio "MEK/ERK" pathway)
-(def-bio "ERK/MEK" pathway)
 
 
 ;;;-------------------------------
@@ -64,5 +166,17 @@
 ; the HER3 promoter
 ; expression of HER3
 ; inhibition of HER3 transcription
+; "enhances the transcription function of p53"
 
+;;;-----------
+;;; Apoptosis
+;;;-----------
+; "RAS can enhance the apoptotic function of p53"
+; intracellular apoptotic signaling
+; apoptotic signals
+; the apoptosis pathway
 
+(define-category apaptosis ;; aka cell death
+  :specializes bio-process
+  :lemma ((:common-noun "apaptosis")
+          (:adjective "apoptotic")))
--- trunk/Sparser/code/s/grammar/model/sl/biology/proteins.lisp
+++ trunk/Sparser/code/s/grammar/model/sl/biology/proteins.lisp
@@ -17,20 +17,6 @@
 
 ;;--- Convience accesor
 
-(defmethod get-protein ((pname string))
-  ;; conventient syntactic sugar. Motivated by explicitly
-  ;; constructed pathways. 
-  (let ((word (resolve pname)))
-    (unless word "The word ~a isn't defined, so it can't ~
-                  name a protein." pname)
-    (get-protein word)))
-
-(defmethod get-protein ((name word))
-  (find-individual 'protein :name name))
-
-(defmethod get-protein ((name polyword))
-  (find-individual 'protein :name name))
-
 ;;;--------------------------------------------
 ;;; for (some of) the abstract in the proposal
 ;;;--------------------------------------------
@@ -46,6 +32,9 @@
 (def-bio "p100" protein :identifier "PR:000011178")
 
 (def-bio "p52" protein)  ;; :identifier ??
+
+(def-bio "p53" protein
+  :mitre-link "Uniprot:P04637")
 
 (def-bio "NIK" kinase 
   :long "NF-kappaB-inducing kinase" 
@@ -73,9 +62,12 @@
 ;;;------------------------
 
 (def-bio "GTP" small-molecule
-  :identifier "CHEBI:15996")
+  :identifier "CHEBI:15996"
+  :mitre-link "PubChem:6830")
+
 (def-bio "GDP" small-molecule 
-  :identifier "CHEBI:17552")
+  :identifier "CHEBI:17552"
+  :mitre-link "PubChem:8977")
 
 ;; "GO:0006184" "GTP loading and deactivated upon hydrolysis" ?????
 ;;  or "GTP hydrolysis"
@@ -83,19 +75,64 @@
 ;; Guanine "CHEBI:16235"
 ;; nucleotide ""CHEBI:36976"
 (def-bio "GEF" protein :synonyms ("guanine nucleotide exchange factors"))
+
+
+
 (def-bio "GAP" protein :synonyms ("GTPase activating proteins"))
+
 ;; compositional version of the long forms would be better
 ;;/// are these small molecules like GDP or are the larger? -- protein
 ;; And these are families of particulars, not the particulars that are
 ;;  actually doing participating in the reactions
 
+(def-bio "RasGRF1" protein
+  :synonyms ( "RASGRF1" )
+  :mitre-link "Uniprot:Q13972")
 
-;; These two are families, but haven't read about particular members
-(def-bio "RasGEF" protein
-  :synonyms ("Ras-GEF" "Ras GEF")) ;; should do that automagically
- 
+(def-bio "RapGEF2" protein
+  :synonyms ( "RAPGEF2")
+  :mitre-link "Uniprot:Q17RH5")
+
+(def-bio "RapGEF3" protein
+  :synonyms ("RAPGEF3")
+  :mitre-link "Uniprot:O95398")
+
+(def-bio "RapGEF4" protein
+  :synonyms ("RAPGEF4")
+  :mitre-link "Uniprot:Q8WZA2")
+
+(def-bio "RAPGEF5" protein
+  :synonyms ("RAPGEF5")
+  :mitre-link "Uniprot:Q92565")
+
+(def-bio "RapGEF6" protein
+  :synonyms ( "RAPGEF6" )
+  :mitre-link "Uniprot:Q8TEU7")
+
+
+	
+
 (def-bio "RasGAP" protein
   :synonyms ("Ras-GAP" "Ras GAP") );; ditto, or beef up morphology
+
+(def-bio "RAPGEFL1" protein
+  :mitre-link "Uniprot:Q9UHV5")
+
+(def-bio "RasGEF1A" protein
+  :synonyms ("RASGEF1A")
+  :mitre-link "Uniprot:Q8N9B8")
+
+(def-bio "RasGEF1B" protein
+  :synonyms ("RASGEF1B")
+  :mitre-link "Uniprot:Q0VAM2")
+
+(def-bio "RasGEF1C" protein
+  :synonyms ("RASGEF1C")
+  :mitre-link "Uniprot:Q8N431")
+
+
+(def-bio "RasGEF" protein
+  :synonyms ("Ras-GEF" "Ras GEF")) ;; should do that automagically
 
 
 
@@ -113,15 +150,18 @@ filligre may be used to distinguish them, etc.
 
 (def-bio "KRas" protein 
   :synonyms ("k-ras" "K-RAS")
-  :identifier "PR:0000009442") ;; gene is "PR:P01116" 
+  :identifier "PR:0000009442" ;; gene is "PR:P01116" 
+  :mitre-link "Uniprot:P01116")
 
 (def-bio "HRas" protein
   :synonyms ("h-ras" "H-RAS")
-  :identifier "PR:000029705") ;; gene is "PR:P01112")
+  :identifier "PR:000029705" ;; gene is "PR:P01112")
+  :mitre-link "Uniprot:P01112:")
 
 (def-bio "NRas" protein
   :synonyms ("n-ras" "N-RAS")
-  :identifier "PR:000011416") ;; gene is "PR:P01111"
+  :identifier "PR:000011416" ;; gene is "PR:P01111"
+  :mitre-link "Uniprot:P01111")
 
 (def-family "Ras" ;;//// need capitalization hacks
   :members ("KRas" "HRas" "NRas")
@@ -132,19 +172,50 @@ filligre may be used to distinguish them, etc.
 
 ;;--- the RAF family
 
+(def-bio "ARaf" protein
+  :synonyms ("ARAF" "A-Raf")
+  :mitre-link "Uniprot:P10398")
+	
 (def-bio "BRaf" protein 
   :identifier "PR:000004801"
-  :synonyms ("b-raf" "B-RAF"))
+  :synonyms ("b-raf" "B-RAF")
+  :mitre-link "Uniprot:P15056")
 
 (def-bio "CRaf" protein
-  :synonyms ("c-raf" "C-RAF"))
+  :synonyms ("c-raf" "C-RAF")
+  :mitre-link "Uniprot:P04049")
 
 (def-family "Raf" 
-  :members ("BRaf" "CRaf")
+  :members ("ARaf" "BRaf" "CRaf")
   :identifier "RAF" ;;/// maybe use the Mitre choice?
   :synonyms ("RAF"))
 
 
+(def-bio "RasA1" protein
+  :synonyms ("RASA1")
+  :mitre-link "Uniprot:P20936")
+
+(def-bio "RasA2" protein
+  :synonyms ("RASA2")
+  :mitre-link "Uniprot:Q15283")
+
+(def-bio "RASA3" protein
+  :synonyms ("RASA3")
+  :mitre-link "Uniprot:Q14644")
+
+	
+	
+(def-bio "RASAL1" protein
+  :mitre-link "Uniprot:O95294")
+
+(def-bio "RASAL2" protein
+  :mitre-link "Uniprot:Q9UJF2")
+
+(def-bio "RASA4" protein
+  :mitre-link "Uniprot:O43374")
+	
+	
+	
 ;;--- the MAPK family
 ;; These are specific to serine, threonine, and tyrosine
 ;; says the Wikipedia
@@ -152,10 +223,12 @@ filligre may be used to distinguish them, etc.
 (def-bio "ERK1" protein 
   :identifier "PR:000000104"
   :synonyms ("ERK1 kinase" "MAPK3"
-             "erk1" "mapk3"))
+             "erk1" "mapk3")
+  :mitre-link "Uniprot:P27361")
 
 (def-bio "ERK2" protein
-  :synonyms ("erk2" "MAPK1" "mapk1"))
+  :synonyms ("erk2" "MAPK1" "mapk1")
+  :mitre-link "Uniprot:P28482")
 
 ;; and many more: ERK3 (MAPK6) and ERK4 (MAPK4), etc.
 ;; I don't understand the Wikipedia write up well enough
@@ -172,17 +245,146 @@ filligre may be used to distinguish them, etc.
 ;; define the family before the conflicting lexical items MEK1 and MEK2 are defined
 ;;  and then add them as family members later
 
-(def-bio "MEK1" protein)
+(def-bio "MEK1" protein
+  :mitre-link  "Uniprot:Q02750")
 
-(def-bio "MEK2" protein)
+(def-bio "MEK2" protein
+  :mitre-link "Uniprot:P36507")
 
 (def-family "MEK" 
   :members ("MEK1" "MEK2")
   :synonyms ("MEK1/2"))
 
 
+
+;;--- These are in Matt's list, but I don't
+;;  know anything about them
+
+(def-bio "ASPP2" protein
+  :long "Apoptosis-stimulating of p53 protein 2"
+  :synonyms ("Bbp" ;; Bcl2-binding protein
+             "p53BP2") ;; tumor suppressor p53-binding protein 2
+  ;; gene is TP53BP2
+  :mitre-link "Uniprot:Q13625")
+;; "ASPP2 plays a central role in regulation of apoptosis
+;; and cell growth via its interactions. 
+;; ASPP2 regulates TP53 by enhancing the DNA binding 
+;; and transactivation function of TP53 on the promoters
+;; of proapoptotic genes in vivo. 
+;;ASPP2 binds to wild-type p53 
+;; but fails to bind to mutant p53, 
+;; suggesting that ASPP2 may be involved in the ability of 
+;; wild-type p53 to suppress transformation. 
+;; ASPP2 induces apoptosis but no cell cycle arrest.
+
+;; in the text, but not Matt's list
+(def-bio "ASSP1" protein)
+
+(def-bio "ATG5" protein
+  :mitre-link "Uniprot:Q9H1Y0")	
+
+(def-bio "RapGEF1" protein
+  :synonyms ("RAPGEF1")
+  :mitre-link "Uniprot:Q91ZZ2")
+
+(def-bio "RALGPS2" protein
+  :synonyms ("RALGPS2")
+  :mitre-link "Uniprot:Q86X27")
+
+(def-bio "RALGPS1" protein
+  :mitre-link "Uniprot:Q5JS13")
+
+(def-bio "Q12967" protein
+  :mitre-link "Uniprot:RALGDS")
+
+(def-bio "Q9P212" protein
+  :mitre-link "Uniprot:PLCE1")
+
+(def-bio "Q76NI1" protein
+  :mitre-link "Uniprot:KNDC1")
+	
+
+(def-bio "RGL1" protein
+  :mitre-link "Uniprot:Q9NZL6")
+	
+
+(def-bio "RasGRP1" protein
+  :synonyms ("RASGRP1")
+  :mitre-link "Uniprot:O95267")
+
+(def-bio "RasGRP2" protein
+  :synonyms ("RASGRP2")
+  :mitre-link "Uniprot:Q7LDG7")
+
+(def-bio "RasGRP3" protein
+  :synonyms ("RASGRP3")
+  :mitre-link "Uniprot:Q8IV61")
+
+(def-bio "RasGRP4" protein
+  :synonyms ("RASGRP4")
+  :mitre-link "Uniprot:Q8TDF6")
+
+(def-bio "RasGRF2" protein
+  :synonyms ("RASGRF2")
+  :mitre-link "Uniprot:O14827")
+	
+	
+	
+(def-bio "SOS1" protein
+  :mitre-link "Uniprot:Q07889")
+	
+
+(def-bio "RGL2" protein
+  :mitre-link "Uniprot:O15211")
+
+(def-bio "RGL3" protein
+  :mitre-link "Uniprot:Q3MIN7")
+
+(def-bio "RGL4" protein
+  :mitre-link "Uniprot:Q8IZJ4")
+	
+	
+	
+
+(def-bio "NF1" protein
+  :mitre-link "Uniprot:P21359")
+	
+
+(def-bio "IQGAP1" protein
+  :mitre-link "Uniprot:P46940")
+
+(def-bio "IQGAP2" protein
+  :mitre-link "Uniprot:Q13576")
+
+(def-bio "IQGAP3" protein
+  :mitre-link "Uniprot:Q86VI3")
+	
+	
+	
+
+(def-bio "GAPVD1" protein
+  :mitre-link "Uniprot:Q14C86")
+
+(def-bio "DAB2IP" protein
+  :mitre-link "Uniprot:Q7VWQ8")
+
+(def-bio "SOS2" protein
+  :mitre-link "Uniprot:Q07890")
+	
+	
+
+(def-bio "SYNGAP1" protein
+  :mitre-link "Uniprot:Q96PV0")
+	
+
+
+
+;;-------------- well known mutated protein
+
+
 (def-bio "V600EBRAF" protein ;; need to figure out how to represent this variant in the ontology
   :synonyms ("B-RAFV600E" "V600EB-RAF" "BRAFV600E"))
+
 
 
 ;;------------- From here on down it's miscelany that
@@ -195,7 +397,6 @@ filligre may be used to distinguish them, etc.
 (def-bio "cot" protein
   :synonyms ("COT"))
 
-(def-bio "growth factor" protein)
 
 
 (def-bio "mek1dd" protein)
@@ -209,4 +410,6 @@ filligre may be used to distinguish them, etc.
 ;; http://en.wikipedia.org/wiki/Growth_factor
 ;; Again, it's a family, not a particular
 (def-bio "growth factors" protein)
+(def-bio "growth factor" protein)
+
 
