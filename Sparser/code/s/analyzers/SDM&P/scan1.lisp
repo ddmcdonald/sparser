8a740a729249aa663495cb1dd3d6c22161e89c74
ddmcdonald50 <ddmcdonald50@gmail.com>
2015-01-10 19:11:32 +0000

    Added *big-mechanism* gated announcement of sdm segment edges

--- Sparser/code/s/analyzers/SDM&P/scan1.lisp
+++ Sparser/code/s/analyzers/SDM&P/scan1.lisp
@@ -171,7 +171,8 @@ to make any semantic or form edges that the grammar dictates.
 		 (otherwise
 		  (break "New type of object as referent of right-suffix: ~a~%~a"
 			 (type-of referent) referent))))))
-
+      (when *big-mechanism*
+        (format t "~&sdm-span-segment: ~a" edge))
       (tr :sdm-span-segment edge)
       edge)))
    
