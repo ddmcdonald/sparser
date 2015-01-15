850c7eee2344b6688834547620e6345d44f079e2
ddmcdonald50 <ddmcdonald50@gmail.com>
2015-01-15 19:25:11 +0000

    Looks good. Still some questions of how neg flows

--- Sparser/code/s/grammar/rules/syntax/have.lisp
+++ Sparser/code/s/grammar/rules/syntax/have.lisp
@@ -1,9 +1,9 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1993-1996,2013 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1993-1996,2013-2015 David D. McDonald  -- all rights reserved
 ;;; 
 ;;;     File:  "have"
 ;;;   Module:  "grammar;rules:syntax:"
-;;;  Version:  0.3 September 2013
+;;;  Version:  0.4 January 2015
 
 ;; moved from [syntax;aux verbs] 5/7/93 v2.3.  Fleshed out 6/4.
 ;; Added negative contractions 1/11/94.  7/21 switch aux rule to use
@@ -15,6 +15,8 @@
 ;; 0.3 (5/23/13) Disabled the realization for have. Need to make it
 ;;      conditional on SDM&P or the like. (9/18/13) Added variant for
 ;;      have+verb-ed since it can also be verb+passive
+;; 0.4 (1/15/15) Substantial make over of rules for tense/aspect. Gave it
+;;      bindings inspired by what's in possessive. Resistered the variables.
 
 (in-package :sparser)
 
@@ -25,26 +27,21 @@
 
 (define-category  have
   :instantiates  self
-  :specializes   event)
-#|   Turnng this off. The constraints are too loose to use
- in 'everything' mode because the standard heuristics will
- create constituents without referents. Needs to run in a light
- version of DM&P where we have something valid for the referents.
+  :specializes   state
+  :binds ((possessor)
+	  (thing-possessed))
+  :mixins (takes-neg)
+  :index (:temporary :list))
 
-  :binds ((agent)
-          (stuff))
-  :index (:temporary :list)
-  :realization (:tree-family transitive
-                :mapping ((agent . agent)
-                          (patient . stuff)
-                          (s . state)
-                          (np/subject . np)
-                          (vp . have-something)
-                          (np/object . np)
-                          (vg . :self))
-                :special-case-head t
-                ))
-|#
+(register-variable category::have
+ (find-variable-in-category 'possessor 'have)
+ :subject-variable)
+
+(register-variable category::have
+ (find-variable-in-category 'thing-possessed 'have)
+ :object-variable)
+
+
 
 
 (def-cfr have ("have")
@@ -74,36 +71,34 @@
 ;;; aux-hopping rules
 ;;;-------------------
 
-;;---- "have" + "ed"
+;;---- "have" + "ed"  perfect
 
 (def-form-rule (have verb+ed)
   :form vg
   :referent (:head right-edge
-             :subtype perfect
-             ))
+             :function add-tense/aspect left-edge right-edge))
 
-(def-form-rule (have verb+passive)
+(def-form-rule (have verb+passive) 
   :form verb
   :referent (:head right-edge
-             ;;:subtype perfect
-             ))
+             :function add-tense/aspect left-edge right-edge))
+
 
 
 ;;---- tns + not
 
-(def-cfr have (have "not")
+(def-cfr have (have not)
   :form verb
   :referent (:head left-edge
-             ;;:subtype negative
-             ))
+             :bind (negation right-edge)))
 
 
 ;;---- "do(n't)" + have
 
 (def-cfr have (do have)
   :form verb
-  :referent (:head right-edge))
-             ;; ?? how do we promulgate the negative ??
+  :referent (:head right-edge
+             :function absorb-auxiliary left-edge right-edge))
 
 
 
@@ -114,20 +109,17 @@
 (def-cfr have ("haven" apostrophe-t)
   :form verb
   :referent (:head have
-             ;;:subtype negative
-             ))
+             :bind (negation right-edge)))
 
 (def-cfr have ("hasn" apostrophe-t)
   :form verb+s
   :referent (:head have
-             ;;:subtype negative
-             ))
+             :bind (negation right-edge)))
 
 (def-cfr have ("hadn" apostrophe-t)
   :form verb+ed
   :referent (:head have
-             ;;:subtype negative
-             ))
+             :bind (negation right-edge)))
 
 
 ;;--- others
--- Sparser/code/s/grammar/rules/syntax/tense.lisp
+++ Sparser/code/s/grammar/rules/syntax/tense.lisp
@@ -21,7 +21,7 @@
 ;;      properly -- the word by itself does not  have an agent linked to it.
 ;; 0.7 (1/14/15) Converting mixins to regular categories because they're
 ;;      somehow getting interpretations as psi, which is presently outlawed.
-;; 1/14/2015 support for negation and (eventually) other tense/aspect features
+;; 1/14/2015 support for negation and other tense/aspect features
 
 (in-package :sparser)
 
@@ -49,7 +49,9 @@
           (past)
           (progressive)
           (perfect))
-  :mixins (takes-neg))
+  :mixins (takes-neg)
+  :documentation "Stored on the aspect variable of
+  any event, see find-or-make-aspect-vector.")
 
 
 (define-category  tense/aspect
