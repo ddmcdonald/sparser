9aaf0dd8ca5c94e458d004044c65a599ae282b3c
ddmcdonald50 <ddmcdonald50@gmail.com>
2015-01-16 17:44:40 +0000

    Checkpoint in sentence grabber

--- Sparser/code/s/interface/grammar/sweep.lisp
+++ Sparser/code/s/interface/grammar/sweep.lisp
@@ -2,7 +2,7 @@
 ;;; copyright (c) 2015 David D. McDonald -- all rights reserved
 ;;;
 ;;;      File:   "sweep"
-;;;    Module:   "tools:basics"
+;;;    Module:   interface/grammar/
 ;;;   Version:   January 2015
 
 ;; Routines for sweeping down through the structure of Krisp referents.
@@ -71,4 +71,37 @@
            (break "Unexpected type of value of a binding: ~a" value))))))
   
       (reverse objects)))
+
+
+;;;--------------------------------------------------------------------
+;;; collecting sentences & new vocabulary automatically from a passage
+;;;--------------------------------------------------------------------
+
+
+(defvar *newly-found-unknown-words* nil
+  "Accumulator for add-new-word-to-catalog")
+
+; make-word/all-properties/or-primed =>
+;   setup-unknown-word-by-default
+;   assign-morph-brackets-to-unknown-word
+;   unpack-primed-word
+;   suitable-for-and-in-OBO => setup-word-denoting-an-OBO
+(defun add-new-word-to-catalog (word source)
+  (declare (ignore source)) ;; Comlex vs. morphology vs. ...
+  (when *sweep-for-sentences*
+    (push word *newly-found-unknown-words*)))
+  
+
+; (setq *sweep-for-sentences* t)
+
+(defun sentence-sweep (sentence &optional (stream *standard-output*))
+  (declare (ignore stream))
+  (let* ((start-pos (starts-at-pos sentence))
+         (end-pos (ends-at-pos sentence))
+         (start-char (pos-character-index start-pos))
+         (end-char (1+ ;; include period
+                    (pos-character-index end-pos))))
+    (let ((string
+           (extract-string-from-char-buffers start-char end-char)))
+      (print string stream))))
 
