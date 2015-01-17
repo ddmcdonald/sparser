327f36dcf8c35045b619f64ab712f714dce81355
ddmcdonald50 <ddmcdonald50@gmail.com>
2015-01-17 02:40:32 +0000

    checkpoint

--- Sparser/code/s/interface/grammar/sweep.lisp
+++ Sparser/code/s/interface/grammar/sweep.lisp
@@ -10,7 +10,10 @@
 
 (in-package :sparser)
 
-
+(defvar *individuals-seen* (make-hash-table)
+  "There are some recursive references in the binding used for
+  prepositional phrases and possibly other things. We use this
+  to avoid an infinite loop while collecting.")
 
 (defmethod collect-model ((n number))
   ;; For debugging
@@ -28,49 +31,55 @@
 (defmethod collect-model ((c category)) nil) ;;`(,c))
 ;; anything else?
 
+(defun initalize-model-collection ()
+  (clrhash *individuals-seen*))
 
 
 (defmethod collect-model ((i individual))
-  (let ((bindings (indiv-binds i))
-        objects )
-    ;; Had been restricting the recursion to types with
-    ;; a subject variable: (subject-variable type), 
-    ;; but that's missing interesting noun phrase referents.
-    (push i objects)
-    (dolist (b bindings)
-      (let* ((var (binding-variable b))
-             (var-name (var-name var))
-             (value (binding-value b)))
-        (unless (or (eq var-name 'category)
-                    (typep value 'mixin-category)) ;; has-determiner
-          (typecase value
-            (number)
-            (list (push (list var-name (list :list value))
-                         objects))
-            (symbol)
-            (string)
-            (word)
-            (polyword)
-            (category)
-            (individual 
-             (cond
-              ((itypep value 'unclear) nil)
-              ((itypep value 'prepositional-phrase)
-               (dolist (bb (indiv-binds value))
-                 (when (eq (var-name (binding-variable bb)) 'pobj)
-                   (push (list var-name (collect-model (binding-value bb)))
-                         objects))))
-              ((itypep value 'bio-family)
-               (push (list var-name value)
-                     objects))
-              (t
-               (push (list var-name (collect-model value))
-                     objects))))
-          (otherwise
-           (push-debug `(,value ,b ,i))
-           (break "Unexpected type of value of a binding: ~a" value))))))
-  
-      (reverse objects)))
+  (unless (gethash i *individuals-seen*)
+    (let ((bindings (indiv-binds i))
+          objects )
+      (push (if (itypep i 'number)
+              (value-of 'value i)
+              i)
+            objects)
+      (setf (gethash i *individuals-seen*) t)
+      (dolist (b bindings)
+        (let* ((var (binding-variable b))
+               (var-name (var-name var))
+               (value (binding-value b)))
+          (unless (or (eq var-name 'category)
+                      (typep value 'mixin-category)) ;; has-determiner
+            (typecase value
+              (individual 
+               (cond
+                ((itypep value 'unclear) nil)
+                ((itypep value 'prepositional-phrase)
+                 (dolist (bb (indiv-binds value))
+                   (when (eq (var-name (binding-variable bb)) 'pobj)
+                     (push (list var-name (collect-model (binding-value bb)))
+                           objects))))
+                ((itypep value 'bio-family)
+                 (push (list var-name value)
+                       objects))
+                (t
+                 (push (list var-name (collect-model value))
+                       objects))))
+              (number)
+              (list (push (list var-name (list :list value))
+                          objects))
+              (category ;; is this right?
+               (push `(,var-name ,value) objects))
+              (symbol)
+              (string)
+              (word)
+              (polyword)
+            
+              (otherwise
+               (push-debug `(,value ,b ,i))
+               (break "Unexpected type of value of a binding: ~a" value))))))
+      
+      (reverse objects))))
 
 
 ;;;--------------------------------------------------------------------
@@ -91,8 +100,9 @@
   (when *sweep-for-sentences*
     (push word *newly-found-unknown-words*)))
   
-
+; (f "/Users/ddm/ws/R3/ws/Mitre December texts/passage 1.txt")
 ; (setq *sweep-for-sentences* t)
+; (setq *sweep-for-sentences* nil)
 
 (defun sentence-sweep (sentence &optional (stream *standard-output*))
   (declare (ignore stream))
