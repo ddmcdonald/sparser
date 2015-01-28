cf9bf5ae7013cf8cba041b2c6307bfb033e968f0
ddmcdonald50 <ddmcdonald50@gmail.com>
2015-01-28 14:10:26 +0000

    File of sentence-corpus snapshots

--- Sparser/code/s/grammar/tests/citations/code/loader.lisp
+++ Sparser/code/s/grammar/tests/citations/code/loader.lisp
@@ -1,16 +1,18 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1993,1994  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1993-1994,2015 David D. McDonald  -- all rights reserved
 ;;;
 ;;;     File:  "loader"
 ;;;   Module:  "grammar;tests:citations:code:"
-;;;  version:  March 1994
+;;;  version:  January 2015
 
 ;; initiated 11/2/93 v2.3.  3/18/94 moved menu to [menus:]
+;; 1/25/15 added [treetop-records]
 
 (in-package :sparser)
 
 (lload "citation-code;object")
 (lload "citation-code;test")
+(lload "citation-code;treetop-records")
 (lload "citation-code;input-output")
 (lload "citation-code;construction")
 (lload "citation-code;batches")
