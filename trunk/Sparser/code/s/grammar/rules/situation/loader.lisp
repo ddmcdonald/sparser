614b07570d75aee93fb73e1540da4b82d77f22c0
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2015-02-03 15:09:54 +0000

    New file for finding and recording the subject and eventually other text structuring relations

--- trunk/Sparser/code/s/grammar/rules/situation/loader.lisp
+++ trunk/Sparser/code/s/grammar/rules/situation/loader.lisp
@@ -1,15 +1,17 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 2013-2014 David D. McDonald  -- all rights reserved
+;;; copyright (c) 2013-2015 David D. McDonald  -- all rights reserved
 ;;; This file is part of the SIFT-Brandeis C3 project
 ;;;
 ;;;     File:  "loader"
 ;;;            grammar/rules/situation/
-;;;  version:  June 2014
+;;;  version:  January 2015
 
 ;; Initiated 10/9/13. Added rules 3/20/14. Moved variables and
 ;; state to objects/situation/ 3/26/14. Moved compose to master 
-;; loader 6/4/14
+;; loader 6/4/14. Added text-structure 1/29/15
 
 (in-package :sparser)
 
-(gload "sit-rules;rules")
+(when *ISR*
+  (gload "sit-rules;rules"))
+(gload "sit-rules;text-structure")
