934766719aedbb56dd26c11b3432928923a2cefd
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2015-02-12 20:10:27 +0000

    Cleanup as part of writing notes

--- trunk/Sparser/code/s/grammar/rules/tree-families/families1.lisp
+++ trunk/Sparser/code/s/grammar/rules/tree-families/families1.lisp
@@ -1,15 +1,17 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 2014 David D. McDonald  -- all rights reserved
+;;; copyright (c) 2014-2015 David D. McDonald  -- all rights reserved
 ;;;
 ;;;     File:  "families"
 ;;;   Module:  "grammar;rules:tree-families:"
-;;;  version:  1.0 December 2014
+;;;  version:  1.1 February 2015
 
 ;; Initiated 2/6/14. Augment the realization options of shortcuts
 ;; 9/19/14 Supplanted original notion with a new one that seems
 ;; simpler, since it's based on copying existing mapping.
 ;; 1.0 (12/11/14) Using this clean copy to facilitate changing
 ;;   the mapping terms to fit the new, simpler source mappings.
+;; 1.1 (2/9/15) Removed the original code. Pulled in extensions
+;;   accidentally stranded in earlier code. 
 
 (in-package :sparser)
 
@@ -88,7 +90,10 @@
 #| In these mappings, the labels on the left come from
 the ETF and the ones on the right from the mapping in
 the short cuts. 
-|#
+
+Use (categories-using-etf <name>) to get examples
+when contemplating using a new tree family
+|# 
 
 (define-realization-scheme sv intransitive
   :head :verb
@@ -122,15 +127,7 @@ the short cuts.
             (np/object . theme-v/r)
             (by-pp . by-v/r)
             (result-type . :self)))
-
-(define-realization-scheme np-by np-with-by-phrase
-  :head :common-noun
-  :mapping ((agent . subj-slot)
-            (np/subject . subj-v/r) ;; by-subj
-            (np . :self)
-            (by-pp . by-v/r)))
             
-
 (define-realization-scheme svcomp that-complement
   :head :verb
   :mapping ((s . :self)
@@ -142,6 +139,12 @@ the short cuts.
             (agent . subj-slot)
             (theme . comp-slot)))
 
+(define-realization-scheme pre-mod premodifier-adds-property 
+  ;; used in quarter: 2004 <quarter>
+  :head :common-noun
+  :mapping ((property . modifier-slot)
+            (np-head . :self)
+            (modifier . modifier-v/r)))
 
 (define-realization-scheme of-nominal empty-head-of-complement
   ;; used in old svo/nominal
@@ -152,121 +155,56 @@ the short cuts.
             (complement . theme-v/r)
             (np . :self)))
 
-                        
+(define-realization-scheme np-by np-with-by-phrase
+  :head :common-noun
+  :mapping ((agent . subj-slot)
+            (np/subject . subj-v/r) ;; by-subj
+            (np . :self)
+            (by-pp . by-v/r)))
 
 
+     
+#|  Mine this
+(define-category  quarter
+  :specializes time
+  :instantiates self  ;; ?? generalization to all these fiscal accounting periods?
+  :binds ((year . year)
+          (position-in-year . ordinal)
+          (end-date . end-date)
+          (company . company))
+  :realization ((:tree-family designated-instance-of-set   ;; "first quarter"
+                 :mapping ((designator . position-in-year)
+                           (np-head . :self)
+                           (qualifier . ordinal)
+                           (np . :self))
+                 :common-noun "quarter")
 
+                (:tree-family  member-of ;; "___ of 1998"
+                 :mapping ((identifier . year)
+                           (complement . year)
+                           (np . :self)))
 
-;;------------------ February version never adequately 
-;;                   thought through. Here to the end
-;;;-----------
-;;; structure
-;;;-----------
+                (:tree-family  modifier-creates-subtype  ;; "fiscal ___"
+                 :mapping ((n-bar . :self)
+                           (subtyping-modifier . fiscal)
+                           (np-head . :self)))
 
-(defvar *name-to-family-of-trees* (make-hash-table))
+                (:tree-family  np-participle  ;; "___ ending June 26, 2004"
+                 :mapping ((np . :self)
+                           (participle . end-date)
+                           (participle-field . end-date)))
 
-(defun get-family-of-trees (name)
-  (gethash name *name-to-family-of-trees*))
+                (:tree-family  premodifier-adds-property  ;; "2004 ___"
+                 :mapping ((modifier . year)
+                           (np-head . :self)
+                           (property . year)))
 
-
-(defclass family-of-trees (named-object)
-  ((root-labels :accessor tf-roots
-    :documentation "The labels on the root nodes of trees,
-      usually maximal categories that have an obvious correspondence
-      in the upper ontology. No duplications. 
-      Most important category first. It dictates the default
-      supercategory of the category generator.")
-   (word-parameters :accessor tf-word-parameters
-    :documentation "The substitution parameters for words
-      in the mappings.")
-   (parameter-default-pairs :accessor tf-parameter-pairs
-    :documentation "An alist of the parameters with their defaults,
-      i.e. the names of the referential categories in the mappings.")
-   (label-default-pairs :accessor tf-label-pairs
-    :documentation "An alist of the labels with their defaults,
-      i.e. the names of the form categories in the mappings.") ; 
-   (family-mapping-fn-pairs :accessor family-mapping-fn-pairs
-    :documentation "For now just the raw etf and mapping plist")
-)
-  (:documentation "Fundamentally just a list of rspecs: tree-family
-    and mapping combination."))
-
-
-(defmacro define-family-of-trees (name  &rest keys)
-  `(define-family-of-trees/expr ',name ',keys))
-
-(defun define-family-of-trees/expr (name keys)
-  (apply #'construct-family-of-trees name keys))
-;;(quote-every-second-one keys)
-
-(defun construct-family-of-trees (name
-                                  &key words
-                                       parameter-defaults
-                                       ((:families eft-and-mapping)))                                       
-  (let ((family (or (get-family-of-trees name)
-                    (let ((new (make-instance 'family-of-trees
-                                 :name name)))
-                      (setf (gethash name *name-to-family-of-trees*) new)
-                      new))))    
-    ;;/// parameter validity checking -- e.g. that there are words
-    (setf (tf-word-parameters family) words)
-    (let ( roots  parameter-pairs  label-pairs family-pairs )
-      (dolist (list eft-and-mapping)
-        (let* ((etf-name (car list))
-               (etf (exploded-tree-family-named etf-name))
-               (mapping (cdr list)))
-          (unless etf
-            (error "There is no ETF named ~a" etf-name))
-          (let ((parameters (etf-parameters etf))
-                (labels (etf-labels etf)))
-            (pushnew (first labels) roots)
-            (dolist (pair mapping)
-              (cond
-               ((memq (car pair) parameters)
-                (push pair parameter-pairs))
-               ((memq (car pair) labels)
-                (push pair label-pairs))
-               (t (push-debug `(,pair ,etf))
-                  (error "mapping pair doesn't make sense: ~a" pair)))))
-          (push `(,etf ,@mapping) family-pairs)))
-      (setf (tf-roots family) (nreverse roots))
-      (setf (tf-parameter-pairs family) (nreverse parameter-pairs))
-      (setf (tf-label-pairs family) (nreverse label-pairs))
-      (setf (family-mapping-fn-pairs family) (nreverse family-pairs)))
-    family))
-
-
-;;;-------
-;;; cases 
-;;;-------
-
-(define-family-of-trees intransitive
-  :words (main-verb)
-  :families ((intransitive
-	      (s . event)
-              (vp . event)
-              (np/subject . individual)
-              (agent . subject))))
-
-#|
-(define-family-of-trees transitive/pasive/nominalized (s np)
-  :words (main-verb common-noun)
-  :parameters (agent patient  of-item result-type)
-  :labels (s vp vg np/subject np/object  np base-np complement)
-  :families ((:tree-family transitive/passive
-              :mapping ((agent . by-whom)
-                        (patient . who)
-                        (s . :self)
-                        (vp . :self)
-                        (vg . :self)
-                        (np/subject . person)
-                        (np/object . person))
-              :main-verb "assassinate")
-             (:tree-family empty-head-of-complement
-              :mapping ((result-type . :self)
-                        (of-item . who)
-                        (base-np . :self)
-                        (complement . person)
-                        (np . :self))
-              :common-noun "assassination")))
+                (:tree-family  possessive/bind-slot   ;; "its ___"
+                 :mapping ((pos-slot . company)
+                           (np . :self)
+                           (possessive . company-s)
+                           (np-head . :self)))))
 |#
+
+                   
+
