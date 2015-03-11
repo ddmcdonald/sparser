29f722f3cef663e1aea7fa753d607c4726f433e1
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2015-03-11 13:50:12 +0000

    First steps in Krisp Ras-switch model

--- trunk/Sparser/code/s/grammar/model/sl/biology/switches.lisp
+++ trunk/Sparser/code/s/grammar/model/sl/biology/switches.lisp
@@ -1,15 +1,47 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER COMMON-LISP) -*-
-;;; Copyright (c) 2014 SIFT LLC. All Rights Reserved
+;;; Copyright (c) 2014-2015 SIFT LLC. All Rights Reserved
 ;;;
 ;;;    File: "switches"
 ;;;  Module: "grammar/model/sl/biology/
 ;;; version: November 2014
 
-;; Initiated 11/12/14
+;; Initiated 11/12/14. Starting giving it content 3/10/15
 
 (in-package :sparser)
 
+
+;; The Ras switch is a two state system / has two alternative states
+;; Like other two-state systems (a lightbulb), 
+;;   one of them is 'on', the other is 'off'
 ;; a switch has (usuall) two states
+; in terms:  (noun "state" :super bio-entity) /// of a system
+
+;; Somethis is in this state, e.g. Ras, the traffc light
+
+
+(define-category switch-state
+  :specializes state
+  :binds ((name :primitive word)))
+
+(defun define-switch-state (name)
+  (find-or-make-individual 'switch-state :name name))
+
+(defparameter *on-state* (define-switch-state "on"))
+(defparameter *off-state* (define-switch-state "off"))
+
+  
+;; "active" is in modifiers
+;; "inactive" is in terms
+
+
+;; Individual Ras molecules are in a state
+(define-category protein-in-a-state
+  :specializes abstract
+  :binds ((protein protein)
+          (state switch-state)))
+
+
 
 
 (np-head "switch") ;;/// trivial standin
+
