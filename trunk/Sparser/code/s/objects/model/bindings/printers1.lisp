4d55aa1955c25557827075aa8f12eca9cd713059
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2015-03-12 15:45:07 +0000

    Wrote binding-short-string and helpers as part of describe-individual. Princ routines print immediately which made them too hard to adapt.

--- trunk/Sparser/code/s/objects/model/bindings/printers1.lisp
+++ trunk/Sparser/code/s/objects/model/bindings/printers1.lisp
@@ -1,16 +1,19 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1991,1992,1993,1994 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1991-1994,2015 David D. McDonald  -- all rights reserved
 ;;;
 ;;;     File:  "printers"
 ;;;   Module:  "objects;model:bindings:"
-;;;  version:  0.2 August 1994
+;;;  version:  0.2 March 2015
 
 ;; initiated 11/30/91 v2.1
 ;; (7/21/92 v2.3) tweeked to fit new regime, 9/3 tweeked the way it looks
 ;; 0.1 (6/16/93) revised it to be shorted by leaving out the body
 ;;     (3/10/94) added Princ-binding and Princ-bound-in
-;;     (8/8) added case for a deallocated binding
-;; 0.2 (8/10) added reference to id
+;;     (8/8)/94 added case for a deallocated binding
+;; 0.2 (8/10/94) added reference to id
+;;     (3/12/15) Wrote binding-short-string and helpers as part
+;;      of describe-individual.Princ routines print immediately
+;;      which made them too hard to adapt. 
 
 (in-package :sparser)
 
@@ -59,6 +62,45 @@
                              (+ indentation
                                 length-of-the-variable
                                 3 ))))
+
+(defmethod binding-short-string ((b binding))
+  (let ((variable  (binding-variable b))
+        (value (binding-value b)))
+    (cond
+     ((deallocated-binding? b)
+      (format nil "#<~A deallocated binding>" (binding-id b)))
+     (t
+      (format nil "#<~a ~a: ~a>"
+              (binding-id b)
+              (string-downcase (var-name variable))
+              (binding-value-short-string value))))))
+
+(defmethod binding-value-short-string ((i individual))
+  (format nil "~a" i))
+
+(defmethod binding-value-short-string ((c category))
+  (format nil "~a" c))
+
+(defmethod binding-value-short-string ((w word))
+  (format nil "~s" (word-pname w)))
+
+(defmethod binding-value-short-string ((pw polyword))
+  (format nil "~s" (pw-pname w)))
+
+(defmethod binding-value-short-string ((v T))
+  (format nil "~a" v))
+
+(defmethod bound-in-short-string ((b binding))
+  (let ((variable  (binding-variable b))
+        (body (binding-body b)))
+    (cond
+     ((deallocated-binding? b)
+      (format nil "#<~A deallocated binding>" (binding-id b)))
+     (t
+      (format nil "#<~a :~a of ~a>"
+              (binding-id b)
+              (string-downcase (var-name variable))
+              body)))))
 
 
 (defun princ-bound-in (b  &optional (stream *standard-output*)
