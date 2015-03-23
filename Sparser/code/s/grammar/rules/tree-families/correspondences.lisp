a688b3661df1a0c32e321256416b65eba91074fa
rjbobrow <rjbobrow@gmail.com>
2015-03-23 16:11:16 +0000

    ;; 3/21/2015 Don't load Mumble when using SBCL -- fix when this becomes necessary

--- Sparser/code/s/grammar/rules/tree-families/correspondences.lisp
+++ Sparser/code/s/grammar/rules/tree-families/correspondences.lisp
@@ -14,6 +14,7 @@
 ;;     (3/22/11) Starting to populate it. Continuing through 4/8/11.
 ;; 0.3 (4/22/12) The defvar doesn't cleanly update in Clozure so switched
 ;;      to hashtable on the etf.
+;; 3/21/2015 Don't load Mumble when using SBCL -- fix when this becomes necessary
 
 (in-package :sparser)
 
@@ -34,32 +35,34 @@ When we need adjunction, it's
 
 |#
 
-(defvar *etf-to-mumble-mapping* (make-hash-table) 
- "Ulimately has one entry for every ETF in Sparser's grammar.
- The format is ETF name in the car. Mumble's data is in the
- cdr, starting with the resource (notice the use of a symbol
- in the :mumble package) and an alist of the binary arguments
- from Sparser's side with their correspondences for the phrase
- on Mumble's side.")
+(unless CL-USER::*NO-MUMBLE*
+  (defvar *etf-to-mumble-mapping* (make-hash-table) 
+    "Ulimately has one entry for every ETF in Sparser's grammar.
+    The format is ETF name in the car. Mumble's data is in the
+    cdr, starting with the resource (notice the use of a symbol
+    in the :mumble package) and an alist of the binary arguments
+    from Sparser's side with their correspondences for the phrase
+    on Mumble's side.")
+  
+  
+  (defun add-etf-mapping (mapping-data)
+    (let* ((etf-name (car mapping-data))
+           (data (cdr mapping-data))
+           (etf (exploded-tree-family-named etf-name)))
+      (unless etf
+        (error "Can't find an etf named ~a" etf-name))
+      (setf (gethash etf *etf-to-mumble-mapping*) data)
+      etf))
+  
+  (defmethod has-mumble-mapping? ((etf-name symbol))
+    (let ((etf (exploded-tree-family-named etf-name)))
+      (unless etf
+        (error "Can't find an etf named ~a" etf-name))
+      (has-mumble-mapping? etf)))
+  
+  (defmethod has-mumble-mapping? ((etf exploded-tree-family))
+    (gethash etf *etf-to-mumble-mapping*))
 
-
-(defun add-etf-mapping (mapping-data)
-  (let* ((etf-name (car mapping-data))
-         (data (cdr mapping-data))
-         (etf (exploded-tree-family-named etf-name)))
-    (unless etf
-      (error "Can't find an etf named ~a" etf-name))
-    (setf (gethash etf *etf-to-mumble-mapping*) data)
-    etf))
-
-(defmethod has-mumble-mapping? ((etf-name symbol))
-  (let ((etf (exploded-tree-family-named etf-name)))
-    (unless etf
-      (error "Can't find an etf named ~a" etf-name))
-    (has-mumble-mapping? etf)))
-
-(defmethod has-mumble-mapping? ((etf exploded-tree-family))
-  (gethash etf *etf-to-mumble-mapping*))
 
 
 ;; probably needs a wrapper
@@ -87,7 +90,7 @@ When we need adjunction, it's
       (arg . mumble::s)))
 
     ))
-
+)
  
 ;;;------------------------
 ;;; single words to phrase
@@ -100,30 +103,32 @@ When we need adjunction, it's
  the point of view of mumble::realize-dtn."))
 ;; return a dtn
 
-(defmethod wrap-in-resource ((dotted-pair cons) ;; from apply-rnode
-                             (dtn mumble::derivation-tree-node))
-  (let ((keyword (car dotted-pair))
-        (word (cdr dotted-pair)))
-    (unless (word-p word)
-      (push-debug `(,dotted-pair ,keyword ,word))
-      (error "Threading probem. Initial resource is weird"))
-    (let* ((phrase (select-resource keyword))
-           (m-word (mumble::find-or-make-word word))
-           (parameter (car (mumble::parameters-to-phrase phrase))))
-      ;;(push-debug `(,dtn ,parameter ,m-word ,phrase)) (break "wrap")
-      (setf (mumble::resource dtn) phrase)
-      (mumble::make-complement-node parameter m-word dtn)
-      dtn)))
-
-(defmethod select-resource ((keyword symbol))
-  (case keyword
-    (:common-noun
-     ;;/// whole big choice set goes here, Criteria are semantic
-     ;; and intentional, not to mention subsequence reference which
-     ;; demands a whole level to itself (Zo model maybe??)
-     (mumble-phrase 'common-noun))
-    (otherwise
-     (break "New case: ~a" keyword))))
+(unless CL-USER::*NO-MUMBLE*
+  (defmethod wrap-in-resource ((dotted-pair cons) ;; from apply-rnode
+                               (dtn mumble::derivation-tree-node))
+    (let ((keyword (car dotted-pair))
+          (word (cdr dotted-pair)))
+      (unless (word-p word)
+        (push-debug `(,dotted-pair ,keyword ,word))
+        (error "Threading probem. Initial resource is weird"))
+      (let* ((phrase (select-resource keyword))
+             (m-word (mumble::find-or-make-word word))
+             (parameter (car (mumble::parameters-to-phrase phrase))))
+        ;;(push-debug `(,dtn ,parameter ,m-word ,phrase)) (break "wrap")
+        (setf (mumble::resource dtn) phrase)
+        (mumble::make-complement-node parameter m-word dtn)
+        dtn)))
+  
+  (defmethod select-resource ((keyword symbol))
+    (case keyword
+      (:common-noun
+       ;;/// whole big choice set goes here, Criteria are semantic
+       ;; and intentional, not to mention subsequence reference which
+       ;; demands a whole level to itself (Zo model maybe??)
+       (mumble-phrase 'common-noun))
+      (otherwise
+       (break "New case: ~a" keyword))))
+  )
 
 
 
@@ -136,80 +141,81 @@ When we need adjunction, it's
 ;;; types
 ;;;------
 
-(defobject bidir-mapping () ;; useful superc?
-  ((sparser-resource)
-   (mumble-resource)))
-
-(defvar *sparser-to-mumble* (make-hash-table) ;; how big?
-  "Given a sparser object, returns its sparser-to-mumble-mapping")
-
-(defun set-bidir-mapping (sparser-item mumble-item)
-  (let ((bidir (make-instance 'bidir-mapping
-                              :sparser-resource sparser-item
-                              :mumble-resource mumble-item)))
-    (setf (gethash sparser-item *sparser-to-mumble*) bidir)
-    bidir))
-
-(defun get-mumble-side-of-bidir (sparser-item)
-  (gethash sparser-item *sparser-to-mumble*))
-
+(unless CL-USER::*NO-MUMBLE*
+  (defobject bidir-mapping () ;; useful superc?
+    ((sparser-resource)
+     (mumble-resource)))
+  
+  (defvar *sparser-to-mumble* (make-hash-table) ;; how big?
+    "Given a sparser object, returns its sparser-to-mumble-mapping")
+  
+  (defun set-bidir-mapping (sparser-item mumble-item)
+    (let ((bidir (make-instance 'bidir-mapping
+                   :sparser-resource sparser-item
+                   :mumble-resource mumble-item)))
+      (setf (gethash sparser-item *sparser-to-mumble*) bidir)
+      bidir))
+  
+  (defun get-mumble-side-of-bidir (sparser-item)
+    (gethash sparser-item *sparser-to-mumble*))
+  )
 
 
 
 ;;;-------------
 ;;; entry point
 ;;;-------------
-
-(defun rule-descriptor-to-nlg-resource (rule-descriptor)
-  (push-debug `(,rule-descriptor))
-  (typecase rule-descriptor
-    (cons
-     (unless (memq :head rule-descriptor)
-       (error "Expected cons rule-descriptor to start with :head:~
-             ~%   ~a" rule-descriptor))
-     (let ((keyword (cadr rule-descriptor)))
-       (case keyword
-	 (:common-noun
-	  ;; There are several minimal NP phrases to choose from.
-	  ;; This choice is somewhat arbitrary and not deliberated.
-	  (mumble::provide-phrase-and-data 'mumble::common-noun))
-	 (otherwise
-	  (error "New keyword in rule-descriptor: ~a~%  ~a"
-		 keyword rule-descriptor)))))
-    (cfr 
-     (cond
-       ((form-rule? rule-descriptor)
-	(let ((ap (get-mumble-side-of-bidir rule-descriptor)))
-	  (unless ap
-	    (error "No mapping to a Mumble resource for~%   ~a"
-		   rule-descriptor))
-	  (values ap))) ;; not parameterized or entailing feature
-       (t (error "New case of a context-free-rule as the ~
-                  rule-descriptor:~%   ~a" rule-descriptor))))
-    (otherwise
-     (error "Unexpected type of rule-descriptor: ~a~%  ~a"
-	    (type-of rule-descriptor) rule-descriptor))))
-
-
-;;;------------------------------------------------------------
-;;; Linking form rule to their corresponding attachment points
-;;;------------------------------------------------------------
-
-(defmacro def-form-rule-to-AP (rhs ap-name)
-  `(def-form-rule-to-AP/expr ',rhs ',ap-name))
-
-(defun def-form-rule-to-AP/expr (rhs ap-name)
-  (let ((category-1 (resolve (car rhs)))
-	(category-2 (resolve (cadr rhs)))
-	(ap (mumble::attachment-point-named 
-	     (intern ap-name (find-package :mumble)))))
-    (unless ap (error "There is no attachment point named ~a" ap-name))
-    (unless category-1 (error "There is no category named ~a" (car rhs)))
-    (unless category-2 (error "There is no category named ~a" (cadr rhs)))
-    (let ((rule (lookup-syntactic-rule `(,category-1 ,category-2))))
-      (unless rule (error "There is no form rule for ~a" rhs))
-      (set-bidir-mapping rule ap))))
-
+(unless CL-USER::*NO-MUMBLE*
+  (defun rule-descriptor-to-nlg-resource (rule-descriptor)
+    (push-debug `(,rule-descriptor))
+    (typecase rule-descriptor
+      (cons
+       (unless (memq :head rule-descriptor)
+         (error "Expected cons rule-descriptor to start with :head:~
+    ~%   ~a" rule-descriptor))
+       (let ((keyword (cadr rule-descriptor)))
+         (case keyword
+           (:common-noun
+            ;; There are several minimal NP phrases to choose from.
+            ;; This choice is somewhat arbitrary and not deliberated.
+            (mumble::provide-phrase-and-data 'mumble::common-noun))
+           (otherwise
+            (error "New keyword in rule-descriptor: ~a~%  ~a"
+                   keyword rule-descriptor)))))
+      (cfr 
+       (cond
+        ((form-rule? rule-descriptor)
+         (let ((ap (get-mumble-side-of-bidir rule-descriptor)))
+           (unless ap
+             (error "No mapping to a Mumble resource for~%   ~a"
+                    rule-descriptor))
+           (values ap))) ;; not parameterized or entailing feature
+        (t (error "New case of a context-free-rule as the ~
+             rule-descriptor:~%   ~a" rule-descriptor))))
+      (otherwise
+       (error "Unexpected type of rule-descriptor: ~a~%  ~a"
+              (type-of rule-descriptor) rule-descriptor))))
+  
+  
+  ;;;------------------------------------------------------------
+  ;;; Linking form rule to their corresponding attachment points
+  ;;;------------------------------------------------------------
+  
+  (defmacro def-form-rule-to-AP (rhs ap-name)
+    `(def-form-rule-to-AP/expr ',rhs ',ap-name))
+  
+  (defun def-form-rule-to-AP/expr (rhs ap-name)
+    (let ((category-1 (resolve (car rhs)))
+          (category-2 (resolve (cadr rhs)))
+          (ap (mumble::attachment-point-named 
+               (intern ap-name (find-package :mumble)))))
+      (unless ap (error "There is no attachment point named ~a" ap-name))
+      (unless category-1 (error "There is no category named ~a" (car rhs)))
+      (unless category-2 (error "There is no category named ~a" (cadr rhs)))
+      (let ((rule (lookup-syntactic-rule `(,category-1 ,category-2))))
+        (unless rule (error "There is no form rule for ~a" rhs))
+        (set-bidir-mapping rule ap))))
+  )
 
 ;;--- cases
 
