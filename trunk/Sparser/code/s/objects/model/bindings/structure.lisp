3c73a9a1761417114bc7d3cd750ab11e21c3414a
rjbobrow@gmail.com <rjbobrow@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2015-03-23 14:40:40 +0000

    ;; 3/21/2015 SBCL-based tuning work -- trying to find out where we are over-using variable binding to ;;  get indexing -- commented out over-write of binding-body do keep track of which variables are ;;  drastically over-used (such as the variable category) to see about ;;  how to do the job better
    CURRENT RESULT IS TO REMOVE BINDING OF CATEGORY VARIABLE FOR GENERATED INDIVIDUALS — THIS WILL MAKE INDEXING HARDER, BUT SHOULD BE DONE IN A BETTER, SIMPLER FASHION

--- trunk/Sparser/code/s/objects/model/bindings/structure.lisp
+++ trunk/Sparser/code/s/objects/model/bindings/structure.lisp
@@ -6,6 +6,10 @@
 ;;;  version:  August 1994
 
 ;; broken out from [object] file 8/12/94
+;; 3/21/2015 SBCL-based tuning work -- trying to find out where we are over-using variable binding to
+;;  get indexing -- commented out over-write of binding-body do keep track of which variables are 
+;;  drastically over-used (such as the variable category) to see about
+;;  how to do the job better
 
 (in-package :sparser)
 
@@ -24,3 +28,15 @@
   id
   )
 
+#|
+(setf (symbol-function 'bbinding-body)(symbol-function 'binding-body))
+(defparameter *binding-vars* (make-hash-table :size 10000))
+(defun binding-body (binding)
+  (if (null (gethash (binding-variable binding) *binding-vars*))
+      (setf (gethash (binding-variable binding) *binding-vars*) 1)
+      (incf (gethash (binding-variable binding) *binding-vars*)))
+  (if
+   (eq (var-name (binding-variable binding)) 'prep)
+   (break "prep"))
+  (bbinding-body binding))
+|#
