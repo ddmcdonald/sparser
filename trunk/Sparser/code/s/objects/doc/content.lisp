3d18194b72d30a4f396ee35727af9fa13d7a4c00
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2015-05-13 00:47:29 +0000

    Moved in all the container-oriented code that had been in other files

--- trunk/Sparser/code/s/objects/doc/content.lisp
+++ trunk/Sparser/code/s/objects/doc/content.lisp
@@ -1,13 +1,15 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
-;;; copyright (c) 2013 David D. McDonald  -- all rights reserved
+;;; copyright (c) 2013-2015 David D. McDonald  -- all rights reserved
 ;;;
 ;;;     File:  "content"
 ;;;   Module:  "objects;doc:"
-;;;  Version:  October 2013
+;;;  Version:  May 2015
 
 ;; initiated 3/13/13. Elaborated through 3/29/13. 9/17/13 fan-out
 ;; from sections make-over. 10/2/19 Fleshed out general notion of
 ;; a container and set up simple accumulator for sentences.
+;; 5/12/15 Reorganized a good deal and pulled out fresh-contents
+;; to go in drivers/forest/parsing-containers.lisp 
 
 (in-package :sparser)
 
@@ -20,6 +22,116 @@
     :documentation "backpointer to the document object that it's part of"))
   (:documentation "Common super class for all container-like things"))
 
+;;;---------------------
+;;; sentence containers
+;;;---------------------
+
+(defparameter *container-for-sentence* :simple ;; :situation
+  "Switch parameter for the kind of container we make for 
+   a sentence, which could be designed for simple accumulation,
+   presumably structured accumulation, long-distance parse state
+   or (the motive for all this) a situation.")
+
+(defun make-sentence-container (sentence)
+  ;; called from start-sentence
+  (declare (ignore sentence))
+  (error "No version of make-sentence-container has been specified"))
+
+(defun designate-sentence-container (&optional (keyword *container-for-sentence*))
+  (setf (symbol-function 'make-sentence-container)
+        (ecase keyword
+          (:simple (symbol-function 'make-sentence-container/simple))
+          (:situation (symbol-function 'make-sentence-container/situation))
+          (:complex (symbol-function 'make-sentence-content-container)))))
+
+;; (designate-sentence-container :simple)  => switch setting
+(defun make-sentence-container/simple (sentence)
+  (make-instance 'simple-container :in sentence))
+
+
+;;;------------------------------------------
+;;; content containers for document elements
+;;;------------------------------------------
+
+(defclass sentence-content (container parsing-status 
+                            local-layout
+                            entities-and-relations
+                            sentence-discourse-history
+                            sentence-text-structure
+                            accumulate-items ordered)
+  ()
+  (:documentation "From container we get :in to point back to the
+    sentence. From ordered we get previous and next so we can link
+    the directly without having to go to the sentence objects."))
+
+(defmethod print-object ((c sentence-content) stream)
+  (print-unreadable-object (c stream :type t)
+    (let ((sentence (bkptr c)))
+      (format stream "p~a -- "
+              (pos-token-index (starts-at-pos sentence)))
+      (if (and (slot-boundp sentence 'ends-at-pos)
+               (ends-at-pos sentence))
+        (format stream "p~a" (pos-token-index (ends-at-pos sentence)))
+        (format stream "?")))))
+
+
+;;---- Making it the container that's used with sentences
+
+; (designate-sentence-container :complex)  ;; run with every change
+;
+(defun make-sentence-content-container (sentence)
+  (make-instance 'sentence-content
+    :in sentence
+    :level :initial))
+
+
+(defclass paragraph-content (container aggregated-bio-terms)
+  ()
+  (:documentation "Will want a bunch more structure just over
+    the enties for the purpose of facilitating anaphora. 
+    Story structure might be paragraph based too."))
+
+(defclass section-content (container aggregated-bio-terms) ())
+
+(defclass article-content (container aggregated-bio-terms) ())
+
+  
+;;;---------------------------------------------------------
+;;; Setting the context of the different levels of document
+;;;---------------------------------------------------------
+
+(defmethod fresh-contents ((a article))
+  (setf (contents a) (make-instance 'article-content)))
+#|   Definition for Grok
+  "Supplies the content field of an article"
+  ;; placeholder for resource -- but only via a generating macro that
+  ;; has some generality. 
+  (let ((contents (make-instance 'text-relation-contents :container article))
+        (symbol (name article)))
+    (if symbol 
+      (setf (name contents) symbol)
+      (setf (name contents) :no-name-in-article))
+    contents))
+|#
+
+(defmethod fresh-contents ((sos section-of-sections))
+  (setf (contents sos) (make-instance 'section-content)))
+
+(defmethod fresh-contents ((s section))
+  (setf (contents s) (make-instance 'section-content)))
+
+(defmethod fresh-contents ((p paragraph))
+  (setf (contents p) (make-instance 'paragraph-content)))
+
+
+
+
+;;;---------------------------
+;;; Grok code to rehabilitate
+;;;---------------------------
+
+;;----------------- simplistic accumulator
+
 (defclass accumulate-items ()
   ((list :initform nil :accessor items
     :documentation "Simple list. Items are just pushed onto it
@@ -31,7 +143,6 @@
 (defclass simple-container (container accumulate-items)
   ())
   
-
 (defmethod print-object ((c accumulate-items) stream)
   (print-unreadable-object (c stream :type t)
     (let ((items (items c))
@@ -70,35 +181,6 @@
 
 
 
-;;;---------------------
-;;; sentence containers
-;;;---------------------
-
-(defparameter *container-for-sentence* :simple ;; :situation
-  "Switch parameter for the kind of container we make for 
-   a sentence, which could be designed for simple accumulation,
-   presumably structured accumulation, long-distance parse state
-   or (the motive for all this) a situation.")
-
-(defun make-sentence-container (sentence)
-  ;; called from start-sentence
-  (declare (ignore sentence))
-  (error "No version of make-sentence-container has been specified"))
-
-(defun designate-sentence-container (&optional (keyword *container-for-sentence*))
-  (setf (symbol-function 'make-sentence-container)
-        (ecase keyword
-          (:simple (symbol-function 'make-sentence-container/simple))
-          (:situation (symbol-function 'make-sentence-container/situation))
-          (:complex (symbol-function 'make-sentence-content-container)))))
-
-;; (designate-sentence-container :simple)  => switch setting
-(defun make-sentence-container/simple (sentence)
-  (make-instance 'simple-container :in sentence))
-
-
-
-
           
 ;;----------- original, clumsy Grok code below here ----------
 
@@ -126,21 +208,6 @@
           (length (modifier-head-relations tc))
           (length (adjacency-relations tc)))
   tc)
-
-
-
-(defun fresh-contents (article)
-  "Supplies the content field of an article"
-  ;; placeholder for resource -- but only via a generating macro that
-  ;; has some generality. 
-  (let ((contents (make-instance 'text-relation-contents :container article))
-        (symbol (name article)))
-    (if symbol 
-      (setf (name contents) symbol)
-      (setf (name contents) :no-name-in-article))
-    contents))
-
-
 
 
 (defun add-text-relation-to-article (relation instance)
