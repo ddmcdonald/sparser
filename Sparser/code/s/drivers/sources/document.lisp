4193c235697b932974c6441201876af8983039c5
ddmcdonald50 <ddmcdonald50@gmail.com>
2015-05-13 00:49:59 +0000

    Substantial rearrangement plus adding final actions and content objects at all levels

--- Sparser/code/s/drivers/sources/document.lisp
+++ Sparser/code/s/drivers/sources/document.lisp
@@ -10,22 +10,47 @@
 
 (in-package :sparser)
 
-
+;;;-------------------------------------------------
+;;; For now -always- do a sweep before doing a read
+;;;-------------------------------------------------
 
 (defmethod sweep-document ((doc document-element))
+  "Creates the document objects, including the sentences, 
+   but does no analysis at all."
   (let ((*sweep-for-terminals* nil))
     (declare (special *sweep-for-terminals*))
     (read-from-document doc)))
 
 
+;;;-------------------------------------------------
+;;; arranging to ignore certain (toplevel) sections
+;;;-------------------------------------------------
+;;//// needs a setter
 
-(defgeneric read-from-document (source)
-  (:documentation "Acquires the document from the source, sets it
-   up to be navigated, manages the character source, initializes
-   the necessary flags to ensure the document isn't gratuitously
-   reclaimed. Arranges for the article to be returned as the value
-   of the analysis. Shallow-binds the kind of processing to be
-   done. "))
+(defparameter *sections-to-ignore* '("method")
+  "Contains a list of section titles or title fragments that
+   name sections we want to skip over")
+
+(defun ignore-this-document-section (section)
+  (unless (slot-boundp section 'title)
+    (push-debug `(,section))
+    (error "title of section hasn't been set"))
+  (when *sections-to-ignore*
+    (let ((title-object (title section)))
+      (unless title-object
+        (push-debug `(,section))
+        (error "Section somehow doesn't have a title object"))
+      (let ((title-string (content-string title-object)))
+        (dolist (ignore-substring *sections-to-ignore* nil)
+          (when (search ignore-substring title-string
+                        :test #'string-equal)
+            (format t "~&~%------- Ignoring section ~a --------" section)
+            (return t)))))))
+
+
+;;;---------
+;;; globals
+;;;---------
 
 ;; These are for debugging so we can pick up where we left off
 (defvar *doc-objects* nil)
@@ -34,21 +59,12 @@
 (defvar *section* nil)
 (defvar *paragraph* nil)
 
-(defmethod read-from-document ((id integer))
-  (unless (find-package :r3)
-    (return-from read-from-document
-      "R3 document reading machinery is not loaded"))
-  (let ((reader (intern (symbol-name '#:make-sparser-doc-structure)
-                        (find-package :r3))))
-    (let* ((doc-objects (funcall reader id))
-           (article (car doc-objects)))
-      (setq *doc-objects* doc-objects
-            *article* article) 
-      (read-from-document article))))
 
-(defmethod read-from-document ((a article))
-  (setq *current-article* a) ;; sets up the function (article)
-  (setf (contents a) (fresh-contents a))
+;;;--------------------------------
+;;; sections-of-sections detection
+;;;--------------------------------
+
+(defun pre-sweep-for-embedded-sections (a)
   (let* ((sections (loop for child in (children a)
                     unless (typep child 'title-text)
                     collect child))
@@ -70,7 +86,13 @@
         ;; make a section-of-sections for it
         (let ((ss (allocate-section-of-sections)))
           (setf (children ss) its-children)
-          (setq child ss)))
+          (let ((title? (when (typep (car its-children) 'title-text)
+                          ;;/// searching for it would be more robust
+                          (car its-children))))
+            (if title?
+              (setf (title ss) title?)
+              (setf (title ss) (make-unknown-title)))
+            (setq child ss))))
       (push child actual-children)
       (when previous-child
         (setf (previous child) previous-child)
@@ -78,19 +100,44 @@
       (setq previous-child child)
       (setq child (car remaining)
             remaining (cdr remaining)))
-    (setf (children a) (nreverse actual-children))
 
-    ;; This is where we'd pick and choose which ones to do
-    ;; //// set up to return this object via (analysis-core-return-value)
-    (dolist (sec (children a))
+    (setf (children a) (nreverse actual-children))))
+
+(defun make-unknown-title ()
+  (let ((tt (make-instance 'title-text)))
+    (setf (content-string tt) "Couldn't find title")))
+
+
+;;;------------------------------------------
+;;; document reading (of pre-swept documents
+;;;------------------------------------------
+
+(defgeneric read-from-document (source)
+  (:documentation "Acquires the document from the source, sets it
+   up to be navigated, manages the character source, initializes
+   the necessary flags to ensure the document isn't gratuitously
+   reclaimed. Arranges for the article to be returned as the value
+   of the analysis. Shallow-binds the kind of processing to be
+   done. "))
+
+
+
+(defmethod read-from-document ((a article))
+  (setq *current-article* a) ;; sets up the function (article)
+  (unless *sweep-for-terminals*
+    (pre-sweep-for-embedded-sections a))
+  (fresh-contents a)
+  (dolist (sec (children a))
+    (unless (ignore-this-document-section sec)
       (setq *section* sec)
-      (read-from-document sec))
-
-    a))
+      (read-from-document sec)))
+  (after-actions a)
+  a)
 
 
 (defmethod read-from-document ((ss section-of-sections))
   (setq *section-of-sections* ss)
+  (fresh-contents ss)
   (let* ((subsections (children ss))
          (title? (when (typep (car subsections) 'title-text)
                    (car subsections))))
@@ -110,13 +157,13 @@
         (when section
           (setf (previous section) previous-section)
           (setf (next previous-section) section)))
-      ;;/// aggregation goes here
+      (after-actions ss)
       (format t "~&~%--------- finished section of sections ~a~%" ss))))
 
 
 (defmethod read-from-document ((s section))
   (setq *current-section* s)
-  ;;//// find a good contents for these
+  (fresh-contents s)
   ;;/// previous/next also not set, and there's a title-text
   ;;  among the children
   (format t "~&~%--------- starting section ~a~%" s)
@@ -142,7 +189,9 @@
         (setf (next previous-paragraph) paragraph)))
     ;;/// section aggregation goes here. 
     ;;(break "finished section")
+    (after-actions s)
     (format t "~%--------- finished section ~a~%~%" s)))
+
 
 (defmethod read-from-document ((p paragraph))
   ;;/// read thuogh do-document-as-stream-of-files
@@ -157,11 +206,28 @@
                       *recognize-sections-within-articles*
                       *accumulate-content-across-documents*))
     (setq *current-paragraph* p)
-    ;;///// needs contents -- look at make-sentence-container
+    (fresh-contents p)
     (let ((text (content-string p)))
       (initialize-sentences) ;; set up or reuse the 1st sentence
       ;; lifted from analyze-text-from-string 
       (establish-character-source/string text)
-      (analysis-core))))
+      (analysis-core)
+      (after-actions p))))
+
+
+#+ignore ;; nice idea, but would need pretty drastic
+;; makeover as of 5/12/15
+(defmethod read-from-document ((id integer))
+  (unless (find-package :r3)
+    (return-from read-from-document
+      "R3 document reading machinery is not loaded"))
+  (let ((reader (intern (symbol-name '#:make-sparser-doc-structure)
+                        (find-package :r3))))
+    (let* ((doc-objects (funcall reader id))
+           (article (car doc-objects)))
+      (setq *doc-objects* doc-objects
+            *article* article) 
+      (read-from-document article))))
+
 
         
