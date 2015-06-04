8d3f573d47bda5d31690095aa534a0189cb451ab
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2015-06-04 18:13:59 +0000

    Added stub file -- rhetoric.lisp -- to hold mixin classes, etc. initially for Chris Ward's scheme for determining whether a sentence is novel in an article

---
+++ trunk/Sparser/code/s/objects/doc/rhetoric.lisp
@@ -0,0 +1,14 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
+;;; copyright (c) 2013-2015 David D. McDonald  -- all rights reserved
+;;;
+;;;     File:  "rhetoric"
+;;;   Module:  "objects;doc;"
+;;;  Version:  June 2015
+
+;; Created 6/4/15 to mixins, forms, and computation that is needed
+;; to identify and work with the rhetorical structure of a text
+;; writ large (epistemic status, story structure, new vs. old information,
+;; etc.)
+
+
+(in-package :sparser)
