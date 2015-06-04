51fdce2f55786d00738c2cb9652b2c7367abb95b
ddmcdonald50 <ddmcdonald50@gmail.com>
2015-06-04 18:15:24 +0000

    Added stub file -- rhetoric.lisp -- to hold the data for Chris Ward's scheme for determining whether a sentence is novel in an article and to be a place to collect up all the vocabulary from other files that has been marked rhetorical.

---
+++ Sparser/code/s/grammar/model/sl/biology/rhetoric.lisp
@@ -0,0 +1,15 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER COMMON-LISP) -*-
+;;; Copyright (c) 2015 SIFT LLC. All Rights Reserved
+;;;
+;;;    File: "rhetoric"
+;;;  Module: "grammar/model/sl/biology/
+;;; version: June 2015
+
+;; Created 6/4/15 to hold all vocabulary that's rhetorical in nature
+;; and any rules of any sort that observations about the retorical 
+;; structure writ large of a molecular biology passage or article. 
+;; See companion file with def forms and structure in objects/doc/
+;; rhetoric.lisp
+
+(in-package :sparser)
+
