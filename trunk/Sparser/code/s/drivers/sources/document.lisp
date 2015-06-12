13f004b5c18bfaf67d4e051e9e27627a276b1a76
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2015-06-12 17:00:30 +0000

    Changed the flag that governs sweeping to something more obvious. Began work on being systematic about titles (the need to be read)

--- trunk/Sparser/code/s/drivers/sources/document.lisp
+++ trunk/Sparser/code/s/drivers/sources/document.lisp
@@ -27,29 +27,159 @@
   "Tracks entering and exiting sections of any type
   including paragraphs")
 
+(defparameter *show-article-progress* t
+  "Announce what article we're working on")
+
+
 ;;;-------------------------------------------------
 ;;; For now -always- do a sweep before doing a read
 ;;;-------------------------------------------------
 
-(defparameter *show-article-progress* t
-  "Announce what article we're working on
-")
-
 (defmethod sweep-document ((doc document-element))
-  "Creates the document objects, including the sentences, 
+  "Creates additional document objects, including the sentences, 
    but does no analysis at all. Uses read-from-document
    to walk the document -- the same method as used to
-   parse it -- but the sweep-for-terminals flag must be
-   up for the initiate-successive-sweeps code to run the
-   parsing, and we dynamically bind it here to nil."
-  (let ((*sweep-for-terminals* nil)
+   parse it."
+  (let ((*sentence-making-sweep* t)
         (*sections-to-ignore* nil))
-    (declare (special *sweep-for-terminals* *sections-to-ignore*))
+    (declare (special *sentence-making-sweep* *sections-to-ignore*))
     (when *show-article-progress*
-      (format t "Sweeping document ~a~&" (name doc)))
+      (format t "~&Sweeping document ~a~%" (name doc)))
     (read-from-document doc)))
 
 
+;;;--------------------------------
+;;; The read-from-document methods 
+;;;--------------------------------
+
+(defgeneric read-from-document (source)
+  (:documentation "Given a document object, sets it
+   up to be navigated, manages the character source, initializes
+   the necessary flags to ensure the document isn't gratuitously
+   reclaimed. Arranges for the article to be returned as the value
+   of the analysis. Looks at control parameter settings to 
+   determine what to do on a given pass."))
+
+(defmethod read-from-document ((a article))
+  (declare (special *sentence-making-sweep* *sections-to-ignore*))
+  (setq *current-article* a) ;; sets up the function (article)
+  (when *sentence-making-sweep*
+    (sweep-for-embedded-sections a))
+  (install-contents a)
+  (when (and *show-article-progress*
+             (not *sentence-making-sweep*))
+      (format t "read-from-document ~a~&" (name a)))
+  (dolist (sec (children a))
+    (unless (ignore-this-document-section sec)
+      (setq *section* sec)
+      (catch 'do-next-paragraph ;; article 3058384 starts with paragraphs
+        (read-from-document sec))))
+  (after-actions a)
+  a)
+
+(defmethod read-from-document ((ss section-of-sections))
+  (setq *section-of-sections* ss)
+  (install-contents ss)
+  (let* ((subsections (children ss))
+         (title (when (typep (car subsections) 'title-text)
+                   (car subsections))))
+    (when title
+      (setf (title ss) (content-string title))
+      (setq subsections (cdr subsections)))
+    (when *show-section-printouts*
+      (format t "~&--------- starting section of sections ~a~%" ss))
+    (let ((section (car subsections))
+          (remaining (cdr subsections))
+          previous-section)
+      (loop
+        (unless section (return))
+        (catch 'do-next-paragraph
+          (read-from-document section))
+        (setq previous-section section)
+        (setq section (car remaining)
+              remaining (cdr remaining))
+        (when section
+          (setf (previous section) previous-section)
+          (setf (next previous-section) section)))
+      (after-actions ss)
+      (when *show-section-printouts*
+        (format t "~&~%--------- finished section of sections ~a~%" ss)))))
+
+
+(defmethod read-from-document ((s section))
+  (setq *current-section* s)
+  (install-contents s)
+  ;;/// previous/next also not set, and there's a title-text
+  ;;  among the children
+  
+  (when *show-section-printouts*
+    (format t "~&~%--------- starting section ~a~%" s))
+  (let* ((all-children (children s))
+         (title (loop for child in all-children
+                  when (typep child 'title-text) return child))
+         (paragraphs (loop for child in all-children
+                       when (typep child 'paragraph)
+                       collect child))
+         (paragraph (car paragraphs))
+         (remaining (cdr paragraphs))
+         previous-paragraph )
+    (when title (setf (title s) (content-string title)))
+    (loop
+      (unless paragraph
+        (return))
+      (setq *paragraph* paragraph)
+      (catch 'do-next-paragraph
+        (read-from-document paragraph))
+      (after-actions paragraph)
+      (setq previous-paragraph paragraph)
+      (setq paragraph (car remaining)
+            remaining (cdr remaining))
+      (when paragraph
+        (setf (previous paragraph) previous-paragraph)
+        (setf (next previous-paragraph) paragraph)))
+    (after-actions s)
+    (when *show-section-printouts*
+      (format t "~%--------- finished section ~a~%~%" s))))
+
+
+(defmethod read-from-document ((p paragraph))
+  ;;/// read thuogh do-document-as-stream-of-files
+  ;;/// do we need a specialized analyser?
+  ;;    lookup-the-kind-of-chart-processing-to-do
+  ;;/// review per-article-initializations
+  (when *show-section-printouts*
+    (format t "~&~%--------- starting paragraph ~a~%" p))
+  (let ((*reading-populated-document* t)
+        (*recognize-sections-within-articles* nil) ;; turn of doc init
+        (*accumulate-content-across-documents* t)) ;; doesn't clear history??
+    (declare (special *reading-populated-document*
+                      *recognize-sections-within-articles*
+                      *accumulate-content-across-documents*))
+    (setq *current-paragraph* p)
+    (install-contents p)
+    (let ((text (content-string p)))
+      (initialize-sentences) ;; set up or reuse the 1st sentence
+      ;; lifted from analyze-text-from-string 
+      (establish-character-source/string text)
+      (analysis-core))))
+
+
+#+ignore ;; nice idea, but would need pretty drastic
+;; makeover as of 5/12/15
+(defmethod read-from-document ((id integer))
+  (unless (find-package :r3)
+    (return-from read-from-document
+      "R3 document reading machinery is not loaded"))
+  (let ((reader (intern (symbol-name '#:make-sparser-doc-structure)
+                        (find-package :r3))))
+    (let* ((doc-objects (funcall reader id))
+           (article (car doc-objects)))
+      (setq *doc-objects* doc-objects
+            *article* article) 
+      (read-from-document article))))
+
+
+        
 ;;;-------------------------------------------------
 ;;; arranging to ignore certain (toplevel) sections
 ;;;-------------------------------------------------
@@ -101,7 +231,7 @@
 ;;; sections-of-sections detection
 ;;;--------------------------------
 
-(defun pre-sweep-for-embedded-sections (a)
+(defun sweep-for-embedded-sections (a)
   (let* ((sections (loop for child in (children a)
                     unless (typep child 'title-text)
                     collect child))
@@ -115,7 +245,7 @@
     (when (typep child 'paragraph)
       ;; 3058384 starts out as <body> <p> ... before it gets to
       ;; a proper section.
-      (return-from pre-sweep-for-embedded-sections nil))
+      (return-from sweep-for-embedded-sections nil))
     (loop
       (unless child
         (return))
@@ -148,137 +278,3 @@
   (let ((tt (make-instance 'title-text)))
     (setf (content-string tt) "Couldn't find title")))
 
-
-;;;------------------------------------------
-;;; document reading (of pre-swept documents
-;;;------------------------------------------
-
-(defgeneric read-from-document (source)
-  (:documentation "Acquires the document from the source, sets it
-   up to be navigated, manages the character source, initializes
-   the necessary flags to ensure the document isn't gratuitously
-   reclaimed. Arranges for the article to be returned as the value
-   of the analysis. Shallow-binds the kind of processing to be
-   done. "))
-
-
-
-(defmethod read-from-document ((a article))
-  (setq *current-article* a) ;; sets up the function (article)
-  (unless *sweep-for-terminals*
-    (pre-sweep-for-embedded-sections a))
-  (fresh-contents a)
-  (when (and *show-article-progress*
-             *sweep-for-terminals*)
-      (format t "read-from-document ~a~&" (name a)))
-
-  (dolist (sec (children a))
-    (unless (ignore-this-document-section sec)
-      (setq *section* sec)
-      (catch 'do-next-paragraph ;; article 3058384 starts with paragraphs
-        (read-from-document sec))))
-  (after-actions a)
-  a)
-
-(defmethod read-from-document ((ss section-of-sections))
-  (setq *section-of-sections* ss)
-  (fresh-contents ss)
-  (let* ((subsections (children ss))
-         (title? (when (typep (car subsections) 'title-text)
-                   (car subsections))))
-    (when title?
-      (setf (title ss) (content-string title?))
-      (setq subsections (cdr subsections)))
-    (when *show-section-printouts*
-      (format t "~&~%--------- starting section of sections ~a~%" ss))
-    (let ((section (car subsections))
-          (remaining (cdr subsections))
-          previous-section)
-      (loop
-        (unless section (return))
-        (catch 'do-next-paragraph
-          (read-from-document section))
-        (setq previous-section section)
-        (setq section (car remaining)
-              remaining (cdr remaining))
-        (when section
-          (setf (previous section) previous-section)
-          (setf (next previous-section) section)))
-      (after-actions ss)
-      (when *show-section-printouts*
-        (format t "~&~%--------- finished section of sections ~a~%" ss)))))
-
-
-(defmethod read-from-document ((s section))
-  (setq *current-section* s)
-  (fresh-contents s)
-  ;;/// previous/next also not set, and there's a title-text
-  ;;  among the children
-  (when *show-section-printouts*
-    (format t "~&~%--------- starting section ~a~%" s))
-  (let* ((all-children (children s))
-         (paragraphs (loop for child in all-children
-                       when (typep child 'paragraph)
-                       collect child))
-         (paragraph (car paragraphs))
-         (remaining (cdr paragraphs))
-         previous-paragraph )
-    (loop
-      (unless paragraph
-        (return))
-      (setq *paragraph* paragraph)
-      (catch 'do-next-paragraph
-        (read-from-document paragraph))
-      (after-actions paragraph)
-      (setq previous-paragraph paragraph)
-      (setq paragraph (car remaining)
-            remaining (cdr remaining))
-      ;;/// sentence->paragraph aggregation goes here. 
-      (when paragraph
-        (setf (previous paragraph) previous-paragraph)
-        (setf (next previous-paragraph) paragraph)))
-    ;;/// section aggregation goes here. 
-    ;;(break "finished section")
-    (after-actions s)
-    (when *show-section-printouts*
-      (format t "~%--------- finished section ~a~%~%" s))))
-
-
-(defmethod read-from-document ((p paragraph))
-  ;;/// read thuogh do-document-as-stream-of-files
-  ;;/// do we need a specialized analyser?
-  ;;    lookup-the-kind-of-chart-processing-to-do
-  ;;/// review per-article-initializations
-  (when *show-section-printouts*
-    (format t "~&~%--------- starting paragraph ~a~%" p))
-  (let ((*reading-populated-document* t)
-        (*recognize-sections-within-articles* nil) ;; turn of doc init
-        (*accumulate-content-across-documents* t)) ;; doesn't clear history??
-    (declare (special *reading-populated-document*
-                      *recognize-sections-within-articles*
-                      *accumulate-content-across-documents*))
-    (setq *current-paragraph* p)
-    (fresh-contents p)
-    (let ((text (content-string p)))
-      (initialize-sentences) ;; set up or reuse the 1st sentence
-      ;; lifted from analyze-text-from-string 
-      (establish-character-source/string text)
-      (analysis-core))))
-
-
-#+ignore ;; nice idea, but would need pretty drastic
-;; makeover as of 5/12/15
-(defmethod read-from-document ((id integer))
-  (unless (find-package :r3)
-    (return-from read-from-document
-      "R3 document reading machinery is not loaded"))
-  (let ((reader (intern (symbol-name '#:make-sparser-doc-structure)
-                        (find-package :r3))))
-    (let* ((doc-objects (funcall reader id))
-           (article (car doc-objects)))
-      (setq *doc-objects* doc-objects
-            *article* article) 
-      (read-from-document article))))
-
-
-        
