fddb345ea74cb83f26ed0286e63b32dcd42593f3
rjbobrow <rjbobrow@gmail.com>
2015-06-15 15:22:33 +0000

    utility functions for finding proteins mostly

--- Sparser/code/s/grammar/model/sl/biology/new-words.lisp
+++ Sparser/code/s/grammar/model/sl/biology/new-words.lisp
@@ -16,7 +16,8 @@
       (let*
           ((rules (rs-single-term-rewrites rs))
            (first-rule (car rules))
-           (form (cfr-form first-rule)))
+           (form (and first-rule
+                      (cfr-form first-rule))))
         (declare (special rules first-rule form))
         form))))
 
@@ -63,3 +64,74 @@
              *pos-ht*)
     (format s "~&))~&")))
       
+(defparameter *UCD-tag-directory* 
+  (string-append *r3-trunk* "code/evaluation/June2015Materials/Eval_tag_lists/*.tags"))
+
+(defparameter *tag-file-ids* (generate-id-list-from-directory-listing *UCD-TAG-DIRECTORY*))
+
+(defun read-tag-file (id)
+  (with-open-file
+      (s (string-append *r3-trunk* "code/evaluation/June2015Materials/Eval_tag_lists/" id ".tags")
+         :direction :input)
+    (read s)))
+
+(defun preferred-protein-id (tag)
+  (or
+   (loop for id in (cdddr tag) when (search "_HUMAN" id) do (return id))
+   (loop for id in (cdddr tag) when (search "PR:" id) do (return id))))
+
+
+(defun proteins-in-pmc-file (id)
+  (let
+      ((tags (read-tag-file id))
+       (pht (make-hash-table :test #'equalp))
+       pl)
+    (loop for tag in tags 
+      do
+      (let* ((pid (preferred-protein-id tag)))
+        (when pid  (pushnew  pid (gethash (third tag) pht) :test #'equalp))))
+    (maphash #'(lambda(pro ids) (push `(define-protein ,pro ,@ids) pl)) pht)
+    pl))
+
+(defparameter *UCD-proteins-any-case* (make-hash-table :size 10000 :test #'equalp))
+(defparameter *UCD-proteins-actual-case* (make-hash-table :size 10000 :test #'equal))
+
+
+(defun record-proteins-in-june-test ()
+  (loop for id in *tag-file-ids* 
+    do
+    (loop for tag in (read-tag-file id) 
+      do
+      (let* ((pid (preferred-protein-id tag)))
+        (when pid  
+          (pushnew  pid (gethash (third tag) *UCD-proteins-any-case*) :test #'equalp)
+          (pushnew  pid (gethash (third tag) *UCD-proteins-actual-case*) :test #'equalp))))))
+
+(defun new-word-protein-defs ()
+  (let
+      ((any nil)
+       (actual nil))
+    (loop for s in *new-word-strings* 
+      do
+      (cond
+       ((gethash s *UCD-proteins-actual-case*)
+        (push 
+         `(define-protein ,s ,@(gethash s *UCD-proteins-actual-case*))
+         actual))
+       ((gethash s *UCD-proteins-any-case*)
+        (push 
+         `(define-protein ,s ,@(gethash s *UCD-proteins-any-case*))
+         any))))
+    (list actual any)))
+  
+
+(defun mixed-case-proteins ()
+  (let
+      ((mcp nil))
+    (maphash #'(lambda (prot label) 
+                 (if (and (not (equal prot (string-downcase prot)))(not (equal prot (string-upcase prot))))
+                     (push prot mcp)))
+             *UCD-proteins-actual-case*)
+    mcp))
+   
+         
