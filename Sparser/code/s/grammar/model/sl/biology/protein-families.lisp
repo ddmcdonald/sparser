2d8318c2206917ea2825a6bb98af04db13f4f73f
amandalynne.p <amandalynne.p@gmail.com>
2015-06-16 16:48:59 +0000

    separated protein familes into new file, protein-families.lisp, loaded after all *proteins* files

---
+++ Sparser/code/s/grammar/model/sl/biology/protein-families.lisp
@@ -0,0 +1,18 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER COMMON-LISP) -*-
+;;; Copyright (c) 2014-2015 SIFT LLC. All Rights Reserved
+;;;
+;;;    File: "protein-families"
+;;;  Module: "grammar/model/sl/biology/
+;;; version: June 2015
+
+;; initiated 06/16/15 migrating from other files 
+
+(in-package :sparser)
+
+
+
+
+;; Protein families
+(def-family "cadherin" :members ("CADH1_HUMAN"))
+(def-family "NF-AT" :members ("NFAT5_HUMAN")) 
+
