9a06980594268af059740eb3a2573c514b770cb4
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2015-06-20 17:11:22 +0000

    Moved lap-break here and added a motivation documentation

--- trunk/Sparser/code/s/tools/basics/no-breaks.lisp
+++ trunk/Sparser/code/s/tools/basics/no-breaks.lisp
@@ -4,7 +4,7 @@
 ;;;
 ;;;      File:  "no breaks"
 ;;;    Module:   "tools:basics"
-;;;   Version:   May 2015
+;;;   Version:   June 2015
 
 ;; Original version 2/1991 for CTI.
 ;; 7/22/09 brought package out of the dark ages. 10/8/09 Added on-error setup.
@@ -15,19 +15,38 @@
 ;;  CCL won't let you redefine break (why did MCL permit it?), so the better
 ;;  treatment is to get our own symbol for break and define it as a call to error
 ;;  that we can trap with an error handler.
+;; (6/19/15) Documented the motivation.
 
 (in-package :sparser)
 
-;;;--------------------
-;;; This version works
-;;;--------------------
+#| There are handlers for calls to error and related conditions
+that catch the call that let us continue (error-trapped-scan-and-core
+is the latest example). 
+   But break is different and will always invoke the error-handling
+machinery. Unless we take steps it will always blow through any error
+handler. That what this code is all about. We make our own version of
+'break' by shadowing the definition we get fom the Lisp and use a
+function we define here in the :sparser package. 
+   There are function here to swtich back and forth between definitions
+or simply use a break function with a different name. N.b. the only real
+practical difference between a call to break and one to error is that
+you can continue from a break (c.f. cerror). 
+|#
+;;;------------------------------
+;;; take over the break function
+;;;------------------------------
 
 (shadow '(#:break) (find-package :sparser)) ;; c.f. the position struct definition.
 
 (defun sparser::break (&optional format-string &rest args)
   (apply #'error format-string args))
 
+#| N.b. When we load Sparser we're using this error version of break. |#
+
+
 (defun revert-to-regular-break ()
+  "Change the definition of sparser::break to the regular one
+   taken from the Lisp."
   (let ((form
          '(defun sparser::break (format-string &rest args)
             #+ccl(apply #'ccl::break format-string args)
@@ -35,12 +54,23 @@
     (eval form)))
 
 (defun revert-to-error-break ()
+  "Switch back to treating break as an error call."
   (let ((form
          '(defun sparser::break (&optional format-string &rest args)
             (apply #'error format-string args))))
     (eval form)))
 
-;; This set used to be effective, but not now (2015)
+
+(defun lsp-break (&rest args)
+  "Alternative with a different name that will always break."
+  #-ccl (apply #'cl-user::break args)
+  #+ccl (apply #'ccl::break args))
+  
+
+
+;;;;---------- Old version. Worked in MCL ----------
+
+
 ;;;-----------------------------------
 ;;; globals for saving the old values
 ;;;-----------------------------------
