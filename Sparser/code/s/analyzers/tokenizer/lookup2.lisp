e688dc8fb8da884544c5c11546bc095bd5a50155
ddmcdonald50 <ddmcdonald50@gmail.com>
2015-07-24 16:46:05 +0000

    Changed lookup to recognize when a 'known' word really isn't and pass it though the routines that would give it an interpretation and a edge-creating rule

--- Sparser/code/s/analyzers/tokenizer/lookup2.lisp
+++ Sparser/code/s/analyzers/tokenizer/lookup2.lisp
@@ -1,10 +1,10 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
 ;;; copyright (c) 1990  Content Technologies Inc.
-;;; copyright (c) 1992,2012  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992,2012-2015  David D. McDonald  -- all rights reserved
 ;;; 
 ;;;     File:  "lookup"
 ;;;   Module:  "tokenizer;"
-;;;  Version:  2.0 November 2012
+;;;  Version:  2.1 July 2015
 
 ;;  1.1 (v1.6 12/14/90) Cleans up the mess in Lookup as part of doing :ignore
 ;;      unknown words.
@@ -12,15 +12,48 @@
 ;;      (11/5/12) Guarded the symbol-value lookup because of cases when
 ;;       introducing words via Comlex where you can get a word started
 ;;       but not finished right away.
+;; 2.1 (7/23/15) Got through the unknown word machinery in new cases
+;;      were we have words that are introduced by polywords and will
+;;      appear in the chart as words rather words covered by edges.
 
 (in-package :sparser)
 
 
+(defun find-word (char-type)
+  ;; Called from finish-token to find or make the word that corresponds
+  ;; to the sequence of characters we just delimited and is resident in
+  ;; the lookup buffer right now.
+  (let ((symbol (lookup-word-symbol)))
+    (if symbol
+      (if (boundp symbol)
+        (let ((word (symbol-value symbol)))
+          (if *edge-for-unknown-words*
+            ;; In this case we have to look at whether there is
+            ;; a rule-set, and if there is that is has unary rule
+            ;; (and a category the rule refers to) rather then
+            ;; the typical case where there's an fsa but no rule.
+            ;; If there isn't one, we pass the word through the
+            ;; unknown word machinery, which will give it one
+            (let ((rs (rule-set-for word)))
+              (cond
+               ((null rs) 
+                (establish-unknown-word char-type))
+               ((memq :function-word (plist-for word)) ;; "than"
+                word)
+               ((null (rs-single-term-rewrites rs))
+                (establish-unknown-word char-type))
+               (t
+                word)))
+            word))
+        (establish-unknown-word char-type))
+      (establish-unknown-word char-type))))
+
+#| Original 9/92
 (defun find-word (char-type)
   (let ((symbol (lookup-word-symbol)))
     (if symbol
       (if (boundp symbol)
         (symbol-value symbol)
         (establish-unknown-word char-type))
-      (establish-unknown-word char-type))))
+      (establish-unknown-word char-type)))) |#
 
