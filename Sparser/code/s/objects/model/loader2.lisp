9b88d126f4719e76b52d2b190524fa2ea97eb5d2
ddmcdonald50 <ddmcdonald50@gmail.com>
2015-08-03 20:33:25 +0000

    Removed the conditioning on a flag that has been obe

--- Sparser/code/s/objects/model/loader2.lisp
+++ Sparser/code/s/objects/model/loader2.lisp
@@ -1,11 +1,10 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
-;;; copyright (c) 1991-1998 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1991-1998,2015 David D. McDonald  -- all rights reserved
 ;;; extensions copyright (c) 2007-2009 BBNT Solutions LLC. All Rights Reserved
-;;; $Id: loader2.lisp 320 2009-10-20 19:36:28Z dmcdonal $
 ;;;
 ;;;     File:  "loader"
 ;;;   Module:  "objects;model:"
-;;;  version:  2.1 October 2009
+;;;  version:  2.2 August 2015
 
 ;; initiated 11/17/91 v2.1, added individuals (moved from dossiers) 12/2
 ;; 1.1 (7/16/92 v2.3) bumped everything to install the new rep.
@@ -23,6 +22,8 @@
 ;;      the moby makeover. 7/12 moved over bindings.loader2. 10/9 bumped
 ;;      the categories loader to 2 and moved that version into the psi-2009
 ;;      section.
+;; 2.2 (8/3/15) Removing the dependency on the flag since it's long
+;;      been OBE.
 
 (in-package :sparser)
 
@@ -31,34 +32,16 @@
 (lload "objects;model:categories:ops structure")
 (lload "objects;model:individuals:structure1")
 (lload "objects;model:bindings:structure")
-(if cl-user::*psi-2009*
-  (then
-    (lload "objects;model:variables:structure1")
-    (lload "objects;model:lattice-points:structure1")
-    (lload "objects;model:psi:structure1"))
-  (else
-    (lload "objects;model:variables:structure")
-    (lload "objects;model:lattice-points:structure")
-    (lload "objects;model:psi:structure")))
-
+(lload "objects;model:variables:structure1")
+(lload "objects;model:lattice-points:structure1")
+(lload "objects;model:psi:structure1")
 
 ;; then load the code proper
-(if cl-user::*psi-2009*
-  (then
-    (lload "objects;model:categories:loader2")
-    (lload "objects;model:bindings:loader2")
-    (lload "objects;model:individuals:loader2")
-    (lload "objects;model:variables:loader2")
-    (lload "objects;model:lattice-points:loader1")
-    (lload "objects;model:psi:loader1"))
-  (else
-    (lload "objects;model:categories:loader1")
-    (lload "objects;model:bindings:loader1")
-    (lload "objects;model:individuals:loader1")
-    (lload "objects;model:variables:loader1")
-    (lload "objects;model:lattice-points:loader")
-    (lload "objects;model:psi:loader")))
-
-
+(lload "objects;model:categories:loader2")
+(lload "objects;model:bindings:loader2")
+(lload "objects;model:individuals:loader2")
+(lload "objects;model:variables:loader2")
+(lload "objects;model:lattice-points:loader1")
+(lload "objects;model:psi:loader1")
 (lload "objects;model:tree-families:loader1")
 
