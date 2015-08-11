c1f8f265b1d9b6bbb33e85402940f1a475d9cdf8
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2015-08-11 13:42:29 +0000

    Small rearrangements. Attempt to move Mumble operations into a single contiguous block failed.

--- trunk/load-nlp.lisp
+++ trunk/load-nlp.lisp
@@ -2,14 +2,14 @@
 ;;; Copyright (c) 2010-2015 David D. McDonald
 ;;;
 ;;;   File:   load-nlp
-;;; Version:  July 2015
+;;; Version:  August 2015
 
 #|
  This file loads the language understanding system Sparser, the
  language generator Mumble, and a shared set of utilities.
  It does -not- compile the code. That step is unnecessary in
  Clozure Common Lisp (CCL) or other lisps that compile functions
- as the read them. It is required for lisps like Franz's Allegro
+ as they read them. It is required for lisps like Franz's Allegro
  which do not. 
 
 |#
@@ -33,15 +33,8 @@
 
 
 (in-package :cl-user)
-(unless (boundp '*no-mumble*)
-  (defparameter *no-mumble*
-    #+sbcl t
-    #-sbcl nil
-    "Controls whether or not the Mumble language generation system
-     is loaded. There are presently issues in geting Mumble to load
-     under SBCL, which is the principal motivation for this switch,
-     but you could decide to leave it out deliberately, and this
-     switch coordinate the inclusion/omission of the requisite files."))
+
+
 
 ;; #1 --- set the base directory
 
@@ -52,22 +45,6 @@
 ;; Change this to fit where you put Sparser
 ;; (setq *nlp-home* "/Users/ddm/sparser/")
 
-(defun sparser-file (&rest file-path-parts) ;; as strings
-  (merge-pathnames (apply #' concatenate 'string file-path-parts) *nlp-home*))
-
-(defun sparser-load-file (&rest file-path-parts)
-  (load  (merge-pathnames (apply #' concatenate 'string file-path-parts) *nlp-home*)))
-
-(defun sparser-sourcefile (s-path-string)
-  (sparser-file "Sparser/code/s/" s-path-string))
-
-;;; load for path under SPARSER/code/s/
-;;; this is what ddm-load should be. 
-(defun s-load (source-path)
-  (load (sparser-sourcefile source-path)))
-
-(defun r3-load (source-path)
-  (load (r3-path source-path)))
 
 ;; #2 --- ASDF setup
 
@@ -77,34 +54,50 @@
         ~%You need to install one. See http://common-lisp.net/project/asdf/"))
 
 (let ((*default-pathname-defaults* cl-user::*nlp-home*))
-  (pushnew (merge-pathnames (make-pathname :directory '(:relative "util")))
+  (pushnew (merge-pathnames (make-pathname :directory 
+                                           '(:relative "util")))
            asdf:*central-registry*)
-  (unless *no-mumble*
-    (pushnew (merge-pathnames (make-pathname :directory '(:relative "Mumble" 
-                                                                    "derivation-trees")))
-             asdf:*central-registry*)))
+  (pushnew (merge-pathnames (make-pathname :directory 
+                                           '(:relative "Mumble" 
+                                                       "derivation-trees")))
+             asdf:*central-registry*))
 
 ;; #3 --- Load the utilities. Note that their exported symbols
 ;; are in the package :ddm-util, which is also the name of its asdf file
 
 (asdf:operate 'asdf:load-op :ddm-util)
 
+
+(defun sparser-file (&rest file-path-parts) ;; as strings
+  (merge-pathnames (apply #' concatenate 'string file-path-parts) 
+                   *nlp-home*))
+
+(defun sparser-load-file (&rest file-path-parts)
+  (load (sparser-file file-path-parts)))
+
+(defun sparser-sourcefile (s-path-string)
+  (sparser-file "Sparser/code/s/" s-path-string))
+
+(defun s-load (source-path)
+  (load (sparser-sourcefile source-path)))
+
+(defun r3-load (source-path)
+  (load (r3-path source-path)))
+
 ;; #4 --- Create the sparser package
-;;   We do this here so that the package is available for reference
-;;   in Mumble's code.
+;;  We do this here so that the package is available for reference
+;;  in Mumble's code. When Sparser/Mumble are part of another 
+;;  system its package may have already have been defined, but
+;;  it can be redefined or elaborated here without a problem.
 
-
-;;; apparently LISP doesnt mind it being redeclared, and this (no unless) avoids the problem in ccl
-;;; that it defines the package wrong when it opens a file to edit.
-
-;;(unless (find-package :sparser)
 (defpackage :sparser
   (:use common-lisp
         ddm-util
         #+apple ccl
         #+openmcl :ccl)
   (:nicknames :sp)
-  (:import-from :cl-user #:sparser-file #:sparser-load-file
+  (:import-from :cl-user 
+                #:sparser-file #:sparser-load-file
                 #:sparser-sourcefile #:s-load))
 
 
@@ -116,17 +109,27 @@
 
 
 ;; #5 Load Mumble
+
+(unless (boundp '*no-mumble*)
+  (defparameter *no-mumble*
+    #+sbcl t
+    #-sbcl nil
+    "Controls whether or not the Mumble language generation system
+     is loaded. There are presently issues in geting Mumble to load
+     under SBCL, which is the principal motivation for this switch,
+     but you could decide to leave it out deliberately, and this
+     switch coordinate the inclusion/omission of the requisite files."))
+
 (unless *no-mumble*
-  (load (concatenate 'string (namestring *nlp-home*) "Mumble/loader.lisp")))
-
-
+  (load (concatenate 'string (namestring *nlp-home*) 
+                     "Mumble/loader.lisp")))
 
 ;; #6  Use the selected script to pick the desired version of Sparser
 ;;  and load it.
 ;;   The easiest way to use something other than the default is to set this
 ;; variable before you load this file. Note that right now we are in the
-;; symbol package :cl-user, which is the usual default package when a Lisp
-;; is loaded.
+;; symbol package :cl-user, which is the customary default package when 
+;; a Lisp is loaded.
 
 (unless (boundp 'script) (defvar script :default))
   
@@ -159,10 +162,13 @@
 
 ;; #7 load the files from Mumble that reference types in Sparser
 
+
 (unless *no-mumble*
   (asdf:operate 'asdf:load-op :mumble-after-sparser)
   (load (concatenate 'string *mumble-location* "interface/tsro/gofers.lisp"))
   (load (concatenate 'string *mumble-location* "grammar/numbers.lisp")))
+
+
 
 
 #|  Notes
