430ddb485e196ef2a0d5e0e224e6006f7c4c3584
ddmcdonald50@gmail.com <ddmcdonald50@gmail.com@c2db2450-100d-c977-add4-9986513c572c>
2015-08-11 13:41:43 +0000

    Added flag to indicate that the caller will handle any probable acronyms

--- trunk/Sparser/code/s/analyzers/traversal/dispatch.lisp
+++ trunk/Sparser/code/s/analyzers/traversal/dispatch.lisp
@@ -1,10 +1,10 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1994-1996,2011-2013  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1994-1996,2011-2015  David D. McDonald  -- all rights reserved
 ;;; extensions copyright (c) 2010 BBNT Solutions LLC. All Rights Reserved
 ;;; 
 ;;;     File:  "dispatch"
 ;;;   Module:  "analyzers;traversal:"
-;;;  Version:  0.2 July 2013
+;;;  Version:  0.3 August 2015
 
 ;; initiated 6/15/94 v2.3.  9/26 Fixed :multiple-initial-edges bug
 ;; 9/13/95 fleshed out stub for hook being a cfr. 9/15 fixed a bug.
@@ -19,6 +19,8 @@
 ;; 0.2 (2/11/13) Adding check for an ordinary word needing to be
 ;;      reanalyzed when it appears in all caps or capitalized
 ;;     (7/23/13) Extended criteria for suspecting a hook.
+;; 0.3 (8/8/15) Added *special-acronym-handling* to stop it always
+;;      converting acronyms to proper nouns.
 
 (in-package :sparser)
 
@@ -26,10 +28,17 @@
   (defparameter *allow-large-paired-interiors* t))
 
 
+(defparameter *special-acronym-handling* nil
+  "Flag what will prohibit the creation of a proper name
+  over single-spans of :all-caps. Set when the caller want
+  to do the handling itself.")
+
+
 (defun do-paired-punctuation-interior (type
                                        pos-before-open pos-after-open
                                        pos-before-close pos-after-close)
 
+  (declare (special *special-acronym-handling*))
   (tr :paired-punct-interior type pos-after-open pos-before-close)
 
   (let ((layout (analyze-segment-layout pos-after-open
@@ -57,24 +66,25 @@
        ;; Some stock tickers and such are ordinary words and could
        ;; be captured as such. But they will be capitalized and
        ;; we can overrule that with another edge
-       (push-debug `(,first-edge))
        (when (one-word-long? first-edge)
          (when (eq (pos-capitalization (pos-edge-starts-at first-edge))
                    :all-caps)
            ;; That's enough evidence to recast the edge and take it
            ;; as an acronym or ticker symbol, but we'll leave that
            ;; to context to determine which one
-           (unless (or (eq (edge-form first-edge) (category-named 'proper-name))
+           (unless (or *special-acronym-handling*
+                       (eq (edge-form first-edge) (category-named 'proper-name))
                        (eq (edge-form first-edge) (category-named 'np)))
              ;; if so, then there's probably a hook for it and
              ;; we leave it alone.
+             (lsp-break "About to convert ordinary word to proper name")
              (convert-ordinary-word-edge-to-proper-name first-edge)))))
 
       (:contiguous-edges
        ;; ?? isn't this redundant because the interior will
        ;; constitute a segment and have already been parsed
        ;; before we traverse the closing punctuation mark?
-       (parse-between-boundaries pos-after-open pos-before-close)
+       (parse-between-parentheses-boundaries pos-after-open pos-before-close)
        (setq first-edge  ;; update the flag that controls the hook
              (right-treetop-at/edge pos-after-open)))
 
@@ -158,11 +168,7 @@
          ;; A more complex layout, which doesn't have a scheme for
          ;; hooks yet.
          (tr :pp-not-single-span layout)
-         (vanila-edge pos-before-open pos-after-close type)))
-
-       (assess-parenthesized-content layout first-edge
-                                     pos-before-open pos-after-open
-                                     pos-before-close pos-after-close))))
+         (vanila-edge pos-before-open pos-after-close type))))))
 
 
 
