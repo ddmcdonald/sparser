7e97ceb1122ac1571dca061f1fb9b6c1a3912e8e
David D. McDonald <davidmcdonald@alum.mit.edu>
2015-09-23 16:17:58 -0400

    Added the assign-lemmas to the loader

--- Sparser/code/s/grammar/model/core/kinds/2d-loader.lisp
+++ Sparser/code/s/grammar/model/core/kinds/2d-loader.lisp
@@ -1,16 +1,17 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 2013 David D. McDonald  -- all rights reserved
+;;; copyright (c) 2013-2015 David D. McDonald  -- all rights reserved
 ;;;
 ;;;     File:  "2d-loader"
 ;;;   Module:  "model;core:kinds:"
-;;;  version:  March 2013
+;;;  version:  September 2015
 
 ;; initiated 3/18/13 to hold general rules over kinds that can be stated earlier
 ;; because all of the categories involved wouldn't have been defined yet.
+;; 9/22/15 added assign-lemmas. 
 
 (in-package :sparser)
 
 (gload "kinds;relationships")
 (gload "kinds;rules-over-referents")
-
+(gload "kinds;assign-lemmas")
 
