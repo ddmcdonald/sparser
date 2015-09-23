1d1e11e720cf56694fc23d4fd8e61abb5147cd2a
David D. McDonald <davidmcdonald@alum.mit.edu>
2015-09-23 16:45:45 -0400

    Made quantified as specialization of predication

--- Sparser/code/s/grammar/rules/syntax/quantifiers.lisp
+++ Sparser/code/s/grammar/rules/syntax/quantifiers.lisp
@@ -1,17 +1,18 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 2011-2013 David D. McDonald  -- all rights reserved
+;;; copyright (c) 2011-2015 David D. McDonald  -- all rights reserved
 ;;; 
 ;;;     File:  "quantifiers"
 ;;;   Module:  "grammar;rules:syntax:"
-;;;  Version:  August 2013
+;;;  Version:  September 2015
 
 ;; initiated 8/31/11. Revised 8/22/13 so that it returns the head of
 ;; the relation (real-body) rather than simply creating the relationship
+;; 9/22/15 Make quantified specialize predication 
 
 (in-package :sparser)
 
 (define-category quantified
-  :specializes nil
+  :specializes predication
   :instantiates :self
   :binds ((quantifier . quantifier)
           (body)))
