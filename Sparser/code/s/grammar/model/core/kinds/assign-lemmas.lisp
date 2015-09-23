91077932673ae0e0802b146c1a2274394e99b63d
David D. McDonald <davidmcdonald@alum.mit.edu>
2015-09-23 16:17:28 -0400

    Added function and cases to define a categories lemma entries after the category has been defined rather than always as part of the define-category expression.

---
+++ Sparser/code/s/grammar/model/core/kinds/assign-lemmas.lisp
@@ -0,0 +1,36 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
+;;; copyright (c) 2015 David D. McDonald  -- all rights reserved
+;;;
+;;;     File:  "assign-lemmas"
+;;;   Module:  "model;core:kinds:"
+;;;  version:  September 2015
+
+;; initiated 9/22/15 so that the upper model can be loaded
+;; (in 1st-loader) without needing word machinery
+
+(in-package :sparser)
+
+
+(defmacro assign-lemma (category-name word-expr)
+  `(assign-lemma/expr ',category-name ',word-expr))
+
+(defun assign-lemma/expr (name word-expr)
+  (let ((category (category-named name :break-if-none)))
+    (setup-category-lemma category word-expr)))
+
+(assign-lemma event (:common-noun "event"))
+(assign-lemma process (:common-noun "process")) ;; used phrasally in prepositions
+(assign-lemma transition (:common-noun "transition"))
+
+(assign-lemma quality (:common-noun "quality"))
+(assign-lemma abstract (:common-noun "abstract"))
+(assign-lemma relation (:common-noun "relation"))
+
+(assign-lemma individual (common-noun "individual"))
+(assign-lemma physical (adjective "physical"))
+(assign-lemma kind (common-noun "kind"))
+(assign-lemma object (common-noun "object"))
+(assign-lemma feature (common-noun "feature"))
+(assign-lemma aggregate(common-noun "aggregate"))
+
+(assign-lemma has-name (:proper-noun "name"))
---
+++ Sparser/code/s/grammar/rules/tree-families/1st-loader.lisp
@@ -0,0 +1,7 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
+;;; copyright (c) 1992-2005,2011-2015 David D. McDonald  -- all rights reserved
+;;; extensions copyright (c) 2009 BBNT Solutions LLC. All Rights Reserved
+;;;
+;;;     File:  "1st-loader"
+;;;   Module:  "grammar;rules:tree-families:"
+;;;  version:  1.3 January 20145
