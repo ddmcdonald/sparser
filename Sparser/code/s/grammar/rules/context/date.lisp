622c6c8d39153cc2b7252f894f4c12bdf5e521fd
David D. McDonald <davidmcdonald@alum.mit.edu>
2015-10-06 13:11:46 -0400

    #+ignored a function that wasn't finished and irritating the compiler

--- Sparser/code/s/grammar/rules/context/date.lisp
+++ Sparser/code/s/grammar/rules/context/date.lisp
@@ -1,11 +1,16 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
-;;; copyright (c) 1992  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992,2015  David D. McDonald  -- all rights reserved
 ;;; 
 ;;;     File:  "date"
 ;;;   Module:  "analyzers;context:"
 ;;;  Version:  June 1992
 
 (in-package :sparser)
+
+;; Written in June 1992
+;; 10/6/15 #+ignored pull-date-out-of-edge-sequence to quiet the
+;; compiler. And this may be moot in any case unless that old
+;; corpus is reused.
 
 
 ;;;----------------------------------------------------------------
@@ -16,7 +21,7 @@
   "This will be bound to the value of the DD field in a DCI or
    Tipster article.  It is a #<date>.")
 
-
+#+ignore ;; unless/until we find a caller
 (defun pull-date-out-of-edge-sequence (start-pos end-pos)
   (declare (ignore start-pos end-pos)))
 
