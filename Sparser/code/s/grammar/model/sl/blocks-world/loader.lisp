cf460eae19655df63341557b4e9d2011dc65dd33
David D. McDonald <davidmcdonald@alum.mit.edu>
2015-10-08 11:01:32 -0400

    First version of blocks world loader

---
+++ Sparser/code/s/grammar/model/sl/blocks-world/loader.lisp
@@ -0,0 +1,16 @@
+;;; -*- Mode: Lisp; Syntax: Common-lisp; -*-
+;;; Copyright (c) 2015 David D. McDonald  All Rights Reserved
+;;;
+;;;      File:  "loader"
+;;;    Module:  grammar/model/sl/blocks
+;;;   version:  October 2015
+
+;; Initiated 10/6/15
+
+(in-package :sparser)
+
+(when (find-package :mumble)
+  (gload "blocks;experiments"))
+; (gload "blocks;xx")
+; (gload "blocks;xx")
+
