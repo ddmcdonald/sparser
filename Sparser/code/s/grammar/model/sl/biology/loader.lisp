6205ddfc5c109c325701e89ccfbe5f091e8da4c7
David D. McDonald <davidmcdonald@alum.mit.edu>
2015-10-09 19:46:26 -0400

    Added the examples file

--- Sparser/code/s/grammar/model/sl/biology/loader.lisp
+++ Sparser/code/s/grammar/model/sl/biology/loader.lisp
@@ -3,7 +3,7 @@
 ;;;
 ;;;    File: "loader"
 ;;;  Module: "grammar/model/sl/biology/
-;;; version: August 2015
+;;; version: October 2015
 
 ;; Initiated 11/5/13. 3/3/14 Added mechanics and NFkappaB while
 ;; commenting out the original molecules and verbs as OBE and requiring
@@ -18,6 +18,7 @@
 ;; when xmls moved out of normal Sparser load. 6/4/15 added [rhetoric]
 ;; so all elements can go in one place. 8/3/15 added [methods] to
 ;; avoid load-time category dependencies. It must be loaded last.
+;; 10/8/15 added examples.
 
 (in-package :sparser)
 
@@ -49,4 +50,6 @@
 ;(gload "bio;molecules")
 
 (gload "bio;methods.lisp") ;; this one goes last
+
+(gload "bio;examples.lisp") ;; no definitions in here
 
