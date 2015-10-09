bbd6567563e54d36d17d346a4fab193adb5f5165
David D. McDonald <davidmcdonald@alum.mit.edu>
2015-10-09 19:49:19 -0400

    Moved two general functions with an eye towards not loading psi in the first place

--- Sparser/code/s/objects/model/psi/object1.lisp
+++ Sparser/code/s/objects/model/psi/object1.lisp
@@ -1,9 +1,9 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
-;;; copyright (c) 1999-2000,2011 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1999-2000,2011,2015 David D. McDonald  -- all rights reserved
 ;;;
 ;;;     File:  "object"
 ;;;   Module:  "objects;model:psi:"
-;;;  version:  1.0 August 2009
+;;;  version:  1.1 October 2015
 
 ;; initiated 2/14/99 pulling bits and pieces from other files. 
 ;; Continued populating it through 2/15. 9/6 Corrected the definition
@@ -13,36 +13,16 @@
 ;; 1.0 (7/22/09) fan-out from variable and psi make-over. Redid category-of-psi
 ;;  now that we can avoid climbing. Working on it through 8/7
 ;;  (3/21/11) Added saturated? predicate.
+;; 1.1 (10/9/15) Removed saturated? and corresponding-lattice-point
+;;  to the LP operations file with an eye towards conditionaling the
+;;  loading of the whole psi directory on whether the don't use flag
+;;  is up.
 
 (in-package :sparser)
-
-;;;------------
-;;; predicates
-;;;------------
-
-(defun saturated? (lp)
-  (let ((bound (lp-variables-bound lp))
-        (possible (lp-variables-free (lp-top-lp lp))))
-    ;; Assumes that there's just one set of variables
-    (= (length bound) (length possible))))
 
 ;;;-----------
 ;;; accessors
 ;;;-----------
-
-(defun corresponding-lattice-point (unit)
-  (typecase unit
-    (psi
-     (psi-lp unit))
-    (individual
-     (cat-lattice-position (first (indiv-type unit))))
-    ;; Do individuals and categories point to the same lattice point for
-    ;; purposes of recording realization-nodes? 
-    (referential-category 
-     (cat-lattice-position unit))
-    (otherwise
-     (error "It doesn't make sense (or does it) to look up the ~
-             lattice-point of a~%~a" unit))))
 
 (defun category-of-psi (psi)
   (base-category-of-psi psi))
