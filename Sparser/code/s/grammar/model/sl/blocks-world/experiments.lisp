f9e7a22af9c3fd393056acc97a1ec4df84445a4d
David D. McDonald <davidmcdonald@alum.mit.edu>
2015-10-15 12:58:04 -0400

    Standalone code to say "build a staircase" from first principles but no KR source. That comes later.

--- Sparser/code/s/grammar/model/sl/blocks-world/experiments.lisp
+++ Sparser/code/s/grammar/model/sl/blocks-world/experiments.lisp
@@ -9,10 +9,27 @@
 
 (in-package :mumble)
 
-;; "let's build a staircase"
-#|
-(defun build-a-staircase ()
-  (let ((staircase
-         (define-lexicalized-phrase common-noun ("staircase") (n))
-
+#| general-np-bundle-driver  derivation-tree  accessory-type
+ make-adjunction-node  feature-driven-prepocessing  realize-dtn
+ phrase-structure-execution  process-negate-accessory
+ add-further-specification  define-word-stream-action-type
 |#
+;; "let's build a staircase"
+
+(defun build-a-staircase ()
+  "Constructs the derivation tree strictly from resources,
+   not as the realization of some actually represented source."
+  (let* ((staircase
+          (make-dtn :resource
+            (define-lexicalized-phrase common-noun ("staircase") (n))))        
+         (a-staircase
+          (kind (singular staircase)))
+         (build
+          (make-dtn :resource
+                    (define-lexicalized-phrase SVO ("build") (v)))))
+; 
+    ;; connect them
+    (make-complement-node 'o a-staircase build)
+    (command build)
+    build))
+
