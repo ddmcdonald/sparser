41667761f59523aa421a46897c7e113feb0cfd52
David D. McDonald <davidmcdonald@alum.mit.edu>
2015-11-01 13:41:17 -0500

    Restructured to set up for handling multi-colon patterns

--- Sparser/code/s/analyzers/psp/patterns/colon-patterns.lisp
+++ Sparser/code/s/analyzers/psp/patterns/colon-patterns.lisp
@@ -22,39 +22,35 @@
         (break "New edge colon pattern ~a" label-pattern)
         (edge-that-punts-edge-inside-pattern words start-pos end-pos edges)))))
 
-(defun resolve-colon-pattern (pattern words colon-positions start-pos end-pos)
+(defun resolve-colon-pattern (pattern words edges colon-positions start-pos end-pos)
   ;; called from ns-pattern-dispatch when the only puctuation is a colon
-  ;; (push-debug `(,pattern ,words ,hyphen-positions ,start-pos ,end-pos))
+  ;; (push-debug `(,pattern ,words ,colon-positions ,start-pos ,end-pos))
   ;; (break "starting colon pattern: ~a" pattern)
-  (let ((count 0))
-    (dolist (item pattern)
-      (when (eq item :colon ) (incf count)))
-    (case count
-      (1 (one-colon-ns-patterns
-          pattern words colon-positions start-pos end-pos))
-      (otherwise
-       (when *work-on-ns-patterns*
-         (push-debug `(,pattern ,words ,colon-positions ,start-pos ,end-pos))
-         (error "Write the code for ~a colons in a no-space sequence" count))))))
+  (if (null (cdr colon-positions))
+    (one-colon-ns-patterns
+     pattern words edges colon-positions start-pos end-pos)
+    (multi-colon-ns-patterns
+     pattern words edges colon-positions start-pos end-pos)))
 
-(defun one-colon-ns-patterns (pattern words colon-positions start-pos end-pos)
+(defun one-colon-ns-patterns (pattern words edges colon-positions start-pos end-pos)
   ;; called from resolve-colon-pattern
   (cond
-   ((or
-     (equal pattern '(:single-digit :colon :single-digit))
-     (equal pattern '(:single-digit :colon :digits))
-     (equal pattern '(:digits :colon :single-digit))
-     (equal pattern '(:digits :colon :digits)))
+   ((equal pattern '(:protein :colon :protein))
+    (make-ns-pair 'protein (first edges) (second edges)
+                  words start-pos end-pos))
+   ((or (equal pattern '(:single-digit :colon :single-digit))
+        (equal pattern '(:single-digit :colon :digits))
+        (equal pattern '(:digits :colon :single-digit))
+        (equal pattern '(:digits :colon :digits)))
     (make-number-colon-number-structure 
      (right-treetop-at/edge start-pos) 
      (left-treetop-at/edge end-pos)))
-   ((or
-     (equal pattern '(:full :colon))
-     (equal pattern '(:lower :colon))
-     (equal pattern '(:lower :colon :full))
-     (equal pattern '(:lower :colon :lower))
-     (equal pattern '(:full :colon :full))
-     (equal pattern '(:full :colon :lower)))
+   ((or (equal pattern '(:full :colon))
+        (equal pattern '(:lower :colon))
+        (equal pattern '(:lower :colon :full))
+        (equal pattern '(:lower :colon :lower))
+        (equal pattern '(:full :colon :full))
+        (equal pattern '(:full :colon :lower)))
     (make-word-colon-word-structure 
      (right-treetop-at/edge start-pos) 
      (left-treetop-at/edge end-pos)))
@@ -67,6 +63,18 @@
    (t (tr :no-ns-pattern-matched)
       (reify-ns-name-and-make-edge words start-pos end-pos))))
 
+
+(defun multi-colon-ns-patterns (pattern words edges colon-positions start-pos end-pos)
+  ;; called from ns-patterns/edge-colon-edge when the only punctuation
+  ;; in the sequence is colon. Do a divide and recombine
+  (push-debug `(,pattern ,words ,edges ,colon-positions ,start-pos ,end-pos))
+  ;; (setq pattern (nth 0 *) words (nth 1 *) edges (nth 2 *) colon-positions (nth 3 *) pos-before (nth 4 *) pos-after (nth 5 *))
+  (lsp-break "multi-colon"))
+
+
+;;;------------------
+;;; colon and hyphen
+;;;------------------
 
 ;; endogenous C-RAF:B-RAF heterodimers
 (defun divide-and-recombine-ns-pattern-with-colon (pattern words 
