a866de4d69559ace04e779b79d05e2db3d64ef12
David D. McDonald <davidmcdonald@alum.mit.edu>
2015-11-01 13:40:34 -0500

    Started process of adding regresion tests for particular patterns

---
+++ Sparser/code/s/grammar/model/sl/biology/citations.lisp
@@ -0,0 +1,17 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER COMMON-LISP) -*-
+;;; Copyright (c) 2015 SIFT LLC. All Rights Reserved
+;;;
+;;;    File: "citations"
+;;;  Module: "grammar/model/sl/biology/
+;;; version: October 2015
+
+(in-package :sparser)
+
+(define-citation "Smad1/HsN3 complex."
+  (1 BIO-COMPLEX 7))
+
+(define-citation "EphB1-induced."
+  (1 INDUCE 5))
+
+(define-citation "Y420."
+  (1 RESIDUE-ON-PROTEIN 3))
