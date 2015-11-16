44fcc43353fc1f6cc0aa0ec517b19eba8a3a81e7
Rusty Bobrow <rjbobrow@gmail.com>
2015-11-15 20:58:58 -0500

    Minor improvement in handling of noun-verb ambiguity, as in "these results led to" where the "results" is allowed to be a noun

--- Sparser/code/s/drivers/chart/psp/chunker1.lisp
+++ Sparser/code/s/drivers/chart/psp/chunker1.lisp
@@ -467,10 +467,7 @@
      (or
       (and (plural-noun-and-present-verb? e)
            (loop for ee in (ev-edges (pos-starts-here (pos-edge-ends-at e)) )
-             thereis 
-             (and
-              (not (eq (cat-name (edge-form ee)) 'verb+ed)) ;; "these results led to..."
-              (ng-start? ee))))
+             thereis (ng-start? ee)))
       ;;in fact nothing should follow a pronoun (except a possessive pronoun)
       ;;(not (eq category::time-unit (edge-category e))) WHY WAS THIS HERE? WE NEED TO HANDLE "3 HOURS"
       #+ignore 
