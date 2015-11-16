2f8bcde89aff433e39f80eed0d69832a138ea240
David D. McDonald <davidmcdonald@alum.mit.edu>
2015-11-16 11:19:04 -0500

    Added let-us using a different scheme, but has something wrong with it.

--- Sparser/code/s/grammar/model/sl/blocks-world/experiments.lisp
+++ Sparser/code/s/grammar/model/sl/blocks-world/experiments.lisp
@@ -3,9 +3,9 @@
 ;;;
 ;;;      File:  "experiments"
 ;;;    Module:  grammar/model/sl/blocks
-;;;   version:  October 2015
+;;;   version:  November 2015
 
-;; Initiated 10/7/15
+;; Initiated 10/7/15. 
 
 (in-package :mumble)
 
@@ -14,22 +14,74 @@
  phrase-structure-execution  process-negate-accessory
  add-further-specification  define-word-stream-action-type
 |#
-;; "let's build a staircase"
+
+;;;------------
+;;; vocabulary
+;;;------------
+#| Define-lexicalized-phrase build nouns by default. If we start
+in Sparser there is a translation routine. In a better world
+rebuilt from scratch there would be a single word stock.
+See Zo code for what that could look like. |#
+
+(define-word "build" (verb) :ed-form "built")
+(define-word "let" (verb))
+
+;;;--------------------------
+;;; Prebuilt phrases (trees)
+;;;--------------------------
+
+;; "build a staircase   (say (build-a-staircase))
 
 (defun build-a-staircase ()
   "Constructs the derivation tree strictly from resources,
    not as the realization of some actually represented source."
   (let* ((staircase
-          (make-dtn :resource
-            (define-lexicalized-phrase common-noun '("staircase") (n))))
+          (make-dtn 
+           :resource (define-lexicalized-phrase common-noun ("staircase") (n))
+           :referent 'build-staircase))
          (a-staircase
           (kind (singular staircase)))
          (build
           (make-dtn :resource
-                    (define-lexicalized-phrase SVO '("build") (v)))))
-; 
+                    (define-lexicalized-phrase SVO ("build") (v)))))
+
     ;; connect them
-    (make-complement-node 'o a-staircase build)
-    (command build)
+    (make-complement-node 'o a-staircase build) ;; bind argument
+    (command build) ;; hack that removes subject
     build))
+
+
+;; "let's X"
+
+(defun let-us (dtn-for-eventuality)
+  ;; Doing this one differently. The question is what is 
+  ;; the best syntatic sugar. This version open-codes much
+  ;; of what's in create-lexicalized-phrase as it wasn't clear
+  ;; how to push the pronoun through that function as it's
+  ;; presently written. 
+  (let* ((let-phrase  ;; s v o c
+          (define-lexicalized-phrase SVOC ("let") (v)))
+         (1st-plural ;; the word
+          (mumble-value 'first-person-plural ;; see gramar/pronouns.lisp
+                        'pronoun))
+         (pn-phrase (phrase-named 'pronominal-np))
+         (pn-parameter (first (parameters-to-phrase pn-phrase)))
+         (binding (make-instance 'parameter-value-pair
+                    :phrase-parameter pn-parameter
+                    :value 1st-plural))
+         (pn-lexicalized-phrase
+          (make-instance 'saturated-lexicalized-phrase
+            :phrase pn-phrase
+            :bound (list binding))))
+
+    (let* ((dtn (make-instance 'derivation-tree-node
+                  :resource let-phrase)))
+      (make-complement-node 'o pn-lexicalized-phrase dtn)
+      (make-complement-node 'c dtn-for-eventuality dtn)
+      (command dtn)
+      dtn)))
+
+; (say (let-us (build-a-staircase)))
+
+        
 
