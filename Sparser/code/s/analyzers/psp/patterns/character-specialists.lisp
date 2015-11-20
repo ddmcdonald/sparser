2ddac22d98974c3345de17dc938487e952bdeaae
David D. McDonald <davidmcdonald@alum.mit.edu>
2015-11-20 10:45:43 -0500

    Moved out hyphen routines as part of getting clearer organization

--- Sparser/code/s/analyzers/psp/patterns/character-specialists.lisp
+++ Sparser/code/s/analyzers/psp/patterns/character-specialists.lisp
@@ -9,179 +9,16 @@
 ;; scan. Tweaking through 10/9/14. Removed slash routines 12/10/14 as
 ;; obsolete. Renewed tweaking/fanout through 5/17/15
 ;; 5/30/2015 short-term patch for problems with ; as in "...with β-, γ-, and α-catenins..."
-;; 10/6/15 Commented out unfinished routine that isn't called
+;; 10/6/15 Commented out unfinished routine that isn't called. Moved out
+;; more general element 11/19/15
 
 (in-package :sparser)
-(defvar *WORK-ON-NS-PATTERNS*)
 
-;;;----------
-;;; hyphens
-;;;----------
-
-(defun nospace-hyphen-specialist (words edges pattern hyphen-position/s 
-                                  pos-before next-position)
-  ;; Cleanup case in one-hyphen-ns-patterns when no defined pattern
-  ;; has matched. 
-  (push-debug `(,hyphen-position/s ,pos-before ,next-position))
-  ;; (setq hyphen-position/s (car *) pos-before (cadr *) next-position (caddr *))
-  (let ((hyphen-count (length hyphen-position/s))
-        (phrase-length (- (pos-token-index next-position)
-                          (pos-token-index pos-before))))
-    (cond
-     ((and (= hyphen-count 1)
-           (= phrase-length 3))
-      ;; Canonical case. "GTP-bound"
-      (let* ((left-edge (first edges))
-             (right-edge (third edges)))
-        (tr :hyphen-specialist left-edge right-edge)
-        (let ((rule
-               (when (and (edge-p left-edge)
-                          (edge-p right-edge))
-                 ;; otherwise we just have unknown words without
-                 ;; preterminal edges so they're not going to compose.
-                 (or
-                  ;; Can we look and know which edge -should- be the
-                  ;; head? If we just assume its the right-edge and
-                  ;; the left-edge is the theme then we will prefer
-                  ;; a direct-object reading:
-                  (multiply-edges right-edge left-edge)
-                  ;; but it that doesn't work lets try the other order
-                  ;; just to get something (as better than nothing)
-                  (multiply-edges left-edge right-edge)))))
-          (if rule
-            (let ((edge (make-completed-binary-edge left-edge right-edge rule)))
-              ;;//// trace goes here
-              (revise-form-of-nospace-edge-if-necessary edge right-edge))
-            (else ;; make a structure if all else fails
-              ;; but first alert to anticipated cases not working
-              (unless (or (word-p left-edge) (word-p right-edge))
-                ;; hyphenated structure assumes it has edges to work with
-                (make-hyphenated-structure left-edge right-edge)))))))
-     ((= hyphen-count 1)
-      ;; E.g. "Figures S1A–S1D"
-      ;; split down the middle, run the two parts through the 
-      ;; pattern reifier, then combine them.
-      (let ((hyphen-pos (car hyphen-position/s)))
-        (cond
-         ((eq hyphen-pos pos-before) ;; it's initial
-          ;; in "green fluorescent protein (GFP)-tagged ERK1"
-          ;; the scanner won't cross the parentheses so we
-          ;; get "-tagged"
-          (resolve-initial-stranded-hyphen 
-           pattern words pos-before next-position))
-         ((eq hyphen-pos next-position) ;; its final
-          (resolve-stranded-hyphen 
-           pattern words pos-before next-position))
-         (t
-          (let ((first-half (resolve-hyphen-segment 
-                             edges pos-before hyphen-pos))
-                (second-half (resolve-hyphen-segment 
-                              edges (chart-position-after hyphen-pos) next-position)))
-            (if (and first-half second-half)
-              (make-hyphenated-structure first-half second-half)
-              (if *work-on-ns-patterns*
-                (then
-                  (push-debug `(,first-half ,second-half))    
-                  (break "One of the patterns to either side of a hyphen ~
-                         did not resolve."))
-                ;; If not working on it, completely punt
-                (reify-ns-name-and-make-edge 
-                 (words-between pos-before next-position) 
-                 pos-before next-position))))))))
-        
-     (t
-      (when *work-on-ns-patterns*
-        (break "New case for hyphens~%  hyphen count = ~a~
-              ~%  phrase-length = ~a" hyphen-count phrase-length))
-      (reify-ns-name-and-make-edge 
-       (words-between pos-before next-position) pos-before next-position)))))
-
-
-;;/// This is surely the same a resolve-slash-segment so they should mergw
-(defun resolve-hyphen-segment (edges start-pos end-pos)
-  (if (eq start-pos end-pos) ;; as in "...with β-, γ-, and α-catenins..."
-    (when *work-on-ns-patterns*
-      (break "start-pos and end-pos are identical"))
-    (let ((single-edge (span-covered-by-one-edge? start-pos end-pos)))
-      (or single-edge
-          (let ((words (words-between start-pos end-pos))
-                (pattern (characterize-words-in-region start-pos end-pos nil)))
-            (let ((*work-on-ns-patterns* nil)) ;; t))
-              (declare (special *work-on-ns-patterns*))
-              (let ((result (resolve-ns-pattern pattern words edges start-pos end-pos)))
-               (unless result 
-                 (push-debug `(,pattern ,words ,start-pos ,end-pos))
-                 ;; (setq pattern (car *) words (cadr *) start-pos (caddr *) end-pos (cadddr *))
-                 (when *work-on-ns-patterns*
-                   (break "no result on hyphen segment pattern: ~a" pattern)))
-               result)))))))
 
 (defun make-edge-into-adjective (edge)
   (declare (special category::adjective))
   (setf (edge-form edge) category::adjective)
   edge)
-
-
-;;;------------------------------------
-;;; Combine them by binding a variable
-;;;------------------------------------
-
-(defun second-imposes-relation-on-first? (right-ref right-edge)
-  (let ((form (edge-form right-edge)))
-    (when (memq (cat-symbol form) '(category::verb+ed ;; assume passive
-                                    category::adjective))
-      ;; now figure out what variable on the second (right)
-      ;; should be bound to the first (left)
-      (let ((variable 
-             (if (eq (cat-symbol form) 'category::adjective)
-                 (let
-                     ((vars (cat-slots 
-                             (if (category-p right-ref)
-                                 right-ref
-                                 (itype-of right-ref))))
-                      (sv (subject-variable right-ref)))
-                   (loop for v in vars when (not (eq v sv))
-                     do (return v)))
-                 (subject-variable right-ref))))
-        ;; Which variable this is really depends on the two referents.
-        ;; For the induced example its an agent (= subject). But the
-        ;; tyrosine goes on the site variable of the phosphoryate.
-        ;; For right now, binding the subject and letting the chips
-        ;; fall as they may. Elevating the right edge as the head
-        ;; but making it an adjective overall. 
-        (unless variable
-          ;;/// clear motivation for structure on variables on perhaps
-          ;; on the mixing in of categories for this same purpose
-          ;; Default to modifier ??
-          (setq variable
-                (single-on-variable-on-category right-ref)))
-        variable))))
-
-;; "EphB1-induced", "tyrosine-phosphorylated"
-(defun do-relation-between-first-and-second (left-ref right-ref left-edge right-edge)
-  ;; Called from resolve-hyphen-between-two-words when the predicate just above 
-  ;; is non-nil.
-  (declare (special category::adjective))
-
-  (tr :make-right-head-with-agent-left)
-
-  (when (category-p right-ref)
-    (setq right-ref (make-individual-for-dm&p right-ref)))
-
-  (let* ((variable (second-imposes-relation-on-first? right-ref right-edge))
-         (edge
-          (make-binary-edge/explicit-rule-components
-            left-edge right-edge
-            :category (edge-category right-edge)
-            :form category::adjective
-            :referent (bind-dli-variable variable left-ref right-ref)
-            :rule-name 'form-relation-between-first-and-second)))
-    (tr :no-space-made-edge edge)
-    edge))
-
-
-
-  
 
 
 ;;;-----------------------
@@ -215,11 +52,6 @@
                       :words words)))
           (tr :made-edge edge)
           edge)))
-
-
-;      (tr :scare-quotes-wrong-number-of-words (length words)))
-
-
 
 
 
