8ba02a3c4685e6c04d58035f65693a777c56708d
David D. McDonald <davidmcdonald@alum.mit.edu>
2015-11-27 16:25:01 -0500

    Deleted unused Sparser-oriented code to make file easier to load

--- Mumble/derivation-trees/builders.lisp
+++ Mumble/derivation-trees/builders.lisp
@@ -6,83 +6,11 @@
 
 ;; Initated 11/20/13 to package up reusable parameterized
 ;; derivation tree patterns at roughly the level of maximal projections.
+;; 11/25/15 Moving away or deleting Sparse-oriented code so this can be 
+;; loaded when the rest of Mumble is loaded rather than afterwards. 
 
 (in-package :mumble)
 
-;;--- just throwing it together to see what's the way to do these
-
-(defun predicated-attribute-value (&key attribute value of)
-  ;; This can be expanded to a proper realization-class giving all
-  ;; the options, but for now just one case to provide a thread
-  ;; all the way through
-  (push-debug `(,attribute ,value ,of))
-  (multiple-value-bind (phrase parameters)
-                       (provide-phrase-and-data 'S-be-COMP)
-    (let ((dt (make-instance 'derivation-tree-node
-                :resource phrase)))
-      (set-derivation-tree dt)
-      ;; In these patterns, it's typically the canse that the
-      ;; natural referent for the subject is the same as the 
-      ;; complement (value). So we setup a special dt for
-      ;; the subject that talks about it as an instance of class.
-      (let ((subj-dt (definite (instance-of-a-class of))))
-        (make-complement-node (first parameters) subj-dt dt)
-        (make-complement-node (second parameters) value dt)
-        (present dt)))))
-
-(defun simple-2-arg-phrase (phrase-name arg1 arg2)
-  (multiple-value-bind (phrase parameters)
-                       (provide-phrase-and-data phrase-name)
-    (let ((dtn (make-instance 'derivation-tree-node
-                 :resource phrase)))
-      (set-derivation-tree dtn)
-      (make-complement-node (first parameters) arg1 dtn)
-      (make-complement-node (second parameters) arg2 dtn)
-      ;; caller applies any needed operators
-      dtn)))
-
-
-
-(defmethod instance-of-a-class ((i sparser::individual))
-  ;; look up the type, get its realization (as an np head) and
-  ;; put together a bare np. Expect the caller to set the
-  ;; definiteness.
-  (let* ((category (sparser::itype-of i))
-         (noun (realization-for category))
-         (np (phrase-named 'common-noun))
-         (noun-parameter (car (parameters-to-phrase np)))
-         (dt (make-instance 'derivation-tree-node
-               :referent category
-               :resource np)))
-    (make-complement-node noun-parameter noun dt)
-    (push-debug `(,dt))
-    dt))
-
-;;;-------
-
-(defun feature-driven-prepocessing (features dtn)
-  ;; This is an experiment that could turn into a hook when
-  ;; the editing style of 'transformations' matures.
-  ;; Writing it with just one case in mind, as more cases
-  ;; accumulate might want to move from location inside
-  ;; realize-dtn in the lexicalized-phrase case
-  (when features
-    (when (assoc (accessory-type-named :command) features)
-      ;; bind the value of the subject to a trace
-      (let* ((lexp (resource dtn))
-             (free-vars (free lexp))
-             (subject (parameter-named 's))
-             (trace (build-trace nil))) 
-        ;; replace the nil of the trace with designator for
-        ;; the interlocutor ("you")
-        (unless (memq subject free-vars)
-          (error "The subject isn't free. Something's wrong"))
-        (make-complement-node subject trace dtn)))))
-
-
-
-
-;;;-------
 
 (defmacro define-lexicalized-phrase (phrase words arguments)
   `(create-lexicalized-phrase ',phrase ',words ',arguments))
