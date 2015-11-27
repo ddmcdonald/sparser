d8bf0f4dfdf6575151910e157489c90438d6e216
David D. McDonald <davidmcdonald@alum.mit.edu>
2015-11-27 18:00:18 -0500

    Stop loading edge-patterns. Still may have useful patterns to mine

--- Sparser/code/s/analyzers/psp/patterns/loader.lisp
+++ Sparser/code/s/analyzers/psp/patterns/loader.lisp
@@ -12,6 +12,7 @@
 ;; [scan-gophers] and [charaterize-words] 5/15/15. Added [edge-patterns],
 ;; [slash-patterns], [hyphen-patterns], [colon-patterns]. 
 ;; 11/12/15 added [edge-gophers], [pattern-resolvers]. 11/19/15 [hyphen-subrs}
+;; 11/27/15 remove edge-patterns from load. 
 
 (in-package :sparser)
 
@@ -19,10 +20,11 @@
 
 (lload "scan-patterns;patterns")
 
-(lload "scan-patterns;edge-patterns")
+;;(lload "scan-patterns;edge-patterns") mine for useful parts then delete
 (lload "scan-patterns;slash-patterns")
 (lload "scan-patterns;hyphen-patterns")
 (lload "scan-patterns;colon-patterns")
+(lload "scan-patterns;protein-patterns")
 
 (lload "scan-patterns;hyphen-subrs")
 
@@ -35,7 +37,6 @@
 (lload "scan-patterns;scan-gophers") ;; help the scan
 (lload "scan-patterns;charaterize-words")
 (lload "scan-patterns;uniform-scan") ;; driver / entry-point
-(lload "scan-patterns;protein-patterns")
 
 (lload "scan-patterns;take transitions")
 (lload "scan-patterns;start")  ;; empty as of 10/5/95
