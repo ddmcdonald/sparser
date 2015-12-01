62e9366471a04aac67399d23b9e375558cced219
David D. McDonald <davidmcdonald@alum.mit.edu>
2015-12-01 15:31:40 -0500

    Stub for a t.b.d. incremental parsing protocal as per our CwC proposal. No contents as yet. Will likely start with the C3 parsing protocal as the model.

---
+++ Sparser/code/s/drivers/chart/psp/incremental.lisp
@@ -0,0 +1,13 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
+;;; copyright (c) 2015 David D. McDonald  -- all rights reserved
+;;; This file is part of the SIFT-Brandeis CwC project
+;;;
+;;;     File:  "incremental"
+;;;            drivers/chart/psp/
+;;;  version:  November 2013
+
+;; Initiated 11/30/15 for a new toplevel driver that runs
+;; incrementally left to right in a grounding situation. 
+
+(in-package :sparser)
+
