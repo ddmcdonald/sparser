84867d4e1f4874d9f04e1dfa37658cd8bddb88cc
David D. McDonald <davidmcdonald@alum.mit.edu>
2015-12-03 20:41:24 -0500

    Moved nearly all the contents out to mid-level

--- Sparser/code/s/grammar/model/sl/ISR/draft-categories.lisp
+++ Sparser/code/s/grammar/model/sl/ISR/draft-categories.lisp
@@ -1,20 +1,21 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 2013 David D. McDonald  -- all rights reserved
+;;; copyright (c) 2013-2015 David D. McDonald  -- all rights reserved
 ;;; This file is part of the SIFT-Brandeis C3 project
 ;;;
 ;;;     File:  "draft-categories"
 ;;;   Module:  "grammar/sl/ISR/
-;;;  version:  June 2014
+;;;  version:  December 2014
 
 ;; Initiated 10/9/13 to hold the more general categories that
 ;; we're only trying out for C3. Pulling out parts through 6/14. 
+;; 12/3/15 Moved most of this to core/mid-level/ontology.lisp
 
 (in-package :sparser)
 
 #| color -- on a physical surface. Vs. a weather front or anything else 
  that is visible only because its contents are. Some materials are transparent. |#
 (define-mixin-category physical-surface
-  :specializes nil ;; ???  border ?
+  :specializes quality  ;; maybe border?
   :binds ((color . color))
   :realization ((:tree-family premodifier-adds-property
                  :mapping ((property . color)
@@ -23,63 +24,4 @@
 ;                (:tree-family thing-is-description
 ;                 :mapping ((
 ))
-
-;;---- These migrate to the upper model
-
-;; What's the point of Endurant as a predicate-contributing category?
-;;   supplies identity somehow
-;;   and type ??  
-;;   predicates associated with its lifetime
-
-;;--- thin thread to handle the 1st NP
-
-
-
-(define-category named-type
-  :specializes has-name
-  :binds ((type-of :primitive category))
-  :realization (:common-noun name)
-  :documentation
-    "Describes or names something. Not a thing itself (not physical)
- because if anything it designates the whole class of the stuff 
- it names. The type-of variable indicates what actual kind of thing
- to instantiate when we have reason to deference this type to something
- physical.")
-
-
-
-(define-category artifact
-  :specializes physical  
-  :binds ((made-by . maker-of-artifacts))
-  ;; also time-created or is it a specialization from the lifetime of Endurant?
-  :realization (:common-noun "artifact"))
-
-
-(define-category make-artifacts
-  :specializes process 
-  :binds (;; (maker . agent) [1]
-          (procedure . process) ;; though instructions aren't a process
-            ;; but what is this by itself?
-          (product ;; ???? Would growing tomatoes count?
-           . artifact)
-          (type-of-product . named-type)))
-; [1] We get more milage if we remove the agent, because then
-; we can treat Ford as an agent and mix this in.
-; It would nice to have a notion of 'activity' which is a description
-; of a process, where in order to carry out the process you need to
-; incorporate an agent. 
-
-(define-category maker-of-artifacts
-  :specializes agent
-  :mixins (make-artifacts named-type)) ;;has-name))
-;; has-name makes proper nouns, hack "ford" to be a common noun.
-
-;; Would "manufacturer" add anything useful? Maybe distinguish crofters
-;; in the Shetlands knitting sweaters from industrial giants?
- ;; ?? company (corporate entity)
-      
-
-
-
-
 
