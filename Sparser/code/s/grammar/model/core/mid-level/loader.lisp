3665e14a07c11f374545b4a47aed14acd33bf855
David D. McDonald <davidmcdonald@alum.mit.edu>
2015-12-03 20:48:58 -0500

    Loaders for the mid-level and extensions to qualites and blocks world

---
+++ Sparser/code/s/grammar/model/core/mid-level/loader.lisp
@@ -0,0 +1,13 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
+;;; copyright (c) 2014-2015 David D. McDonald  -- all rights reserved
+;;;
+;;;     File:  "loader"
+;;;   Module:  "model;core:mid-level:"
+;;;  version:  December 2015 
+
+;; Initiated 12/3/15
+
+(in-package :sparser)
+
+(gload "mid-level;ontology")
+(gload "mid-level;relations")
