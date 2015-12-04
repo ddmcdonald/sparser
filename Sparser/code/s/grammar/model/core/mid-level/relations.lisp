0a6868baf67d84c5752f38058179f7fc41ffd9a0
David D. McDonald <davidmcdonald@alum.mit.edu>
2015-12-03 20:45:51 -0500

    Fixed the headers

--- Sparser/code/s/grammar/model/core/mid-level/relations.lisp
+++ Sparser/code/s/grammar/model/core/mid-level/relations.lisp
@@ -1,6 +1,11 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
 ;;; copyright (c) 2014-2015 David D. McDonald  -- all rights reserved
 ;;;
-;;;     File:  "verbs"
-;;;   Module:  "model;core:relations:"
+;;;     File:  "relations"
+;;;   Module:  "model;core:mid-level::"
 ;;;  version:  December 2015 
+
+;; Initiated 12/3/15
+
+(in-package :sparser)
+
