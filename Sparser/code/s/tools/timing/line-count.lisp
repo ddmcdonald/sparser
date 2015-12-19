135a16e456b81b9ecb27b887fae99ead78968f14
Alex Plotnick <aplotnick@sift.net>
2015-12-19 15:29:09 -0500

    Clean up line counting routines
    Also more correct: previously, toplevel forms that were contained in #| ... |#
    comments were also counted.

--- Sparser/code/s/tools/timing/line-count.lisp
+++ Sparser/code/s/tools/timing/line-count.lisp
@@ -13,73 +13,66 @@
 ;;; accumulators
 ;;;--------------
 
-(defvar *total-line-count* nil)
-(defvar *total-toplevel-forms* nil)
-(defvar *total-number-of-files* nil)
+(defvar *total-line-count* 0)
+(defvar *total-toplevel-forms* 0)
+(defvar *total-number-of-files* 0)
 
+(defun reset-line-count-accumulators ()
+  (setq *total-line-count* 0
+        *total-toplevel-forms* 0
+        *total-number-of-files* 0))
+
+(defun report-line-counts ()
+  (format t "~&~%~
+               ~%-----------------------------------------~
+               ~%  ~A files~
+               ~%  ~A lines of code~
+               ~%  ~A toplevel forms~
+               ~%-----------------------------------------"
+          *total-number-of-files*
+          *total-line-count*
+          *total-toplevel-forms*))
 
 ;;;---------------
 ;;; toplevel call
 ;;;---------------
 
 (defun count-lines-in-system ()
-  ;; Toplevel call. Sets up the needed parameters and then calls
-  ;; load to go through the master-loader, opening files as dictated
-  ;; by the current switch settings.
+  "Toplevel call. Sets up the needed parameters and then calls
+  load to go through the master-loader, opening files as dictated
+  by the current switch settings."
   (let ((*just-count-lines* t)
         (*compile* nil)
         (*insist-on-binaries* nil))
-
-    (setq *total-line-count* 0
-          *total-toplevel-forms* 0
-          *total-number-of-files* 0)
-    
+    (reset-line-count-accumulators)
     (load cl-user::master-loader)
-
-    (format t "~&~%~
-               ~%-----------------------------------------~
-               ~%  ~A files~
-               ~%  ~A lines of code~
-               ~%  ~A toplevel forms~
-               ~%-----------------------------------------"
-            *total-number-of-files*
-            *total-line-count*
-            *total-toplevel-forms*)))
-
+    (report-line-counts)))
 
 ;;;----------------
 ;;; embedded calls
 ;;;----------------
 
 (defun just-count-the-lines-of-code (raw-namestring)
-  ;; Called from LLoad as the file processor when the flag *just-count-lines*
-  ;; is up. This sets up the file processing and presentations but it
-  ;; offloads the actual counting to a subroutine.
-  ;;   Written by c&s on Just-note-if-changed.
+  "Called from lload as the file processor when the flag *just-count-lines*
+  is up. This sets up the file processing and presentations but it offloads
+  the actual counting to a subroutine. Written by c&s on just-note-if-changed."
   (let ((namestring (expand-namestring raw-namestring)))
-    (multiple-value-bind (line-count toplevel-forms)
-                         (lines-in-file namestring)
-      (format t "~%~A~4,2T~A~10,2T~S"
-              toplevel-forms line-count raw-namestring)
-
-      (setq *total-line-count* (+ line-count *total-line-count*)
-            *total-toplevel-forms* (+ toplevel-forms *total-toplevel-forms*))
+    (multiple-value-bind (line-count toplevel-forms) (lines-in-file namestring)
+      (format t "~%~A~4,2T~A~10,2T~S" toplevel-forms line-count raw-namestring)
+      (incf *total-line-count* line-count)
+      (incf *total-toplevel-forms* toplevel-forms)
       (incf *total-number-of-files*)
-      
       (when (load-file? raw-namestring)
         (load namestring)))))
 
-
-
 (defun lines-in-file (namestring)
-  ;; Called from Just-count-the-lines-of-code and does the heavy lifting
-  (let ((count 0) 
+  "Do the heavy lifting for just-count-the-lines-of-code."
+  (let ((lines 0)
         (toplevel-forms 0)
         line  1st-char  line-ends-file?  pending-sharpsign-comment )
-
     (with-open-file (file-stream namestring
-                          :direction :input
-                          :if-does-not-exist :error)
+                     :direction :input
+                     :if-does-not-exist :error)
       (loop
         (multiple-value-setq (line line-ends-file?)
           (read-line file-stream nil :reached-eof))
@@ -93,21 +86,20 @@
 
          ((eql (setq 1st-char (elt line 0)) #\;))  ;; comment line
 
-         ((eql 1st-char #\( )
-          (incf toplevel-forms)
-          (incf count))
+         ((and (eql 1st-char #\( )) ;; toplevel form
+          (unless pending-sharpsign-comment
+            (incf toplevel-forms))
+          (incf lines))
 
          (pending-sharpsign-comment  ;; end of #| ... |#
-          (when (search "|#" line)
-            (setq pending-sharpsign-comment nil)))
+          (setq pending-sharpsign-comment (not (search "|#" line))))
 
          ((and (eql 1st-char #\# )         ;; beginning of #| ... |#
                (eql (elt line 1) #\| ))
           (setq pending-sharpsign-comment t))
 
          (t 
-          ;(format t "~&~S" line)
-          (incf count))))
+          (incf lines))))
 
-      (values count toplevel-forms))))
+      (values lines toplevel-forms))))
 
