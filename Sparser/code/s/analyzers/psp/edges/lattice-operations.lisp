000593e200e9f76423e5cacc844aaac8b8182e73
Rusty Bobrow <rjbobrow@gmail.com>
2016-02-06 02:15:44 -0500

    Bunch of work to make it possible to efficiently find individuals more specific than a given individual, even if they have modifiers in a different order, or modifiers which are more specific than the modifier in the new individual
    Work in progress

--- Sparser/code/s/analyzers/psp/edges/lattice-operations.lisp
+++ Sparser/code/s/analyzers/psp/edges/lattice-operations.lisp
@@ -120,14 +120,24 @@
     (find-or-make-lattice-description-for-cat-list base))
    (t (lsp-break "what type of base is this? ~s" base))))
 
+(defun dli-ref-cat (c)
+  (cond
+    ((individual-p c)
+     (find-or-make-lattice-description-for-cat-list (indiv-type c)))
+    ((category-p c)
+     (fom-lattice-description c))
+    ((symbolp c)
+     (fom-lattice-description (category-named c)))
+    (t
+     (lsp-break "can'f find dli-ref-cat for ~s~&" indiv))))
+
 (defun find-or-make-lattice-description-for-individual (base)
   (declare (special category::collection))
   (or
    (get-dli base)
    (if (memq category::collection (indiv-type base)) ;; likely a conjunction
     (find-or-make-lattice-description-for-collection base) ;; not quite right -- ehat to do here?
-    (let* ((lattice-cat-entry
-             (find-or-make-lattice-description-for-cat-list (indiv-type base)))
+    (let* ((lattice-cat-entry (dli-ref-cat base))
            (current-dli lattice-cat-entry)
            (dl-vvs nil))
       (declare (special lattice-cat-entry current-dll dll-vvs))
@@ -167,6 +177,32 @@
         (setf (indiv-restrictions new-dli) (list base))
 	(set-dli base new-dli))))
 
+(defparameter *non-phrasal-class-names* nil)
+
+(defun non-phrasal-class-names ()
+  (when (null *non-phrasal-class-names*)
+    (setq *non-phrasal-class-names*
+	  (make-hash-table :test #'eq))
+    (loop for c in 
+	 '(NAME DETERMINER PRONOUN TIME KINASE SMALL-MOLECULE PROTEIN-METHOD AMOUNT
+	   BIB-REFERENCE MOLECULE-STATE CATALYSIS BIOCHEMICAL-REACTION MODAL
+	   MUTATION NAMED-BIO-PROCESS BIO-WHETHERCOMP BIO-IFCOMP DISEASE
+	   BIO-OBSERVATION BIO-REAGENT BIO-PREPARATION TYPE-MARKER BIO-AGGREGATE
+	   AGGREGATE TAKES-NEG RECEPTOR BIO-ABSTRACT PHOSPHORYLATION-MODIFICATION
+	   MECHANISM QUANTIFIER PHOSPHOLIPID LIPID OLIGOMERIZE BIO-SCALAR
+	   SCALAR-QUALITY QUALITY BIO-QUALITY BIO-THATCOMP BIO-COMPLEMENT
+	   CELLULAR-LOCATION MOLECULAR-FUNCTION BIO-SELF-MOVEMENT BIO-MOVEMENT
+	   PREPOSITIONAL-OPERATOR FRACTIONAL-TERM TENSE/ASPECT
+	   POST-TRANSLATIONAL-ENZYME ADJECTIVE-ADVERB TUMOR ADVERBIAL
+	   POSITIVE-BIO-CONTROL NUCLEOTIDE-EXCHANGE-FACTOR REACTOME-CATEGORY
+	   IN-RAS2-MODEL NON-CELLULAR-LOCATION BIO-LOCATION NEGATIVE-BIO-CONTROL
+	   CAUSED-BIO-PROCESS BIO-RELATION BIO-PREDICATION STATE BIO-CHEMICAL-ENTITY
+	   PHYSICAL-OBJECT MODIFIER OPERATOR BIO-METHOD KIND PHYSICAL ENDURANT
+	   HAS-UID RELATION ABSTRACT HAS-NAME BIOLOGICAL WITH-QUANTIFIER
+	   BIO-RHETORICAL PERDURANT TOP)
+       do (setf (gethash c *non-phrasal-class-names*) t)))
+  *non-phrasal-class-names*)
+
 (defparameter *sub-vv* (find-or-make-dlvv-from-var-val :subc nil))
 (defparameter *super-vv* (find-or-make-dlvv-from-var-val :superc nil))
 
@@ -195,7 +231,7 @@
   (or
    (get-dli indiv-collection)
    (let ((new-dli (make-individual :id (incf *dl-lattice-index*) :type (indiv-type indiv-collection))))
-     (add-uplink new-dli (find-or-make-lattice-description-for-cat-list (indiv-type indiv-collection)))
+     (add-uplink new-dli (dli-ref-cat indiv-collection))
      (loop for member in (value-of 'members indiv-collection)
        do
        (add-downlink new-dli (fom-lattice-description member)))
@@ -224,7 +260,7 @@
   (let* ((parent (if (referential-category-p oparent)
 		   (find-or-make-lattice-description-for-ref-category oparent)
 		   oparent))
-         (var (find-var-from-var/name var/name nil))
+         (var (find-var-from-var/name var/name parent))
          (dl-vv (when var (find-or-make-dlvv-from-var-val var value)))
          (downlinks (indiv-downlinks parent)))
     (declare (special parent var dl-vv downlinks))
@@ -237,12 +273,11 @@
   ;; the new binding
   (declare (special oparent var/name binding))
   (let* ((parent (if (referential-category-p oparent)
-		   (find-or-make-lattice-description-for-ref-category oparent)
-		   oparent))
-         (lattice-cat-parent
-          (find-or-make-lattice-description-for-cat-list (indiv-type oparent)))
+		     (find-or-make-lattice-description-for-ref-category oparent)
+		     oparent))
+         (lattice-cat-parent (dli-ref-cat oparent))
          (parent-restrictions (indiv-restrictions parent))
-         (var (find-var-from-var/name var/name category))
+         (var (find-var-from-var/name var/name (or category parent)))
          (dl-vv (when var (find-or-make-dlvv-from-var-val var value)))
          (downlinks (indiv-downlinks parent)))
     (declare (special parent var dl-vv downlinks))
@@ -258,15 +293,43 @@
                      (link-to-other-parents new-child parent dl-vv)
                      (link-to-existing-children new-child parent dl-vv)
                      (setf (indiv-restrictions new-child) (cons dl-vv parent-restrictions))
+		     (set-dli new-child new-child)
                      new-child))))
-          (pushnew result (gethash var (indiv-all-subs lattice-cat-parent)))
-          (let
-              ((supers (indiv-all-supers result)))
-            (setf (gethash lattice-cat-parent supers) t)
-            (maphash #'(lambda (k h) (setf (gethash k supers) t)) (indiv-all-supers parent)))
-          (values
-           result
-           (get-binding-of var result value))))))
+         
+	  (link-sub-super result parent)
+	  (maphash #'(lambda (k h) (link-sub-super result k))
+		   (indiv-all-supers parent))
+	  (all-subs-link result var value lattice-cat-parent)
+	  (values
+	   result
+	   (get-binding-of var result value))))))
+
+(defun link-sub-super (sub super)
+  (let ((supers (indiv-all-supers sub)))
+    (setf (gethash super supers) t)))
+
+(defun all-subs-link (sub var value lattice-cat-parent)
+  (declare (special sub var value lattice-cat-parent))
+  (let ((val-supers
+	 (if (individual-p value)
+	     (indiv-all-supers value)
+	     value))
+	(subs (or
+	       (gethash var (indiv-all-subs lattice-cat-parent))
+	       (setf (gethash var (indiv-all-subs lattice-cat-parent))
+		     (make-hash-table)))))
+    (if (hash-table-p val-supers)
+	(maphash #'(lambda (k h)
+		     (when (interesting-super? k)
+		       (pushnew sub (gethash k subs))))
+		 val-supers)
+	(pushnew sub (gethash val-supers subs)))))
+
+(defun interesting-super? (c)
+  (or
+   (not (category-p c))
+   (not (gethash (cat-name c)
+		 (non-phrasal-class-names))))) ;; need to define this
 
 (defun as-specific? (sub-dli super-dli) ;; super-dli lies above sub-dli in the description lattice
   (cond
@@ -295,28 +358,31 @@
       (setf (gethash super-dli (indiv-all-supers sub-dli)) t)))))
 ;; was -- incorrectly -- (subsetp  (indiv-restrictions super-dli) (indiv-restrictions sub-dli)))
 
-(defun find-var-from-var/name (var/name category)
-  (declare (special parent))
+(defun find-var-from-var/name (var/name parent)
   (cond
-   ((typep var/name 'lambda-variable) var/name)
-   ((typep var/name 'anonymous-variable) 
-    (cond
-     ((null (find-variable-for-category 
-             (avar-name var/name)
-             (if (individual-p parent)
-               (itype-of parent)
-               parent)))
-      #+ignore(format t "~&~&!! Can't dereference anonymous variable ~a against category ~a.~
+    ((typep var/name 'lambda-variable) var/name)
+    ((typep var/name 'anonymous-variable) 
+     (cond
+       ((null (find-variable-for-category 
+	       (avar-name var/name)
+	       (if (individual-p parent)
+		   (itype-of parent)
+		   parent)))
+	#+ignore(format t "~&~&!! Can't dereference anonymous variable ~a against category ~a.~
          ~%Can't do binding. Leaving object unchanged.~%" var/name parent)
-      nil)
-     (t
-      (dereference-variable var/name parent))))
-   ((symbolp var/name)
-    (or (find-variable-from-individual var/name parent)
-        (when category
-          (find-variable-for-category var/name category))))
-   (t (break "var/name ~s can't be mapped to a variable in ~s"
-             var/name parent))))
+	nil)
+       (t
+	(dereference-variable var/name parent))))
+    ((symbolp var/name)
+     (or
+      (cond
+	((individual-p parent)
+	 (find-variable-from-individual var/name parent))
+	((category-p parent)
+	 (find-variable-for-category var/name parent)))
+      (lambda-variable-named var/name)))
+    (t (break "var/name ~s can't be mapped to a variable in ~s"
+	      var/name parent))))
 
 (defun get-binding-of (var i value)
   "Get binding in i that binds var to value"
@@ -348,8 +414,7 @@
 (defparameter *ref-counts* (make-hash-table))
 
 (defun survey-bindings ()
-  (setq *dlis* nil)
-  (maphash #'(lambda(e dli)(declare (ignore e))(push dli *dlis*)) *lattice-ht*)
+  (setq *dlis* (all-dlis*))
   (format t "There are ~S dlis" (length *dlis*))
   (setq *bmax* 0)
   (setq *maxb* nil)
@@ -358,5 +423,44 @@
     (when (> (length (indiv-binds d)) *bmax*) 
       (setq *bmax* (length (indiv-binds d)))
       (setq *maxb* d))
-    (push d (gethash (length (gethash d *source-ht*)) *ref-counts*))))
+       (push d (gethash (length (gethash d *source-ht*)) *ref-counts*))))
 
+(defun all-dlis ()
+  (setq *dlis* nil)
+  (maphash #'(lambda(e dli)(declare (ignore e))(push dli *dlis*)) *lattice-ht*)
+  *dlis*)
+
+(defgeneric find-all-subs (c)
+  )
+
+(defmethod find-all-subs ((c referential-category))
+  (find-all-subs (dli-ref-cat c)))
+
+(defmethod find-all-subs ((i individual))
+  (let*
+      ((var-hts (mapcar #'cdr (hashtable-to-alist (indiv-all-subs i)))))
+    (remove-duplicates
+     (loop for ht in var-hts
+	append
+	  (let (l)
+	    (loop for vv in
+		 (mapcar #'cdr (hashtable-to-alist ht))
+	       do
+		 (loop for v in vv do (pushnew v l)))
+	    l)))))
+
+
+(defun all-phrasal-dlis ()
+  (all-dlis)
+  (loop for i in *dlis* when (retrieve-surface-string i) collect i))
+
+(defun phrasal-dli-cat-refs ()
+  (remove-duplicates
+   (loop for i in (all-phrasal-dlis) collect (dli-ref-cat i))))
+
+#|
+(length (setq missing (loop for ii in (hashtable-to-alist *surface-strings*) unless (get-dli (car ii)) collect ii)))
+(length (setq qq  (loop for i in missing unless (eq (cat-name (itype-of (car i))) 'prepositional-phrase) collect i)))
+
+(length (setq pcd (all-phrasal-dlis)))
+|#
