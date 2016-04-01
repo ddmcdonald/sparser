d7e01ec2191856662bcb1a48d01e8871bfe68527
David D. McDonald <davidmcdonald@alum.mit.edu>
2016-04-01 18:28:42 -0400

    Removed the category anchor -- wasn't adequately thought through

--- Sparser/code/s/grammar/model/core/time/anchor.lisp
+++ Sparser/code/s/grammar/model/core/time/anchor.lisp
@@ -1,33 +1,21 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 2013-2014 David D. McDonald  -- all rights reserved
+;;; copyright (c) 2013-2016 David D. McDonald  -- all rights reserved
 ;;;
 ;;;     File:  "anchor"
 ;;;   Module:  "model;core:time:"
-;;;  version:  May 2014
+;;;  version:  April 2016
 
 ;; Initialized as a shell 8/27/13. Finished copying over what the shell
 ;; was supposed to do 5/27/14 and started setting up computations. 
 ;; This file subsumes the former anchor-date and anchor-amount 
 
 
-;;the anchor category is meant to capture time expressions which put events/entities at specific points
-;;i.e., these timex 'anchor' the event to a particular time
-;;currently the anchor captures the linear order of sequencers and different timex
-;;should flesh out into multiple categories, with super class as a general anchor?
-;;this is the abstract class for anchors
+;; the anchor-date captures the complex time adverbials of sequencers followed by date
+;; should this category also include seasons/season-year ? 
 
-(define-category anchor
-  :documentation "May be little here")
-
-
-;;the anchor category is meant to capture time expressions which put events/entities at specific points
-;;i.e., these timex 'anchor' the event to a particular time
-;;the anchor-date captures the complex time adverbials of sequencers followed by dates
-;;should this category also include seasons/season-year ? 
-;;captures "after March 1"
-
+;; captures "after March 1"
 (define-category anchor-date
-  :specializes anchor
+  :specializes time-interval  ;; sequencer determines what sort of interval
   :instantiates self
   :binds ((sequencer . sequencer)
           (date . date))
@@ -38,9 +26,9 @@
 
 
 ;; captures "last three years", "two months later"
-
+;;   hmm -- very different sort of things -- interval vs. reference point
 (define-category anchor-amount
-  :specializes anchor
+  :specializes time-interval
   :instantiates self
   :binds ((sequencer . sequencer)
           (amount-of-time . amount-of-time))
