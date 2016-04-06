accc0400a712a1d82d970dec43076bf5c58712a2
David D. McDonald <davidmcdonald@alum.mit.edu>
2016-04-06 10:21:40 -0400

    Added another subclass of job-event to better fit to some verbs

--- Sparser/code/s/grammar/model/sl/whos-news/job-events/je-object.lisp
+++ Sparser/code/s/grammar/model/sl/whos-news/job-events/je-object.lisp
@@ -1,13 +1,13 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1993,1994,1995  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1993-1995,2016  David D. McDonald  -- all rights reserved
 ;;;
 ;;;     File:  "je object"
 ;;;   Module:  "model;sl:whos news:job events:"
-;;;  version:  March 1995
+;;;  version:  April 2016
 
 ;; initiated 6/15/93 v2.3.  Made them referential categories 7/22.
 ;; Made them inherit 3/3/95.  Started moving the standard variables
-;; here 3/14.
+;; here 3/14/95. Added retain 4/5/16
 
 (in-package :sparser)
 
@@ -41,3 +41,10 @@
           (title . title)
           (company . company)))
 
+(define-category retain-position
+  :instantiates nil
+  :specializes  job-event
+  :binds ((person . person)
+          (position . position)
+          (title . title)
+          (company . company)))
