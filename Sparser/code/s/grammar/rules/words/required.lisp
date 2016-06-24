4120dd049c406b3b4f9b1075909cfcd7bd6867df
David D. McDonald <davidmcdonald@alum.mit.edu>
2016-06-24 18:45:51 -0400

    Made period-hook-off undo all of what period-hook-on now sets

--- Sparser/code/s/grammar/rules/words/required.lisp
+++ Sparser/code/s/grammar/rules/words/required.lisp
@@ -1,10 +1,10 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
 ;;; copyright (c) 1990,1991  Content Technologies Inc.
-;;; copyright (c) 1992-1993,2015  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992-1993,2015-2016  David D. McDonald  -- all rights reserved
 ;;;
 ;;;      File:   "required"
 ;;;    Module:   "grammar;rules:words:"
-;;;   Version:   1.2 December 2015
+;;;   Version:   June 2016
 
 ;; 0.1 (2/15/91 v1.8.1) Added comma and period.
 ;;     (11/17 v2.1) elevated to words; from words;basics:
@@ -14,7 +14,8 @@
 ;;      could be removed.
 ;; 1.2 (7/15/93) changed them to defparameters because they were blocking the
 ;;      compilation
-;;     (12/22/15) Added period and the period hook. 
+;;     (12/22/15) Added period and the period hook. So they'd be
+;;      available early.
 
 (in-package :sparser)
 
@@ -68,10 +69,14 @@
   ;; don't occur in the text we're looking at. 
 
 (defun period-hook-on ()
+  ;; The question mark is defined later, but this function
+  ;; is called still later
   (declare (special word::period word::question-mark))
   (define-completion-action word::period :hook 'period-hook)
-  (define-completion-action word::QUESTION-MARK :hook 'period-hook))
+  (define-completion-action word::question-mark :hook 'period-hook))
 
 (defun period-hook-off ()
-  (delete-completion-action word::period :hook))
+  (declare (special word::period word::question-mark))
+  (delete-completion-action word::period :hook)
+  (delete-completion-action word::question-mark))
 
