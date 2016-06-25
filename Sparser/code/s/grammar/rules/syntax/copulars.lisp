fe9326ae4b97636dfadad0d2d6ae36dc07aafa75
Rusty Bobrow <rjbobrow@gmail.com>
2016-06-25 14:52:44 -0400

    Tentative, very rough, mechanism for defining a bunch of verbs that act like BE in terms of handling adject phrases. This needs to be reviewed with David -- but I need something to handle some critical comments.

---
+++ Sparser/code/s/grammar/rules/syntax/copulars.lisp
@@ -0,0 +1,69 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
+;;; copyright (c) 1993-1994,2013-2016  David D. McDonald  -- all rights reserved
+;;; extensions copyright (c) 2009 BBNT Solutions LLC. All Rights Reserved
+;;; 
+;;;     File:  "copulars"
+;;;   Module:  "grammar;rules:syntax:"
+;;;  Version:  May 2016
+
+
+;;;--------------------------
+;;; hedged copular relations
+;;;--------------------------
+
+
+#| Have to figure out an equivalent of biological in the restriction.
+Examples in the localization articles -- exhaustive list
+
+  "Mouse embryo fibroblasts (MEFs) isolated from ERK1 knockout mice 
+seemed to proliferate faster than control cells."
+  "These experiments seem to indicate that the proliferative signal 
+is mediated by ERK2."
+  "Bradykinin stimulation of protein kinase C seems to be 
+such a required pathway."
+  "this mechanism seems unlikely at present."
+  "Therefore, it seems unlikely that the receptor is simply 
+phosphorylated by Src."
+  "but rather seem dependent on phosphatidylinositol 
+3-kinase activity."
+  "The initiating event seems to be an influx of extracellular Ca."
+  "UV seems to provoke receptor activation."
+  "the possible role of ligand involvement would seem unlikely."
+  "While UV does seem to mimic growth factor activation of the receptor."
+  "this interaction seems to be of  low stoichiometry."
+|#
+
+(define-category copula :specializes abstract) ;; not sure what to do beyond this
+
+(defun make-copular-def (word-string)
+  (let ((verb (intern word-string)))
+    `(progn
+       (define-category ,verb :specializes copula
+                        :binds ((tocomp t)
+                                (subject t)
+                                (theme t))
+                        :realization
+                        (:verb ,word-string
+                               :etf (svo)
+                               :s subject
+                               :to-comp tocomp
+                               :o theme))
+
+       (def-form-rule (,verb adjective)
+           :form vg
+           :referent (:function make-copular-adjective left-edge right-edge))
+
+       (def-form-rule (,verb ap)
+           :form vp
+           :referent (:function make-copular-adjective left-edge right-edge)))))
+
+(loop for v in '("seem" "appear" "remain"
+                 "become" "stay")
+     do
+     (eval (make-copular-def v)))
+
+
+(defun make-copular-adjective (copula adjective)
+  (let ((i (individual-for-ref adjective)))
+    (bind-dli-variable :copular-verb copula i)))
+
