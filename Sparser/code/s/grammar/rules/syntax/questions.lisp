08784405f61340cfbe8c7ea5c5521fa097678ee8
David D. McDonald <davidmcdonald@alum.mit.edu>
2016-08-05 13:48:56 -0400

    Checkpoint. Most of the old stuff commented out in anticipation of new.

--- Sparser/code/s/grammar/rules/syntax/questions.lisp
+++ Sparser/code/s/grammar/rules/syntax/questions.lisp
@@ -1,43 +1,50 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER COMMON-LISP) -*-
-;;; copyright (c) 2011 David D. McDonald  -- all rights reserved
+;;; copyright (c) 2011,2016 David D. McDonald  -- all rights reserved
 ;;; Copyright (c) 2009 BBNT Solutions LLC. All Rights Reserved
-;;; $Id:$
 ;;; 
 ;;;     File:  "questions"
 ;;;   Module:  "grammar;rules:syntax:"
-;;;  Version:  September 2011
+;;;  Version:  August 2016
 
 ;; Broken out from /grammar/model/sl/checkpoint/rules 6/17/09
 ;; Elaborated through 7/23/09. 9/28/11 removed spatial-orientation
-;; and cleaned up. 
+;; and cleaned up. Completely redesigned 8/16.
 
 (in-package :sparser)
 
-;;--- Haphazard treatment of WH questions
-;; In a treatment that wasn't haphazard we'd keep track of
-;; the gaps in the clauses and use the traces or their
-;; equivalent. Also needs the non-question usage and 
-;; more systematic way to notice and sweep up the 'question'
-;; aspects so that polar question fall out from the same
-;; inversion of subject and verb.
+(define-category question-core
+  :specializes linguistic ;;/// tentative - whole area needs thought
+  :instantiates nil 
+  :documentation "Common base for questions of all sorts.
+ The 'statement' variable holds the proposition or event that
+ is being questioned. Think of it as the question restated in
+ declarative form.
+   Specializations elaborate this according to the kind of question
+ being asked. The basic split is between polar questions and the
+ diverse kinds of WH questions."
+  :binds ((statement perdurant)))
 
-(define-category question
-  :specializes event
-  :binds ((type) ;; which WH word
-          (content)
-          (participant)))
+#| In biology/verbs
+(define-category question :specializes bio-rhetorical
+    :mixins (bio-thatcomp)
+    :realization
+    (:verb "question" ;; keyword: ENDS-IN-ED 
+	   :noun "hypothesis"
+	   :etf (svo-passive)))
+|#
+
 
 ;;--- swallowing the inverted auxiliary into WH/be
 
-(def-cfr WH/be (oblique-pronoun be) ;; gets all the WH pronouns
+(def-cfr WH/be (wh-pronoun be) ;; gets all the WH pronouns
   :form question-marker
   :referent (:daughter left-edge))
 
-(def-cfr WH/be (oblique-pronoun apostrophe-s) ;; gets all the WH pronouns
+(def-cfr WH/be (wh-pronoun apostrophe-s) ;; gets all the WH pronouns
   :form question-marker
   :referent (:daughter left-edge))
 
-(def-cfr WH/be (oblique-pronoun do) ;; "where did you come from"
+(def-cfr WH/be (wh-pronoun do) ;; "where did you come from"
   :form question-marker
   :referent (:daughter left-edge))
 
@@ -56,7 +63,7 @@
 ;;  This almost certainly has a clean generalization that can be cast
 ;; as a form rule, but I'm not certain yet what that would be. Need more
 ;; cases
-
+#|
 (def-cfr event (WH/be location)
   :form s
   :referent (:instantiate-individual question
@@ -96,23 +103,26 @@
   :referent (:instantiate-individual question
              :with (type left-edge
 		            content right-edge)))
-
-
+|#
+#|   Ignoring now (8/4/16) because it will likely succeed
+     and distort things before they're done.
 ;; Handles commonly used (but not necessarily grammatical)
 ;; examples like "where you going" etc.
 
-(def-form-rule (oblique-pronoun verb+ing)
+(def-form-rule (wh-pronoun verb+ing)
   :form s
   :referent (:instantiate-individual question
              :with (type left-edge
 		            content right-edge)))
 
-;(def-cfr WH/be (oblique-pronoun pronoun/second)
+;(def-cfr WH/be (wh-pronoun pronoun/second)
 ;  :form question-marker
 ;  :referent (:daughter left-edge
 ;	     :bind (participant . right-edge)))
+|#
 
 
+#|   Harvest for patterns
 ;; For "why are you out today" -- really smacks into the
 ;; sticky psi issue though...
 
@@ -148,7 +158,9 @@
   :referent (:instantiate-individual question
              :with (type left-edge
 		    content right-edge)))
+|#
 
+#|   These don't belong here
 
 ;; odds and ends...
 
@@ -162,8 +174,9 @@
   :referent (:head left-edge
 	     :bind (location . right-edge)))
 
-(def-cfr event (oblique-pronoun event)
+(def-cfr event (wh-pronoun event)
   :form s
   :referent (:instantiate-individual question
              :with (type left-edge
-		    content right-edge)))
+                         content right-edge)))
+|#
