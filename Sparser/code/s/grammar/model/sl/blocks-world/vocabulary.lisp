ddmcdonald/sparser

commit b563cb88ec0bd6911c2d45996c84d5ede8209f34

Make Mumble say "push the blocks together" from semantics.

Sparser/code/s/grammar/model/sl/blocks-world/vocabulary.lisp
@@ -184,27 +184,17 @@ support a substantial number of blocks.
     :specializes process
     :mixins (with-an-agent)
     :binds ((theme physical))
-    :documentation "The meaning of a puah depends largly
+    :documentation "The meaning of push depends largly
     on what is pushed (= the type of the theme): block
     wall, door, etc. If the specific action to take
     is object (sort) specific then co-composition is
     involved in the interpretation of the literal 
     directive."
-    :realization
-    (:verb "push"
-           :etf (svo-passive)
-           :o theme
-           :s agent
-           :mumble ("push" svo :s agent :o theme)))
-
-
-(define-category push-together
-  :mixins (with-an-agent)
-  :binds ((items collection))
-  :realization
-    ((:verb "push")
-     (:mumble (transitive-with-final-adverbial "push" "together"))))
-
+    :realization (:verb "push"
+                  :etf (svo-passive)
+                  :s agent
+                  :o theme
+                  :mumble ("push" svo :s agent :o theme)))
 
 ;; 1.2 "Put a block on the table"
 (define-category put-something-somewhere

test/mumble-blocks-world.lisp
@@ -115,3 +115,11 @@
 (deftest (say add block at right end of row)
   (mumble-says "Add a red block at the right end of the row.")
   "add a red block on right end of the row") ; missing determiner
+
+(deftest (say push two blocks together)
+  (mumble-says "Push the two blocks together.")
+  "push the two blocks together")
+
+(deftest (say push together block and another)
+  (mumble-says "Push together the block and another block.")
+  "push the block and another block together")

Sparser/code/s/interface/mumble/binding-centric.lisp
@@ -168,6 +168,9 @@
             (prep
              (attach-pp (sp::get-mumble-word-for-sparser-word prep)
                         value dtn pos)))))
+  (:method (binding (var-name (eql 'sp::adverb)) dtn pos)
+    "Attach a final adverb."
+    (attach-adjective (sp::binding-value binding) dtn pos))
   (:method (binding (var-name (eql 'sp::aspect)) dtn pos)
     "Attach tense/aspect/modality/etc."
     (loop with t/a = (sp::binding-value binding)

commit 3a70c332bfa11781e0afb8e8986a92ff6e6ff867

Restore PUSH-TOGETHER category.
Removed in commit b563cb88ec0bd6911c2d45996c84d5ede8209f34,
which broke the CLiC BW tests.

Sparser/code/s/grammar/model/sl/blocks-world/vocabulary.lisp
@@ -196,6 +196,15 @@ support a substantial number of blocks.
                   :o theme
                   :mumble ("push" svo :s agent :o theme)))
 
+(define-category push-together
+  :specializes push
+  :binds ((items collection))
+  :realization (:verb "push together"
+                :etf (svo-passive)
+                :s agent
+                :o theme
+                :mumble (transitive-with-final-adverbial "push" "together")))
+
 ;; 1.2 "Put a block on the table"
 (define-category put-something-somewhere
   :specializes process

test/mumble-blocks-world.lisp
@@ -122,4 +122,4 @@
 
 (deftest (say push together block and another)
   (mumble-says "Push together the block and another block.")
-  "push the block and another block together")
+  "push together the block and another block")