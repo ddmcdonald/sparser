1c83f685738ee5d2d1589eb29eeab5fcadd36c13
David D. McDonald <davidmcdonald@alum.mit.edu>
2016-08-30 14:17:21 -0400

    prepositional-operator now specializes predicate. Sharpened some doc.

--- Sparser/code/s/grammar/rules/syntax/prepositions.lisp
+++ Sparser/code/s/grammar/rules/syntax/prepositions.lisp
@@ -1,9 +1,9 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 2011-2013  David D. McDonald  -- all rights reserved
+;;; copyright (c) 2011-2016  David D. McDonald  -- all rights reserved
 ;;; 
 ;;;     File:  "prepositions"
 ;;;   Module:  "grammar;rules:syntax:"
-;;;  Version:  January 2013
+;;;  Version:  August 2016
 
 ;; initiated 9/30/11.  Fixed the method 1/29/13
 ;; 3/2/2015 commented out mechanism to collect instances of prepositions folled by verbal elements -- infinitives, -ing complements, and others that may be errors
@@ -14,7 +14,7 @@
   ;; This is the default superc-class of the classes created for
   ;; prepositions (see define-preposition) and provides the 'top'
   ;; in methods over them (analyze-pp).
-  :specializes operator)
+  :specializes predicate)
 
 
 (defgeneric analyze-pp (preposition complement)
@@ -23,28 +23,27 @@
                   they want. The default is the referent of the complement, which
                   is equivalent to :daughter right-edge."))
   
-(when *clos*
+(when *clos* ;;/// n.b. very unfinished
   (defmethod analyze-pp ((prep sh::prepositional-operator) (complement t))
     (declare (ignore prep))
     (tr :analyze-pp_t+t)
     (dereference-shadow-individual complement)))
 
-(defparameter *prep-complements* nil)
+(defparameter *prep-complements* nil
+  "For collecting examples of how this is uesd.")
 
 (defun apply-preposition-to-complement (prep comp)
-  ;; Used by preposition+s or +vg
-  ;; Poor man's ad-hoc version of analyze-pp since I don't want
-  ;; to take the time to properly situation the method call.
-  ;; Returns the referent of the whole edge, see ref/function 
-  (push-debug `(,prep ,comp))
-  (unless
-      (and nil ;; don't want to do this unless we are collecting examples
-           *subcat-test*)
-    (push
-     (terminals-in-segment/one-string 
-      (pos-edge-starts-at (left-edge-for-referent))
-      (pos-edge-ends-at (right-edge-for-referent)))
-     *prep-complements*))
+  ;; Used by syntactic rule for {preposition + vg+ing} and {... + vp+ing}
+  ;; but it needs a lot of work since it only does anything interesting
+  ;; with "upon", otherwise it drops the preposition on the ground
+  ;; and returns the complement as the meaning of the phrase.
+
+  (unless (and nil ;; don't want to do this unless we are collecting examples
+               *subcat-test*)
+    (push (terminals-in-segment/one-string 
+           (pos-edge-starts-at (left-edge-for-referent))
+           (pos-edge-ends-at (right-edge-for-referent)))
+          *prep-complements*))
 
   (cond
    ((eq prep (category-named 'upon))
@@ -52,9 +51,9 @@
    (t ;; equivalent of daughter
     comp)))
 
-;; Consumed as 'circumstance' at event level by adjoint-pp-to-vp
+;; Consumed as 'circumstance' at event level by adjoin-pp-to-vg
 (define-category upon-condition
   :instantiates :self
-  ;; supercategory is a contingency or 'when' or some such
+  :specializes predication ;; should be a contingency or 'when' or some such
   :binds ((condition))
   :index (:key condition))
