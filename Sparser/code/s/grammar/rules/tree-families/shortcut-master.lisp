beec2c22129217a5c4e0e9357fa8ec23eca40476
Alex Plotnick <aplotnick@sift.net>
2016-09-15 16:22:21 -0400

    Always make realization data from shortcuts.
    Previously, realization data was created only if an ETF was supplied,
    so it was missing for shortcut-form definitions of nouns, adjectives, etc.

--- Sparser/code/s/grammar/rules/tree-families/shortcut-master.lisp
+++ Sparser/code/s/grammar/rules/tree-families/shortcut-master.lisp
@@ -3,7 +3,7 @@
 ;;;
 ;;;     File:  "shortcut-master"
 ;;;   Module:  "grammar;rules;tree-families;"
-;;;  Version:  August 2016
+;;;  Version:  September 2016
 
 ;; Initiated 9/14/14 to make more flexible, complete shortcuts.
 ;; 11/11/14 added keyword for obo-id.
@@ -30,22 +30,11 @@
 
 (in-package :sparser)
 
-;;;--------------------
-;;; Shortcut detection
-;;;--------------------
-
-(defun shortcut-rdata-p (rdata)
-  "Return true if the given realization plist is shortcut-style, not long-form."
-  (and (consp rdata)
-       (keywordp (car rdata)) ; and thus not a list
-       (not (get-properties rdata '(:tree-family :mapping)))))
-
-
 ;;;-------------------------------------
 ;;; Decoder that actually does the work
 ;;;-------------------------------------
 
-(defun decode-shortcut-rdata (category &key etf
+(defun decode-shortcut-rdata (category &rest rdata &key etf
                               adjective (adj adjective)
                               noun verb
                               control-relations
@@ -63,16 +52,19 @@
   (let* ((sf (fom-subcategorization category :category category :slots slots))
          (subj-pat (find-subcat-pattern :subject sf))
          (obj-pat (find-subcat-pattern :object sf))
-         (m-pat (find-subcat-pattern :m sf))
-         substitution-map
-         word-map)
+         (m-pat (find-subcat-pattern :m sf)))
     (setf (control-relations sf) control-relations)
-    (dolist (schema-name (setq etf (ensure-list etf)))
-      ;; Iterate through the etf, adding to the substituions and word list.
-      (let* ((rschema (realization-scheme-named schema-name))
-             (lexical-class (schema-head-keyword rschema)))
+
+    ;; Exactly one ETF is allowed, but for compatibility we assume a list.
+    (setq etf (ensure-list etf))
+    (check-type etf (or null (cons symbol null)))
+    (let* ((schema-name (car etf))
+           (scheme (and schema-name (realization-scheme-named schema-name)))
+           substitution-map
+           word-map)
+      (when etf
         ;; Set up the word map.
-        (ecase lexical-class
+        (ecase (schema-head-keyword scheme)
           (:verb
            (unless verb
              (error "The etf ~a requies a :verb parameter." schema-name))
@@ -101,67 +93,68 @@
             (register-variable category o-var :object-variable)
             (register-variable category optional-object :optional-object)
             (register-variable category
-                               (mapcar #'(lambda(prep)
-                                           (resolve (string-downcase prep)))
+                               (mapcar (lambda (prep)
+                                         (resolve (string-downcase prep)))
                                        loc-pp-complement)
                                :loc-pp-complement)))
 
-        (when c ; complement, e.g. "reported that ..."
+        ;; Complement, e.g. "reported that ..."
+        (when c
           (let* ((var (variable/category c category))
                  (v/r (var-value-restriction var)))
             (push `(comp-slot . ,var) substitution-map)
             (push `(comp-v/r . ,v/r) substitution-map)
             (register-variable category var :complement-variable)))
 
-	(when m-pat ; modifier, normally to a head noun
+        ;; Modifier, normally to a head noun.
+        (when m-pat
           (let ((m-var (subcat-variable m-pat))
                 (m-v/r (subcat-restriction m-pat)))
             (unless m-var (error "No ~a variable associated with ~a." m-pat category))
             (push `(modifier-slot . ,m-var) substitution-map)
-            (push `(modifier-v/r . ,m-v/r) substitution-map)))))
+            (push `(modifier-v/r . ,m-v/r) substitution-map))))
 
-    (when (and verb prep)
-      ;; Preposition 'owned' by the verb, which appears
-      ;; immediately after the verb, making it effectively
-      ;; a compound verb name.
-      (apply-preposition verb prep category))
+      (when (and verb prep)
+        ;; Preposition 'owned' by the verb, which appears
+        ;; immediately after the verb, making it effectively
+        ;; a compound verb name.
+        (apply-preposition verb prep category))
 
-    (when noun
-      ;; A noun can just expect to get all the pp's w/o an etf.
-      (unless (assq :common-noun word-map)
-        ;; If it's on the map then the realization machinery will expand it.
-        (check-type noun
-                    (or (or string word polyword)
-                        (cons (or string word polyword)
-                              (cons irregular-keyword
-                                    (cons (or string word polyword)
-                                          null))))
-                    "an irregular noun spec")
-        (let* ((word-string (if (consp noun) (car noun) noun))
-               (word (resolve/make word-string))
-               (special-cases (when (consp noun) (cdr noun)))
-               (cn-rules (apply #'make-rules-for-head
-                                :common-noun word category category
-                                special-cases)))
-          (make-corresponding-mumble-resource word :common-noun)
-          (add-rules cn-rules category))))
+      (when noun
+        ;; A noun can just expect to get all the pp's w/o an etf.
+        (unless (assq :common-noun word-map)
+          ;; If it's on the map then the realization machinery will expand it.
+          (check-type noun
+                      (or (or string word polyword)
+                          (cons (or string word polyword)
+                                (cons irregular-keyword
+                                      (cons (or string word polyword)
+                                            null))))
+                      "an irregular noun spec")
+          (let* ((word-string (if (consp noun) (car noun) noun))
+                 (word (resolve/make word-string))
+                 (special-cases (when (consp noun) (cdr noun)))
+                 (cn-rules (apply #'make-rules-for-head
+                                  :common-noun word category category
+                                  special-cases)))
+            (make-corresponding-mumble-resource word :common-noun)
+            (add-rules cn-rules category))))
 
-    (when adj
-      ;; Adjectives are analyzed as being able to take subjects and/or objects
-      ;; as well as subcategorizations ('slots').
-      (let* ((word (resolve/make adj))
-             (adj-rules (make-rules-for-head :adjective word category category)))
-        (make-corresponding-mumble-resource word :adjective)
-        (add-rules adj-rules category)))
+      (when adj
+        ;; Adjectives are analyzed as being able to take subjects and/or objects
+        ;; as well as subcategorizations ('slots').
+        (let* ((word (resolve/make adj))
+               (adj-rules (make-rules-for-head :adjective word category category)))
+          (make-corresponding-mumble-resource word :adjective)
+          (add-rules adj-rules category)))
 
-    (when adjp-complement
-      (register-variable category adjp-complement :adjp-complement))
+      (when adjp-complement
+        (register-variable category adjp-complement :adjp-complement))
 
-    (when (or etf substitution-map word-map)
-      (check-type etf (cons symbol null)) ; exactly one ETF allowed
-      (let ((scheme (realization-scheme-named (first etf))))
-        (make-realization-data category
-                               :etf (schema-tree-family scheme)
-                               :mapping (make-scheme-mapping scheme substitution-map category)
-                               :heads (decode-rdata-heads (assoc (schema-head-keyword scheme) word-map)
-                                                          category))))))
+      (make-realization-data category
+                             :etf (and scheme (schema-tree-family scheme))
+                             :mapping (and scheme (make-scheme-mapping scheme substitution-map category))
+                             :heads (decode-rdata-heads (if scheme
+                                                          (assoc (schema-head-keyword scheme) word-map)
+                                                          rdata)
+                                                        category)))))
