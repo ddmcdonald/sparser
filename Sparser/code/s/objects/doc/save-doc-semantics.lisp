20c4a058ebf151656b9b0e9f19a4596d79034e8a
Rusty Bobrow <rjbobrow@gmail.com>
2016-10-03 17:00:57 -0400

    redefine tree walker for article semantics to directly work on KRISP objects. Use CLOS methods for most of the printing. Old code still in place but will probably disappear.
    This refactoring should make it much easier to produce XML or JSON output for HMS...

--- Sparser/code/s/objects/doc/save-doc-semantics.lisp
+++ Sparser/code/s/objects/doc/save-doc-semantics.lisp
@@ -61,6 +61,8 @@
                     (format nil "~a-semantics.lisp" (name article))))
 
 
+(defparameter *one-expression-per-sentence* t)
+
 (defmethod write-semantics ((sent sentence) s)
   (declare (special sent))
   (when (actually-reading)
@@ -68,34 +70,9 @@
         (write-combined-sentence-results sent s)
         (write-sentence-results sent s))))
 
-(defparameter *one-expression-per-sentence* t)
 
 
-(defmethod write-combined-sentence-results ((s sentence) stream)
-  (declare (special *show-syn-tree*))
-  ;; we assume that this is called immediately after the sentence is parsed
-  (format stream "~%___________________~% ~s~%~%"
-          (sentence-string s))
-  (write-sem-tree
-   `(semantics
-     ,.     
-     (loop for edge in (all-tts (starts-at-pos s)(ends-at-pos s))
-        collect 
-          (let*
-              ((*no-edge-info* t)
-               (*suppress-indiv-uids* t)
-               (mention (and
-                         (edge-p edge)
-                         (edge-mention edge)))
-               (ref (if mention
-                        (base-description (edge-mention edge))
-                        edge)))            (declare (special ref *no-edge-info* *suppress-indiv-uids*))
-            `(sem-element
-              ,(if (word-p ref)
-                `(wd ,(pname ref))
-                (spire-tree ref))))))
-   stream)
-  (terpri stream))
+
 
 (defmethod write-sentence-results ((s sentence) stream)
   (declare (special *show-syn-tree*))
@@ -104,11 +81,14 @@
      do 
        (let*
 	   ((*no-edge-info* t)
-	    (*suppress-indiv-uids* t)
+	    ;;(*suppress-indiv-uids* t)
 	    (ref (if (and
                       (edge-p edge)
                       (edge-mention edge))
-                     (contextual-description (edge-mention edge))
+                     (edge-referent edge)
+                     ;; was (contextual-description (edge-mention edge)),
+                     ;; but this seems to be wrong for conjunctions --
+                     ;; need to look at conjunction distribution
                      edge)))
 	 (declare (special ref *no-edge-info* *suppress-indiv-uids*))
          (format stream "~% --- ~s~%"
@@ -117,14 +97,153 @@
                      edge))         
 	 (if (word-p ref)
 	     (format stream "  ~s" ref)
-	     (write-sem-tree (spire-tree ref) stream))
+	     (write-sem-tree ref stream))
          (terpri stream)
          (terpri stream))))
 
-(defun write-sem-tree (tree stream)
-  (setq ddm-util::*indentation* 0) ;; make sure indentation is restarted
-  (print-sem-tree tree stream))
 
+(defparameter *direct-from-sem* t)
+
+(defun write-sem-tree (ref stream)
+  (setq ddm-util::*indentation* 0) ;; make sure indentation is restarted
+  (if *direct-from-sem*
+      (write-sem ref stream)
+      (print-sem-tree (spire-tree ref) stream)))
+
+(defmethod write-combined-sentence-results ((s sentence) stream)
+  (declare (special *show-syn-tree*))
+  ;; we assume that this is called immediately after the sentence is parsed
+  (format stream "~%___________________~% ~s~%~%"
+          (sentence-string s))
+  (if *direct-from-sem*
+      (write-sem s stream)
+      (write-sem-tree
+       `(interpretation
+         ,.
+         (loop for edge in (all-tts (starts-at-pos s)(ends-at-pos s))
+            collect 
+              (let*
+                  ((*no-edge-info* t)
+                   (*suppress-indiv-uids* t)
+                   (mention (and
+                             (edge-p edge)
+                             (edge-mention edge)))
+                   (ref (if mention
+                            (base-description (edge-mention edge))
+                            edge)))            (declare (special ref *no-edge-info* *suppress-indiv-uids*))
+                            `(element
+                              ,(if (word-p ref)
+                                   `(wd ,(pname ref))
+                                   (spire-tree ref))))))
+       stream))
+  (terpri stream))
+
+(defmethod write-sem ((s sentence) stream)
+  (setq ddm-util::*indentation* 0) ;; make sure indentation is restarted
+  (emit-list-start "interpretation" stream)
+  (loop for tt in (all-tts (starts-at-pos s)(ends-at-pos s))
+     do
+       (push-indentation)
+       (emit-list-start "element" stream)
+       (if (and (edge-p tt)
+                (not (small-binding-list (edge-referent tt))))
+           (push-indentation))
+       (write-sem (if (edge-p tt)
+                      (edge-referent tt)
+                      tt)
+                  stream)
+       (emit-line-continue stream ")")
+       (if (and (edge-p tt)
+                (not (small-binding-list (edge-referent tt))))
+           (pop-indentation))
+       (pop-indentation))
+  (emit-line-continue stream ")"))
+
+
+(defmethod write-sem ((i individual) stream)
+  (cond ((simple-number? i)
+         (space-prin1 (value-of 'value i) stream))
+        ((small-binding-list (indiv-old-binds i))
+         (emit-list-start-continue (cat-string (car (indiv-type i))) stream)
+         (let ((bl (filter-bl (indiv-old-binds i))))
+           (when bl
+             (emit-list-start-continue (string-downcase (var-name (binding-variable (car bl)))) stream)
+             (write-sem (binding-value (car bl)) stream)
+             (emit-line-continue stream ")")))
+         (emit-line-continue stream ")"))
+        (t
+         (emit-list-start (cat-string (car (indiv-type i))) stream)
+         (print-binding-list (indiv-old-binds i) stream)
+         (emit-line-continue stream ")"))))
+
+(defmethod write-sem ((w string) stream)
+  (space-prin1 (pname w) stream))
+(defmethod write-sem ((w word) stream)
+  (space-prin1 (pname w) stream))
+(defmethod write-sem ((w polyword) stream)
+  (space-prin1 (pname w) stream))
+(defmethod write-sem ((w symbol) stream)
+  (lcase-space-prin1 (pname w) stream))
+(defmethod write-sem ((w number) stream)
+  (lcase-space-prin1 w stream))
+(defmethod write-sem ((val cons) stream)
+  (emit-list-start "" stream)
+  (loop for element in val
+     do
+       (write-sem element stream))
+  (format stream ")"))
+
+
+(defmethod write-sem ((c referential-category) stream)
+  (lcase-space-prin1 (cat-symbol c) stream))
+
+(defmethod small-binding-list ((bl symbol)) (null bl))
+(defmethod small-binding-list ((x word)) t)
+(defmethod small-binding-list ((x polyword)) t)
+(defmethod small-binding-list ((x number)) t)
+(defmethod small-binding-list ((x string)) t)
+(defmethod small-binding-list ((x referential-category)) t)
+(defmethod small-binding-list ((bl cons))
+  (setq bl (filter-bl bl))
+  (when (binding-p (car bl)) ;; can be passed the value of the ITEMS variable, which is a list
+    (and (null (cdr bl))
+         (typecase (binding-value (car bl))
+           ((or category string number symbol word polyword) t)
+           (t nil)))))
+
+(defmethod small-binding-list ((i individual))
+  (let ((bl (indiv-binds i)))
+    (and (null (cdr bl))
+         (binding-p (car bl))
+         (typecase (binding-value (car bl))
+           ((or category string number symbol word polyword) t)
+           (t nil)))))
+
+(defun filter-bl (bl)
+  (loop for b in bl
+     when (meaningful-binding? b)
+       collect b))
+
+       
+(defun meaningful-binding? (b)
+  (and (binding-p b)
+       (not (member (var-name (binding-variable b)) '(ras2-model has-determiner)))))
+
+(defun print-binding-list (bindings stream)
+  (push-indentation)
+  (loop for binding in (filter-bl bindings)
+     as var = (binding-variable binding)
+     do
+       (emit-list-start (string-downcase (var-name var)) stream)
+       (if (small-binding-list (binding-value binding))
+           (write-sem (binding-value binding) stream)
+           (else
+             (push-indentation)
+             (write-sem (binding-value binding) stream)
+             (pop-indentation)))
+       (format stream ")"))
+  (pop-indentation))
+      
 
 (defmethod print-sem-tree ((i individual) &optional stream)
   (print-sem-tree (semtree i) stream))
@@ -132,14 +251,17 @@
 (defmethod print-sem-tree ((s string) &optional stream)
   (space-prin1 s stream))
 
+(defun simple-number? (i)
+  (and (individual-p i)
+       (itypep i 'number)
+       (not (collection-p i))))
+
 (defmethod print-sem-tree ((sem-tree cons) &optional (stream *standard-output*))
   (declare (special *for-spire* *sentence-results-stream*))
   (let ((*suppress-indiv-uids* t))
     (declare (special *suppress-indiv-uids*))
     (cond 
-      ((and (individual-p (car sem-tree))
-            (itypep (car sem-tree) 'number)
-            (not (collection-p (car sem-tree))))
+      ((simple-number? (car sem-tree))       
        (space-prin1 (value-of 'value (car sem-tree)) stream))
       ((short-vvs (cdr sem-tree))
        (lcase-emit sem-tree stream))
@@ -168,25 +290,35 @@
 (defun emit-list-start (item stream)
   (emit-line stream (concatenate 'string "(" item)))
 
+
+(defun emit-list-start-continue (item stream)
+  (emit-line-continue stream (concatenate 'string " (" item)))
+
 (defun space-prin1 (item stream)
   (format stream " ~s" item))
 
 (defun short-vvs (vvs)
-  (and (not (cdr vvs))
+  (and (null (cdr vvs))
        (or (not (consp (car vvs)))
            (not (consp (second (car vvs)))))))
 
-(defun print-var-vals (vvs stream) 
+(defun print-var-vals (vvs stream)
+  (declare (special *for-spire*))
   (push-indentation)
   (loop for item in vvs
      unless (and (not *for-spire*) (consp item)
                  (member (car item) '(name uid)))
      do
        (if (consp item)
-           (then 
+           (then
              (emit-list-start (string-downcase (format nil "~a" (car item))) stream)
              (push-indentation)
-             (print-sem-tree (second  item) stream)
+             (if
+              (eq (car item) :members)
+              (loop for element in (cdr item)
+                 do
+                   (print-sem-tree element stream))
+              (print-sem-tree (second  item) stream))
              (format stream ")")
              (pop-indentation))
            (lcase-space-prin1 item stream)))
