c009de8f4f5da421374825aa73fffde169b8737a
Rusty Bobrow <rjbobrow@gmail.com>
2016-10-14 16:11:20 -0400

    new file, has to be loaded explicitly, uses cl-json, just contains functions to dump out the Krisp taxonomy in json format for TRIPS

---
+++ Sparser/code/s/tools/jsonify.lisp
@@ -0,0 +1,35 @@
+;;; THIS FILE NEEDS TO HAVE A HOME, BUT SHOULD ONLY BE LOADED IN A SYSTEM THAT HAS CL-JSON
+
+(in-package :sparser)
+(defun jsonify-cats (&optional
+                       (cats
+                        (sort
+                         (loop for cat in sp::*categories-defined*
+                            when (and (not (sp::form-category? cat))
+                                      (sp::subcategories-of cat))
+                            collect cat)
+                         #'string<
+                         :key #'sp::pname))
+                       (stream t))
+  (let ((json::*json-output* stream))
+    (declare (special json::*json-output*))
+      
+    (json::with-object  nil
+      (loop for cat in cats do (jsonify-cat cat)
+           (terpri json::*json-output*)))))
+
+(defun jsonify-cat (cat)
+  (json::as-object-member
+      ((sp::pname cat))
+    (json::with-object
+        nil
+      (json::encode-object-member
+       "parent"
+       (sp::pname (sp::lp-super-category (sp::cat-lattice-position cat))))
+      (json::as-object-member
+          ("roles")
+        (json::with-array
+            nil
+          (loop for var in (sp::cat-slots cat)
+             do
+               (json::encode-array-member (sp::pname var))))))))
