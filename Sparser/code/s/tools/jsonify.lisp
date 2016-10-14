6537cf3f2baa5b6c16238922472bd8349475b044
Rusty Bobrow <rjbobrow@gmail.com>
2016-10-14 16:24:45 -0400

    allow printing of leaves, and take care not to try to print items which have no lattice-point

--- Sparser/code/s/tools/jsonify.lisp
+++ Sparser/code/s/tools/jsonify.lisp
@@ -1,17 +1,18 @@
 ;;; THIS FILE NEEDS TO HAVE A HOME, BUT SHOULD ONLY BE LOADED IN A SYSTEM THAT HAS CL-JSON
 
 (in-package :sparser)
-(defun jsonify-cats (&optional
-                       (cats
-                        (sort
-                         (loop for cat in sp::*categories-defined*
-                            when (and (not (sp::form-category? cat))
-                                      (sp::subcategories-of cat))
-                            collect cat)
-                         #'string<
-                         :key #'sp::pname))
-                       (stream t))
-  (let ((json::*json-output* stream))
+(defun jsonify-cats (&optional cats (show-leaves nil) (stream t))
+  (let ((json::*json-output* stream)
+        (cats (or cats
+                  (sort
+                   (loop for cat in sp::*categories-defined*
+                      when (and (not (sp::form-category? cat))
+                                (sp::cat-lattice-position cat)
+                                (or show-leaves
+                                    (sp::subcategories-of cat)))
+                      collect cat)
+                   #'string<
+                   :key #'sp::pname))))
     (declare (special json::*json-output*))
       
     (json::with-object  nil
