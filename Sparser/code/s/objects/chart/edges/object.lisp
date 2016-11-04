d25511a5f2c4204027dae4d14db51cbd16a78253
Rusty Bobrow <rjbobrow@gmail.com>
2016-11-04 12:21:09 -0400

    replace a lsp-break with a warning -- not worth stopping for. Fix at leisure...

--- Sparser/code/s/objects/chart/edges/object.lisp
+++ Sparser/code/s/objects/chart/edges/object.lisp
@@ -105,7 +105,7 @@
 
 (defun set-used-by (daughter edge)
   (when (eq (edge-used-in edge) daughter)
-    (lsp-break "circularity detected in set-used-by")
+    (warn "circularity detected in set-used-by")
     (return-from set-used-by nil))
             
   (cond
