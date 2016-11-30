a9260deeb96e4935e07b3dc223221f91cb111e57
David D. McDonald <davidmcdonald@alum.mit.edu>
2016-11-30 15:30:53 -0500

    Make artifact inherit from object rather than physical. Adds in needed properties and isn't too unreasonable if 'objects' can be really big

--- Sparser/code/s/grammar/model/core/mid-level/ontology.lisp
+++ Sparser/code/s/grammar/model/core/mid-level/ontology.lisp
@@ -122,19 +122,16 @@ files once an area looks big enough to warrant it.  |#
 (define-category maker-of-artifacts
   :specializes physical-agent
   :mixins (make-artifacts named-type has-name))
-;; has-name makes proper nouns, hack "ford" to be a common noun.
+;; has-name makes proper nouns, hack "Ford" to be a common noun.
 
 ;; Would "manufacturer" add anything useful? Maybe distinguish crofters
 ;; in the Shetlands knitting sweaters from industrial giants?
  ;; ?? company (corporate entity)
       
 (define-category artifact
-  :specializes physical  
+  :specializes object
   :mixins (has-name partonomic)
   :binds ((made-by . maker-of-artifacts))
   ;; also time-created or is it a specialization from the lifetime of Endurant?
   :lemma (:common-noun "artifact"))
-
-
-
 
