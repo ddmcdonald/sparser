ec54fc2b6161cfd66d7a5df9aa7a1d8e4c3ad388
Rusty Bobrow <rjbobrow@gmail.com>
2017-01-03 17:27:49 -0500

    Fix typo that left #| in place

--- Sparser/code/s/grammar/rules/syntax/syntactic-rules.lisp
+++ Sparser/code/s/grammar/rules/syntax/syntactic-rules.lisp
@@ -939,7 +939,7 @@ similar to an oncogenic RasG12V mutation (9)."))
            :referent (:head right-edge) ;;  :bind (np left-edge)
            :form than-np)))
 
-#| almost never needed -- usually wrong
+
 ;;;--------------------------------------------------------
 ;; new rules for numbered items -- needs review
 ;;;--------------------------------------------------------
