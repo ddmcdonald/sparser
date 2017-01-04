280fb6de59df92082db3712233601dd42ac1c722
David D. McDonald <davidmcdonald@alum.mit.edu>
2017-01-04 16:11:40 -0500

    Added a doc-string that reminds the reader of what happens after they invoke this and what they should have done before they do

--- Sparser/code/s/analyzers/psp/terminate.lisp
+++ Sparser/code/s/analyzers/psp/terminate.lisp
@@ -1,16 +1,24 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 199,19932  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1991-1992,2017 David D. McDonald  -- all rights reserved
 ;;; 
 ;;;     File:  "terminate"
 ;;;   Module:  "analyzers;psp:"
-;;;  Version:  0.0 October 1992
+;;;  Version:  January 2017
 
 ;; original version lost. Initiating this file 10/5/92
 
 (in-package :sparser)
 
-
 (defun terminate-chart-level-process ()
+  "Called when there is absolutely nothing left for the
+   parser to do and we just want to exit. Any cleanup
+   or finishing off of incomplete analyses or display
+   must be done by the caller before they invoke this.
+   The thow is caught in chart-based-analysis which
+   then returns to its caller, analysis-core, which
+   will do any needed after-analysis-actions and
+   then invoke analysis-core-return-value to actually
+   exit back to the invoking process."
   (tr :terminate-chart-process)
   (throw 'terminating-chart-processing nil))
 
