2d5f5bcd0917e96b9bab5777f483444195b7eea5
Rusty Bobrow <rjbobrow@gmail.com>
2017-01-13 12:53:43 -0500

    Move still-valid code to before #+ignore'd code

--- Sparser/code/s/analyzers/psp/patterns/edge-patterns.lisp
+++ Sparser/code/s/analyzers/psp/patterns/edge-patterns.lisp
@@ -18,6 +18,93 @@
 ;;; prebuilt-multiword edges w/in the span
 ;;;----------------------------------------
 
+(defun ns-patterns/edge-slash-edge (label-pattern
+                                    start-pos end-pos edges words)
+  (cond
+   ((equal label-pattern '(:protein :forward-slash :protein))
+    (make-ns-pair 'protein (first edges) (second edges)
+                  words start-pos end-pos))
+   (t (if *work-on-ns-patterns*
+        (break "New edge slash pattern ~a" label-pattern)
+        (edge-that-punts-edge-inside-pattern words start-pos end-pos edges)))))
+
+(defun ns-patterns/edge-hyphen-edge (label-pattern
+                                     start-pos end-pos edges words)
+  (cond
+   ((equal label-pattern '(:protein :hyphen  :protein))
+    (make-ns-pair 'protein (first edges) (second edges)
+                  words start-pos end-pos))
+   ((equal label-pattern '(:protein :hyphen :bio-entity))
+    (make-protein-pair/convert-bio-entity 
+     start-pos end-pos edges words :right))
+   ((equal label-pattern '(:protein :hyphen :lower))
+    ;; same as (:full :hyphen :lower))  "GTP-bound" "EGFR-positive"
+    (resolve-hyphen-between-two-words label-pattern words start-pos end-pos))
+   (t (if *work-on-ns-patterns*
+        (break "New edge hyphen pattern ~a" label-pattern)
+        (edge-that-punts-edge-inside-pattern words start-pos end-pos edges)))))
+
+
+
+
+(defun edge-that-punts-edge-inside-pattern (words start-pos end-pos edges)
+  (let ((edge (make-edge-over-long-span
+               start-pos
+               end-pos
+               (edge-category (car edges)) 
+               :rule 'edge-that-punts-edge-inside-pattern
+               :form (edge-form (car edges))
+               :referent (edge-referent (car edges))
+               :constituents edges
+               :words words)))
+    ;;/// trace goes here
+    edge))
+
+
+;;; --------------------
+;;; Obsolete code below here
+;;; --------------------
+
+
+#+ignore ;; no longer called
+(defun identify-edge-ns-pattern (pattern start-pos end-pos)
+  "All of the edges in the no-space region as over single words
+  so we've not gone through the edge-based pattern route. Still,
+  this can sometime yield a more informative pattern, and can
+  permit things like coersion of bio-entities."
+  (push-debug `(,pattern ,start-pos ,end-pos))
+  ;; (setq pattern (car *) start-pos (cadr *) end-pos (caddr *))
+  (let (;;(pos-count (length pattern)) -- unused
+        (count 0)
+        (next-start start-pos)
+        edge  edges )
+    (loop
+      (incf count)
+      (setq edge (top-edge-at/starting next-start))
+      (unless (edge-p edge)
+        (cond
+         ((eq edge :multiple-initial-edges) ;; dev/s(#)
+          ;; in other situations we take the top one.
+          ;; Doesn't work in real ambiguities, but it's
+          ;; easy. Another option is chasing up used-in.
+          (setq edge (highest-edge (pos-starts-here next-start)))
+          (push edge edges))
+         ((null edge) ;; "re" in "re-activaton is a word without
+          ;; an edge so put in this part of the pattern instead
+          ;;//// need to give them meanings and not punt.
+          (push (nth (1- count) pattern) edges))
+         (t (error "Unexpected situation with edge vector ~
+                    at p~a" (pos-token-index next-start)))))
+      (setq next-start (chart-position-after next-start))
+      (when (eq next-start end-pos)
+        (return)))
+    (convert-pattern-edges-to-labels (nreverse edges))))
+
+
+
+
+
+#+ignore ;; no longer used
 (defun ns-sort-out-pattern-with-edges (pattern start-pos end-pos edges words
                                        hyphen-positions slash-positions
                                        colon-positions other-punct)
@@ -92,83 +179,3 @@
         (break "Work on one edge pattern"))
        (t
         (edge-that-punts-edge-inside-pattern words start-pos end-pos edges))))) |#
-
-(defun ns-patterns/edge-slash-edge (label-pattern
-                                    start-pos end-pos edges words)
-  (cond
-   ((equal label-pattern '(:protein :forward-slash :protein))
-    (make-ns-pair 'protein (first edges) (second edges)
-                  words start-pos end-pos))
-   (t (if *work-on-ns-patterns*
-        (break "New edge slash pattern ~a" label-pattern)
-        (edge-that-punts-edge-inside-pattern words start-pos end-pos edges)))))
-
-(defun ns-patterns/edge-hyphen-edge (label-pattern
-                                     start-pos end-pos edges words)
-  (cond
-   ((equal label-pattern '(:protein :hyphen  :protein))
-    (make-ns-pair 'protein (first edges) (second edges)
-                  words start-pos end-pos))
-   ((equal label-pattern '(:protein :hyphen :bio-entity))
-    (make-protein-pair/convert-bio-entity 
-     start-pos end-pos edges words :right))
-   ((equal label-pattern '(:protein :hyphen :lower))
-    ;; same as (:full :hyphen :lower))  "GTP-bound" "EGFR-positive"
-    (resolve-hyphen-between-two-words label-pattern words start-pos end-pos))
-   (t (if *work-on-ns-patterns*
-        (break "New edge hyphen pattern ~a" label-pattern)
-        (edge-that-punts-edge-inside-pattern words start-pos end-pos edges)))))
-
-
-
-
-(defun edge-that-punts-edge-inside-pattern (words start-pos end-pos edges)
-  (let ((edge (make-edge-over-long-span
-               start-pos
-               end-pos
-               (edge-category (car edges)) 
-               :rule 'edge-that-punts-edge-inside-pattern
-               :form (edge-form (car edges))
-               :referent (edge-referent (car edges))
-               :constituents edges
-               :words words)))
-    ;;/// trace goes here
-    edge))
-
-
-(defun identify-edge-ns-pattern (pattern start-pos end-pos)
-  "All of the edges in the no-space region as over single words
-  so we've not gone through the edge-based pattern route. Still,
-  this can sometime yield a more informative pattern, and can
-  permit things like coersion of bio-entities."
-  (push-debug `(,pattern ,start-pos ,end-pos))
-  ;; (setq pattern (car *) start-pos (cadr *) end-pos (caddr *))
-  (let (;;(pos-count (length pattern)) -- unused
-        (count 0)
-        (next-start start-pos)
-        edge  edges )
-    (loop
-      (incf count)
-      (setq edge (top-edge-at/starting next-start))
-      (unless (edge-p edge)
-        (cond
-         ((eq edge :multiple-initial-edges) ;; dev/s(#)
-          ;; in other situations we take the top one.
-          ;; Doesn't work in real ambiguities, but it's
-          ;; easy. Another option is chasing up used-in.
-          (setq edge (highest-edge (pos-starts-here next-start)))
-          (push edge edges))
-         ((null edge) ;; "re" in "re-activaton is a word without
-          ;; an edge so put in this part of the pattern instead
-          ;;//// need to give them meanings and not punt.
-          (push (nth (1- count) pattern) edges))
-         (t (error "Unexpected situation with edge vector ~
-                    at p~a" (pos-token-index next-start)))))
-      (setq next-start (chart-position-after next-start))
-      (when (eq next-start end-pos)
-        (return)))
-    (convert-pattern-edges-to-labels (nreverse edges))))
-
-
-
-
