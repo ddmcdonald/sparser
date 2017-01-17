cd5bb704af0699a2286fb238608fbc2983161c7c
David D. McDonald <davidmcdonald@alum.mit.edu>
2017-01-17 13:25:04 -0500

    Added measure-fsa-facts to tally various data about rule sets and the fsa-field

--- Sparser/code/s/objects/rules/rule-links/object.lisp
+++ Sparser/code/s/objects/rules/rule-links/object.lisp
@@ -1,9 +1,9 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
-;;; copyright (c) 1992-1995,2016 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992-1995,2016-2017 David D. McDonald  -- all rights reserved
 ;;;
 ;;;      File:   "object"
 ;;;    Module:   "objects;rules:rule links:"
-;;;   Version:   September 2016
+;;;   Version:   January 2017
 
 ;; 2.0 (9/4/92 v2.3) Changed the names and contents descriptions of the
 ;;      left/right combinations.
@@ -61,8 +61,44 @@
 (defun rs-distinct-categories (rs)
   (remove-duplicates
    (loop for r in (rs-single-term-rewrites rs)
-         when
-           (and (cfr-p r)
+      when (and (cfr-p r)
                 (not (consp (cfr-referent r))))
-         collect
-           (itype-of (cfr-referent r)))))
+      collect (itype-of (cfr-referent r)))))
+
+
+;;;----------------------
+;;; statistics gathering
+;;;----------------------
+
+(defun measure-fsa-facts ()
+  "Count what's in the fsa field of the rule-sets of the defined words"
+  ;; Listed counts from a biology load 1/17/17
+  (declare (special *words-defined*))
+  (let ((words-without-rs 0) ;; 22,282
+        (words-without-fsa-field 0)  ;; 29,109
+        (words-with-fsa 0) ;; 20
+        (words-with-pw-start 0) ;; 17,946
+        (words-with-field-count-over-two 0) ;; 0
+        rule-set  fsa-field )
+    (dolist (word *words-defined*) ;; 69,376
+      (setq rule-set (rule-set-for word))
+      (cond
+        ((null rule-set)
+         (incf words-without-rs))
+        ((eq rule-set :whitespace)) ;; e.g. for #<word 21-spaces>
+        (t
+          (setq fsa-field (rs-fsa rule-set))
+          (if (null fsa-field)
+            (incf words-without-fsa-field)
+            (else
+              (etypecase fsa-field
+                (cons
+                 (when (> (length fsa-field) 2)
+                   (incf words-with-field-count-over-two))
+                 (dolist (item fsa-field)
+                   (etypecase item
+                     (polyword-state
+                      (incf words-with-pw-start))
+                     (symbol
+                      (incf words-with-fsa)))))))))))
+    (lsp-break "look around")))
