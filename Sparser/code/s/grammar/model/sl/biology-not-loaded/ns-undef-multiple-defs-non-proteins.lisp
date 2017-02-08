3799cf2b379b0574b02b348e9422280b6dcd5110
Rusty Bobrow <rjbobrow@gmail.com>
2017-02-08 00:31:20 -0500

    material created in the process of absorbing definitions from TRIPS

---
+++ Sparser/code/s/grammar/model/sl/biology-not-loaded/ns-undef-multiple-defs-non-proteins.lisp
@@ -0,0 +1,390 @@
+
+(def-bio "A-1" cell-line :long "A1 [Human leukemia]" :identifier "CVCL:6G43") 
+nil 
+nil 
+nil 
+nil 
+(def-bio "A02" cell-line :long "A02" :identifier "CVCL:W875") 
+(def-bio "A2B5" drug :long "ganglioside GQ" :identifier "NCIT:C104201") 
+nil 
+nil 
+(define-category aaa :specializes protein-domain :bindings (uid "XFAM:PF00004.27") :realization (:noun "AAA")) 
+nil 
+nil 
+nil 
+(def-bio "AB1" cell-line :long "AB1 [Mouse ESC]" :identifier "CVCL:N791") 
+(def-bio "ABC" disease :long "aneurysmal bone cyst" :identifier "NCIT:C3516") 
+nil 
+nil 
+nil 
+nil 
+(def-bio "AC2M2" cell-line :long "AC2M2" :identifier "CVCL:AZ67") 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+(def-bio "ADPribose" molecule :long "ADP-D-ribose" :identifier "CHEBI:16960") 
+nil 
+nil 
+nil 
+(def-bio "AE1" cell-line :long "AE-1" :identifier "CVCL:4500") 
+nil 
+nil 
+(def-bio "AGN194204" drug :long "rexinoid NRX 194204" :identifier "NCIT:C74076") 
+(def-bio "AIN" cancer :long "anal intraepithelial neoplasia" :identifier "NCIT:C4595") 
+nil 
+nil 
+nil 
+nil 
+nil 
+(def-bio "ALF" cell-line :long "ALF" :identifier "CVCL:M035") 
+(def-bio "ALH" cancer :long "atypical lobular breast hyperplasia" :identifier "NCIT:C4730") 
+nil 
+nil 
+(def-bio "AM580" molecule :long "4-{[(5,5,8,8-tetramethyl-5,6,7,8-tetrahydronaphthalen-2-yl)carbonyl]amino}benzoic acid" :identifier "CHEBI:64210") 
+(def-bio "AMC" disease :long "Arthrogryposis multiplex congenita" :identifier "ORPHANET:1037") 
+(def-bio "AMCs" disease :long "Arthrogryposis multiplex congenita" :identifier "ORPHANET:1037") 
+nil 
+nil 
+(define-category |AMPKα| :specializes protein-family :bindings (uid "NCIT:C116019") :realization (:noun "AMPKα")) 
+(def-bio "AMPPNP" molecule :long "AMP-PNP" :identifier "CHEBI:47785") 
+nil 
+nil 
+(define-category anth :specializes protein-domain :bindings (uid "XFAM:PF07651.14") :realization (:noun "ANTH")) 
+nil 
+nil 
+(def-bio "AP21967" drug :long "AP 21967" :identifier "MESH:C539629") 
+(def-bio "APA" cell-line :long "APA" :identifier "CVCL:E458") 
+nil 
+(def-bio "APD" cell-line :long "APD" :identifier "CVCL:E459") 
+(def-bio "AR1" cell-line :long "CHO-AR1" :identifier "CVCL:5A24") 
+nil 
+nil 
+(def-bio "ARCMT2" disease :long "Autosomal recessive axonal Charcot-Marie-Tooth disease type 2" :identifier "ORPHANET:91024") 
+nil 
+nil 
+nil 
+nil 
+(def-bio "AS1" cell-line :long "AS-1" :identifier "CVCL:DC71") 
+(def-bio "AS252424" drug :long "5-(5-(4-fluoro-2-hydroxyphenyl)furan-2-ylmethylene)thiazolidine-2,4-dione" :identifier "MESH:C512438") 
+(def-bio "AS703026" drug :long "MEK inhibitor AS703026" :identifier "NCIT:C84864") 
+nil 
+(def-bio "ASV" drug :long "asunaprevir" :identifier "NCIT:C114982") 
+nil 
+nil 
+nil 
+nil 
+(def-bio "ATLD" disease :long "Ataxia-telangiectasia-like disorder" :identifier "ORPHANET:251347") 
+nil 
+nil 
+nil 
+nil 
+(def-bio "ATRA" drug :long "vitamin A acid" :identifier "NCIT:C900") 
+nil 
+nil 
+nil 
+(def-bio "AW13516" cell-line :long "AW 13516" :identifier "CVCL:D652") 
+(def-bio "AZQ" drug :long "diaziquone" :identifier "NCIT:C1363") 
+nil 
+(def-bio "Ab-1" cell-line :long "AB1 [Mouse ESC]" :identifier "CVCL:N791") 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+(def-bio "Abu" cell-line :long "31227ABO" :identifier "CVCL:E443") 
+nil 
+nil 
+nil 
+nil 
+nil 
+(def-bio "AcCoA" molecule :long "acetyl-CoA" :identifier "CHEBI:15351") 
+nil 
+nil 
+nil 
+(def-bio "Acanthamoeba" organism :long "acanthamoeba" :identifier "NCIT:C118934") 
+nil 
+nil 
+nil 
+(define-category |Acetylation| :specializes bio-process :bindings (uid "NCIT:C16255") :realization (:noun "Acetylation")) 
+nil 
+(def-bio "Acinetobacter" bacterium :long "acinetobacter" :identifier "NCIT:C76200") 
+nil 
+nil 
+nil 
+(def-bio "Acrolein" molecule :long "acrolein" :identifier "CHEBI:15368") 
+nil 
+nil 
+nil 
+nil 
+(def-bio "ActivinA" molecule :long "Activin A" :identifier "CHEBI:81351") 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+(def-bio "Alginate" molecule :long "alginate" :identifier "CHEBI:58187") 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+(def-bio "Aminoguanidine" molecule :long "aminoguanidine" :identifier "CHEBI:40618") 
+(def-bio "Aminopyridine" molecule :long "aminopyridine" :identifier "CHEBI:38207") 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+(def-bio "Anakinra" drug :long "anakinra" :identifier "NCIT:C38717") 
+nil 
+nil 
+nil 
+nil 
+(def-bio "Andromeda" organism :long "andromeda" :identifier "NCIT:C74316") 
+nil 
+nil 
+nil 
+nil 
+nil 
+(def-bio "Aniridia" disease :long "aniridia" :identifier "NCIT:C84563") 
+nil 
+(def-bio "Ankyloblepharon" disease :long "fused eyelid" :identifier "NCIT:C87077") 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+(def-bio "Antimycin" drug :long "antimycin" :identifier "MESH:C032456") 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+(def-bio "Anvirzel" drug :long "Anvirzel" :identifier "MESH:C430926") 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+(def-bio "Arp1" cell-line :long "ARP-1" :identifier "CVCL:D523") 
+nil 
+nil 
+nil 
+nil 
+(def-bio "Ash" cell-line :long "ASH" :identifier "CVCL:R966") 
+nil 
+nil 
+nil 
+(define-category |Asia| :specializes protein-domain :bindings (uid "XFAM:PF09010.8") :realization (:noun "Asia")) 
+(def-bio "Aspergillus" organism :long "aspergillus" :identifier "NCIT:C77180") 
+nil 
+nil 
+(def-bio "Astringinin" drug :long "3,3',4,5'-tetrahydroxystilbene" :identifier "MESH:C041525") 
+nil 
+(def-bio "Atopy" disease :long "atopy" :identifier "EFO:0002686") 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+nil 
+(def-bio "B6C3F1" organism :long "B6C3 mouse" :identifier "NCIT:C76182") 
+(def-bio "B6H12" cell-line :long "B6H12.2" :identifier "CVCL:J980") 
+nil 
+nil 
+(def-bio "BBN" molecule :long "N-butyl-N-(4-hydroxybutyl)nitrosamine" :identifier "CHEBI:91275") 
+(def-bio "BCM" drug :long "carbendazim" :identifier "NCIT:C1154") 
+nil 
+nil 
+(define-category bdm :specializes protein-family :bindings (uid "XFAM:PF10684.7") :realization (:noun "BDM")) 
+(def-bio "BE13" cell-line :long "BE-13" :identifier "CVCL:1081") 
+nil 
+nil 
+nil 
+nil 
+nil 
+(def-bio "BL6" cell-line :long "BL-6" :identifier "CVCL:M564") 
+(def-bio "BMAA" molecule :long "L-BMAA" :identifier "CHEBI:73169") 
+nil 
+(def-bio "BMDC" cell-type :long "bone marrow-derived dendritic cell" :identifier "BTO:0003857") 
+nil 
+nil 
+nil 
+(def-bio "BOY" cell-line :long "BOY-12E" :identifier "CVCL:7948") 
+nil 
+(def-bio "BPD" disease :long "Bronchopulmonary dysplasia" :identifier "ORPHANET:70589") 
+nil 
+nil 
+nil 
+nil 
+nil 
+(def-bio "BRN3" cell-line :long "BRN-3" :identifier "CVCL:0U17") 
+nil 
+nil 
+nil 
+nil 
+nil 
