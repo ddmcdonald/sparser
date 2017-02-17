c8839fc61573fd8ef3b5eaf381b8e25b960bca3c
David D. McDonald <davidmcdonald@alum.mit.edu>
2017-02-17 18:03:18 -0500

    Creeping up on doing "high", "low" as levels. Trouble is that should do "level" at the same time, and it's got too many citations for it to be obvious how to do it.

--- Sparser/code/s/grammar/model/core/mid-level/attributes.lisp
+++ Sparser/code/s/grammar/model/core/mid-level/attributes.lisp
@@ -2,8 +2,8 @@
 ;;; copyright (c) 2017 David D. McDonald  -- all rights reserved
 ;;; This file is part of the SIFT-Brandeis CwC project
 ;;;
-;;;     File:  "height"
-;;;   Module:  "grammar/core/qualities/
+;;;     File:  "attributes"
+;;;   Module:  "grammar/core/mid-level/
 ;;;  version:  February 2017
 
 ;; Initiated 1/11/17 to collect all the attributes together
@@ -15,6 +15,10 @@
 (define-attribute height) ;; "tall"
 (define-attribute width)  ;; "narrow"
 (define-attribute length) ;; "long"
+
+(define-attribute amount-as-level) ;; high, low
+;; The word "level" is a bio-scalar in bio, and used extensively
+;; so have to prepare for fanout when naming this 'level'
 
 (define-attribute rate-of-change) ;; fast, slow
 ;; vs 'rate' as defined in amounts/measurements.lisp
