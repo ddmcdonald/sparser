3b627193f8de038f819f348e3844556973d67460
David D. McDonald <davidmcdonald@alum.mit.edu>
2017-02-27 14:56:20 -0500

    work space

--- Sparser/code/s/grammar/model/core/mid-level/relations.lisp
+++ Sparser/code/s/grammar/model/core/mid-level/relations.lisp
@@ -1,11 +1,18 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 2014-2015 David D. McDonald  -- all rights reserved
+;;; copyright (c) 2015-2017 David D. McDonald  -- all rights reserved
 ;;;
 ;;;     File:  "relations"
 ;;;   Module:  "model;core:mid-level::"
-;;;  version:  December 2015 
+;;;  version:  February 2017
 
 ;; Initiated 12/3/15
 
 (in-package :sparser)
 
+#| <something> wants 
+     <to do something> ;; they do something
+     <something> (to have somthing)
+
+ desire
+ expect
+|#
