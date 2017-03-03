cfa1774953a6735a4bcaf6ecea824cd7b834242c
David D. McDonald <davidmcdonald@alum.mit.edu>
2017-03-03 10:12:23 -0500

    Wrote a mix-in that covers the principal elements of a control verb. Works on "want", so easy to add many more.

--- Sparser/code/s/grammar/model/core/mid-level/relations.lisp
+++ Sparser/code/s/grammar/model/core/mid-level/relations.lisp
@@ -9,35 +9,73 @@
 
 (in-package :sparser)
 
-(define-category action-by-on
-  :specializes process
+(define-mixin-category action-verb
+  :specializes linguistic
   :instantiates nil
-  :binds ((subject endurant) ;; matches bio-process
-          (object endurant))
+  :binds ((subject (:or pronoun endurant))
+          (object endurant)) 
   :documentation "Provides standard bindings for ordinary transitive
-   verbs. Could take most of what's on bio-process")
+    verbs. Could take most of what's on bio-process. Added 'action'
+    to mimic bio;aspectual-relation"
+  :realization
+    (:s subject
+     :o object ))
+        
+#|
+;; I failed to find a block
+(define-category fail
+  :specializes process ;; acomplishment?
+  :mixins (action-verb)
+  :realization 
+    (:verb "fail"  
+     :etf (sv)
+     ) |#
+
 
 (define-category find ;; see bio;harvard-terms
-  :specializes action-by-on
+  :specializes action-verb
+  :mixins (action-verb)
   :restrict ((object (:or physical-object location))) ;; find a block
   :realization
     (:verb ("find" :past-tense "found")
-     :etf (svo-passive)
-     :s subject       
-     :o object))
+     :etf (svo-passive)))
 
+
+
+;; get / have
+;; go
+;; going to X / gonna
 
 ;; -- BDI  --self action vs other action
 #| <something> wants 
      <to do something> ;; they do something
-     <something> (to have somthing)
+     <something> (to do somthing)
 
+
+ attempt try
  desire
  expect
+ intend |#
 
+;; https://en.wikipedia.org/wiki/Control_(linguistics)
+(define-mixin-category control-verb
+  :specializes linguistic ;; daughter of abstract
+  :mixins (with-an-agent) ;; v/r = physical-agent
+  :binds ((patient physical) ;; "I want a block"
+          (theme process)) ;; "I want to go home"
+                ;; T: neutral, VN: theme
+  :realization
+    (:s agent
+     :o patient
+     :to-comp theme))
+;; "I want a block" -- agent v patient
+;; "I want to go home" -- agent v theme
+;; "I want you to go home" -- agent v patieht theme
+;;     But roll-up from the right parsing might
+;;     argue for different structure
 
 (define-category want ;; -something -to-do-something
   :specializes state
-  :mixins (with-an-agent)
-  :binds ((action ???)))
-|#
+  :mixins (control-verb)
+  :realization (:verb "want"))
+    
