a44f4be28812750f01d32d8cb3f300ed26e6fb13
David D. McDonald <davidmcdonald@alum.mit.edu>
2017-03-08 12:05:20 -0500

    Extend the definition of wh pronouns to include the variable the are querying. This will want some playing with. Wrote first method to handle an embedded question, method for 'what', as in "tell me what you want to do now"

--- Sparser/code/s/grammar/rules/syntax/wh-word-semantics.lisp
+++ Sparser/code/s/grammar/rules/syntax/wh-word-semantics.lisp
@@ -28,13 +28,14 @@ for the word and a rule that rewrite to it. |#
 
 (define-category wh-pronoun
   :specializes pronoun
-  :binds ((type :primitive category)))
+  :binds ((variable :primitive lambda-variable))
+  :documentation "")
 
 ;;;---------------
 ;;; defining form
 ;;;---------------
 
-(defun define-wh-pronoun (string &key variable category)
+(defun define-wh-pronoun (string &key variable)
   (let* ((word (define-function-word string
                  :brackets `( ].phrase .[np  np].  phrase.[ )
                  :form category::wh-pronoun))
@@ -46,7 +47,7 @@ for the word and a rule that rewrite to it. |#
          (word-rule 
           (define-cfr category `(,word)
             :form category::wh-pronoun
-            :schema (get-schematic-word-rule :word) ;;/// make one for wh ??
+            :schema (get-schematic-word-rule :word) ;;/// make schema for wh ??
             :referent category)))
     (add-rule word-rule category)
     (values category
@@ -60,16 +61,20 @@ for the word and a rule that rewrite to it. |#
 
 ;; N.b. this replaces rules/words/WH words
 
-(define-wh-pronoun "who" :variable 'agent)
-(define-wh-pronoun "whoever")
+(define-wh-pronoun "who" :variable 'actor)
 (define-wh-pronoun "whom")
 (define-wh-pronoun "whose") ;; possession
 (define-wh-pronoun "which")
-(define-wh-pronoun "what")
-(define-wh-pronoun "where" :category 'location)
-(define-wh-pronoun "when"  :category 'time)
-(define-wh-pronoun "why") ;; reason?
-(define-wh-pronoun "how") ;; not really manner, more 'method'
+(define-wh-pronoun "what"  :variable 'patient)
+(define-wh-pronoun "where" :variable 'location)
+(define-wh-pronoun "when"  :variable 'time)
+
+(define-wh-pronoun "why"   :variable 'purpose)
+;;  more like reason, motive.
+;; 'purpose' is more like an attribute
+
+(define-wh-pronoun "how"   :variable 'manner)
+;; not really manner, more 'method'
 
 (define-wh-pronoun "whether") ;; polarity ?
 ;; overnight #15, aspp2 #20
@@ -77,9 +82,37 @@ for the word and a rule that rewrite to it. |#
 ;;/// There's a whole flock of these -- need a proper treatment
 ;;(define-wh-pronoun "how many")
 
+;; "who else <vp>"
+
+(define-wh-pronoun "whoever")
+;; wherever, whenever
+
 ;;;----------
 ;;; grammmar
 ;;;----------
 
-(def-k-method compose ((wh category::wh-pronoun) (i individual))
-  (break "i = ~a" i))
+
+;;---- What
+
+;; (p/s "tell me what you want to do now.")
+
+(def-k-method compose ((wh category::what) (i individual))
+  ;; called from make-subordinate-clause for wh + s
+  (declare (special category::wh-question))
+  (let ((j (define-an-individual category::wh-question
+               :wh wh
+               :statement i)))
+    ;; The individual is probably open in their object
+    ;; and the statement is a essentially a description of it
+    (let* ((var (object-variable i))
+           (wh-var
+            (if (and var (null (value-of 'variable i)))
+              var
+              (value-of 'variable wh))))
+      (setq j ;;/// bind-variable should do this for us (!!)
+            (bind-variable 'var wh-var j))
+
+      (setq j (add-category-to-individual j (itype-of i)))
+      (format t "~&j = ~a ~%"  j)
+      j)))
+
