600fc8c1b8f30581f73825f3aef127a543b08eb8
Rusty Bobrow <rjbobrow@gmail.com>
2017-03-26 14:52:42 -0400

    unknown word strings from the phase3 articles

---
+++ Sparser/code/s/tools/ns-stuff/ns-unknown-rd-items-phase3-49-50.lisp
@@ -0,0 +1,18 @@
+
+(IN-PACKAGE :SP)
+(DEFPARAMETER *NS-RD-PHASE3-49-50*
+  '("ASK-1" "A–C" "A–D" "C-type" "DEVD" "Dolastatin" "Dolastatin-10" "Fourier"
+    "GFP-Bax–transfected" "GTP" "G–I" "HIV-2" "Hoechst" "I-type" "KIF1A" "Kin"
+    "M-type" "N-type" "PSD-95" "RTTs" "Rev" "Rev-induced" "Rev-promoted"
+    "Rev–Tubulin" "Rev–tubulin" "SNP" "X-ray" "XKCM1" "XKIF2" "XKIF2-tubulin"
+    "XKIF2–tubulin" "XMAP215" "apoptosis" "catalytic" "cell-permeable"
+    "chromosome-to-pole" "citrate" "colchicine" "cold-induced" "cross-section"
+    "cryo-EM" "dark-field" "deprivation-induced" "dichroism" "helices" "helix"
+    "helix–helix" "injury-induced" "kD" "katanins" "kinesin" "kinesin-like"
+    "kinesins" "lower-energy" "lysozyme" "mass-per-unit-length" "maytansine"
+    "mg" "micrographs" "minus-end–directed" "ml" "motor" "nNOS" "nitroprusside"
+    "nm" "pH" "pan-caspase" "phase-contrast" "plus-end" "plus-ends"
+    "polyanions" "polyglutamate" "proteins" "ring-like" "side-views" "sub-G1"
+    "subunits" "survival" "thin-rimmed" "tubulin" "tubulin–drug" "vinca"
+    "x-axis" "zDEVD-AFC" "α-helical" "α-helices" "β-sheet" "β-subunits"
+    "β-tubulin"))
