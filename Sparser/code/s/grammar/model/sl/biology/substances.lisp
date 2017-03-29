57fa15ef5b33d529555ffe346e88063fa2410dc1
lbobrow <lbobrow@sift.net>
2017-03-29 17:07:18 -0400

    new file to cover TRIPS def type

---
+++ Sparser/code/s/grammar/model/sl/biology/substances.lisp
@@ -0,0 +1,8 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER COMMON-LISP) -*-
+;;; Copyright (c) 2017 SIFT LLC. All Rights Reserved
+;;;
+;;;    File: "loader"
+;;;  Module: "grammar/model/sl/biology/
+;;; version: March 2017
+
+;; For items identified as substances from TRIPS
