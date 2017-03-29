46b22d46e3251841ca4439563fe4c5f111f19bc1
lbobrow <lbobrow@sift.net>
2017-03-28 23:37:12 -0400

    updated loader to get new trips def file

--- Sparser/code/s/grammar/model/sl/biology/loader.lisp
+++ Sparser/code/s/grammar/model/sl/biology/loader.lisp
@@ -66,6 +66,7 @@
 ;;(gload "bio;trips-terms.lisp")
 (gload "bio;trips-terms-new.lisp")
 (gload "bio;trips-terms-phase3-1-2000-non-proteins")
+(gload "bio;trips-terms-phase3-2nd-run-non-proteins")
 ;;(gload "bio;trips-proteins")
 (gload "bio;reach-terms.lisp")
 
