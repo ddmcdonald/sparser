707c6cec0c340b676e7a0ab53f7478884845f720
lbobrow <lbobrow@sift.net>
2017-04-03 11:06:14 -0400

    minor corrections

--- Sparser/code/s/grammar/model/sl/biology/bio-complexes.lisp
+++ Sparser/code/s/grammar/model/sl/biology/bio-complexes.lisp
@@ -1,7 +1,7 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER COMMON-LISP) -*-
 ;;; Copyright (c) 2017 SIFT LLC. All Rights Reserved
 ;;;
-;;;    File: "loader"
+;;;    File: "bio-complexes"
 ;;;  Module: "grammar/model/sl/biology/
 ;;; version: February 2017
 
