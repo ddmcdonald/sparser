e71518e0ba4b61d8d7a6cdcdb43a4995dd50e2ae
David D. McDonald <davidmcdonald@alum.mit.edu>
2017-04-24 12:09:17 -0400

    Repaired paste error in the mode line

--- Sparser/code/s/grammar/rules/tree-families/verbs-taking-pps.lisp
+++ Sparser/code/s/grammar/rules/tree-families/verbs-taking-pps.lisp
@@ -1,7 +1,6 @@
-;;; -*- Mode:LISP; Syntax:Common/Users/ddm/sparser/Sparser/code/s/grammar/rules/tree-families/verbs-taking-pps.lisp-Lisp; Package:(SPARSER LISP) -*-
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
 ;;; copyright (c) 1995-2005 David D. McDonald  -- all rights reserved
 ;;; extensions copyright (c) 2009 BBNT Solutions LLC. All Rights Reserved
-;;; $Id:$
 ;;;
 ;;;     File:  "verbs taking pps"
 ;;;   Module:  "grammar;rules:tree-families:"
