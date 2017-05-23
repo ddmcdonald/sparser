44a6b20b13f04f990819df49fd724251f0b3dffa
David D. McDonald <davidmcdonald@alum.mit.edu>
2017-05-23 11:36:53 -0400

    In get-mumble-word-for-sparser-word provided more ways to get from a category to its head word.

--- Sparser/code/s/interface/mumble/gofers.lisp
+++ Sparser/code/s/interface/mumble/gofers.lisp
@@ -1,9 +1,9 @@
 ;;; -*- Mode: Lisp; Syntax: Common-Lisp; Package: SPARSER; -*-
-;;; Copyright (c) 2013-2016 David D. McDonald. All Rights Reserved.
+;;; Copyright (c) 2013-2017 David D. McDonald. All Rights Reserved.
 ;;;
 ;;;     File: "gofers"
 ;;;   Module: "interface;mumble;"
-;;;  Version: April 2016
+;;;  Version: May 2017
 
 ;; Initated 1/7/16 to consolidate usable gofer-type code from files
 ;; that for various reasons (OBE mostly) are not being loaded.
@@ -14,25 +14,45 @@
 ;;; mumble words for sparser words and vice-versa
 ;;;-----------------------------------------------
 
+;;--- words
+
 (defgeneric get-mumble-word-for-sparser-word (s-word pos)
-  (:method ((s-word individual) pos)
-    (get-mumble-word-for-sparser-word (or (value-of 'word s-word)
-                                          (category-of s-word))
+  (:documentation "The types that represent words are different in Mumble
+   and Sparser -- for no good reason but merging them would entail a good
+   deal of labor for uncertain gain. Given a Sparser word object return the 
+   corresponding Mumble word object. On the Mumble side, retrieving the
+   correct word object requires us to specify the part of speech.
+   If the Mumble-side word does not exist then we create it.")
+  (:method ((i individual) pos)
+    (get-mumble-word-for-sparser-word (or (value-of 'word i)
+                                          (category-of i))
                                       pos))
-  (:method ((s-word category) pos)
-    (get-mumble-word-for-sparser-word (or (value-of 'word s-word)
-                                          (pname s-word))
+  (:method ((c category) pos)
+    (get-mumble-word-for-sparser-word (or (value-of 'word c)
+                                          (rdata-head-word c pos)
+                                          (lemma c pos)
+                                          (string-downcase (pname c)))
                                       pos))
   (:method ((s-word word) pos)
     (get-mumble-word-for-sparser-word (pname s-word) pos))
   (:method ((s-word polyword) pos)
     (get-mumble-word-for-sparser-word (pname s-word) pos))
-  (:method ((s-word string) pos)
-    (or (mumble::find-word s-word pos)
-        (mumble::define-word/expr s-word (list pos)))))
+  
+  (:method ((s-pname string) pos)
+    "If the mumble word doesn't exist (or not for this part of speech)
+     then create it."    
+    (or (mumble::find-word s-pname pos)
+        (else
+          #+ignore(when (or (string-equal s-pname "top")
+                    (string-equal s-pname "top-qua-location"))
+            (lsp-break "Ok to word for ~s ?" s-pname))
+          (mumble::define-word/expr s-pname (list pos))))))
 
 (defun get-sparser-word-for-mumble-word (m-word)
   (word-named (mumble::pname m-word)))
+
+
+;;--- symbols
 
 (defgeneric mumble-symbol (symbol)
   (:method ((s symbol)) (mumble-symbol (symbol-name s)))
