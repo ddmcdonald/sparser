1f5818cb82819ba50453d4a9fd3e0ef12c655056
lbobrow <lbobrow@sift.net>
2017-05-31 14:47:00 -0400

    the new def-indiv-with-id definitions from the trips/reach stuff

---
+++ Sparser/code/s/grammar/model/sl/biology/new-defs/new-rna.lisp
@@ -0,0 +1,9 @@
+(in-package :sparser)
+
+
+(def-indiv-with-id micro-rna "MiR-200c" "NCIT:C82157" :name "MIR200C") 
+(def-indiv-with-id rna "si-RNA" "NCIT:C2191" :name "small interfering RNA") 
+(def-indiv-with-id rna "pri-miRNA" "NCIT:C25967" :name "pri-miRNA") 
+(def-indiv-with-id rna "hsa-let-7" "NCIT:C48183" :name "let-7") 
+(def-indiv-with-id rna "LncRNA" "NCIT:C88924" :name "lincRNA") 
+(def-indiv-with-id rna "pre-mRNA" "NCIT:C111298" :name "precursor mRNA") 
