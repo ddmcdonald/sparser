ed1bee2df268b245a9cc3049901c557fa3947717
David D. McDonald <davidmcdonald@alum.mit.edu>
2017-06-06 13:09:21 -0400

    Put in a couple missing cases and took another stab at the case for derivation tree node but it's still not right

--- Mumble/derivation-trees/printers.lisp
+++ Mumble/derivation-trees/printers.lisp
@@ -21,16 +21,18 @@
       ,@(when (referent dtn)
              `(:referent ,(pp-dtn (referent dtn))))
       (:resource ,(pp-dtn (resource dtn)))
-      ,@(when (features dtn)
+      ,(when (features dtn)
              `(:features ,(pp-dtn (features dtn))))
-      ,@(when (adjuncts dtn)
-             `(:adjuncts
-               ,@(loop for an in (adjuncts dtn)
-                    collect (pp-dtn an))))
-      ,@(when (complements dtn)
-             `(:complements
-               ,@(loop for cn in (complements dtn)
-                    collect (pp-dtn cn))))))
+      ,@(if (adjuncts dtn)
+           `(:adjuncts
+             ,@(loop for an in (adjuncts dtn)
+                  collect (pp-dtn an)))
+           '(:adjuncts nil))
+      ,@(if (complements dtn)
+           `(:complements
+             ,@(loop for cn in (complements dtn)
+                  collect (pp-dtn cn)))
+           nil)))
 
   ;; method for sp::individual is in
   ;; Sparser/.../interface/mumble/interface.lisp
@@ -47,11 +49,14 @@
       (t
        `(,@(loop for item in list
               collect (pp-dtn item))))))
-  
+
+  (:method ((s string)) s)
   (:method ((w word))
     (pname w))
   (:method ((p pronoun))
     (mname p))
+  (:method ((tr ttrace))
+    ":trace")
   (:method ((at accessory-type))
     (mname at))
   (:method ((av accessory-value))
@@ -86,15 +91,24 @@
 ;;; print methods for derivation-tree types
 ;;;-----------------------------------------
 
+(defun phrase-name-for-printing (phrase)
+  (or (when (typep phrase 'has-name) (mname phrase))
+      (name phrase)))
+
+(defgeneric pprint-value (value)
+  (:documentation "Shared printer for simple objects in value fields.
+     Returns a string.")
+  (:method ((w word))
+    (format nil "~s" (pname w))))
+
+
 (defmethod print-object ((dtn base-dt-node) stream)
   (print-unreadable-object (dtn stream)
     (format stream "dtn for ~a" (or (referent dtn) "null referent"))))
 
 (defmethod print-object ((slp saturated-lexicalized-phrase) stream)
   (print-unreadable-object (slp stream)
-    (format stream "lp ~a" (if (typep slp 'has-name)
-                             (mname slp)
-                              (name (phrase slp))))
+    (format stream "lp ~a" (phrase-name-for-printing (phrase slp)))
     (print-lp-bound-values slp stream)))
 
 (defmethod print-object ((lp partially-saturated-lexicalized-phrase) stream)
@@ -103,17 +117,24 @@
     (format stream " ~a" (mapcar #'name (free lp)))
     (print-lp-bound-values lp stream)))
 
+(defmethod print-object ((la lexicalized-attachment) stream)
+  (print-unreadable-object (la stream)
+    (format stream "la: ~a ~a"
+            (name (point la)) (pprint-value (value la)))))
+            
 (defgeneric print-lp-bound-values (lp stream)
   (:documentation "Shared subroutine of several object printers")
   (:method ((lp saturated-lexicalized-phrase) stream)
     (loop for pvp in (bound lp)
-       do (format stream " ~a = ~a"
+       do (format stream " ~a=~a"
                   (name (phrase-parameter pvp))
-                  (value pvp)))))
+                  (pprint-value (value pvp))))))
+
 (defmethod print-object ((pvp parameter-value-pair) stream)
   (print-unreadable-object (pvp stream)
     (format stream "pvp: ~a = ~a" 
-            (name (phrase-parameter pvp)) (value pvp))))
+            (name (phrase-parameter pvp))
+            (pprint-value (value pvp)))))
 
 (defmethod print-object ((cn complement-node) stream)
   (print-unreadable-object (cn stream)
@@ -135,9 +156,3 @@
     (let ((param (corresponding-parameter pvp))
           (var (corresponding-variable pvp)))
       (format stream "~a : ~a" param var))))
-
-(defmethod print-object ((clp category-linked-phrase) stream)
-  (print-unreadable-object (clp stream)
-    (let ((category (linked-category clp))
-          (lp (linked-phrase clp)))
-      (format stream "clp: ~a ~a" category lp))))
