e91da1bdace33904c2fbd9ce812ac4617be959f6
David D. McDonald <davidmcdonald@alum.mit.edu>
2017-06-07 16:13:50 -0400

    Commented out loading of unused files. Particularly entities because it definitions are somehow 'sticky' on the mumble side and I didn't want to hold things up while figuring it out

--- Sparser/code/s/grammar/model/sl/blocks-world/loader.lisp
+++ Sparser/code/s/grammar/model/sl/blocks-world/loader.lisp
@@ -3,7 +3,7 @@
 ;;;
 ;;;      File:  "loader"
 ;;;    Module:  grammar/model/sl/blocks
-;;;   version:  March 2017
+;;;   version:  June 2017
 
 ;; Initiated 10/6/15. Added vocabulary 12/3/15. interface on 1/2/16.
 ;; resources 1/17/16. Entities 1/20/16. Staging 2/25/16.
@@ -11,23 +11,31 @@
 (in-package :sparser)
 
 (when (find-package :mumble)
-  (gload "blocks;resources")
-  (gload "blocks;experiments")
+;;  (gload "blocks;resources")  empty -- recycle
+  (gload "blocks;experiments") ;; rather dusty.
+  ;;Mostly same as regression tests
 
   (gload "blocks;subcat-patterns")
   (gload "blocks;vocabulary")
   (gload "blocks;verbs")
 
-  (gload "blocks;entities")
+  ;;  (gload "blocks;entities")
+  ;; Not used anymore (move to spg)
+  ;; Caused not-yet-debugged 'sticky' lexicalized phrases
+  ;; for all blocks -> the UPS block
 
-  (gload "blocks;specialists")
-  (gload "blocks;speech-acts")
+  (gload "blocks;specialists") ;; mine for heuristics
+  (gload "blocks;speech-acts") ;; ditto
 
-  (gload "blocks;test-messages")
+  ;;(gload "blocks;test-messages")
+  ;; Depends on entities. Mine for treatment of context
 
-  (gload "blocks;interface")
+  ;;(gload "blocks;interface")
+  ;; OBE interface to sgp planner
 
-  (gload "blocks;staging")
+  (gload "blocks;staging")  ;; experiment in higher abstraction
+  
   (gload "blocks;blocksworld-sents")
+  ;; list of examples roughly corresponding to a conversation
 
   )
