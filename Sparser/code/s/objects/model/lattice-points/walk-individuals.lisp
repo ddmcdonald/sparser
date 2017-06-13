95aef553beac6949949a548804e43d5b5d89e727
David D. McDonald <davidmcdonald@alum.mit.edu>
2017-06-13 17:09:59 -0400

    checkpoint

--- Sparser/code/s/objects/model/lattice-points/walk-individuals.lisp
+++ Sparser/code/s/objects/model/lattice-points/walk-individuals.lisp
@@ -10,6 +10,52 @@
 
 (in-package :sparser)
 
+(defun vv-of (i)
+  "Walk up the dlvv chain and return all the values"
+  (format t "  ~a" i)
+  (let ((uplink (indiv-uplinks i)))
+    ;; e.g. ((#<dl-vv has-determiner + #<the 106>> . #<protein 74245>))
+    (let* ((wrapper (car uplink))
+           (dlvv (car wrapper))
+           (variable (dlvv-variable dlvv))
+           (value (dlvv-value dlvv))
+           (individual-added-to (cdr wrapper))
+           (list `(,dlvv)))
+      ;;(lsp-break "wrapper = ~a" wrapper)
+      (unless (eq variable :superc)
+        (cons (car (vv-of individual-added-to))
+              wrapper)))))
+  
+#|
+sp> (p/s "the phorphorylated human liver protein")
+[the phorphorylated human liver protein]
+                    source-start
+e10   PROTEIN       1 "the phorphorylated human liver protein" 6
+                    end-of-source
+(#<protein 74246> 
+   (has-determiner (#<the 106> (word "the")))
+   (predication (#<phorphorylate 74244> (object *lambda-var*)))
+   (organism (#<human 1830>)) 
+   (non-cellular-location (#<liver 74240>))
+   (raw-text "protein"))
 
-
+sp> (defvar i (i# 74246))
+i
+sp> (indiv-uplinks i)
+((#<dl-vv has-determiner + #<the 106>> . #<protein 74245>))
+sp> (caar *)
+#<dl-vv has-determiner + #<the 106>>
+sp> (indiv-uplinks (i# 74245))
+((#<dl-vv predication + #<phorphorylate 74244>> . #<protein 74243>))
+sp> (indiv-uplinks (i# 74243))
+((#<dl-vv organism + #<human 1830>> . #<protein 74242>))
+sp> (indiv-uplinks (i# 74242))
+((#<dl-vv non-cellular-location + #<liver 74240>> . #<protein 74241>))
+sp> (indiv-uplinks (i# 74241))
+((#<dl-vv raw-text + protein> . #<protein 2183>))
+sp> (indiv-uplinks (i# 2183))
+((#<dl-vv superc + nil> . #<in-ras2-model 2189>)
+ (#<dl-vv superc + nil> . #<reactome-category 2188>)
+ (#<dl-vv superc + nil> . #<peptide 2184>))
+|#
 
