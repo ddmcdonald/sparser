8928c6733562948b8139d4ac4c9a123ed526c4e5
David D. McDonald <davidmcdonald@alum.mit.edu>
2017-06-27 16:37:55 -0400

    Added new file which loads early, 'roles.lisp', which consists just of mixin categories whose purpose is to make single-copies of the major variables early and with no value restrictions. The restrictions are to be added later in the specific 'regular' categories that use them.

--- Sparser/code/s/grammar/model/core/kinds/1st-loader.lisp
+++ Sparser/code/s/grammar/model/core/kinds/1st-loader.lisp
@@ -1,23 +1,26 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 2014-2016 David D. McDonald  -- all rights reserved
+;;; copyright (c) 2014-2017 David D. McDonald  -- all rights reserved
 ;;;
 ;;;     File:  "1st-loader"
 ;;;   Module:  "model;core:kinds:"
-;;;  version:  August 2016
+;;;  version:  June 2017
 
 ;; Initiated 3/28/14 to allow the upper-model file to be decomposed
-;; into more managable pieces. 4/14/14 moved in files from ISR.
+;; into more managable pieces.
+;; 4/14/14 moved in files from ISR.
 ;; Added time 2/1/16.
 
 (in-package :sparser)
 
 (gload "kinds;upper-model")
 (gload "kinds;tense-aspect")
+(gload "kinds;roles") ;; before substantive categories
 (gload "kinds;adjuncts")
+
 (gload "kinds;processes")
+(gload "kinds;things")
 (gload "kinds;predicate")
 (gload "kinds;attribution")
-(gload "kinds;things")
 (gload "kinds;space")
 (gload "kinds;time")
 (gload "kinds;movement")
---
+++ Sparser/code/s/grammar/model/core/kinds/roles.lisp
@@ -0,0 +1,55 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
+;;; copyright (c) 2017 David D. McDonald  -- all rights reserved
+;;;
+;;;     File:  "predicate"
+;;;   Module:  "model;core:kinds:"
+;;;  version:  June 2017
+
+;; To move all the standard thematic roles up to a place that
+;; loads early so they're available for restriction as lower levels
+;; of the hierarchy. Written as mixins without restrictions.
+
+(in-package :sparser)
+
+(define-mixin-category with-an-agent
+  :specializes relation
+  :binds ((agent (:or pronoun physical-agent))))
+;; Dolce has 'agency' as the union of physical and social agent
+
+(define-mixin-category actor
+  :specializes relation
+  :binds ((actor))
+  :documentation "")
+
+(define-mixin-category takes-adverb
+  :specializes relation
+  :binds ((adverb)))
+
+(define-mixin-category beneficiary
+  :specializes relation
+  :binds ((beneficiary))
+  :documentation "")
+
+(define-mixin-category experiencer
+  :specializes relation
+  :binds ((experiencer))
+  :documentation "")
+
+(define-category has-location
+  :specializes relation
+  :binds ((location location))
+  :documentation "Said of something that must have a location
+    Supplies a location variable whose value says where
+    this thing is.")
+
+(define-mixin-category patient
+  :specializes relation
+  :binds ((patient))
+  :documentation "")
+
+(define-mixin-category theme
+  :specializes relation
+  :binds ((theme))
+  :documentation "Often characterized as the thing that moves.
+    Specialized (restricted) by move.")
+
