a9e479ef33b6d47fd43c3d65105df1309d77bd47
David D. McDonald <davidmcdonald@alum.mit.edu>
2017-07-11 11:49:05 -0400

    Cleaned up. Added most-recent-binding and prior-individual answering questions about individuals by looking at their uplinks

--- Sparser/code/s/objects/model/lattice-points/walk-individuals.lisp
+++ Sparser/code/s/objects/model/lattice-points/walk-individuals.lisp
@@ -3,36 +3,55 @@
 ;;;
 ;;;     File:  "walk-individuals"
 ;;;   Module:  "objects;model:lattice-points:"
-;;;  version:  June 2017
+;;;  version:  July 2017
 
 ;; Initialized 6/12/17. Routines for walking around the description lattice
 ;; to find individuals with various properties.
 
 (in-package :sparser)
 
-(defun uplinks-of (i) ;; Rusty's lister
-  (let ((up (indiv-uplinks i)))
-    (when up
-      (unless (eq (dlvv-variable (caar up)) :superc)
-        (cons (car up)(uplinks-of (cdar up)))))))
-#|
-(defun indiv-vvs (i)
-  (let ((up (indiv-uplinks i)))
-    (unless (eq (dlvv-variable (caar up)) :superc)
-        (cons (car up) (indiv-vvs (cdar up))))))   |#
+#| An uplink is a cons of a v+v and an individual, e.g.
+    ((#<dl-vv has-determiner + #<the 106>> . #<protein 74245>))
+ The uplinks field of an individual is presently a list of just
+ one uplink. The uplink of an individual i is the variable+value
+ of the binding that was added the individual j to creeate i.
+|#
 
-;; e.g. ((#<dl-vv has-determiner + #<the 106>> . #<protein 74245>))
-;; In principle there are multiple uplinks though a the moment
-;; there is just one, whose car is a vv and whose cdr is the
-;; individual we started from to get i by adding that vv.
+;; sugar for the uplink sexp
 (defun uplink-indiv (uplink) (cdr uplink))
 (defun uplink-vv (uplink) (car uplink))
 
+
+(defun uplinks-of (i)
+  (let ((up (indiv-uplinks i)))
+    (when up
+      (unless (eq (dlvv-variable (caar up)) :superc)
+        (cons (car up) (uplinks-of (cdar up)))))))
+
+
+(defun most-recent-binding (i)
+  "Return the variable that was most recent bound on this individual"
+  (let* ((uplink (car (indiv-uplinks i)))
+         (vv (uplink-vv uplink))
+         (var (dlvv-variable vv))
+         (j (uplink-indiv uplink)))
+    (values var j)))
+
+(defun prior-individual (i)
+  "Use the uplink information to return the individual that 
+   was bound to the most recent variable+value to produce this
+   individual (i)"
+  (uplink-indiv (car (indiv-uplinks i))))
+
+
+
+;;/// Still being sorted out -- we'd like a base individual
+;; plus a list of bindings
 (defun unwind-from-right (i)
-  (let* ((uplinks (reverse (indiv-vvs i)))
+  (let* ((uplinks (reverse (uplinks-of i)))
          (j (uplink-indiv (car uplinks)))
          (vv (loop for uplink in (cdr uplinks)
-                collect (uplink-vv uplink))))
+                collect (uplinks-of (uplink-indiv uplink)))))
     (values j vv)))
 
 #|sp> (unwind-from-right i)
