f454fc126668ed87628a024ba4b9c252efded673
David D. McDonald <davidmcdonald@alum.mit.edu>
2017-08-12 17:11:46 -0400

    Documented all the steps/alternatives in introducing edges over terminals. Added function, word-for-scanned-chars, that will determine whether there is a defined word for the actual characters that were scanned.

--- Sparser/code/s/analyzers/psp/assess/terminal-edges.lisp
+++ Sparser/code/s/analyzers/psp/assess/terminal-edges.lisp
@@ -1,9 +1,9 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1992-1995,2011-2016  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992-1995,2011-2017  David D. McDonald  -- all rights reserved
 ;;; 
 ;;;     File:  "terminal edges"
 ;;;   Module:  "analyzers;psp:assess:"
-;;;  Version:  January 2016
+;;;  Version:  August 2017
 
 ;; initiated 9/12 v2.3
 ;; 1.1 (10/23) reorganized what kinds of property edges are created
@@ -43,6 +43,8 @@
 ;;;-------------
 ;;; entry point
 ;;;-------------
+
+;; (*trace-network*)
 
 (defun install-terminal-edges (word position-scanned next-position)
   "Called from introduce-terminal-edges or from do-just-terminal-edges which
@@ -105,17 +107,17 @@
 ;;;-------------------------
 
 (defun preterminals-for-known (rule-set word position-scanned next-position)
-  ;; called from Install-terminal-edges
-  (let ((variants
-         (word-capitalization-variants word)))
-
+  "Called from install-terminal-edges. If the word is known to have variants
+   with different capitalizations, prefer the version (and rule set) that
+   match the capitalization in this particular instance."
+  (let ((variants (word-capitalization-variants word)))
     (if variants
-        (then
-          (tr :install/has-variants-with-other-capitalization word)
-          (known-preterminals/check-caps
-           rule-set word variants position-scanned next-position))
-        (preterminals/word
-         rule-set word position-scanned next-position))))
+      (then
+        (tr :install/has-variants-with-other-capitalization word)
+        (known-preterminals/check-caps
+         rule-set word variants position-scanned next-position))
+      (preterminals/word
+       rule-set word position-scanned next-position))))
 
 
 (defun preterminals-for-unknown (word position-scanned next-position)
@@ -125,17 +127,17 @@
     ;; aren't defined by the grammar are rendered by the tokenizer
     ;; as this constant word, which, by definition, initiates no edges
 
-    (let ((capitalization-counts
-           (word-capitalization-variants word)) ;; field lookup
+    (let ((capitalization-counts (word-capitalization-variants word))
           (actual-state (pos-capitalization position-scanned)))
       ;; it's not really unknown if it's the lowercase standin that was
       ;; created for a word that has rules only when it occurs in some
-      ;; kind of upper or mixed case.
+      ;; kind of upper or mixed case. See do-word-canonical-form for details
 
       (cond
        ((eq :digits actual-state)
-        ;; it's a new number, i.e. one that doesn't have a simple,
-        ;; non-compound name
+        ;; It's a new number, i.e. a number that wasn't defined
+        ;; in the dossier and hasn't already gone through this
+        ;; path --  37 rather than 2. 
         (when (or *make-edges-over-new-digit-sequences*
                   *make-edges-for-unknown-words-from-their-properties*)
           (let ((edge (make-edge-over-unknown-digit-sequence
@@ -145,9 +147,9 @@
             (list edge))))
        
        (capitalization-counts
-        ;; it's the lowercase version of a word that has rules for
-        ;; one or more of its uppercase variants, but not for
-        ;; this lowercase variant.  Accordingly, if this instance of
+        ;; This the lowercase version of a word that has rules for
+        ;; one or more of its uppercase variants, though not for
+        ;; this lowercase variant. Accordingly, if this instance of
         ;; the word is lowercase then we stop looking for rules,
         ;; otherwise we dispatch to the routine that sorts out
         ;; the cases.
@@ -156,7 +158,7 @@
             (consider-morphology-based-edges
              word position-scanned next-position))
           (else
-            (check-caps-variations capitalization-counts
+            (try-caps-variations capitalization-counts
                                    actual-state
                                    position-scanned next-position))))
        
@@ -176,25 +178,24 @@
   ;; The lowercase version of the word has associated rules, but there are
   ;; capitalized variants of the word that may as well, so we check them
   ;; and give them priority.
-  (let ((actual-state
-         (pos-capitalization position-scanned)))
+  (let ((actual-state (pos-capitalization position-scanned)))
     (tr :install/actual-capitalization actual-state position-scanned)
 
     (if (eq :lower-case actual-state)
       ;; then it's the variant we already have the word for (lc-word)
-      (preterminals/word rule-set lc-word position-scanned next-position)
+      (preterminals/word rule-set lc-word
+                         position-scanned next-position)
       (else
         ;; otherwise we look for rules associated with the variants,
-        ;; but go with those of the lowercase version if there aren't 
-        ;; any
-        (unless (check-caps-variations capitalized-variants actual-state
+        ;; but go with those of the lowercase version if there aren't any
+        (unless (try-caps-variations capitalized-variants actual-state
                                        position-scanned next-position)
           (preterminals/word rule-set lc-word
                              position-scanned next-position))))))
 
 
-(defun check-caps-variations (capitalized-variants actual-state
-                              position-scanned next-position)
+(defun try-caps-variations (capitalized-variants actual-state
+                            position-scanned next-position)
 
   ;; Called from Known-preterminals/check-caps.
   ;; The variants parameter is a list of words with specific, non-lowercase
@@ -204,8 +205,7 @@
   ;; it's already been checked for upstream.
   (tr :install/caps-variations-are capitalized-variants)
 
-  (if (null (cdr capitalized-variants))
-    ;; there's only one
+  (if (null (cdr capitalized-variants)) ;; there's only one
     (let ((word (first capitalized-variants)))
       (if (eq actual-state (word-capitalization word))
         (if (word-rules word)
@@ -239,21 +239,20 @@
 
 (defun preterminals/word (rule-set word
                           position-scanned next-position)
-
-  ;; If the word is mentioned as a literal in some non-unary rule
-  ;; then we install an edge for it, and we look to see if there
-  ;; is a single-term rule for the word that rewrites it as another
-  ;; word in which case we install that rule's edge rather than
-  ;; the default edge that isn't motivated by a rule. This lets
-  ;; us handle default form categorizations of words without adding
-  ;; more edges than are really called for.
-  ;;   This is the final target for many of the threads, including
-  ;; check-caps-variations, ///.
-
+  "This is the final target for many of the threads, including
+   check-caps-variations. It where we make and install the edge(s).
+   If the word is mentioned as a literal in some non-unary rule
+   then we install an edge for it. Also we look to see if there
+   is a single-term rule for the word that rewrites it as another
+   word, in which case we install that rule's edge rather than
+   the default edge that isn't motivated by a rule. This lets
+   us handle default form categorizations of words without adding
+   more edges than are really called for."
   (let ( edge-for-literal form-rule single-term-rules single-term-edges )
 
     (when (setq single-term-rules (rs-single-term-rewrites rule-set))
       (tr :word-has-n-single-term-rules word single-term-rules)
+      ;; Sweep the rules for tacit form characterizations
       (dolist (cfr single-term-rules)
         (when (word-p (cfr-category cfr))
           (setq form-rule cfr)
@@ -261,6 +260,7 @@
 
     (when (or (rs-left-looking-ids rule-set)
               (rs-right-looking-ids rule-set))
+      ;; Look for literals. See above.
       (tr :mentioned-in-rule word)
       (setq edge-for-literal
             (if form-rule
@@ -283,7 +283,7 @@
                     cfr word position-scanned next-position)
                    single-term-edges))
             (edge-for-literal
-             ;; we've already installed this one, but
+             ;; We've already installed this one, but
              ;; we can improve it's description
              (if (edge-rule edge-for-literal)
                (push cfr (edge-rule edge-for-literal))
@@ -315,17 +315,13 @@
 (defun consider-morphology-based-edges (word
                                         position-scanned
                                         next-position )
-
-  ;; Called from Preterminals-for-unknown 
-  ;; New digit tokens are handled in that routine itself (since it
-  ;; has to check for them anyway), and new capitalized words are
-  ;; handled by the proper name fsa.  This does all the other cases.
-  ;; Return value is the newly formed edges (if any)
-
+  "Called from Preterminals-for-unknown when known capitalized variants
+   don't apply because the actual capitalization is :lower-case or when
+   there are no capitalized variants (new digits are handled in a different
+   subroutine). Return value is the newly formed edges (if there are any)"
+  
   (when (word-morphology word)
-    ;; don't bother to make edges unless there's some affix
-    ;; to react to.
-
+    ;; Don't bother to make edges unless there's some affix to react to.
     (when *make-edges-for-unknown-words-from-their-suffixes*
       (make-edge-based-on-morphology word
                                      position-scanned
@@ -339,9 +335,26 @@
 
 (defun setup-multiple-initial-edges (position-before
                                      position-after)
-
+  "Called from install-terminal-edges when the edges-installing routines
+   returned more than one edge. This is where the flag :multiple-initial-edges
+   is installed into the edge vector objects."
   (setf (ev-top-node (pos-starts-here position-before))
         :multiple-initial-edges)
   (setf (ev-top-node (pos-ends-here position-after))
         :multiple-initial-edges))
+
+
+;;;------------------------------------------------------
+;;; What word fits the precise presenting capitalization
+;;;------------------------------------------------------
+
+(defgeneric word-for-scanned-chars (pos-before pos-after)
+  (:documentation "Is there a word that corresponds to the specific
+    characters between these two positions. Useful with there are
+    words and rules for several capitalization variants of the
+    same set of characters.")
+  (:method ((pos-before position) (pos-after position))
+    (let* ((raw (extract-characters-between-positions pos-before pos-after))
+           (pname (trim-whitespace raw)))
+      (word-named pname))))
 
