07e9fbc69b7b55d7f48f439acba3e27168a47166
Rusty Bobrow <rjbobrow@gmail.com>
2017-10-01 11:37:03 -0400

    repository for code that has been made obsolete in the simipification of multi-sentence scans and parsing

---
+++ Sparser/code/s/drivers/chart/psp/obsolete-code.lisp
@@ -0,0 +1,87 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
+;;; copyright (c) 2014-2017 David D. McDonald  -- all rights reserved
+;;;
+;;;     File:  "obsolete-code"
+;;;   Module:  "drivers/chart/psp/"
+;;;  version:  August 2017
+
+;; Initiated  1-Oct-2017
+;; Code removed in refactoring of the various parse paths for documents and strings
+(in-package :sparser)
+
+
+
+#+ignore
+(defun scan-sentences-to-eof (first-sentence &aux (sentence first-sentence)start-pos)
+  "Called from initiate-successive-sweeps when we're
+   in the initial sweep phase and need to identify
+   and populate the sentences of the paragraphs.
+   Does scan-next-terminal and detects sentence boundaries 
+   but no substantive processing. Does not return.
+   We leave the loop via a throw to sentences-finished
+   from simple-eos-check from inside scan-words-loop."
+  (declare (special *show-sentence-for-early-errors* *current-sentence*)
+           (optimize (debug 3)(speed 1)))
+  (tr :start-scan-to-eof first-sentence)
+  (setq *current-sentence* first-sentence)
+  (lsp-break "foo")
+  (loop
+    (when (null sentence) (return-from scan-sentences-to-eof nil))
+    (setq start-pos (starts-at-pos sentence))
+    (tr :scan-to-eof-start-pos start-pos)
+    (catch :end-of-sentence ;; Thrown from period-hook
+      (let ((first-word (pos-terminal start-pos)))
+        (unless first-word
+          (scan-next-position)
+          (setq first-word (pos-terminal start-pos)))
+        (scan-words-loop start-pos first-word)
+        (when *show-sentence-for-early-errors*
+          (format t "  in sentence: ~s ~%"
+                  (sentence-string sentence))
+          (setq *show-sentence-for-early-errors* nil))))
+    (setq sentence (next sentence))))
+#+ignore
+(defun scan-words-loop (position-before word)
+  "This routine is called by ... which itself
+   is called by initiate-successive-sweeps when we are reading
+   a document and need to populate (create) all the sentences.
+   Starting at the beginning of a sentence, add words into the
+   chart until a sentence-ending period (or question-mark) is encountered.
+   It's a recursive loop. We get out of it when the period-hook
+   runs and throws to :end-of-sentence."
+  ;; position-before - word - position-after
+  ;;/// rewrite this as a loop. Maybe incorporate the
+  ;; by-hand invocation of period-hook
+  ;; (tr :scan-words-loop) <-- needs to be loop before not loud
+  (declare (optimize (speed 1)(debug 3)))
+  (simple-eos-check position-before word)  
+  (let ((position-after (chart-position-after position-before)))
+    (unless (includes-state position-after :scanned)
+      (scan-next-position);; unknown words are noticed here
+      ;; this can actually set up the pos-terminal of position-before
+      (unless word (setq word (pos-terminal position-before))))
+    (simple-eos-check position-before word)
+
+    (when *trace-scan-words-loop*
+      (format t " ~s" (word-pname word)))
+    
+    (let* ((where-pw-ended (polyword-check position-before word))
+           (position-after (or where-pw-ended
+                               (chart-position-after position-before))))
+      (when where-pw-ended
+         (tr :scanned-pw-ended-at word where-pw-ended)
+         (setq position-before where-pw-ended
+               position-after (chart-position-after position-before))
+         (unless (includes-state where-pw-ended :scanned)
+           ;; PW can complete without thinking about the
+           ;; word that follows it.
+           (scan-next-position))
+         (setq word (pos-terminal where-pw-ended)))
+      
+      ;; Trigger the period-hook (n.b. also gets conjunctions,
+      ;; parentheses, etc.)
+      
+      (complete-word/hugin word position-before position-after)
+
+      (let ((next-word (pos-terminal position-after)))
+        (scan-words-loop position-after next-word)))))
