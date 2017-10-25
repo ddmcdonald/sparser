d5eac3e44b735655981cb1829ae34bd3cf65e2a3
David D. McDonald <davidmcdonald@alum.mit.edu>
2017-10-25 10:21:06 -0400

    Better handling (e.g. not losing track of) wh-phrase components that are aren't proper attributes, e.g "gene" vs "color".

--- Sparser/code/s/grammar/rules/syntax/questions.lisp
+++ Sparser/code/s/grammar/rules/syntax/questions.lisp
@@ -4,7 +4,7 @@
 ;;; 
 ;;;     File:  "questions"
 ;;;   Module:  "grammar;rules:syntax:"
-;;;  Version:  March 2017
+;;;  Version:  October 2017
 
 ;; Broken out from /grammar/model/sl/checkpoint/rules 6/17/09
 ;; Elaborated through 7/23/09. 9/28/11 removed spatial-orientation
@@ -16,8 +16,13 @@
 ;;; question categories
 ;;;---------------------
 
+(defparameter *debug-questions* nil
+  "Should only be set when debugging. Signals an error so we can
+   see what's going on. Usually paired with a warn.")
+
 (defparameter *show-wh-problems* nil
-  "complain about problems in creating WH questions -- there are strange things in articles...")
+  "Break about problems in creating WH question and other types.
+   Usually in an if statement with a warn as the alternative.")
 
 
 (define-category question
@@ -45,24 +50,32 @@
 (define-category wh-question
   :specializes question
   :instantiates :self
-  :binds ((wh :primitive category) ;; #<ref-category HOW>
+  :binds ((wh :primitive category) ;; e.g. #<ref-category HOW>
           (var :primitive lambda-variable) ;; height, color
           ;;(focus ;; Trips variable for 'how <much> ..."
           #| the rest is the statement |# )
   ;;  :index (:temporary :sequential-keys wh attribute statement)
-  :documentation "Draft that should be able to make enough of
- the content of the WH question explicit that we have the basis
- for computing an answer to it.")
+  :documentation "The primary aim of the var(iable) is to provide
+ the information to answer the question: where would we find it
+ in an individual. For a simple wh-question ('where is the ball?')
+ that's assumed to be the variable associated with the WH.")
 
 (define-category wh-question/attribute
   :specializes wh-question
   :instantiates :self
-  :binds ((attribute attribute-value)) ;; #<tall>, w/ var = height
-  :documentation "")
+  :binds ((attribute attribute-value) ;; #<tall>, w/ var = height
+          (other)) ;; "which pathways utilize SRF?"
+  :documentation "We are asking about a particular, possibly arbitrary
+ thematic argument of the statement. If it is a known type of
+ attribute we take advantage of that to refine the choice of
+ variable. For arbitrary, 'other', cases we have to dig around
+ after we've finished the full parse.")
 
 ;;--- constructors
 
-(defun make-wh-object (wh &key variable statement attribute)
+(defun make-wh-object (wh &key variable statement attribute other)
+  (declare (optimize debug))
+  (push-debug `(,variable ,statement ,attribute ,other))
   (let* ((attribute-var
           (when attribute (variable-for-attribute attribute)))
          (var-to-use (or variable
@@ -77,6 +90,8 @@
         (setq q (add-category-to-individual q (itype-of statement))))
       (when attribute
         (setq q (bind-variable 'attribute attribute q)))
+      (when other
+        (setq q (bind-variable 'other other q)))
       q)))
 
 (defun extend-wh-object (q &key variable statement attribute)
@@ -104,33 +119,42 @@
 
 (defun make-this-a-question-if-appropriate (sentence)
   "Called from post-analysis-operations after all parsing and
-   contextual interpretation is finished."
-  (declare (special category::question))
+   contextual interpretation is finished. This is a generic
+   check in that we don't already know that this sentence is
+   supposed to be a question. We establish that within this call.
+     The early pass by detect-early-information will have
+   sorted its results (if any) on the sentence data structure
+   which we access here via preposed-aux? and/or initial-wh?.
+     If one of these holds we try to formulate a question
+   from the sentence, which can be complicated depending on
+   how well we've parsed it.
+     Runs for side-effects."
+  (declare (special category::question *show-wh-problems*))
   (when (or (preposed-aux?)
             (initial-wh?))
-    ;; span-covered-by-one-edge? -- won't work until we do
-    ;; something with the deliberately unparse-able initial
-    ;; aux (at least not syntactically parse-able).
-    (let* ((preposed? (preposed-aux?)) ;; make them local flags
+    (let* ((preposed? (preposed-aux?)) ;; make them into local flags
            (wh-initial? (initial-wh?))
            (start-pos (starts-at-pos sentence))
            (end-pos (ends-at-pos sentence))
            (edge (span-covered-by-one-edge?
-                  (if preposed? 
-                    (chart-position-after start-pos) ;; hack, kinda
+                  (if preposed?
+                    ;; just past the preposed aux -- not reliable with WH
+                    (chart-position-after start-pos)
                     start-pos)
                   end-pos))
            (edges (all-tts start-pos end-pos)))
-      ;; -- DAVID
-      ;; (p "Is the ibogaine congener 18-Methoxycoronaridine safer than ibogaine?.")
-      ;; was throwing an error because (third edges) was a word  #<word QUESTION-MARK>
-      ;; I think this should be a question, but I don't have time to make this work
-      ;; I am just testing that (third edges) is an edge 
+      ;; In most cases, the proposed aux will have been accommodated by
+      ;; the operations in the post-vg-hook, though that's just for explicit
+      ;; auxiliaries.
 
+      ;; Look for heuristic ways we could get a full sentence
+      ;; from a partial parse. The detection is in this cond.
+      ;; The construction is mostly in the subroutines just below.
       (cond
         ((edge-p edge)
          (when preposed?
            ;; The wh-initial? case doesn't need further handling
+           ;; when the sentence parsed completely.
            (let ((q (make-polar-question (edge-referent edge))))
              (let ((spanning-edge
                     (make-edge-over-long-span
@@ -142,14 +166,15 @@
                spanning-edge))))     
         
         ((and (= 3 (length edges))
-              (edge-p (car edges))
-             	;;(edge-p (third edges))
-              (itypep (edge-referent (car edges)) 'be))              
+              (edge-p (first edges))
+              (itypep (edge-referent (first edges)) 'be))
+         
          (cond ((member (cat-name (edge-form (third edges)))
                         '(adjp adjective comparative superlative
                           comparative-adjp superlative-adjp))
                 ;; <is> <something> <adj>
                 (make-polar-adjective-question start-pos end-pos edges))
+               
                ((member (cat-name (edge-form (third edges)))
                         '(pp))
                 ;; stray time phrase?
@@ -159,37 +184,49 @@
                           vp+passive))
                 ;; <is> <something> <x-ing?
                 (make-polar-participle-question start-pos end-pos edges))
-               ((eq (cat-name (edge-category (second edges)))
-                    ;; "is there <something: a cat on the rug>?"
-                    'syntactic-there)
-                (make-polar-there-question start-pos end-pos edges))
+               
+               ((or (eq (cat-name (edge-category (second edges))) 'syntactic-there)
+                    (eq (cat-name (edge-category (second edges))) 'deictic-location))
+                    ;; "is there <something, e.g. a cat on the rug>?"
+                    (make-polar-there-question start-pos end-pos edges))
                (t
                 ;; don't break here -- just warn
                 ;;  have gotten some cases in articles
-                (warn "unhandled 3 edge question: ~a" edges))))
+                (if *show-wh-problems*
+                  (lsp-break "unhandled 3 edge question: ~a" edges)
+                  (warn "unhandled 3 edge question: ~a" edges)))))
 
-        (wh-initial?
+        (wh-initial? ;; use assimilate-subject (subj vp) to refine the variable
          (cond
            ((= 2 (length edges)) ;; take the second as the statement
             (wh-initial-two-edges wh-initial? edges start-pos end-pos))
            
-           ((and (and (= 3 (length edges)) (edge-p (second edges))) ;; "How many blocks did you add to the row?"
+           ((and (and (= 3 (length edges))
+                      (edge-p (second edges))) ;; "How many blocks did you add to the row?"
                  (edge-over-aux? (second edges)))
             (wh-initial-three-edges wh-initial? edges start-pos end-pos))
            
            (t
-            (when *warn-when-can-not-formulate-question*
-              (warn "Could not resolve edges into a WH question: ~a" edges)))))
+            (if *show-wh-problems*
+              (lsp-break "Could not resolve edges into a WH question: ~a" edges)
+              (when *warn-when-can-not-formulate-question*
+                (warn "Could not resolve edges into a WH question: ~a" edges))))))
 
         (preposed?
-         (when *warn-when-can-not-formulate-question*
-           (warn "Could not resolve edges into a polar question: ~a" edges)))
+         (if *show-wh-problems*
+           (lsp-break "Could not resolve edges into a polar question: ~a" edges)
+           (when *warn-when-can-not-formulate-question*
+             (warn "Could not resolve edges into a polar question: ~a" edges))))
 
         (t
-         (when *warn-when-can-not-formulate-question*
-           (warn "Could not resolve edges into a question: ~a~
-                ~%   preposed-aux = ~a  initial-wh = ~a"
-                 edges preposed? wh-initial?)))))))
+         (if *show-wh-problems*
+           (lsp-break "Could not resolve edges into a question: ~a~
+                     ~%   preposed-aux = ~a  initial-wh = ~a"
+                      edges preposed? wh-initial?)
+           (when *warn-when-can-not-formulate-question*
+             (warn "Could not resolve edges into a question: ~a~
+                  ~%   preposed-aux = ~a  initial-wh = ~a"
+                   edges preposed? wh-initial?))))))))
 
 
 ;;;--------------------------
@@ -270,8 +307,6 @@
 ;;;--------------
 ;;; WH questions
 ;;;--------------
-
-
 #| (p "What color is the block?")
    (p/s "Is the block on the table?")
    (p "Could we put on one more?")
@@ -300,10 +335,14 @@ e15   BE            3 "Are Scc1 and SA2 Phosphorylation In Vivo" 12
   lung epithelium.")
 Error: New type of wh-obj passed in: #<ref-category INFANT>
 
+(p "Is the ibogaine congener 18-Methoxycoronaridine safer than ibogaine?.")
+;; was throwing an error because (third edges) was a word  #<word QUESTION-MARK>
+;; no replicable 10/17
+
 (p "Until now, it has been unclear how RAS could affect ASPP2 to enhance 
 p53 function") ;; dry-run $40
 From :id "PMC1702556" :corpus :jun15eval
-   This wh-np has no aux per se and blew out the looop
+   This wh-np has no aux per se and blew out the loop
 (p "Whether all EGFR ectodomain mutants share a common mechanism of 
 oncogenic receptor conversion warrants further study.")
 9th June article
@@ -320,15 +359,15 @@ the one connecting Ras to Rac, a member of the Rho subfamily of small GTPases."
 
 (defun delimit-and-label-initial-wh-term (pos-before wh-edge)
   "WH questions always also include inverting subject
-   and auxiliary, and the auxiliary will occur right after the
+   and auxiliary, where the auxiliary will occur right after the
    possibly lengthy WH phrase has finished. E.g. 'How many
-   people will be going to the party?'. So starting with the 
+   people will be going to the party?'. Starting with the 
    WH word, scan forward until you reach an aux. How to best
    divide up the region between the two largly depends on the
    identity of the WH and what its asking for.
       When detect-early-information calls this we collect
    the WH information and populate the object while we're
-   walking along the sentence prefix. The we record the fact
+   walking along the sentence prefix. Then we record the fact
    that we have the wh-edge in hand on the sentence context."
   
   (let* ((wh-type (edge-referent wh-edge)) ;; the category 
@@ -338,8 +377,8 @@ the one connecting Ras to Rac, a member of the Rho subfamily of small GTPases."
          aux-edge  attr-edge  value-edge  other-edges)
 
     (flet ((cover-wh (q end-pos)
-             ;; Make a phrase over the whole span of WH edges
-             ;; up to but not including the aux
+             "Make a phrase over the whole span of WH edges
+              up to but not including the aux"
              (let ((edge
                     (make-edge-over-long-span
                      pos-before end-pos
@@ -377,27 +416,32 @@ the one connecting Ras to Rac, a member of the Rho subfamily of small GTPases."
                  next-word (pos-terminal next-pos)
                  next-edge (highest-edge (pos-starts-here next-pos))))
 
-          ;;(push-debug `(,aux-edge ,attr-edge ,other-edges))
-          ;; (lsp-break "wh-type = ~a" wh-type)
-          (unless aux-edge
-            (when *show-wh-problems*
-              (warn "No aux-edge with ~a" wh-type)))
+          ;;(lsp-break "wh-type = ~a" wh-type)
+         (unless aux-edge
+           (if *debug-questions*
+             (error "No aux-edge with ~a" wh-type)
+             (when *show-wh-problems*
+               (warn "No aux-edge with ~a" wh-type))))
           
-          (let ((attr
-                 (cond
-                   (attr-edge ;; "what color is ..."
-                    (edge-referent attr-edge))
-                   (value-edge ;; "how big is the block?"
-                    (let ((value-class (itype-of (edge-referent value-edge)))) ;; size-value
-                      (when value-class (value-of 'attribute value-class))))
-                   (other-edges ;; "How important is ..."
-                    (if (null (cdr other-edges))
-                      (edge-referent (car other-edges))
-                      (when *show-wh-problems*
-                        (warn "Multiple 'other edges' in ~s: ~a" (current-string) other-edges)))))))
-            (let ((q (if attr
-                       (make-wh-object wh-type :attribute attr)
-                       (make-wh-object wh-type))))
+         (multiple-value-bind (attr other?)
+             (cond
+               (attr-edge ;; "what color is ..."
+                (edge-referent attr-edge))
+               (value-edge ;; "how big is the block?"
+                (let ((value-class (itype-of (edge-referent value-edge)))) ;; size-value
+                  (when value-class (value-of 'attribute value-class))))
+               (other-edges ;; "How important is ..."
+                (if (null (cdr other-edges))
+                  (values (edge-referent (car other-edges)) t)
+                  (when *debug-questions*
+                    (error "Multiple 'other edges' in ~s: ~a" (current-string) other-edges)))))
+
+           ;;(lsp-break "attr ?")
+           (let ((q (if attr
+                      (if other?
+                        (make-wh-object wh-type :other attr)
+                        (make-wh-object wh-type :attribute attr))
+                      (make-wh-object wh-type))))
               (cover-wh q next-pos))))))))
 
 (defun wh-is-declarative-heuristics (next-edge)
@@ -433,8 +477,12 @@ the one connecting Ras to Rac, a member of the Rho subfamily of small GTPases."
                           :referent i)))
                (tr :wh-apply-question-marker edge)
                edge)))
+           ;;((itypep vg 'do)
+           ;; (p/s "What proteins does PLX-4720 target?")
            (t
-            (warn "Don't know how to formulate a wh question whose vg is ~a" vg)))))
+            (if *debug-questions*
+              (lsp-break "Don't know how to formulate a wh question whose vg is ~a" vg)
+              (warn "Don't know how to formulate a wh question whose vg is ~a" vg))))))
              
 
 
