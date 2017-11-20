4ef88ba48ea34d58d7f67fbe0582256c2ab1a47e
David D. McDonald <davidmcdonald@alum.mit.edu>
2017-11-20 15:10:03 -0500

    Gave pname a case for getting a list argument

--- Sparser/code/s/objects/chart/generics/pname.lisp
+++ Sparser/code/s/objects/chart/generics/pname.lisp
@@ -1,9 +1,9 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1992-1994,2013  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992-1994,2013-2017  David D. McDonald  -- all rights reserved
 ;;;
 ;;;      File:   "pname"
 ;;;    Module:   "objects;chart;generics;"
-;;;   Version:   August 2016
+;;;   Version:   November 2017
 
 ;; (9/3/92 v2.3) added referential-category
 ;; (1/6/94) fixed accessor for polywords.  (7/11) added mixin-category
@@ -19,6 +19,7 @@
   (:method ((s symbol)) (symbol-name s))
   (:method ((w word)) (word-pname w))
   (:method ((pw polyword)) (pw-pname pw))
+  (:method ((list cons)) (loop for l in list collect (pname l)))
   #+mumble
   (:method ((w mumble::word)) (mumble::pname w)))
 
