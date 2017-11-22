f24f1e90bac23048f279697325ee81cbbd132ea4
David D. McDonald <davidmcdonald@alum.mit.edu>
2017-11-21 22:24:35 -0500

    First working version. Needs abstraction so can get same effects without having to be tightly tied to Sparser and can systematically get shorter answers rather than the whole question redone as a statement.

--- Sparser/code/s/interface/mumble/answers.lisp
+++ Sparser/code/s/interface/mumble/answers.lisp
@@ -8,13 +8,31 @@
 ;; initiated 11/17/17 for test-jig to frame answers to questions
 ;; using the form of the question.
 
-(in-package :sparser) ;; mumble instead?
+(in-package :mumble) ;; mumble instead?
 
 #|
 (answer-polar-question
  :yes "are there drugs that treat pancreatic cancer?")
 |#
 
+(defun answer-polar-question (y/n text)
+  ;; Get raw material
+  (multiple-value-bind (i dtn-for-i)
+      (sp::process-question text)
+    (let ((yes/no ;; get the appropriate individual
+           (ecase y/n
+             (:yes (sp::find-individual 'acknowledgement :name "yes"))
+             (:no (sp::find-individual 'acknowledgement :name "no")))))
+      (let ((dtn (make-dtn :referent i
+                           :resource (phrase-named 'response-s))))
+        (make-complement-node 'r yes/no dtn)
+        (make-complement-node 's dtn-for-i dtn)
+        (let ((bigger-dtn (discourse-unit dtn)))
+          (say bigger-dtn))))))
+
+        
+(in-package :sparser)
+        
 (defun process-question (text-string)
   ;; 1. Parse the text. It should return one individual
   ;; For general use this will need an error catch
@@ -23,7 +41,8 @@
   (let ((*return-a-value* :referent-of-last-edge)
         (*what-value-to-return* :object))
     (declare (special *return-a-value* *what-value-to-return*))
-    (let ((q (analyze-text-from-string text-string)))
+    (let ((q (with-total-quiet
+               (analyze-text-from-string text-string))))
       (unless (itypep q 'polar-question)
         (error "Presently just doing polar questions.~%~s isn't"
                text-string))
@@ -35,4 +54,4 @@
           (m::say statement))
         (let ((mentioned-in-question (copy-list m::*mentions*))
               (statement-dtn (m::get-dtn statement)))
-          statement-dtn)))))
+          (values statement statement-dtn))))))
