3bdd7d439a514f5cc8150513e56e3e5ab38187d6
David D. McDonald <davidmcdonald@alum.mit.edu>
2018-04-08 15:40:22 -0400

    The rule-set object of a label (category, word, pw) that is used in a binary rule records an index (integer) for use in the multiplication lookup. Now it goes further and makes a record, on the rule-set object of the actual rules involved. Was useful in tracking down what rule used "that" as a literal.

--- Sparser/code/s/objects/rules/cfr/knit-in.lisp
+++ Sparser/code/s/objects/rules/cfr/knit-in.lisp
@@ -1,12 +1,10 @@
-
-
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
 ;;; copyright (c) 1990,1991  Content Technologies Inc.
-;;; copyright (c) 1992-2000  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992-2000,2018  David D. McDonald  -- all rights reserved
 ;;;
 ;;;      File:   "knit in"
 ;;;    Module:   "objects;rules:cfr:"   ;; "context free rules"
-;;;   Version:   3.2 May 1993
+;;;   Version:   April 2018
 
 ;; 1.1 (4/19/91 v1.8.2)  Revised Knit-in-binary to appreciate the case
 ;;      of the two terms of the rule being the same thing
@@ -20,6 +18,9 @@
 
 
 (defun knit-into-psg-tables (cfr)
+  "Called by construct-cfr and the deprecated dotted rule route.
+   Sets up the rule-sets of the labels in the cfr to provide the
+   information used to determine how to apply rules."
   (case (length (cfr-rhs cfr))
     (1 (knit-in-single-term-rewrite-rule cfr))
     (2 (knit-in-binary-rule cfr))
@@ -68,6 +69,9 @@
           (id2 (lookup/make-left-looking-label-id  rs2)))
       (establish-multiplier id1 id2 cfr)
 
+      (record-rule-by-id term1 id1 rs1 cfr :right-looking)
+      (record-rule-by-id term2 id2 rs2 cfr :left-looking)
+
       cfr )))
 
 
@@ -76,7 +80,6 @@
 ;;;---------------------------------
 
 (defun knit-form-rule-into-psg-tables (cfr)
-
   ;; this part is the same for regular rules
   (let* ((label1 (first  (cfr-rhs cfr)))
          (label2 (second (cfr-rhs cfr)))
@@ -93,4 +96,61 @@
           (id2 (lookup/make-left-looking-form-id  rs2)))
       (establish-multiplier id1 id2 cfr)
 
+      ;;//// record rules against form ids -- sort out how to
+      ;; set that up with minimal redundancy.
+      
       cfr )))
+
+
+;;;---------------------------------
+;;; Associating rules to id numbers
+;;;---------------------------------
+
+(defun record-rule-by-id (label id rule-set cfr direction)
+  "The left/right ids on rule sets are opaque as to what rule they
+ go with. Add a record of the id and corresponding cfrs on the
+ plist of the rule set."
+  (let ((entry (ecase direction
+                 (:right-looking (get-tag :rules-for-right-looking-id rule-set))
+                 (:left-looking (get-tag :rules-for-left-looking-id rule-set)))))
+    (ecase direction
+      (:right-looking
+       (if entry
+         (push cfr (cdr entry))
+         (setf (get-tag :rules-for-right-looking-id rule-set)
+               `(,id ,cfr))))
+      (:left-looking
+       (if entry
+         (push cfr (cdr entry))
+         (setf (get-tag :rules-for-left-looking-id rule-set)
+               `(,id ,cfr)))))))
+
+
+(defgeneric rules-for-label (label)
+  (:documentation "When the righthand side of a binary rule is knit into
+ the psg-tables, each of the two rhs labels gets a number in the appropriate
+ field of its rule set. We often look at rule sets to see what's the
+ grammatical properties of the label are, but until now we only had numbers
+ telling us that the label was involved in at least one binary rule 
+ but we didn't know what the rules were. N.b. the rules could be 
+ semantic rules, form rules, context-sensitive rules, or syntactic rules.
+ They're all binary, so they all go through the knitting functions.")
+  (:method ((pname string))
+    (let ((word/pw (resolve pname)))
+      (unless word/pw (error "~s does not resolve for word (polyword)" pname))
+      (rules-for-label word/pw)))
+  (:method ((cat-name symbol))
+    (rules-for-label (category-named cat-name t)))
+  (:method ((w word))
+    (rules-for-label (rule-set-for w)))
+  (:method ((pw polyword))
+    (rules-for-label (rule-set-for pw)))
+  (:method ((c category))
+    (rules-for-label (rule-set-for c)))
+  (:method ((rs rule-set))
+    (let ((llr (get-tag :rules-for-left-looking-id rs))
+          (rlr (get-tag :rules-for-right-looking-id rs)))
+      (when (or llr rlr)
+        (values (cons :looking-left llr)
+                (cons :looking-right rlr))))))
+
