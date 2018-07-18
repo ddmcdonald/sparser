a2a4783a1f56ef07339b017cab3b7801c9d17c76
lbobrow <lbobrow@sift.net>
2018-07-18 15:36:25 -0400

    a bit of clean up on some duplicate mirnas

--- Sparser/code/s/grammar/model/sl/biology/new-defs/new-rna.lisp
+++ Sparser/code/s/grammar/model/sl/biology/new-defs/new-rna.lisp
@@ -26,11 +26,13 @@
 (def-indiv-with-id rna "snRNA" "NCIT:C815" :name "small nuclear RNA") 
 (def-indiv-with-id rna "PCA3" "NCIT:C92559" :name "prostate cancer antigen 3") 
 (def-indiv-with-id micro-rna "miRs181" "NCIT:C116021" :name "microRNA 181") 
-(def-indiv-with-id micro-rna "MIR155" "MIPF0000157" :name "microRNA 155" :synonyms ("MiR-155"))
 (def-indiv-with-id micro-rna "miR-7" "MI0000263" :name "microRNA 7" :synonyms ("MiR-7" "miR7"))
 (def-indiv-with-id micro-rna "miR-10" "MIPF0000033" :name "microRNA 10" :synonyms ("miR10"))
 (def-indiv-with-id micro-rna "miR-1" "MIPF0000038" :name "microRNA 1" :synonyms ("miR1"))
 (def-indiv-with-id micro-rna "miR-101" "MI0000103" :name "microRNA 101" :synonyms ("miR101"))
+;; gene
+(def-indiv-with-id micro-rna "MIR155" "MIPF0000157" :name "microRNA 155" :synonyms ("MiR-155"))
+;; mirna
 (def-indiv-with-id micro-rna "miR-155" "MI0000681" :name "microRNA 155" :synonyms ("miR155"))
 (def-indiv-with-id micro-rna "miR-145" "MI0000461" :name "microRNA 145" :synonyms ("miR145"))
 (def-indiv-with-id micro-rna "miR-21" "MI0000077" :name "microRNA 21" :synonyms ("miR21"))
@@ -41,9 +43,7 @@
 (def-indiv-with-id micro-rna "miR-148a-3p" "MIMAT0000243" :name "microRNA 148a-3p" :synonyms ("miR148a-3p"))
 (def-indiv-with-id micro-rna "miR-335" "MI0000816" :name "microRNA 335" :synonyms ("miR335"))
 (def-indiv-with-id micro-rna "miR-335-5p" "MIMAT0000765" :name "microRNA 335-5p" :synonyms ("miR335-5p"))
-(def-indiv-with-id micro-rna "miR-155" "MI0000681" :name "microRNA 155" :synonyms ("miR155"))
 (def-indiv-with-id micro-rna "miR-155-5p" "MIMAT0000646" :name "microRNA 155-5p" :synonyms ("miR155-5p"))
-(def-indiv-with-id micro-rna "miR-145" "MI0000461" :name "microRNA 145" :synonyms ("miR145"))
 (def-indiv-with-id micro-rna "miR-145-5p" "MIMAT0000437" :name "microRNA 145-5p" :synonyms ("miR145-5p"))
 
 
