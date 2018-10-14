0b28062410a3eb0821bb0fe588bfe0092cb10226
David D. McDonald <davidmcdonald@alum.mit.edu>
2018-10-14 15:14:15 -0400

    Formatting article

--- Sparser/code/s/grammar/model/sl/hurricanes/workspace.lisp
+++ Sparser/code/s/grammar/model/sl/hurricanes/workspace.lisp
@@ -19,11 +19,15 @@ Published: June 7, 2011
 Updated: June 9, 2011 at 8:54 PM ET
 
  
-MIAMI (AP) -- Forecasters say Hurricane Adrian has strengthened to a powerful Category 4 storm off the Pacific coast of Mexico but is still expected to stay offshore.
+MIAMI (AP) -- Forecasters say Hurricane Adrian has strengthened to a
+powerful Category 4 storm off the Pacific coast of Mexico but is still
+expected to stay offshore.
 
-Forecasters say maximum sustained winds for the first hurricane of the 2011 season increased Thursday evening to about 135 mph (217 kph).
+Forecasters say maximum sustained winds for the first hurricane of the
+2011 season increased Thursday evening to about 135 mph (217 kph).
 
-The U.S. National Hurricane Center in Miami predicts that the storm's center will stay well offshore.
+The U.S. National Hurricane Center in Miami predicts that the storm's
+center will stay well offshore.
 
-The center of the storm was about 320 miles (515 kilometers) south of Manzanillo, Mexico. It is moving west-northwest at 9 mph (14 kph).
-|#
+The center of the storm was about 320 miles (515 kilometers) south of
+Manzanillo, Mexico. It is moving west-northwest at 9 mph (14 kph).  |#
