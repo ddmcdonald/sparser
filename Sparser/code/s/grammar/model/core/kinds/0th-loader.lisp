9dba9e559d08b4570e32674e09cdddad37e545d9
David D. McDonald <davidmcdonald@alum.mit.edu>
2018-10-14 15:13:58 -0400

    No content in adjuncts file anymore

--- Sparser/code/s/grammar/model/core/kinds/0th-loader.lisp
+++ Sparser/code/s/grammar/model/core/kinds/0th-loader.lisp
@@ -1,9 +1,9 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 2014-2017 David D. McDonald  -- all rights reserved
+;;; copyright (c) 2014-2018 David D. McDonald  -- all rights reserved
 ;;;
-;;;     File:  "1st-loader"
+;;;     File:  "0th-loader"
 ;;;   Module:  "model;core:kinds:"
-;;;  version:  June 2017
+;;;  version:  October 2018
 
 ;; Initiated 3/28/14 to allow the upper-model file to be decomposed
 ;; into more managable pieces.
@@ -15,4 +15,4 @@
 (gload "kinds;upper-model")
 (gload "kinds;tense-aspect")
 (gload "kinds;roles") ;; before substantive categories
-(gload "kinds;adjuncts")
+
