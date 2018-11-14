02e07361a63caaee091a4c138cc8fda0e591bb71
David D. McDonald <davidmcdonald@alum.mit.edu>
2018-11-14 13:09:25 -0500

    Added a few more

--- Sparser/code/s/grammar/model/sl/music/verbs.lisp
+++ Sparser/code/s/grammar/model/sl/music/verbs.lisp
@@ -18,6 +18,41 @@
   :restrict ((theme (:or endurant sequence)))
   :realization (:verb "delete"))
 
+;; "insert a F4 whole note on beat 1 of measure 4"
+;;
+(define-category insert ;; something somewhere -- see "put"
+  :specializes process
+  :mixins (move-something-verb)
+  :realization (:verb "insert")
+  :documentation "The move mixin provides for a physical or
+ social agent as the subject. Something physical as the direct
+ object (theme), and location variable for where it moves.")
+
+;; "invert all the notes around G4"
+(define-category invert
+  :specializes process
+  :mixins (simple-action)
+  :realization (:verb "invert"))
+
+;; "Move all the notes in measure 2 down an octave
+;;    -- move their pitch, not their location.
+;;  Should fail the 'can-change-location' constraint on the theme
+;;  or the pitch interval will turn up odd for a location
+
+;; "put a C in the rest"
+;; "put a C where the rest is"
+;; "put a C on top of the E"
+
+;; "reverse the first and last notes"
+;; "reverse everything between the C in measure 1 and the E in measure 2"
+;;   feels like a single post-nominal argument, but it has a requirement
+;;   that there are two elements that are going to exchange places
+;;
+(define-category reverse
+  :specializes process
+  :mixins (simple-action)
+  :realization (:verb "reverse"))
+
 ;; "transpose the C up 1 half step"
 ;;
 (define-category transpose
@@ -26,3 +61,5 @@
   :binds ((amount step))
   :realization (:verb "transpose"
                 :up amount))
+
+;; "work on measures 1 and 2"
