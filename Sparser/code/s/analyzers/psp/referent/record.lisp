80c781779f2f20d6f06b194acd07696da8a7cf93
David D. McDonald <davidmcdonald@alum.mit.edu>
2018-12-31 17:24:35 -0500

    Pretty thorough in-line documentation of how to use it

--- Sparser/code/s/analyzers/psp/referent/record.lisp
+++ Sparser/code/s/analyzers/psp/referent/record.lisp
@@ -8,11 +8,34 @@
 ;; Initiated 12/28/18 to tabulate rule and semantic function usage
 ;; and correlation with test passages in texts.
 
-#| The rule counting is pretty lightweight happens automatically
+#| This codes keeps a tally of what rules are used, as well as which
+syntactic functions are used by those rules (principally syntactic
+rules) in order to interpret them.
+
+The needed structure and cross-records are set up as part of post-processing.
+The rule counting itself is pretty lightweight and happens automatically,
+i.e. there's no governing switch. 
+
+The recording accumulates as you run over more texts. If you want a clean
+start call reinitialize-rule-recording, which will reset the pre-run 
+accumulators. 
+
+Presently report-syntactic-function-usage is the only report function that's
+been written, but the possibilities are endless. In particular I'd like to
+correlate rules and more so syntactic functions to examples of the texts that
+use them in order to have tailored regression tests.
 
 |#
 
 (in-package :sparser)
+
+;;--- clear
+
+(defun reinitialize-rule-recording ()
+  "Reset all records of previous runs"
+  (clear-rule-records)
+  (clear-syntactic-function-usage-data))
+
 
 ;;--- Accumulators
 
@@ -22,10 +45,29 @@
 (defvar *rules-ever-fired* nil
   "Simple list of all the rules ever fired.")
 
-(defun clear-rule-records ()   ; (clear-rule-records)
+(defun clear-rule-records ()
   "To initialize accumulators between runs that we want data on."
   (clrhash *rule-firing-counts*)
   (setq *rules-ever-fired* nil))
+
+
+;;--- Driver
+
+(defun record-rule (rule)
+  "Called from referent-from-rule. Always on.
+   Adds to the incidence count of the rule.
+   If the rule involves a semantic function it is listed and
+   counted as well (if the semantic function structures have been
+   initialized.)"
+  (pushnew rule *rules-ever-fired*)
+  (unless (gethash rule *rule-firing-counts*)
+    (setf (gethash rule *rule-firing-counts*) 0))
+  (incf (gethash rule *rule-firing-counts*))
+  (let ((fn-data (uses-syntactic-function rule)))
+    (when fn-data
+      ;; Could increment `(,rule ,count) if we have a use-case
+      (incf (callers fn-data)))))
+
 
 ;;--- accessors
 
@@ -41,24 +83,6 @@
   (:method ((s symbol))
     (gethash s *rule-firing-counts*)))
     
-
-;;--- Driver
-
-(defun record-rule (rule)
-  "Called from referent-from-rule. Always on.
-   Adds to the incidence count of the rule.
-   If the rule involves a semantic function, that is listed and
-   counted as well (if the semantic function structures have been
-   initialized."
-  (pushnew rule *rules-ever-fired*)
-  (unless (gethash rule *rule-firing-counts*)
-    (setf (gethash rule *rule-firing-counts*) 0))
-  (incf (gethash rule *rule-firing-counts*))
-  (let ((fn-data (uses-syntactic-function rule)))
-    (when fn-data
-      ;; Could increment `(,rule ,count) if there turns out to be screw
-      (incf (callers fn-data)))))
-
 
 ;;--- reports
 
@@ -90,19 +114,34 @@
 
   
 
-;;--- use of syntactic functions or methods
+;;--- Record use of syntactic functions or methods
 
 (defvar *syntactic-functions-in-use* 0
-  "A list of the names of all of the syntactic funtions or
-   methods that are the reference data for some rule")
+  "A list of the names of all of the syntactic functions or
+   methods that are the reference data for some rule.
+   Initialized during postprocessing.")
+
+(defvar *uses-syntactic-function* (make-hash-table)
+  "From a rule to its syntactic-function-data")
+
+(defgeneric uses-syntactic-function (rule)
+  (:documentation "Return the data for the syntactic function
+    associated with this rule if there is any.")
+  (:method ((rule-number integer))
+    (let ((rule (psr# rule-number)))
+      (assert rule)
+      (uses-syntactic-function rule)))
+  (:method ((r cfr))
+    (gethash r *uses-syntactic-function*)))
+
 
 (defclass syntactic-function-data (named-object)
   ((rules :initform nil :accessor rules-using)
    (instances :initform 0 :accessor callers))
-  (:documentation "Represents data about one particular
+  (:documentation "Each instance represents data about one
  syntactic function. The set of this data is created by
- a one-time call to collect-syntactic-function-usage or after
- significant chaves to the grammar. Explicitly called by
+ a one-time call to collect-syntactic-function-usage, or after
+ significant changes to the grammar. Explicitly called by
  the postprocessing routines that report the statistic
  on the model and grammar.
  Rules is a list of all the rules that invoke the function.
@@ -111,30 +150,10 @@
  only once we know what we'll do with the information."))
 
 (setup-find-or-make syntactic-function-data)
-;; Generates:
+;; Automatically generates:
 ;; get-syntactic-function-data
 ;; *syntactic-function-data-table*
 ;; clear-syntactic-function-data
-
-(defun update-fixed-data-for-syntactic-function (name rule)
-  "Encountered a rule that references this function ('name').
-   Create the instance if needed and add this rule to its list."
-  (let ((data (find-or-make-syntactic-function-data name)))
-    (push rule (rules-using data))
-    data))
-
-(defvar *uses-syntactic-function* (make-hash-table)
-  "From a rule to its syntactic-function-data")
-
-(defgeneric uses-syntactic-function (rule)
-  (:method ((r cfr))
-    (gethash r *uses-syntactic-function*)))
-
-(defun reinitialize-syntactic-function-data ()
-  "Completely zeros the data. //Need a more delicate function
-   for clearing out the instance could between runs"
-  (clear-syntactic-function-data)
-  (clrhash *uses-syntactic-function*))
 
 (defun collect-syntactic-function-usage ()
   "Walk through all the context-free rules. For each one that
@@ -148,9 +167,31 @@
                (data (update-fixed-data-for-syntactic-function fn-name r)))
           (setf (gethash r *uses-syntactic-function*) data))))))
 
+(defun update-fixed-data-for-syntactic-function (name rule)
+  "There is a rule that references this function ('name').
+   Create the instance if needed and add this rule to its list."
+  (let ((data (find-or-make-syntactic-function-data name)))
+    (push rule (rules-using data))
+    data))
+
+
 (defun syntactic-functions-defined () ;; 93 on 12/28/18
   (all-hash-keys *syntactic-function-data-table*))
 
 (defun rules-using-syntactic-functions () ;; 1,016 on 12/28/18
   (all-hash-keys *uses-syntactic-function*))
 
+
+(defun reinitialize-syntactic-function-data ()
+  "Completely zeros the data (e.g. for debugging)
+   Use clear-syntactic-function-usage-data for clearing out
+   the instance data could between runs"
+  (clear-syntactic-function-data)
+  (clrhash *uses-syntactic-function*))
+
+(defun clear-syntactic-function-usage-data ()
+  "Clears the per-run information on the syntactic function data,
+   leaving the permanent information such as which rules use them."
+  (loop for fn-name in *syntactic-functions-in-use*
+     as fd = (get-syntactic-function-data fn-name)
+     do (setf (callers fd) nil)))
