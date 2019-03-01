42a7c4928b45598fc230622c07382c9b9bc74dc0
lbobrow <lbobrow@sift.net>
2019-03-01 18:12:33 -0500

    added a function to compute the Damerau–Levenshtein distance between two words, and integrated it into the spelling suggestions to sort them by distance

--- Sparser/code/s/grammar/model/sl/biology/spell.lisp
+++ Sparser/code/s/grammar/model/sl/biology/spell.lisp
@@ -1,10 +1,11 @@
 ; Spellcheck program in lisp inspired by Peter Norvig's spellcheck algorithm
                                         ; Ryan Riddle (http://github.com/RyanRiddle
+;; adapted to our dictionary/library along with sorting by Damerau–Levenshtein distance
 
 (in-package :sp)
 (defparameter *dictionary* (make-hash-table :test #'equal))
 (defparameter *misspellings* (make-hash-table :test #'equal))
-(defparameter *alphabet* "abcdefghijklmnopqrstuvwxyz")
+(defparameter *alphabet* "abcdefghijklmnopqrstuvwxyz") 
 
 (defun spell-add (word)
   (setf (gethash word *dictionary*) t))
@@ -100,4 +101,67 @@
              (sugs (mapcar (lambda (del)
                              (gethash del *misspellings*))
                            deletes)))
-        (remove-duplicates (apply #'append sugs) :test #'equal ))))
+        (sort (remove-duplicates (apply #'append sugs) :test #'equal )
+              #'< :key #'(lambda(sug) (edit-distance word sug))))))
+
+;; implementing the Damerau–Levenshtein distance from wikipedia
+;; pseudocode with some input from cl-edit-distance although that is
+;; Levenshtein distance only (makes transposition cost 2 instead of
+;; cost 1) and some input/clarity on implementation from a python
+;; version of the wikipedia code that is here:
+;; https://gist.github.com/pombredanne/0d83ad58f45986ddeb0917266e106be0
+;; although without that version's "improvement" since I agree with the original
+;; algorithm that "def" for "fad" should be a distance of 3 not a
+;; distance of 2
+
+(defun edit-distance (s1 s2)
+  (declare (optimize (speed 1)))
+  (let ((m (length s1))
+        (n (length s2)))
+     (unless (typep s1 'simple-array)
+    (setf s1 (make-array m :element-type 'string :initial-contents s1)))
+  (unless (typep s2 'simple-array)
+    (setf s2 (make-array n :element-type 'string :initial-contents s2)))
+    ;; Check trivial cases
+    (cond ((equal s1 s2) (return-from edit-distance 0))
+          ((= 0 n) (return-from edit-distance m))
+	  ((= 0 m) (return-from edit-distance n)))
+    (let* ((maxdist (+ m n))
+          (d (make-array `(,(+ 2 m) ,(+ 2 n)) :element-type '(or null fixnum)
+                         :initial-element maxdist))
+          (last-row (make-hash-table :test #'equal :size 20)))
+      (declare ((simple-array (or null fixnum)) d))
+      (setf (aref d 0 0) maxdist)
+      (loop for i from 1 to (1+ m)
+            do (setf (aref d i 1) (1- i)))
+      (loop for j from 1 to (1+ n)
+            do (setf (aref d 1 j) (1- j)))
+      (loop for row from 1 to m
+            do (let ((s1-char (elt s1 (- row 1)))
+                     (last-match-col 0))
+                 (loop for col from 1 to n
+                       do (let* ((s2-char (elt s2 (- col 1)))
+                                 (last-match-row (or (gethash s2-char last-row)
+                                                     0))
+                                 (cost (if (eql s1-char s2-char)
+                                           0
+                                           1)))
+                            (setf (aref d (1+ row) (1+ col))
+                                  (min (+ (aref d row col) cost) ;; substitution
+                                       (1+ (aref d (1+ row) col)) ;; addition
+                                       (1+ (aref d row (1+ col))) ;; deletion
+                                       ;; transposition
+                                       (+ (aref d last-match-row last-match-col)
+                                          (- row last-match-row 1)
+                                          (- col last-match-col 1)
+                                          1)))
+                            (when (eq cost 0) (setq last-match-col col))))
+                 #+ignore
+                 (format t "row: ~s~%" (loop for i from 1 to (1+ n)
+                                             collect (aref d row i)))
+                 (setf (gethash s1-char last-row) row)))
+      #+ignore
+      (format t "row: ~s~%" (loop for i from 1 to (1+ n)
+                                  collect (aref d (1+ m) i)))
+      ;; final distance
+      (aref d (1+ m) (1+ n)))))
