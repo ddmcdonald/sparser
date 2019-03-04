e8a4cfe47d6ba6894f0d4fa55ae9592cd3990f5d
David D. McDonald <davidmcdonald@alum.mit.edu>
2019-03-03 22:27:58 -0500

    Cleaned up, rationalized cases in validation code. Added routine to cope with case where more than one form rule is retrieved

--- Sparser/code/s/analyzers/psp/check/multiply-aux.lisp
+++ Sparser/code/s/analyzers/psp/check/multiply-aux.lisp
@@ -3,7 +3,7 @@
 ;;; 
 ;;;     File:  "multiply-aux"
 ;;;   Module:  "analyzers;psp:check:"
-;;;  Version:   February 2019
+;;;  Version:   March 2019
 
 ;; 2/26/19 initiated to clear non-essentials from multiply.lisp
 
@@ -42,67 +42,55 @@
                 (car syntactic-rule-triples)))))))))
 
 
+(defun choose-between-form-rules (rule-lf/rc ;; left-form/right-category
+                                  rule-rf/lc
+                                  left-edge right-edge)
+  (let ((t1 (make-edge-triple left-edge right-edge rule-lf/rc))
+        (t2 (make-edge-triple left-edge right-edge rule-rf/lc)))
+    (let ((stronger-triple (filter-by-rule-strength (list t2 t2))))
+      (triple-rule stronger-triple))))
+
+
 
 ;;;--------------------------------------------------
 ;;; restrict rule application to compatible contexts
 ;;;--------------------------------------------------
 
 (defun valid-rule? (rule left-edge right-edge chunk)
+  "A candidate rule has been identified. Now run a set of tests
+   to determine whether it's presently valid to apply it.
+   Returns non-nil to declare that the rule is valid.
+   When called by multiply-edges, returning nil means that
+   the rule is not valid and multiply-edges will set its
+   local that holds the candidate to nil, making it fall through
+   to the next option.
+     When the caller is trie-multiply-edges we return the rule
+   to signal validity. In multiply-edges is run for side-effects
+   and just has to return non-nil to sanction the rule."
+  (declare (special *check-semantic-applicability* *check-chunk-forms*))
   (when rule
-    ;; The form of the rule has to match the context,
-    ;; and, if there's a chunk argument and the *check-chunk-forms*
-    ;; flag is up, does the chunk form test pass.
-    (when (and (check-rule-form rule left-edge right-edge) 
+    (when (and (validate-rule-form rule left-edge right-edge) 
                (or (null chunk)
                    (not *check-chunk-forms*)
-                   (check-rule-result-form-against-chunk rule right-edge chunk))
+                   (validate-rule-result-form-against-chunk rule right-edge chunk))
                (or (not *check-semantic-applicability*)
                    (test-semantic-applicability rule left-edge right-edge)))
       rule)))
 
 (defun test-semantic-applicability (rule left-edge right-edge)
-  (or (not (and (consp (cfr-referent rule))
-                (eq (car (cfr-referent rule)) :funcall)))
-      (test-subcat-rule (list left-edge right-edge) rule)))
+  "If this rule invokes a syntactic-method or the equivalent function
+   that function will make a quick check to determine whether
+   it's valid to apply it.  Rules with explicit references or
+   references constructed in ways that don't involve syntactic
+   methods invoked by :function are automatically valid."
+  (declare (special *check-semantic-applicability*))
+  (if *check-semantic-applicability*
+    (if (and (consp (cfr-referent rule))
+             (eq (car (cfr-referent rule)) :funcall))
+      (test-subcat-rule (list left-edge right-edge) rule)
+      t)
+    t))
 
-(defun check-rule-form (rule left-edge right-edge) 
-  ;; only accept rules that are compatible with their context
-  ;;  this check can be turned off for particular ETFs by calling
-  ;;  dont-check-rule-form-for-etf-named with the name of the family
-  (if (not *check-forms*) ;; controlling switch
-    rule
-    (let ((rf (rule-forms rule))) ;; recorded at rule-creation time
-      (cond
-       ((or
-         (null rf)
-         (and
-             (compatible-form (first rf) left-edge)
-             (compatible-form (second rf) right-edge)))
-        rule)
-       (t
-        (when *report-form-check-blocks*
-          (format t "~&***------>> blocking ~a~
-                     ~%   ~a applied to~
-                     ~%   (~a, ~a)~
-                     ~%   (~a, ~a)~%"
-                  rule (rule-forms rule)
-                  (edge-form left-edge) left-edge
-                  (edge-form right-edge) right-edge)
-          nil))))))
- 
-(defun check-rule-result-form-against-chunk (rule right-edge chunk)
-  (cond
-   ((chunk-head? right-edge chunk) 
-    ;; is the right-edge the head of the chunk?
-    (case (car (chunk-forms chunk))
-      (NG (memq (rule-lhs-form rule) ;; the symbol of the lhs category
-                '(N-BAR NG NP COMMON-NOUN COMMON-NOUN/PLURAL 
-                  np-head PRONOUN PROPER-NAME PROPER-NOUN)))
-      (VG t)
-      (ADJG t)))
-   ;; If right-edge isn't the head of the chunk
-   ;; then it's always ok.
-   (t t)))
 
 
 
@@ -165,6 +153,46 @@
     (or (null compatible-forms) ;; the rule-form is not taken to constrain anything
         (memq (cat-name (edge-form edge)) compatible-forms))))
 
+(defun validate-rule-form (rule left-edge right-edge) 
+  ;; only accept rules that are compatible with their context
+  ;;  this check can be turned off for particular ETFs by calling
+  ;;  dont-check-rule-form-for-etf-named with the name of the family
+  (if (not *check-forms*) ;; controlling switch
+    rule
+    (let ((rf (rule-forms rule))) ;; recorded at rule-creation time
+      (cond
+        ((or
+          ;; if this clause returns nil, then we fall through
+          ;; to the next clause of this cond and report the problem
+         (null rf)
+         (and (compatible-form (first rf) left-edge)
+              (compatible-form (second rf) right-edge)))
+        rule)
+       (t
+        (when *report-form-check-blocks*
+          (format t "~&***------>> blocking ~a~
+                     ~%   ~a applied to~
+                     ~%   (~a, ~a)~
+                     ~%   (~a, ~a)~%"
+                  rule (rule-forms rule)
+                  (edge-form left-edge) left-edge
+                  (edge-form right-edge) right-edge))
+        nil)))))
+ 
+(defun validate-rule-result-form-against-chunk (rule right-edge chunk)
+  "If the right-edge being composed by this rule is the head edge of
+   then chunk we're in, then check that the rule will give the 
+   new edge a compatible form label."
+  (cond
+   ((chunk-head? right-edge chunk) 
+    ;; is the right-edge the head of the chunk?
+    (case (car (chunk-forms chunk))
+      (ng (memq (rule-lhs-form rule) ;; the symbol of the lhs category
+                '(n-bar ng np common-noun common-noun/plural 
+                  np-head pronoun proper-name proper-noun)))
+      (vg t)
+      (adjg t)))
+   (t t)))
 
 
 ;;;---------------------
