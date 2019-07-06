dd98d6a7bb886a6995fcf8936ef9b731b56b3f99
Rusty Bobrow <rjbobrow@gmail.com>
2019-07-05 22:30:10 -0400

    change a break to a warn in walk-up-bound-in-to-indiv

--- Sparser/code/s/grammar/rules/syntax/q-auxiliary.lisp
+++ Sparser/code/s/grammar/rules/syntax/q-auxiliary.lisp
@@ -97,7 +97,10 @@
            (push var variables)
            (when (eq j i-end) (return))
            (setq i j)
-           (break "WH-nominal: looping ok?: i = ~a  j = ~a" i j))))
+           (warn "WH-nominal: looping ok?: i = ~a  j = ~a" i j)
+           (return-from walk-up-bound-in-to-indiv nil)
+           ;;(break "WH-nominal: looping ok?: i = ~a  j = ~a" i j)
+           )))
     (nreverse variables)))
 
 (defgeneric starts-with-wh-pronoun? (place)
