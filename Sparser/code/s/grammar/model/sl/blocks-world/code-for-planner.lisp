aee2401bcea204684e4a6f6b044438eecefb0971
Molly <mollymoran@brandeis.edu>
2019-10-24 19:15:07 -0400

    Various functions to filter out spatial relations.
    Given a relation space R and a focus object b:
    1) We can filter R for relations where b is the focus,
    2) We can check whether a relation holding true for b is only true "transitively" (i.e. if b is left of c and c is left of d, b is left of d - but this is not "interesting" or worth including in the description), and
    3) We can search the relation-space for the object that has the most relations with b. (i.e., "left" and "touching" vs. just "left")

--- Sparser/code/s/grammar/model/sl/blocks-world/code-for-planner.lisp
+++ Sparser/code/s/grammar/model/sl/blocks-world/code-for-planner.lisp
@@ -27,6 +27,7 @@
   '(;; (slot "doc")
     (dt "The derivation tree we're working on")
     (focus "Identifiers the block (if any) that is in focus")
+    (ground "Identifies the block(s) if any that will serve as the ground in the eventual realization")
     )
   "A schema that is used to define the state parameters and any other
    scrachpad information the text planner needs.")
@@ -81,4 +82,39 @@
         `(progn ,@getter-functions ,@setter-functions)))))
 
 ;; (instantiate-tp-parameters)
+
+(defvar *R* nil "The relation-space (situation), set of all relations that hold")
+(defvar *O* nil "The object-space, set of all objects involved in the situation 
+  (is this something we can reasonably expect to be passed down to us?)") 
+
+;;;--------------------------------------------------
+;;; Simple test situation for debugging
+;;;--------------------------------------------------
+
+(setq *R* (list (list "left" "b1" "b2") (list "left" "b2" "b3") (list "left" "b1" "b3")
+(list "touch" "b1" "b2")))
+
+(setq *O* (list "b1" "b2" "b3"))
+
+;;;--------------------------------------------------
+;;; Various functions that filter *R* for relevant relations
+;;;--------------------------------------------------
+
+;;filters *R* for those with arg as the focus
+(defun filter-focus-relations (arg)
+ (remove-if-not #'(lambda (x) (equal (cadr x) arg)) *R*))
+
+;;given a relation, checks whether it's only true by transitivity in *R*
+(defun true-by-transitivity? (rel)
+  (let ((relation (car rel))
+        (arg1 (cadr rel))
+        (arg2 (caddr rel)))
+        (some #'(lambda (x) (and 
+          (member `(,relation ,x ,arg2) *R* :test #'equal) 
+          (member `(,relation ,arg1 ,x) *R* :test #'equal))) *O*)))
+
+;;returns the object(s) with which arg has the most relations in *R*
+(defun has-most-relations-with (arg)
+  (let ((counts (mapcar #'(lambda (x) (cons x (count x *R* :key #'caddr :test #'equal))) *O*)))
+    (car (alexandria::extremum counts #'> :key #'cdr))))
 
