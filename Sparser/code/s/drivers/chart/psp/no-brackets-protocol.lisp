64dd5e2d36df7c939992f15bada2eec49b6442b4
Rusty Bobrow <rjbobrow@gmail.com>
2019-11-05 22:58:47 -0500

    set *warn-or-error-choice* to allow for running compare-to-snapshots without throwing errors

--- Sparser/code/s/drivers/chart/psp/no-brackets-protocol.lisp
+++ Sparser/code/s/drivers/chart/psp/no-brackets-protocol.lisp
@@ -82,14 +82,15 @@
   "some errors or interesting events happen in the sentence creating sweep, 
    and we want to se the entire sentence context")
 
-(defparameter *warn-or-error-choice* :error)
+;;(defparameter *warn-or-error-choice* :error)
+(defparameter *warn-or-error-choice* :warn)
 
 
 
 (defun warn-or-error (datum &rest arguments)
   (case *warn-or-error-choice*
     (:error (apply #'error (cons datum arguments)))
-    (:error (apply #'warn (cons datum arguments)))))
+    (:warn (apply #'warn (cons datum arguments)))))
 
 
 
