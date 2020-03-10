5971a16f13eb511a56dfce8fe27703d0835dc74b
David D. McDonald <davidmcdonald@alum.mit.edu>
2020-03-10 19:08:13 -0400

    Completely rebuilt the function for running over a directory of files so it fits the present control scheme for operating over files.

--- Sparser/code/s/drivers/sources/directory.lisp
+++ Sparser/code/s/drivers/sources/directory.lisp
@@ -1,10 +1,10 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
 ;;; copyright (c) 1990-1991  Content Technologies Inc.
-;;; copyright (c) 1992,2013 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992,2013,2020 David D. McDonald  -- all rights reserved
 ;;;
-;;;     File:  "string"
+;;;     File:  "directory"
 ;;;   Module:  "drivers;sources:"
-;;;  Version:   May 2013
+;;;  Version:   March 2020
 
 ;; 1/30/13 Added .txt to the string passed to directory so it would work.
 ;;  Might be Clozure-specific requirement. 3/29/13 Added keyword to pass
@@ -15,10 +15,27 @@
 
 (in-package :sparser)
 
-(defun analyze-text-from-directory (dir &key doc-set-name)
-  (declare (ignore doc-set-name))
-  (let* ((file-list (directory (format nil "~a/*.txt" dir)))
-         (stream (define-document-stream (gensym "document-stream")
-                     :style-name 'hand-typed/no-headers
-                     :file-list file-list)))
-    (do-document-as-stream-of-files stream)))
+
+(defun analyze-directory-files (directory
+                                &key (file-extension ".txt")
+                                  ((:paragraph orthographic-paragraphs) t) ; *paragraphs-from-orthography*
+                                  ((:prescan prescan-buffer?) t) ; *prescan-character-input-buffer*
+                                  ((:trace run-traces) t)
+                                  ((:skip ignore-errors?) t))
+                                  
+  "Collect the files in the directory then analyze each one in sequence.
+   Most of the keyword arguments are for use by analyze-text-from-file
+   which is the ultimate driver"
+  (let* ((pathname (decode-file-expression/pathname directory))
+         (wildcard-namestring (string-append pathname "*" file-extension)))
+    (let ((files (directory wildcard-namestring)))
+      (loop for filename in files
+         do (progn
+              (when run-traces
+                (format t "~&~%~%analyzing ~A~%~%" filename))
+              (analyze-text-from-file filename
+                                      :paragraph orthographic-paragraphs
+                                      :prescan prescan-buffer?
+                                      :trace run-traces
+                                      :skip ignore-errors?))))))
+
