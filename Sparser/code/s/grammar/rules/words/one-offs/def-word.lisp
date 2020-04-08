be87c1f6ce8081d8e6a69e128391f1b8c06d1345
David D. McDonald <davidmcdonald@alum.mit.edu>
2020-04-08 13:40:32 -0400

    Converted setf of symbol function to a case statement. Tested on comlex case.

--- Sparser/code/s/grammar/rules/words/one-offs/def-word.lisp
+++ Sparser/code/s/grammar/rules/words/one-offs/def-word.lisp
@@ -1,34 +1,38 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; $Id$
-;;; Copyright 2011 David D. McDonald, all rights reserved
+;;; Copyright 2011,2020 David D. McDonald, all rights reserved
 ;;;
 ;;;     File: "def-word"
 ;;;   Module: "grammar;rules:words:one-offs:"
-;;;  Version:  July 2011
+;;;  Version:  April 2020
 
 ;; initiated 7/27/11.
 
 (in-package :sparser)
 
-(unless (boundp '*def-word-definition*)
-  (defvar *def-word-definition* nil
+(defparameter *def-word-definition* :comlex
     "Determines what the def-word macro expands into.
-     Possible values are :comlex and :2010-frequency."))
+     Possible values are :comlex and :2010-frequency.")
 
+(defun establish-version-of-def-word (keyword)
+  (unless (memq keyword '(:comlex :word-frequency))
+    (error "keyword must be either :comlex or :word-frequency"))
+  (setq *def-word-definition* keyword))
 
 (defmacro def-word (string &rest data)
-  `(def-word/expr ,string ',data))
+  (declare (special *def-word-definition*))
+  (ecase *def-word-definition*
+    (:comlex
+     `(prime-word-from-comlex ,string ',data))
+    (:word-frequency
+     `(define-2010-words-frequency-data ,string ',data))))
 
-;; Changing the macro itself is unnecessary since it
-;; expands into an ordinary function. Were we to change
-;; the macro we'd need to recompile all the sites that
-;; referenced it.
-
+#+ignore
 (defun def-word/expr (string data)
   (declare (ignore string data))
   (error "No definition for def-word yet.~
         ~%Use establish-version-of-def-word"))
 
+#+ignore
 (defun establish-version-of-def-word (keyword)
   (case keyword
     (:2010-frequency
