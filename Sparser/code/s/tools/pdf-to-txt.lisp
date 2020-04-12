484e5503e7d55b479b8bf37a09b4d598d3f6805d
Matt McLure <mmclure@sift.edu>
2020-04-12 14:30:47 -0400

    Use most recent dehyphenation code, fold it into pdf-to-txt.lisp (from dehyphenate.lisp), and move pdf-to-txt to tools directory.

--- Sparser/code/s/tools/pdf-to-txt.lisp
+++ Sparser/code/s/tools/pdf-to-txt.lisp
@@ -7,19 +7,21 @@
 (defparameter *python3-path* "/usr/local/bin")
 
 (defparameter *pdf2txt-path*
-  (asdf:system-relative-pathname :sparser "Sparser/code/s/drivers/sources/pdf2txt_mod.py"))
+  (asdf:system-relative-pathname :sparser "Sparser/code/s/tools/pdf2txt_mod.py"))
 
-(defparameter *hyphenated-word-whitelist*
+(defparameter *word-whitelist*
   '("SARS-CoV"
     "MERS-CoV"
     "SARS-CoV-2"
     "COVID-19"
+    "pathogen"  ;; should we bother adding words like this here?
     ))
+
 
 ;;  ==  PDF-TO-TXT =============================================================
 ;; (pdf-to-txt "/path/to/file.pdf")
 
-(defun pdf-to-txt (pdf-file &key (txt-file (txt-from-pdf pdf-file))
+(defun pdf-to-txt (pdf-file &key (txt-file (txt-from-pdf-path pdf-file))
                               (collapse-paragraphs t))
   ;; Run the pdf2txt script to turn pdf to txt
   (let ((pdf-namestring (namestring pdf-file))
@@ -28,8 +30,9 @@
                        :output t :error-output t)
     ;; Clean up hyphens
     (remove-hyphen-line-breaks-in-file txt-namestring txt-namestring)
-    ;; Remove within-paragraph line breaks
-    (remove-within-paragraph-line-breaks-in-file txt-namestring txt-namestring)
+    ;; Optionally remove within-paragraph line breaks
+    (when collapse-paragraphs
+      (remove-within-paragraph-line-breaks-in-file txt-namestring txt-namestring))
     txt-namestring))
 
 
@@ -54,28 +57,40 @@
 ;; (remove-hyphen-line-breaks <string>)
 
 (defun remove-hyphen-line-breaks (str)
-  (cl-ppcre::regex-replace-all "(\\S+)-\\s*[\\r\\n]+\\s*(\\S+)\\s*" str #'replace-hyphen-line-break))
+  (cl-ppcre::regex-replace-all "(\\S+)-\\s*[\\r\\n]+(\\S+)\\s*" str #'replace-hyphen-line-break))
 
-;;  == Internals ==
+;;  == internals ==
 
 ;; a replacement function passed to cl-ppcre::regex-replace-all in remove-hyphen-line-breaks
 (defun replace-hyphen-line-break (target-string start end match-start match-end reg-starts reg-ends)
   (declare (ignore start end reg-starts reg-ends))
   (declare (type vector target-string))
-  ;;construct a replacement
-  (let* ((match-str (subseq target-string match-start match-end))
-         (scanner (cl-ppcre::create-scanner "^(\\S+)-\\s*[\\r\\n]+\\s*(\\S+)$" :multi-line-mode t))
-         (hyphenated-word-with-punc (cl-ppcre::regex-replace scanner match-str "\\1-\\2"))
-         (hyphenated-word (cl-ppcre::regex-replace-all "[^A-Za-z0-9-]" hyphenated-word-with-punc ""))
-         (replacement match-str))
-    (declare (type string replacement))
-    (cond ((member hyphenated-word *hyphenated-word-whitelist* :test #'string-equal)
-           ;; if removing the newline results in something on the whitelist, return it.
-           (setf replacement hyphenated-word-with-punc))
-          (t   ;; otherwise, remove the hyphen too
-           (setf replacement
-                 (cl-ppcre::regex-replace scanner match-str "\\1\\2"))))
-    (format nil "~a~%" replacement)))
+  (flet ((approved-word? (word)
+           (when (or (member word *word-whitelist* :test #'string-equal)
+                     (resolve word))
+             t)))
+    (let* ((match-str (subseq target-string match-start match-end))
+           (scanner (cl-ppcre:create-scanner "^(\\S+)-\\s*[\\r\\n]+(\\S+)\\s*$" :multi-line-mode t))
+           (hyphenated-word-with-punc (cl-ppcre::regex-replace scanner match-str "\\1-\\2"))
+           (hyphenated-word (cl-ppcre:regex-replace-all "[^A-Za-z0-9-]" hyphenated-word-with-punc ""))
+           (member-words (cl-ppcre:split "-" hyphenated-word))
+           (merged-word-with-punc (cl-ppcre:regex-replace scanner match-str "\\1\\2"))
+           (merged-word (cl-ppcre:regex-replace-all "[^A-Za-z0-9-]" merged-word-with-punc ""))
+           (replacement match-str))
+      (declare (type string replacement))
+      (cond ((approved-word? merged-word) ; if the merged word is an approved word, use that
+             (setf replacement merged-word-with-punc)) ; keep the trailing punctuation with the word
+            ((or (approved-word? hyphenated-word)
+                 (every #'approved-word? member-words))
+             ;; if removing the newline but leaving the hyphen results in an approved word, or something
+             ;; whose parts (separated by hyphens) are all approved words, use the hyphenated version.
+             (setf replacement hyphenated-word-with-punc)) ; keep the trailing punctuation with the word
+            (t   ; otherwise, signal a warning and assume the merged version.
+             ;; We may decide to remove this warning if it's too annoying.
+             (warn "~a is not known to be a word or white-listed in sp::*word-whitelist*, nor is the hyphenated version ~a, nor are (all of) its constituents (~{~A~^, ~}). Default: Using merged version: ~a."
+                   merged-word hyphenated-word member-words merged-word)
+             (setf replacement merged-word-with-punc)))  ; keep the trailing punctuation with the word
+      (format nil "~a~%" replacement))))
 
 
 ;;  ==  REMOVING WITHIN-PARAGRAPH LINE BREAKS ==================================
@@ -95,11 +110,11 @@
 
 (defun remove-within-paragraph-line-breaks (str)
   ;; Replace every line break with a space, except the ones that look like paragraph line breaks.
-  ;; Specifically, don't replace it if there are two or more successive line breaks where the
+  ;; Specifically, leave a (single) break where there are two or more successive line breaks and the
   ;; first word after the line breaks is capitalized.
   (cl-ppcre::regex-replace-all "[\\r\\n]+\\s*\\S" str #'remove-within-paragraph-line-break))
 
-;;  == Internals ==
+;;  == internals ==
 
 ;; a replacement function passed to cl-ppcre::regex-replace-all in remove-within-paragraph-line-breaks
 (defun remove-within-paragraph-line-break (target-string start end match-start match-end reg-starts reg-ends)
@@ -151,7 +166,7 @@
       (concatenate 'string namestring suffix)))
 
 ;;the default way to construct an output filename from an input filename
-(defun txt-from-pdf (namestring)
+(defun txt-from-pdf-path (namestring)
   (cl-ppcre::regex-replace "\\.pdf$" namestring ".txt"))
 
 (defun contains-multiple-breaks (str)
