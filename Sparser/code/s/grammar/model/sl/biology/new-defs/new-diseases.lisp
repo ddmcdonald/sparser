4e95e0b7bec2525320b3cced8146de6c012eaba6
Robert Bobrow <rjbobrow@gmail.com>
2020-04-23 14:56:41 -0400

    provided a name for "breast cancer"

--- Sparser/code/s/grammar/model/sl/biology/new-defs/new-diseases.lisp
+++ Sparser/code/s/grammar/model/sl/biology/new-defs/new-diseases.lisp
@@ -114,7 +114,7 @@
 (def-indiv-with-id cancer "angiosarcoma" "EFO:0003968" :name "angiosarcoma") 
 (def-indiv-with-id cancer "astrocytoma" "EFO:0000272" :name "astrocytoma")
 (def-indiv-with-id cancer "bladder cancer" "NCIT:C2901")
-(def-indiv-with-id cancer "breast cancer" "TS-0591")
+(def-indiv-with-id cancer "breast cancer" "TS-0591" :name "BREAST-CANCER")
 (def-indiv-with-id cancer "breast carcinoma" "TS-0592") 
 (def-indiv-with-id cancer "carcinoma" "EFO:0000313" :name "carcinoma")
 (def-indiv-with-id cancer "carcinosarcoma" "NCIT:C34448" :name "carcinosarcoma") 
