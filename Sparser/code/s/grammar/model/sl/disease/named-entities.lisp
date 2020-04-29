2b8757860110819de59d7982bd364eb502ba07cf
David D. McDonald <davidmcdonald@alum.mit.edu>
2020-04-29 13:59:30 -0400

    Wrote macro for def-organization which pre-sets some standard variables. Applied to most of Laurel's cases, but a residue remains

--- Sparser/code/s/grammar/model/sl/disease/named-entities.lisp
+++ Sparser/code/s/grammar/model/sl/disease/named-entities.lisp
@@ -1,15 +1,19 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER COMMON-LISP) -*-
-;;; copyright (c) 2013  David D. McDonald  -- all rights reserved
+;;; copyright (c) 2013-2014,2020  David D. McDonald  -- all rights reserved
 ;;;
 ;;;      File:   "named-entities"
 ;;;    Module:   "sl;disease:"
-;;;   version:   August 2014
+;;;   version:   April 2020
 
 ;; initiated 5/6/13
 ;; updated 28/8/14--added WHO, Doctors Without Borders
 
-
 (in-package :sparser)
+
+#| April 2020 -- shallow version of define-company using the shortcut forms
+ with polywords and synonyms. Includes alternative/additional categories
+ to generalize from strict notion of a 'company'.
+|#
 
 (when (fboundp 'define-company)
   (define-with-all-instances-permanent
@@ -25,73 +29,71 @@
     (define-company '("Doctors" "Without" "Borders"))
 
     ))
+
+;; endurant > non-physical > social-object > social-agent
+
+(define-category organization
+  :specializes social-agent
+  :binds ((name :primitive word)
+          (UID :primitive string))
+  :realization (:proper-name name)
+  :documentation "covering category for amalgams of social agents with
+ a common purpose")
+
+(defmacro def-organization (short-pname &key id name synonyms)
+  "Variation on what it calls where several fields are already filled in"
+  `(define-individual-with-id 'organization ,short-pname ,id
+                              :no-plural t :pos 'proper-name
+                              ,.(when synonyms `(:synonyms ',synonyms))
+                              ))
+
+(def-organization "COG" :id "NCIT:C39353" :name "Children's Oncology Group")
+(def-organization "DSMB" :id "NCIT:C94204" :name "Data and Safety Monitoring Board")
+(def-organization "FDA" :id "NCIT:C17237" :name "Food and rug Administration")
+(def-organization "FIGO" :id "NCIT:C89808" :name "Federation of Gynecology and Obstetrics")
+(def-organization "IUPAC" :id "NCIT:C82878" :name "International Union of Pure and Applied Chemistry")
+(def-organization "MSKCC" :id "NCIT:C39457" :name "Memorial Sloan-Kettering Cancer Center")
+(def-organization "NCI" :id "NCIT:C19202"   :name "National Cancer Institute")
+(def-organization "NHLBI" :id "NCIT:C82613" :name "National Heart, Lung, and Blood Institute")
+(def-organization "NICHD" :id "NCIT:C19754" :name "National Institute of Child Health and Human Development")
+(def-organization "NIAID" :id "NCIT:C19753" :name "National Institute of Allergy and Infectious Disease")
+(def-organization "NCBI" :id "NCIT:C45799" :name "National Center for Biotechnology Information")
+(def-organization "NIH" :id "NCIT:C16895"   :name "National Institutes of Health")
+(def-organization "nlm" :id "NCIT:C82620" :name "National Library of Medicine")
+(def-organization "SCI" :id "NCIT:C105626" :name  "Stanford Cancer Institute")
+(def-organization "USDA" :id "NCIT:C68708" :name  "United States Department of Agriculture")
+
+
 #| professional organizations to add:
-potentially make them of the form:
-(def-individual-with-id prof-org "FDA" "NCIT:C17237" :name "Food and Drug Administration" :no-plural t)
-but not sure what prof-org should fall under
-("NIH" "ONT:PROFESSIONAL-ORGANIZATION" :ID "NCIT:C16895" :NAME "National Institutes of Health")
-("COG" "ONT:PROFESSIONAL-ORGANIZATION" :ID
-                                "NCIT:C39353" :NAME
-                                "children's oncology group")
-                               ("DSMB" "ONT:PROFESSIONAL-ORGANIZATION" :ID
-                                "NCIT:C94204" :NAME
-                                "data and safety monitoring board")
-                               ("FIGO" "ONT:PROFESSIONAL-ORGANIZATION" :ID
-                                "NCIT:C89808" :NAME
-                                "federation of gynecology and obstetrics")
-("IUPAC" "ONT:PROFESSIONAL-ORGANIZATION" :ID
-                                "NCIT:C82878" :NAME
-                                "international union of pure and applied chemistry")
-                               ("MSKCC" "ONT:PROFESSIONAL-ORGANIZATION" :ID
-                                "NCIT:C39457" :NAME
-                                "memorial sloan-kettering cancer center")
-                               ("NCI" "ONT:PROFESSIONAL-ORGANIZATION" :ID
-                                "NCIT:C19202" :NAME
-                                "national cancer institute")
-                               ("NHLBI" "ONT:PROFESSIONAL-ORGANIZATION" :ID
-                                "NCIT:C82613" :NAME
-                                "national heart, lung, and blood institute")
-                               ("NICHD" "ONT:PROFESSIONAL-ORGANIZATION" :ID
-                                "NCIT:C19754" :NAME
-                                "national institute of child health and human development")
-                               ("SCI" "ONT:PROFESSIONAL-ORGANIZATION" :ID
-                                "NCIT:C105626" :NAME
-                                "stanford cancer institute")
-                               ("USDA" "ONT:PROFESSIONAL-ORGANIZATION" :ID
-                                "NCIT:C68708" :NAME
-                                "united states department of agriculture")
-;; lower case probably come from url/email addresses
-                               ("cshl" "ONT:PROFESSIONAL-ORGANIZATION" :ID
+
+                               
+                               ("cshl"  :ID
                                 "NCIT:C39335" :NAME
                                 "cold spring harbor laboratory cancer
                                 center")
-                                ("jax" "ONT:PROFESSIONAL-ORGANIZATION" :ID
+                                ("jax"  :ID
                                 "NCIT:C39529" :NAME
                                 "the jackson laboratory cancer
                                 center")
 
-                               ("ncbi" "ONT:PROFESSIONAL-ORGANIZATION" :ID
-                                "NCIT:C45799" :NAME
-                                "national center for biotechnology information")
-                               ("nlm" "ONT:PROFESSIONAL-ORGANIZATION" :ID
-                                "NCIT:C82620" :NAME
-                                "national library of medicine")
-                               ("org" "ONT:PROFESSIONAL-ORGANIZATION" :ID
+
+                               
+
+                               ("org"  :ID
                                 "NCIT:C19711" :NAME
                                 "professional organization or group")
-                               ("niaid" "ONT:PROFESSIONAL-ORGANIZATION" :ID
-                                "NCIT:C19753" :NAME
-                                "national institute of allergy and infectious disease")
+
+                               
 
 ;; some have no UID:
 "BGI" "bgi" "Beijing Genomics Institute"
 "JCVI" "J. Craig Venter Institute" ;; does genomic and virus research
 
 slightly different -- local institutions so could have plurals:
-("IACUC" "ONT:PROFESSIONAL-ORGANIZATION" :ID
+("IACUC"  :ID
                                 "NCIT:C19487" :NAME
                                 "institutional animal care and use committee")
-                               ("IRB" "ONT:PROFESSIONAL-ORGANIZATION" :ID
+                               ("IRB"  :ID
                                 "NCIT:C16741" :NAME
                                 "institutional review board")
 
