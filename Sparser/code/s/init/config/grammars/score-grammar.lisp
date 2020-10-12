fb8660a810780653c08b233433992678f0f7f393
David D. McDonald <davidmcdonald@alum.mit.edu>
2020-10-12 11:46:14 -0400

    Tweaked choice of grammar modules

--- Sparser/code/s/init/config/grammars/score-grammar.lisp
+++ Sparser/code/s/init/config/grammars/score-grammar.lisp
@@ -159,6 +159,7 @@
 
 (include-grammar-module  *pronoun-objects*)
 (include-grammar-module  *resolve-pronouns*)
+(include-grammar-module  *incrementally-resolve-pronouns*)
 (include-grammar-module  *topic*)
 
 (include-grammar-module  *ha*)
