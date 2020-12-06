338dad41ecaa23f335c7680b0889a219f80b498e
Robert Bobrow <rjbobrow@gmail.com>
2020-12-05 19:02:40 -0500

    comments on need for "purposive-process" and difficulty caused by moving "measure" out from under bio-metod

---
+++ Sparser/documentation/open-tasks/problems/measure-and-purposive-processes.lisp
@@ -0,0 +1,15 @@
+
+-*-org-*-
+#+DATE: 12/5/20
+
+by moving "measure" out from under bio-method, a bunch of fallback
+occurred 
+
+the class "purposive-process" seems to have been defined (probably by
+me, RJB) to be able to have a purpose modifier (e.g. "in order to
+...". It is possible that this is really a case where the licensing is
+from the AGENT of the process -- if the agent can be said to have a
+purpose, then such a modifier is licensed, and more generally "John
+did X in order to accomplish Y" can be replaced with "John
+accomplished Y by doing X"
+
