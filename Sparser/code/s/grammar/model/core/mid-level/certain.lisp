910a5c5c313f3e680f088f662892df3d3a280d97
David D. McDonald <davidmcdonald@alum.mit.edu>
2020-12-10 10:21:42 -0500

    Cleaned up an sorted. Now needs a more appropriate category

--- Sparser/code/s/grammar/model/core/mid-level/certain.lisp
+++ Sparser/code/s/grammar/model/core/mid-level/certain.lisp
@@ -3,7 +3,7 @@
 ;;;
 ;;;     File:  "certain"
 ;;;   Module:  "model;core:mid-level:"
-;;;  version:  November 2020
+;;;  version:  December 2020
 
 ;; Broken out of Harvard-terms 11/16/20
 
@@ -16,28 +16,34 @@
    ///Needs a notion of polarity, since many of these have
    'un' forms: 'uncertain', 'unlikely'. If we used a mixin for
    this we could have a method on 'not' that flips the polarity."
+  
   :specializes top ;;bio-rhetorical//// need mid-level event types!!!
   ;; inherits 'statement' from bio-complement > bio-thancomp
   ;;  > bio-rhetorical. Should we restrict it
+
   :mixins (qualifiable ;; "quite certain", "entirely certain"
-                       nominal-attribute
-                       with-agent
-                       raising-to-subject) ;; "I am confident ..."
+           nominal-attribute ;; "__ of" "__ that" "__ Spencer will come back"
+           with-agent
+           raising-to-subject) ;; "I am confident ..."
+  
   :restrict ((agent (:or endurant perdurant abstract)))
+  
   :realization (:adj "certain"
                 :adverb "certainly"
                 :s agent
                 :thatcomp theme))
 
-(adv "certainly" :super certain)
 
 (adj "confident" :super certain)
-
 (adj "sure" :super certain)
+(adj "uncertain" :super certain)
+(adj "possible" :super certain)
+(adj "plausible" :super certain)
 
-(adj "uncertain" :super certain
- :realization (:adj "uncertain" :mumble ("uncertain" adj-that-comp :c theme)))
-
+(adv "definitely" :super certain)
+(adv "probably" :super certain)
+(adv "possibly" :super certain)
+(adv "potentially" :super certain)
 
 
 ;;--- nominal perspective
@@ -46,23 +52,11 @@
   :specializes certain
   :realization (:noun "certainty")
   :documentation "The word 'certainty' doesn't pattern like
-     'likelyhood' or 'probability' but this probably does
-     no substantial harm")
+     'likelyhood' or 'probability' but this probably doesn't
+     do any substantial harm")
 
 (noun "chance" :super certainty)
-
 (noun "probability" :super certainty)
-
-(adj "possible" :super certain)
-(adj likely-adj :super certain :adj "likely")  ;; this is to handle uses like "it is likely that ..."
-(adj "plausible" :super certain) ;; this is to handle uses like "it is likely that ..."
-
-(adv "possibly" :super certain)
-
-(adv "probably" :super certain)
-(adv "definitely" :super certain)
-(adv "potentially" :super certain)
-
 
 (noun "likelihood" :super certainty)
 
@@ -71,12 +65,10 @@
 ;; is likely to be mediated by
 ;; is likely that this possible feedback
 ;; will likely be useful
-(define-category likely-adj
-  :specializes certain
-  :realization
-    (:adj "likely"))
-;; bio-relation and bio-rhetorical appear to differ mostly
-;; on their value restrictions. 
+
+
+(adj likely-adj :super certain :adj "likely")
+;; this is to handle uses like "it is likely that ..."
 
 (define-category unlikely
   :specializes certain
