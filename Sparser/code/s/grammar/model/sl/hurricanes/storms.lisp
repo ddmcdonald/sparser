c330bbe41259f4d4435e5c15e7b88fa107b5ca95
David D. McDonald <davidmcdonald@alum.mit.edu>
2020-12-30 18:47:13 -0500

    toyed with fleshing out the sublanguage while looking for learning targets

--- Sparser/code/s/grammar/model/sl/hurricanes/storms.lisp
+++ Sparser/code/s/grammar/model/sl/hurricanes/storms.lisp
@@ -1,9 +1,9 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
-;;; copyright (c) 2011  David D. McDonald  -- all rights reserved
+;;; copyright (c) 2011.2020  David D. McDonald  -- all rights reserved
 ;;;
 ;;;     File:  "storms"
 ;;;   Module:  "grammar/model/sl/hurricanes/"
-;;;  version:  December 2011
+;;;  version:  December 2020
 
 ;; Initiated 12/17/11
 
@@ -13,3 +13,9 @@
   :specializes perdurant
   :instantiates self
   :realization (:common-noun "storm"))
+
+(define-category make-landfall
+  :specializes accomplishment)
+
+(def-cfr make-landfall (make landfall)
+  :form vg)
