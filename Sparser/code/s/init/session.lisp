1831ea4e41ee43d2d4156cef63fa25c27c233cc3
David D. McDonald <davidmcdonald@alum.mit.edu>
2021-01-11 12:07:43 -0500

    Update for 2021

--- Sparser/code/s/init/session.lisp
+++ Sparser/code/s/init/session.lisp
@@ -1,9 +1,9 @@
 ;;; -*- Mode: LISP; Syntax: Common-Lisp; Package: SPARSER -*-
-;;; Copyright (c) 2016-2020 SIFT LLC. All Rights Reserved.
+;;; Copyright (c) 2016-2021 SIFT LLC. All Rights Reserved.
 ;;;
 ;;;      File: "session"
 ;;;    Module: "init;"
-;;;   Version: January 2020
+;;;   Version: January 2021
 
 (in-package :sparser)
 
@@ -34,7 +34,7 @@
 
 ;; Finally, print a salutation.
 (format t "~%Welcome to the Sparser natural language analysis system.~
-           ~%Copyright (c) David D. McDonald 1991-2005,2010-2020.~
+           ~%Copyright (c) David D. McDonald 1991-2005,2010-2021.~
            ~%Distributed under the Eclipse Public License.~
            ~%~
            ~%Type (in-package :sparser) to use Sparser symbols directly.~
