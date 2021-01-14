b01567261d157cb9cf16ad87673bf43d46092d26
David D. McDonald <davidmcdonald@alum.mit.edu>
2021-01-14 10:31:31 -0500

    Sketch of the idea. Nothing runs yet

---
+++ Sparser/code/s/drivers/chart/psp/interventions.lisp
@@ -0,0 +1,38 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
+;;; copyright (c) 2021 David D. McDonald  -- all rights reserved
+;;;
+;;;      File: "interventions"
+;;;   Module:  "drivers;chart:psp:"
+;;;  Version:  January 2021
+
+;; Initiated 1/13/21 to let us experiment with extensions to the suite
+;; of control structures we use to apply rules, etc.
+
+(in-package :sparser)
+
+#| We salt the standard two-phase 'island-driven' control structure
+with calls to one or more highly parameterized 'intervention' functions
+according to what processessing we want to be completed before engaging
+these additional operations.
+  This mechanism lets us experiment with alternatives in the control
+structure freely while staying tentative about whether we are doing
+operations in the appropriate order.
+
+The control parameters should be define in this file. The action code
+can of course be anywhere. The intervention functions are organized as
+as series of (when <parameter> <do action>) clauses, which will let us
+pose self-contradictory operations as the same time driven by different
+values of the parameters
+
+|#
+
+(defparameter *introduce-likely-prep-args* nil
+  "Sweep over the post phrase one debris for standed preposition
+   and uncomposed prepositional phrases and write rules to handle
+   them.")
+
+
+(defun operations-after-phase-one (sentence)
+  (when *introduce-likely-prep-args*
+    )
+  )
--- Sparser/code/s/drivers/chart/psp/loader.lisp
+++ Sparser/code/s/drivers/chart/psp/loader.lisp
@@ -1,9 +1,9 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1990-1995,2012-2020 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1990-1995,2012-2021 David D. McDonald  -- all rights reserved
 ;;; 
 ;;;     File:  "loader"
 ;;;   Module:  "drivers;chart:psp:"
-;;;  Version:  October 2020
+;;;  Version:  January 2021
 
 ;; initiated summer 1990
 ;;  3.0  (9/2/92) bumped to 3 and moved in the newly rationalized set
@@ -64,3 +64,4 @@
 (lload "psp-drivers;chunker")
 (lload "psp-drivers;generalize")
 (lload "psp-drivers;embedded-da")
+(lload "psp-drivers;interventions")
