7641719a5b575afce79243fb26237403e6141852
David D. McDonald <davidmcdonald@alum.mit.edu>
2021-01-15 17:48:23 -0500

    Serious make over to the sweep-sentence-treetops operation that populates a layout object that describes what's in the chart prior to any forest-level processing. The walking routine returns edge-vectors when the position has multiple initial edges and the previous version of this function just took the top edge and ignored the others. With the increase in the interpretations on words these day the effect was frequently to drop the prepositions, which broke out verb+prep handling in many cases. Now every edge in a vector is passed on to be part of the layout description.

--- Sparser/code/s/drivers/forest/sweep.lisp
+++ Sparser/code/s/drivers/forest/sweep.lisp
@@ -1,9 +1,9 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 2014-2018 David D. McDonald  -- all rights reserved
+;;; copyright (c) 2014-2021 David D. McDonald  -- all rights reserved
 ;;; 
 ;;;     File:  "sweep"
 ;;;   Module:  "drivers;forest:"
-;;;  Version:  November 2018
+;;;  Version:  January 2021
 
 ;; Initiated 8/30/14. To hold the new class of containers to support
 ;; analysis and discourse structure to go with the new forest protocol
@@ -31,7 +31,7 @@
   "Set to nil when you want to see which edges have no forms")
 
 (defparameter *break-on-new-tt-sweep-cases* nil
-  "Used controls whether we look at new cases")
+  "Used to control whether we look at new cases")
 
 
 ;;;--------
@@ -44,21 +44,28 @@
   "Scan the treetops of the sentence from left to right, 
    notice their form, and stash them into state variables 
    that will be consulted during the island-driven parsing that follows."
-  (declare (special category::that word::comma word::|of|
-                    *incrementally-resolve-pronouns* *pending-pronoun*
-                    *nps-seen*))
+  (declare (special *incrementally-resolve-pronouns* *pending-pronoun*
+                    *nps-seen*
+                    subject-seen? main-verb-seen?)) ;; state varibles in forest-gophers
   (tr :sweep-sentence-treetops start-pos end-pos)
   (clear-sweep-sentence-tt-state-vars)
   (let ((rightmost-pos start-pos)
         (layout (make-sentence-layout sentence))
         (sentence-initial? t)
+        (subject-seen? nil)
+        (main-verb-seen? nil)
         (count 0) ;; how many treetops have been scanned
-        tt  prior-tt  form  pos-after  multiple?  )
+        treetop tt  edges  prior-tt  form  pos-after  multiple?  )
+    (declare (special sentence-initial? subject-seen? main-verb-seen?))
 
+    ;; Loop over the treetops
     (loop
-       (multiple-value-setq (tt pos-after multiple?)
+       (multiple-value-setq (treetop pos-after multiple?)
          (next-treetop/rightward rightmost-pos))
-       (incf count)
+       ;; The treetops of the chart as returns by next-treetop/rightward
+       ;; can be either single edges or an edge-vector with multiple (initial)
+       ;; edges. If there is no edge at the position ('rightmost-pos') then
+       ;; the word is returned as the tt. 
 
        (unless (pos-assessed? pos-after)
          ;; catches bugs in the termination conditions
@@ -66,119 +73,38 @@
                   (edge-spans-position? prior-tt end-pos))
            (return)
            (error "Walked beyond the bounds of the sentence")))
-
-       (when (and (word-p tt)
-                  (eq tt *the-punctuation-period*))
+       
+       (incf count) ; of treetops
+       
+       (when (and (word-p treetop)
+                  (eq treetop *the-punctuation-period*))
          (tr :terminated-sweep-at pos-after)
          (return))
-       (tr :next-tt-swept tt pos-after)
-
-       (when multiple?
-         ;; Presume that we want the topmost edge. 
-         ;; This ignores real ambiguities
-         (when (eq 0 (ev-number-of-edges tt))
-           (break "0 edges in sweep-sentence-treetops"))
-         (setq tt (elt (ev-edge-vector tt)
-                       (1- (ev-number-of-edges tt)))))
-
-       (when (edge-p tt)
-         (setq form (edge-form tt)))
        
        ;; Periods can get edges over them by accidentally
        ;; being given as a literal in a rule.
        ;; This check also catches all kinds of punctuation
-       (when (and (edge-p tt)
-                  (null form))
+       (when (and (edge-p treetop) (null (edge-form treetop)))
          (cond
-           ((eq (edge-category tt)
+           ((eq (edge-category treetop)
                 *the-punctuation-period*)  ;; we're done
             (return))
-           ((edge-over-punctuation? tt)) ;; flag it?          
+           ((edge-over-punctuation? treetop)) ;; flag it?          
            (t 
             (unless *no-error-on-no-form*
-              (push-debug `(,tt ,pos-after))
+              (push-debug `(,treetop ,pos-after))
               (error "No form value on ~a" tt)))))
        
-       (when (category-p form)
-         (case (cat-name form)
-           ;; this is a gross control structure, but it works
-           
-           ((np proper-name proper-noun n-bar common-noun
-                pronoun wh-pronoun reflexive/pronoun possessive/pronoun)
-            (unless (word-p tt)
-              ;;/// pull back inline when it's all worked out
-              (catalog-np-for-sweep tt prior-tt count form main-verb-seen? layout)))
+       (tr :next-tt-swept treetop pos-after)
 
-           (vg
-            (if main-verb-seen?
-              ;;/// need to modify verb builder and set of form categories
-              ;; to retain the participlial nature of, e.g. "inhibiting"
-              (push-post-mvs-verbs tt)
-              (set-main-verb tt))) ;;/// won't work for preposed participles
-           
-           (vp
-            (push-verb-phrase tt))
-           
-           (adjective
-            (push-loose-adjective tt))
-           
-           (s
-            (push-loose-clauses tt))
-           
-           (subj+verb
-            (push-loose-subj+verb tt))
-           
-           (adverb
-            (if sentence-initial?
-              (setf (starts-with-adverb (layout)) tt)
-              (push-loose-adverb tt)))
-           
-           ((preposition spatial-preposition spatio-temporal-preposition) ;; under
-            (when sentence-initial?
-              (setf (starts-with-prep (layout)) tt))
-            (let ((prep (edge-left-daughter tt)))
-              (if (eq prep word::|of|)
-                (push-of tt)
-                (push-preposition tt))))
-           
-           (pp
-            (push-prepositional-phrase tt))
-           
-           (conjunction
-            (push-conjunction tt))
-           
-           (subordinate-conjunction
-            (push-subordinate-conjunction tt))
-           
-           ((parentheses square-brackets)
-            (push-parentheses tt))
-           
-           (quantifier
-            ;; drop it on the floor for now: "each of"
-            )
-           (det
-            (if (eq (edge-category tt) category::that)
-              (then
-                (when *show-thatcomps* 
-                  (print "IGNORING LIKELY THATCOMP IN SWEEP")))
-              (else
-                (when *break-on-new-tt-sweep-cases*
-                  (push-debug `(,tt ,form))
-                  (break "deal with determiner that's not 'that'.~
-                     ~% tt = ~a~
-                     ~% form = ~a"
-                         tt form)))))
-           
-           (otherwise
-            (when *break-on-new-tt-sweep-cases*
-              (push-debug `(,tt ,form))
-              (break "New case in sweep.~
-                     ~% tt = ~a~
-                     ~% form = ~a"
-                     tt form)))))
-
-       (when (known-subcategorization? tt)
-         (push-subcat tt))
+       (setq edges (etypecase treetop
+                     (word (ensure-list treetop))
+                     (edge (ensure-list treetop))
+                     (edge-vector (all-edges-on treetop))))
+       ;; Now examine each of these edges
+       (loop for tt in edges
+          do (sweep/form-dispatch
+               tt prior-tt count layout))
 
        (when (eq pos-after end-pos)
          (return)) ;; leave the loop
@@ -188,14 +114,118 @@
        
        (setq rightmost-pos pos-after
              form nil ; reinitialize
-             prior-tt tt)
+             prior-tt treetop)
 
-       ) ; bottom of the loop
+       ) ; bottom of the loop over treetops
 
     (when *pending-pronoun*
       (attempt-to-dereference-pronoun *pending-pronoun* layout))
 
     layout))
+
+
+
+(defun sweep/form-dispatch (tt prior-tt count
+                            layout &aux form)
+  "Encapsulates the actual form-based dispatch. Calls subroutines in
+   forest-gophers to set the fields in the layout and help maintain
+   state."
+  (declare (special word::|of| category::that word::comma
+                    sentence-initial? subject-seen? main-verb-seen?
+                    waiting-for-non-verb))
+  
+  (when (edge-p tt) (setq form (edge-form tt)))
+  (tr :sweep-dispatching-on tt)
+  
+  (when waiting-for-non-verb
+    (unless (vg-category? tt)
+      (tr :non-verb-seen)
+      (setq main-verb-seen? t
+            waiting-for-non-verb nil)))
+
+  (when (category-p form)
+    (case (cat-name form) ;; this is a gross control structure, but it works
+      
+      ((np proper-name proper-noun n-bar common-noun
+           pronoun wh-pronoun reflexive/pronoun possessive/pronoun)
+       (unless (word-p tt)
+         ;;/// pull back inline when it's all worked out
+         (catalog-np-for-sweep tt prior-tt count form layout)))
+
+      (vp
+       (push-verb-phrase tt))
+
+      ((verb vg)
+       (if main-verb-seen?
+         ;;/// need to modify verb builder and set of form categories
+         ;; to retain the participlial nature of, e.g. "inhibiting"
+         ;; which then would be picked up by a different clause here
+         (push-post-mvs-verbs tt)
+         (else
+           (set-main-verb tt)
+           ;; accommodate mulitple main verbs, seen across several calls
+           (setq waiting-for-non-verb t))))
+      
+      (adjective
+       (push-loose-adjective tt))
+      
+      (s
+       (push-loose-clauses tt))
+      
+      (subj+verb
+       (push-loose-subj+verb tt))
+      
+      (adverb
+       (if sentence-initial?
+         (setf (starts-with-adverb (layout)) tt)
+         (push-loose-adverb tt)))
+      
+      ((preposition spatial-preposition spatio-temporal-preposition) ;; under
+       (when sentence-initial?
+         (setf (starts-with-prep (layout)) tt))
+       (let ((prep (edge-left-daughter tt)))
+         (if (eq prep word::|of|)
+           (push-of tt)
+           (push-preposition tt prior-tt))))
+      
+      (pp
+       (push-prepositional-phrase tt))
+      
+      (conjunction
+       (push-conjunction tt))
+      
+      (subordinate-conjunction
+       (push-subordinate-conjunction tt))
+      
+      ((parentheses square-brackets)
+       (push-parentheses tt))
+      
+      (quantifier
+       ;; drop it on the floor for now: "each of"
+       )
+      (det
+       (if (eq (edge-category tt) category::that)
+         (then
+           (when *show-thatcomps* 
+             (print "IGNORING LIKELY THATCOMP IN SWEEP")))
+         (else
+           (when *break-on-new-tt-sweep-cases*
+             (push-debug `(,tt ,form))
+             (break "deal with determiner that's not 'that'.~
+                     ~% tt = ~a~
+                     ~% form = ~a"
+                    tt form)))))
+      
+      (otherwise
+       (when *break-on-new-tt-sweep-cases*
+         (push-debug `(,tt ,form))
+         (break "New case in sweep.~
+                     ~% tt = ~a~
+                     ~% form = ~a"
+                tt form))))) ;; end of form dispatch case
+
+  (when (known-subcategorization? tt)
+    (push-subcat tt)))
 
 
 ;;--- NP handler
@@ -206,14 +236,15 @@
    the catalog NP call, particularly pronouns) along with the set of
    properties we can deduce about them.")
 
-(defun catalog-np-for-sweep (tt prior-tt count form main-verb-seen? layout)
+(defun catalog-np-for-sweep (tt prior-tt count form layout)
   "Broken out as a subroutine just to make it easier to write alternatives"
   ;; maintain a stack of sentential nps..
-  (declare (special *try-incrementally-resolve-pronouns*))
+  (declare (special *try-incrementally-resolve-pronouns*
+                    main-verb-seen?))
   (let ((pending-np tt)
         (properties nil) ; to add to
         (referent (edge-referent tt)))
-    
+
     (when (np-over-that? tt)
       (push :that properties)
       (push-that tt))
@@ -223,12 +254,17 @@
       (push :subject properties)
       (set-subject tt))
 
+    (when main-verb-seen?
+      (push :post-verb properties)
+      (push-loose-np tt))
+    
     (when (and (edge-p prior-tt)
                (not main-verb-seen?)
                (or (eq (edge-category prior-tt) word::comma)
                    (and (category-p (edge-category prior-tt))
                         (memq (edge-cat-name prior-tt) '(pp adverb)))))
       (push :subject properties)
+      (break "subject =? ~a" tt)
       (set-subject tt))
     
     (when (pronoun-category? form)
@@ -250,10 +286,6 @@
             (attempt-to-dereference-pronoun tt layout)
             #+ignore(enqueue-pronoun tt))
           (push-pronoun tt))))
-
-    (when main-verb-seen?
-      (push :post-verb properties)
-      (push-loose-np tt))
 
     (when (individual-p referent)
       (when (plural? referent)
