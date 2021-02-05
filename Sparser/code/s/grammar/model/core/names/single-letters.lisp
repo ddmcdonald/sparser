4fb022a56440d51cced345acdba755f2d61ac4ad
David D. McDonald <davidmcdonald@alum.mit.edu>
2021-02-05 15:50:04 -0500

    Dealt with long-standing hassle of sentence initial capital #\a -- good reason for completion actions

--- Sparser/code/s/grammar/model/core/names/single-letters.lisp
+++ Sparser/code/s/grammar/model/core/names/single-letters.lisp
@@ -1,9 +1,9 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1992-1994,2017  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992-1994,2017,2021  David D. McDonald  -- all rights reserved
 ;;;
 ;;;     File:  "single letters"
 ;;;   Module:  "model;core:names:"
-;;;  version:  September 2017
+;;;  version:  February 2021
 
 ;; 2.0 (11/20/92 v2.3) Completely made over for new semantics
 ;;     (5/15/93) finished the makeover, 5/26 finished debugging it
@@ -58,5 +58,22 @@
         "N" "O" "P" "Q" "R" "S" "T" "U" "V" "W" "X" "Y" "Z"))
 
 (defparameter *capital-a* (find-individual 'single-capitalized-letter
-                            :letter "A"))
+                                           :letter "A"))
+
+(define-completion-action (category-named 'single-capitalized-letter)
+    :disambiguate 'is-A-actually-a-determiner)
+
+(defun is-A-actually-a-determiner (edge)
+  "Sentence-initial instances of an #\a will be capitalized, but not
+   with the intent that's the point of single capitalized letter
+   (e.g. supporting the model of initials). If this is a capital a and it's
+   sentence initial then we remake the edge as a determiner."
+  (when (eq (edge-referent edge) *capital-a*)
+    (when (eq (starts-at-pos (sentence))
+              (pos-edge-starts-at edge))
+      (revise-edge edge
+                   (category-named 'a) ; category
+                   (category-named 'det)  ; form
+                   (find-individual (category-named 'a) :word word::a)) ; referent
+      )))
 
