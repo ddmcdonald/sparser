3eb192b92cddabfba4c2393b4a9c69477343d22a
Robert Bobrow <rjbobrow@gmail.com>
2021-02-07 17:36:51 -0500

    add a large number of related-things -- nouns which take an of complement that specifies an entity/event that has, as a related item, the item specified as the head e.g. (noun "implementation" :specializes related-thing) (noun "indicator" :specializes related-thing) (noun "intolerance" :specializes related-thing) (noun "limitation" :specializes related-thing) (noun "locus" :specializes related-thing) (noun "meaning" :specializes related-thing) These are not so closely related that they should be an "attribute" of the of-complement, but they are closely related to the of complement -- in many cases they can be referred to using a possessive on the of-complement, but not all

--- Sparser/code/s/grammar/model/sl/biology/attributes.lisp
+++ Sparser/code/s/grammar/model/sl/biology/attributes.lisp
@@ -7,17 +7,13 @@
 
 ;; Moving all attributes defined in biology to this file
 
-;; non-scalar attributes
-(noun "behavior" :super attribute)
-
-(noun "content" :super attribute)
 
 (define-category time-course :specializes attribute
   :realization 
   (:noun "time course"))
 
 ;; "role" is unusual in that it is an attribute of a PAIR of items
-(noun "role" :super attribute
+(noun "role" :specializes attribute
       :binds ((process bio-process))
       :realization
       (:in process))
@@ -79,13 +75,13 @@ have with phosphorylated as a preposed modifier.) |#
   :specializes process-rate
   :realization (:noun "catalysis rate"))
 
-(noun "insensitivity" :super scalar-attribute ;; HUH?
+(noun "insensitivity" :specializes scalar-attribute ;; HUH?
       :binds ((cause biological))
       :realization
       (:noun "insensitivity"
        :to cause))
 
-(noun "sensitivity"  :super scalar-attribute
+(noun "sensitivity"  :specializes scalar-attribute
       :binds ((cause biological))
       :realization
       (:noun "sensitivity"
@@ -95,28 +91,226 @@ have with phosphorylated as a preposed modifier.) |#
   :realization
   (:noun "strength"))
 
-
+;;;;;; These should be moved to a higher level file for more general "attributes"
 
 (define-category duration :specializes scalar-attribute
-  :binds ((theme (:or process bio-method bio-mechanism)))
+  :binds ((theme top))
   :realization
   (:noun "duration"))
 
-
 ;;(delete-noun-cfr (resolve "number"))
 ;;(delete-noun-cfr (resolve "numbers"))
-
-(define-category count-of :specializes scalar-attribute 
-  :realization
-    (:noun "number"))
-
-
-
-(noun "extent" :super scalar-attribute) 
-(noun "mass" :super scalar-attribute)
-(noun "scale" :super scalar-attribute
+(noun "age" :specializes scalar-attribute)
+(noun "coefficient" :specializes scalar-attribute)
+(define-category count-of :specializes scalar-attribute
+  :realization (:noun ("number" "count")))
+(noun "consistency" :specializes scalar-attribute)
+(noun "cost" :specializes scalar-attribute) 
+(noun "degree" :specializes scalar-attribute) 
+(noun "extent" :specializes scalar-attribute)
+(noun "intensity" :specializes scalar-attribute)
+(noun "mass" :specializes scalar-attribute)
+(noun "necessity" :specializes scalar-attribute)
+(noun "odds" :specializes scalar-attribute)
+(noun "percentage" :specializes scalar-attribute)
+(noun "proportion" :specializes scalar-attribute)
+(noun "scale" :specializes scalar-attribute
               :binds ((source top)) ;; "the scale from xxx test"
-      :realization (:from source))
+      :realization (:from source)) ;;possibly just attribute
+(noun "score" :specializes scalar-attribute)
+(noun "validity" :specializes scalar-attribute)
+(noun "weight" :specializes scalar-attribute)
+(noun index-attribute :noun ( "index"  :plural ("indexes" "indices")) :specializes scalar-attribute)
+(noun value-attribute :noun  "value" :specializes scalar-attribute)
+(noun factor-attribute :noun  "factor" :specializes scalar-attribute)
 
-(noun "dynamics" :super scalar-attribute) ;;possibly just attribute
-(noun "proportion" :super scalar-attribute) ;;possibly just attribute
+;; non-scalar attributes and related-entities
+;; "true" attributes (by my intuition)
+(noun "sex" :specializes attribute)
+
+;; related things which are not quite plausible as variables on categories
+(noun "accuracy" :specializes related-thing)
+(noun "adoption" :specializes related-thing)
+(noun "aim" :specializes related-thing)
+(noun "antecedent" :specializes related-thing)
+(noun "avoidance" :specializes related-thing)
+(noun "behavior" :specializes related-thing)
+(noun "characteristics" :specializes related-thing)
+(noun "consequence" :specializes related-thing)
+(noun "collapse" :specializes related-thing)
+(noun "content" :specializes attribute)
+(noun "definition" :specializes related-thing)
+(noun "design" :specializes related-thing)
+(noun "dissemination" :specializes related-thing)
+(noun "distribution" :specializes related-thing)
+(noun "ease" :specializes related-thing)
+(noun "effectiveness" :specializes related-thing)
+(noun "feature" :specializes  related-thing) ;; not really -- but WHAT??
+
+(noun "goal" :specializes related-thing)
+(noun "implementation" :specializes related-thing)
+(noun "indicator" :specializes related-thing)
+(noun "intolerance" :specializes related-thing)
+(noun "limitation" :specializes related-thing)
+(noun "locus" :specializes related-thing)
+(noun "meaning" :specializes related-thing)
+(noun "means" :specializes related-thing)
+(noun "pattern" :specializes related-thing)
+(noun "politicization" :specializes related-thing)
+(noun "prevalence" :specializes related-thing)
+(noun "purpose" :specializes related-thing)
+(noun "recipient" :specializes related-thing)
+(noun results :noun "result" :specializes related-thing)
+(noun "rise" :specializes related-thing)
+(noun "risk" :specializes related-thing)
+(noun "sense" :specializes related-thing)
+(noun "shortage" :specializes related-thing) ;; maybe scalar-attribute??
+(noun "shortness" :specializes related-thing)
+(noun "source" :specializes related-thing)
+(noun "stage" :specializes related-thing)
+(noun "structure" :specializes related-thing)
+(noun "threat" :specializes related-thing)
+(noun "time" :specializes related-thing)
+
+(define-category spread  :specializes related-thing :realization (:noun "spread" :verb  "spread"))
+(define-category transmission  :specializes related-thing
+  :binds
+  ((ACROSS top)
+       (OVER top)
+       (THROUGH top)
+       (TO top))
+  :realization
+  (:noun "transmission"
+   :verb ( "transmit"  :TENSED/SINGULAR "transmits" :PRESENT-PARTICIPLE "transmitting" :PAST-TENSE "transmitted")
+   :across ACROSS :over OVER :through THROUGH :to TO))
+
+
+(define-category course :specializes related-thing ;;"course of the disease"
+  :mixins (biological)
+  :binds
+  ((ON top)
+   (OFF top)
+   (IN top))
+  :realization (:noun  "course" :on ON :off OFF :in IN))
+(noun "dynamics" :specializes course) ;;possibly just attribute
+
+(define-category estimate :specializes related-thing
+  :binds
+  ((BY top)
+   (BEYOND top)
+   (|ACCORDING TO| top))
+  :realization (:noun  "estimate" :by BY :beyond BEYOND :|ACCORDING TO| |ACCORDING TO|))
+
+(define-category phase :specializes related-thing
+  :binds
+  ((IN top)
+   (|OUT OF| top))
+  :realization (:noun  "phase" :in IN :|OUT OF| |OUT OF|))
+
+
+;;;;;; bulk addition to be reviewed
+(noun "area" :specializes scalar-attribute)
+
+(noun "acceptance" :specializes related-thing)
+(noun "acknowledgment" :specializes related-thing)
+(noun "adequacy" :specializes related-thing)
+(noun "advantage" :specializes related-thing)
+(noun "appraisal" :specializes related-thing)
+(noun "authority" :specializes related-thing)
+(noun "basics" :specializes related-thing)
+(noun "battery" :specializes related-thing)
+(noun "body" :specializes related-thing)
+(noun "booster" :specializes related-thing)
+(noun "breakdown" :specializes related-thing)
+(noun "bulletin" :specializes related-thing)
+(noun "capital" :specializes related-thing)
+(noun "choice" :specializes related-thing)
+(noun "cohort" :specializes related-thing)
+(noun "committee" :specializes related-thing)
+(noun "concept" :specializes related-thing)
+(noun "conduit" :specializes related-thing)
+(noun "conscientiousness" :specializes related-thing)
+(noun "council" :specializes related-thing)
+(noun "dearth" :specializes related-thing)
+(noun "demographics" :specializes related-thing)
+(noun "diagnosis" :specializes related-thing)
+;;(noun "domain" :specializes related-thing) conflict with protein-domain
+(noun "element" :specializes related-thing)
+(noun "epicenter" :specializes related-thing)
+(noun "epicentre" :specializes related-thing)
+(noun "epidemiology" :specializes related-thing)
+(noun "episode" :specializes related-thing)
+(noun "error" :specializes related-thing)
+(noun "forefront" :specializes related-thing)
+(noun "foundation" :specializes related-thing)
+(noun "genesis" :specializes related-thing)
+(noun "graph" :specializes related-thing)
+(noun "guideline" :specializes related-thing)
+(noun "guise" :specializes related-thing)
+(noun "habit" :specializes related-thing)
+(noun "immediacy" :specializes related-thing)
+(noun "impression" :specializes related-thing)
+(noun "influx" :specializes related-thing)
+(noun "instruction" :specializes related-thing)
+(noun "interface" :specializes related-thing)
+(noun "language" :specializes related-thing)
+(noun "law" :specializes related-thing)
+(noun "life" :specializes related-thing)
+(noun "literacy" :specializes related-thing)
+(noun "lot" :specializes related-thing)
+(noun "majority-auto" :specializes related-thing)
+(noun "manifestation" :specializes related-thing)
+(noun "margin" :specializes related-thing)
+(noun "matrix" :specializes related-thing)
+(noun "midst" :specializes related-thing)
+(noun "ministry" :specializes related-thing)
+(noun "multitude" :specializes related-thing)
+(noun "network" :specializes related-thing)
+(noun "onset" :specializes related-thing)
+(noun "origin" :specializes related-thing)
+(noun "parameter" :specializes related-thing)
+(noun "patchwork" :specializes related-thing)
+(noun "platform" :specializes related-thing)
+(noun "policy" :specializes related-thing)
+(noun "preference" :specializes related-thing)
+(noun "principle" :specializes related-thing)
+(noun "priority" :specializes related-thing)
+(noun "procedure" :specializes related-thing)
+(noun "product" :specializes related-thing)
+(noun "prohibition" :specializes related-thing)
+(noun "property" :specializes related-thing)
+(noun "psychology" :specializes related-thing)
+(noun "rating" :specializes related-thing)
+(noun "reader" :specializes related-thing)
+(noun "recommendation" :specializes related-thing)
+(noun "relevance" :specializes related-thing)
+(noun "reluctance" :specializes related-thing)
+(noun "scenario" :specializes related-thing)
+(noun "scope" :specializes related-thing)
+(noun "series" :specializes related-thing)
+(noun "seriousness" :specializes related-thing)
+(noun "society" :specializes related-thing)
+(noun "specter" :specializes related-thing)
+(noun "standard-endurant" :specializes related-thing)
+(noun "statistics" :specializes related-thing)
+(noun "subgroup" :specializes related-thing)
+(noun "supporter" :specializes related-thing)
+(noun "swathe-endurant" :specializes related-thing)
+(noun "task" :specializes related-thing)
+(noun "tedium" :specializes related-thing)
+(noun "think-endurant" :specializes related-thing)
+(noun "tightness" :specializes related-thing)
+(noun "topic" :specializes related-thing)
+(noun "trait" :specializes related-thing)
+(noun "trustworthiness" :specializes related-thing)
+(noun "unaware" :specializes related-thing)
+(noun "union" :specializes related-thing)
+(noun "unit" :specializes related-thing)
+(noun "upside-endurant" :specializes related-thing)
+(noun "uptake" :specializes related-thing)
+(noun "variety" :specializes related-thing)
+(noun "venue" :specializes related-thing)
+(noun "volume" :specializes related-thing)
+(noun "waiver" :specializes related-thing)
+(noun "welfare" :specializes related-thing)
+(noun "worthy-endurant" :specializes related-thing)
