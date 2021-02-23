e1238f4a2281283ea93a0cda1160f69ec59fa3e4
David D. McDonald <davidmcdonald@alum.mit.edu>
2021-02-23 13:28:16 -0500

    cleaning up for easier reading

--- Sparser/code/s/grammar/model/core/mid-level/events.lisp
+++ Sparser/code/s/grammar/model/core/mid-level/events.lisp
@@ -23,30 +23,26 @@
      transition
      accomplishment
 
- Event-relation is also in that file.
-    
 |# 
 
 ;; It is possible that this class carries the essential wwight of "method"
 
 (define-category activity-with-a-purpose ;; aka 'purposive activity'
-  :specializes process ;; RJB -- can't see why this would ever be a "state"
+  :specializes process 
   :mixins (with-agent) ;; see below -- only agents can have a purpose
   :documentation "An activity done with the aim of achieving a particular
  thing (dictionary.cambridge.org). Motivated by 'experiment' and 'study'
  which are elaborate, extended activities that want to test some conjecture.
  Could make the agents, aim, and methods explicit but they can be implicit
- in the meaning of the word --
- the variable resoult-or-purpose only occurs when the subject of the activity-with-a-purpose
- is an agent that can have a purpose -- animal, organizational, social-agent, ..."
+ in the meaning of the word -- the variable resoult-or-purpose only occurs
+ when the subject of the activity-with-a-purpose is an agent that can have
+ a purpose -- animal, organizational, social-agent, ..."
   :binds ((result-or-purpose process))
-  :realization
-  (:s agent
-   :by agent
-   :to-comp result-or-purpose
-   :for result-or-purpose
-   :based\ on reason
-))
+  :realization (:s agent
+                :by agent
+                :to-comp result-or-purpose
+                :for result-or-purpose
+                :based\ on reason))
 
 (define-category directed-activity-with-a-purpose
   :specializes activity-with-a-purpose
@@ -63,7 +59,6 @@
 
 
 ;;;;;;;; EVENT RELATIONS -- sequential and otherwise... NEEDS a bunch more work
-
 
 (define-category event-relation
   :specializes perdurant
