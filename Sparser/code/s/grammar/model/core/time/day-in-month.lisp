d3a8edab7f8e244e4a51eb918cf92c37d9810da3
David D. McDonald <davidmcdonald@alum.mit.edu>
2021-02-24 14:02:00 -0500

    Rule for "March 5th"

--- Sparser/code/s/grammar/model/core/time/day-in-month.lisp
+++ Sparser/code/s/grammar/model/core/time/day-in-month.lisp
@@ -1,10 +1,10 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
 ;;; copyright (c) 1991  Content Technologies Inc.
-;;; copyright (c) 1992,1993 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992-1993,2021 David D. McDonald  -- all rights reserved
 ;;;
 ;;;     File:  "day-in-month"
 ;;;   Module:  "model;core:time:"
-;;;  version:  1.1 September 1993
+;;;  version:  February 2021
 
 ;; initiated 7/31 v1.8.6
 ;; 1.0 (12/15/92 v2.3) setting up for new semantics
@@ -32,6 +32,11 @@
 (def-cfr day-of-the-month (month number)
   :form np
   :referent (:instantiate-individual day-of-the-month
-                :with (month left-edge
-                       number right-edge)))
+             :with (month left-edge
+                    number right-edge)))
 
+(def-cfr day-of-the-month (month ordinal)
+  :form np
+  :referent (:instantiate-individual day-of-the-month
+             :with (month left-edge
+                    number right-edge)))
