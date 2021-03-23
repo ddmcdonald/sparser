89107fb8cb321f909a8016256d42e7033529be80
David D. McDonald <davidmcdonald@alum.mit.edu>
2021-03-23 13:00:17 -0400

    Added a sanity check to analyze-directory-files to help debugging bad namestrings

--- Sparser/code/s/drivers/sources/directory.lisp
+++ Sparser/code/s/drivers/sources/directory.lisp
@@ -1,10 +1,10 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
 ;;; copyright (c) 1990-1991  Content Technologies Inc.
-;;; copyright (c) 1992,2013,2020 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992,2013,2020-20201 David D. McDonald  -- all rights reserved
 ;;;
 ;;;     File:  "directory"
 ;;;   Module:  "drivers;sources:"
-;;;  Version:   March 2020
+;;;  Version:   March 2021
 
 ;; 1/30/13 Added .txt to the string passed to directory so it would work.
 ;;  Might be Clozure-specific requirement. 3/29/13 Added keyword to pass
@@ -28,7 +28,10 @@
    which is the ultimate driver"
   (let* ((pathname (decode-file-expression/pathname directory))
          (wildcard-namestring (string-append pathname "*" file-extension)))
+    (push-debug `(,wildcard-namestring))
     (let ((files (directory wildcard-namestring)))
+      (when (null files)
+        (break "No files retrived:~%~a" wildcard-namestring))
       (loop for filename in files
          do (progn
               (when run-traces
