de24b1b7fe3ea46beea043d24cca7ae0d45d93e7
David D. McDonald <davidmcdonald@alum.mit.edu>
2021-04-06 14:20:25 -0400

    Upgraded documentation on how to use these timers

--- Sparser/code/s/drivers/timing/calculation.lisp
+++ Sparser/code/s/drivers/timing/calculation.lisp
@@ -1,13 +1,21 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:sparser -*-
-;;; copyright (c) 1992,2014,2017,2020 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992,2014,2017,2020-2021 David D. McDonald  -- all rights reserved
 ;;; 
 ;;;     File:  "calculation"
 ;;;   Module:  "drivers;timing:"
-;;;  Version:   June 2020
+;;;  Version:   March 2021
 
 ;; initiated 2/91. Added some usage documentation 1/27/14.
 
+;; Provides a simple basis for managing diverse timers based on
+;; symbols that record the internal real time when they are
+;; started and subtract that from the current eal time when
+;; they are stoped. Pairs of start and stop calls are places
+;; around the process you wan to time.
+
 (in-package :sparser)
+
+
 
 ;;--- Timer globals
 
@@ -21,7 +29,9 @@
 
 (defvar *time-to-read-document* 0
   "Started in the 'when-reading' segment of the article method of
-   read-from-document. Stopped in that segment at the method's end")
+   read-from-document. Stopped in that segment at the method's end.
+   For operating over files, analyze-text-from-file starts the
+   timer and finish-paragraph-based-article stops it.")
 
 
 ;;--- start/stop timers
@@ -29,14 +39,14 @@
 (defun start-timer (symbol)
   (unless (boundp symbol)
     (error "The symbol passed to the timer is undefined.~
-            ~%       Check the spelling."))
+          ~%       Check the spelling."))
   (set symbol (get-internal-real-time)))
 
 
 (defun stop-timer (symbol)
   (unless (boundp symbol)
     (error "The symbol passed to the timer is undefined.~
-            ~%       Check the spelling."))
+          ~%       Check the spelling."))
   (let ((delta (- (get-internal-real-time)
                   (symbol-value symbol))))
     ;; avoid potential downstream divide by zero
