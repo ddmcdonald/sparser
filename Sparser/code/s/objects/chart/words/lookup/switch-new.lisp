2d3b544ec93c550114be9e6bc61108885778cb87
David D. McDonald <davidmcdonald@alum.mit.edu>
2021-04-07 11:45:39 -0400

    Rewrote establish-unknown-word as a dispatch instead of an operation of symbol-function

--- Sparser/code/s/objects/chart/words/lookup/switch-new.lisp
+++ Sparser/code/s/objects/chart/words/lookup/switch-new.lisp
@@ -1,9 +1,9 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
-;;; copyright (c) 1992-1994,2012-2015  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992-1994,2012-2015,2021  David D. McDonald  -- all rights reserved
 ;;; 
 ;;;     File:  "switch new"
 ;;;   Module:  "objects;chart:words:lookup:"
-;;;  Version:   1.0 October 2015
+;;;  Version:   April 2021
 
 ;; 1.0 (2/6/92 v2.2) Pulled the original version of Make-new... and
 ;;      replaced it with two more specific cases
@@ -14,45 +14,25 @@
 
 (in-package :sparser)
 
-
-(defun establish-unknown-word (&optional symbol second-optional)
-  "Switched function -- select an unknown word handler and call
-   what-to-do-with-unknown-words with the appropriate keyword.
-   Extend the code if you need another treatment. All but one
-   of the handlers take at most single argument, the character type 
-   of the word that is presently in the word-buffer. One of them
-   wants to know if the word already exists (see find-word) so it
-   takes a second argument."
-  (declare (ignore symbol second-optional))
-  (error "Switch that determines what protocol to use with unknown ~
-          words~%   has not been set"))
-
-
 (defun what-to-do-with-unknown-words (keyword)
   (declare (special *unknown-word-policy*))
-  ;(unless *the-constant-unknown-word*
-  ;  (create-the-unknown-word))
-  (case keyword
-    (:ignore
-     (setf (symbol-function 'establish-unknown-word)
-           (symbol-function 'constant-unknown-word)))
-    (:no-properties
-     (setf (symbol-function 'establish-unknown-word)
-           (symbol-function 'make-word/no-properties)))
-    (:capitalization-&-digits
-     (setf (symbol-function 'establish-unknown-word)
-           (symbol-function 'make-word/capitalization-&-digits)))
-    (:capitalization-digits-&-morphology
-     (setf (symbol-function 'establish-unknown-word)
-           (symbol-function 'make-word/all-properties)))
-    (:capitalization-digits-&-morphology/or-primed
-     (setf (symbol-function 'establish-unknown-word)
-           (symbol-function 'make-word/all-properties/or-primed)))
-    (:check-for-primed
-     (setf (symbol-function 'establish-unknown-word)
-           (symbol-function 'look-for-primed-word-else-all-properties)))
-    (otherwise
-     (error "Unknown unknown-word policy: ~a" keyword)))
-
   (setq *unknown-word-policy* keyword))
 
+(defun establish-unknown-word (character-type &optional existing-word)
+  "Called from find-word (and its subroutines) when the tokenizer
+   returns a word that we don't know. There are different things we
+   can do to enrich (or not) how much we know about the word.
+   The options here are ordered from most-extensive to least.
+   They all return a word."
+  (declare (special *unknown-word-policy*))
+  (ecase *unknown-word-policy*
+    (:capitalization-digits-&-morphology/or-primed
+     (make-word/all-properties/or-primed character-type existing-word))
+    (:capitalization-digits-&-morphology
+     (make-word/all-properties character-type existing-word))
+    (:check-for-primed
+     (look-for-primed-word-else-all-properties character-type existing-word))
+    (:capitalization-&-digits
+     (make-word/capitalization-&-digits character-type existing-word))
+    (:no-properties
+     (make-word/no-properties character-type existing-word))))
