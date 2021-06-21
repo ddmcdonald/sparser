7b73753ea916c7c83ae2613332c56b6d007901ae
David D. McDonald <davidmcdonald@alum.mit.edu>
2021-06-21 17:20:40 -0400

    elevated categories indefinite, definite, and possessive to their own file

--- Sparser/code/s/grammar/rules/tree-families/np.lisp
+++ Sparser/code/s/grammar/rules/tree-families/np.lisp
@@ -1,11 +1,10 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1992-2005,2011,2020 David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992-2005,2011,2020-2021 David D. McDonald  -- all rights reserved
 ;;; extensions copyright (c) 2009 BBNT Solutions LLC. All Rights Reserved
-;;; $Id:$
 ;;;
 ;;;     File:  "NP"
 ;;;   Module:  "grammar;rules:tree-families:"
-;;;  version:  October 2020
+;;;  version:  June 2021
 
 ;; initiated 8/31/92 v2.3
 ;; 0.1 (6/2/93) added explicit definitions for the mix-in category
@@ -50,28 +49,6 @@
    np-common-noun
    np-common-noun/possessive-pns
 |#
-
-
-;;;--------------------------------------------
-;;; Categories used for subtype specifications
-;;;--------------------------------------------
-;; But look at elaborations / alternatives in rules/syntax/articles
-;;  and in rules/words/determiners
-
-(define-mixin-category  indefinite
-  :instantiates nil
-  :specializes linguistic)
-  ;; The idea is to be able to search off of this in the dh
-  ;; to convert over any that are unresolved at the end.
-  ;; //never exploited yet
-
-(define-mixin-category definite
-  :instantiates nil
-  :specializes linguistic)
-
-(define-mixin-category possessive
-  :instantiates nil
-  :specializes linguistic)
 
 ;;;------------
 ;;; form rules
