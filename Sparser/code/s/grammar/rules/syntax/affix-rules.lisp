89ace22a36e1764fb843afe9b13a1951376b40d1
David D. McDonald <davidmcdonald@alum.mit.edu>
2021-08-30 14:47:43 -0400

    formatting

--- Sparser/code/s/grammar/rules/syntax/affix-rules.lisp
+++ Sparser/code/s/grammar/rules/syntax/affix-rules.lisp
@@ -38,10 +38,11 @@
     (declare (special *source-of-unknown-words-definition*))  
     (add-new-word-to-catalog word :default)
     (if *edge-for-unknown-words*
-        (unless
-            (resolve (concatenate 'string (pname word) "s"))
-          ;; Last-ditch attempt to avoid defining a typo of foreign language word which will conflict with an existing definition
-          ;;  example "studie" in some German papers which conflicts with "studies" and "study"
+        (unless (resolve (concatenate 'string (pname word) "s"))
+          ;; Last-ditch attempt to avoid defining a typo of foreign
+          ;;  language word which will conflict with an existing
+          ;;  definition example "studie" in some German papers which
+          ;;  conflicts with "studies" and "study"
           (setup-common-noun word))
         (assign-brackets-as-a-common-noun word))))
 
