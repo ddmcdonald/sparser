c3d36c1c5da612bb0638514f70ee87f262c4f53d
David D. McDonald <davidmcdonald@alum.mit.edu>
2021-09-22 21:33:47 -0400

    Added "Press"

--- Sparser/code/s/grammar/model/dossiers/co-indicating-words.lisp
+++ Sparser/code/s/grammar/model/dossiers/co-indicating-words.lisp
@@ -1,9 +1,9 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:SPARSER -*-
-;;; copyright (c) 1994  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1994,2021  David D. McDonald  -- all rights reserved
 ;;;
 ;;;     File:  "co indicating words"
 ;;;   Module:  "model;dossiers:"
-;;;  version:  July 1994
+;;;  version:  September 2021
 
 ;; initiated 1/20/94 v2.3. Renamed the function 3/15
 
@@ -12,13 +12,8 @@
 (define-company-generalization-word "airlines")
 (define-company-generalization-word "holdings")
 (define-company-generalization-word "industries")
-
-;; 4/17
 (define-company-generalization-word "online")
-
-;; 4/30
+(define-company-generalization-word "Press")
 (define-company-generalization-word "Retail")
-
-;; 4/30
 (define-company-generalization-word "Services")
 
