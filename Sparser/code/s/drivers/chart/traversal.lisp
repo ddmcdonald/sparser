b484cdab1ac50305d2804ded50f41f5da9b6b463
David D. McDonald <davidmcdonald@alum.mit.edu>
2021-10-05 19:03:11 -0400

    documentation

--- Sparser/code/s/drivers/chart/traversal.lisp
+++ Sparser/code/s/drivers/chart/traversal.lisp
@@ -1,9 +1,9 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
-;;; copyright (c) 1992,1993,1994,1995  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1992-1995,2021 David D. McDonald  -- all rights reserved
 ;;; 
 ;;;     File:  "traversal"
 ;;;   Module:  "drivers;chart:"
-;;;  Version:   1.0 December 1992
+;;;  Version:  September 2021
 
 ;; 1.0 (12/6/92 v2.3) Simplified it all considerably
 
@@ -14,6 +14,19 @@
 ;;;------------
 
 (defun word-traversal-hook (word position next-position)
+  "Called from a scan operation, e.g. sweep-to-span-parentheses
+   or scan/word-level-actions, which moves word by word.
+   Traversal actions are managed by a hash table from the word
+   qua label (i.e. could be applied to edges as well) to a function
+   that takes these same arguments. This is used for bracket pairs
+   such as parentheses, double quotes, etc. Check with a call to
+   (list-hash-table *traversal-routine-table*)
+      The action is always on the matching close. The open
+    notes its oposition so the close knows what span to operate
+    over. We check for traversal hits before the no-space check
+    because the ns is greedy and moves the position, which can
+    cause the open to be missed.  "
+  
   (declare (special *trace-traversal-hook*
                     *trace-traversal-hits*))
   (when *trace-traversal-hook*
