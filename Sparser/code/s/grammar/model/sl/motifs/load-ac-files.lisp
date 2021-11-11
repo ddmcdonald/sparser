7006a379777acae24f6ae36247aa474cfedad75e
David D. McDonald <davidmcdonald@alum.mit.edu>
2021-11-11 17:27:44 -0500

    Added file with one of the steps we want to happen automatically when we run an acumen system

---
+++ Sparser/code/s/grammar/model/sl/motifs/load-ac-files.lisp
@@ -0,0 +1,44 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER COMMON-LISP) -*-
+;;; Copyright (c) 2021 SIFT LLC. All Rights Reserved
+;;;
+;;;    File: "load-ac-files"
+;;;  Module: "grammar/model/sl/motifs/
+;;; version: November 2021
+
+;; Started 11/2/21 to support auto-loading the last few
+;; steps of getting our acumen setup working
+
+
+#| What I've been doing by hand
+(load "/Users/ddm/ws/Acumen/acumen/trunk/code/sparser-driver.lisp")
+(compute-all-acumen-namestrings) 
+(compute-motific-file-names)
+
+But we also have
+  (setq *acumen-base* "/Users/ddm/ws/Acumen/acumen/trunk/")
+and the homedir conditionalization in sparser-driver.
+
+-----------
+
+Laurel contribute a pattern in her init file that does
+the first step of loading sparser-driver, ddm added the
+last step of initializing the file names.
+
+(defmacro acumen-test ()
+  (defparameter cl-user::*use-all-proteins* t)
+  (handler-bind
+      ((warning #'muffle-warning))
+    (asdf:load-system :sparser/acumen))
+  (defvar *acumen-base* "/Users/ddm/ws/Acumen/acumen/trunk/")
+  (load (concatenate 'string *acumen-base* "code/sparser-driver.lisp"))
+  (in-package :sparser)
+  (initialize-acumen-file-names) ;; -- read in to common-lisp-user
+  :loaded-acumen-configuration)
+
+|#
+
+(in-package :common-lisp-user)
+
+(defun initialize-acumen-file-names ()
+  (sparser::compute-all-acumen-namestrings)
+  (sparser::compute-motific-file-names))
--- Sparser/code/s/grammar/model/sl/motifs/loader.lisp
+++ Sparser/code/s/grammar/model/sl/motifs/loader.lisp
@@ -3,11 +3,12 @@
 ;;;
 ;;;    File: "loader"
 ;;;  Module: "grammar/model/sl/motifs/
-;;; version: July 2021
+;;; version: November 2021
 
 ;;; started 7/13/21 to hold classes for motifs.
 
 (in-package :sparser)
 
 (gload "motifs;base-categories")
+(gload "motifs;load-ac-files")
 
