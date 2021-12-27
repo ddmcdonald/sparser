7abea6a8f30440146602ef84b976ed4aca2cfa87
David D. McDonald <davidmcdonald@alum.mit.edu>
2021-12-27 18:54:58 -0500

    Small initial list of degrees

---
+++ Sparser/code/s/grammar/model/dossiers/academic-degrees.lisp
@@ -0,0 +1,19 @@
+;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER COMMON-LISP) -*-
+;;; copyright (c) 2021 David D. McDonald  -- all rights reserved
+;;;
+;;;      File:   "academic-degrees"
+;;;    Module:   grammar/model/dossiers/
+;;;   version:   December 2021
+
+;; initiated 12/27/21 to organize degrees and such evacuated from
+;; Middle-east files
+
+(in-package :sparser)
+
+(make-academic-degree "B.A")
+(make-academic-degree "B.Sc.") ;; only one in javan-online
+
+(make-academic-degree "M.B.A.")
+;; (make-academic-degree )
+;; (make-academic-degree )
+;; (make-academic-degree )
