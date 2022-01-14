3f278c96f0a21f19d81f898c3fcee33d7f310b5e
David D. McDonald <davidmcdonald@alum.mit.edu>
2022-01-14 15:06:05 -0500

    unloaded scratch file

---
+++ Sparser/code/s/grammar/model/sl/motifs/configs-remaining.lisp
@@ -0,0 +1,20 @@
+;;; Uncategorized edge records
+;;; Written 1/12/22 16:13:46
+
+
+From 640 through 791
+
+Article 640 ask_the_rabbi__does_the_torah_endorse_genocide_
+Uncategorized instances:
+  e932  (proper-noun np vp+past)
+        "asserted that Amalek"
+
+Article 731 sun__we_dont_understandgilats_mysterious_fall_might_explained_by_starband_temporarily_suspending
+Uncategorized instances:
+  e559  (proper-noun np than-np comparative-adjp vp)
+        "is nothing less than a Tower of Babel"
+
+Article 777 second_chupacabra_novel_may_be_a__curse__but_it_comes_with_a_free_ebook
+Uncategorized instances:
+  e1332 (proper-noun np parentheses quotation)
+        "'Night of the Chupacabra,' (Chupacabra Series #1)"
