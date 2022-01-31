5daeb464d9e4cb783c290d96c572b040b5d713f7
David D. McDonald <davidmcdonald@alum.mit.edu>
2022-01-31 09:34:10 -0500

    Removed the switch

--- Sparser/code/s/interface/files/read-chars.lisp
+++ Sparser/code/s/interface/files/read-chars.lisp
@@ -1,10 +1,10 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
-;;; copyright (c) 1990  Content Technologies Inc.
-;;; copyright (c) 1992  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1990 Content Technologies Inc.
+;;; copyright (c) 1992,2022 David D. McDonald  -- all rights reserved
 ;;; 
 ;;;     File:  "read chars"
 ;;;   Module:  "interface;file ops:"
-;;;  Version:  1.0 September 1992
+;;;  Version:  January 2022
 
 ;;  initiated 1/90
 ;;  0.1 (12/19/90, v1.6)  Driver renamed so it could be governed by
@@ -18,9 +18,13 @@
                                           character-array
                                           file-stream
                                           maximum-count)
+  "Called from establish-character-source/open-file. The 'first-index'
+   is relevant when using small character buffers that have to be
+   regularly refilled. Otherwise until we reach the maximum-count
+   (essentially the length of the character buffer),  we read a
+   character from the file and insert it in to the character array."
   (let ((eof (code-char 0))
         char )
-
     (dotimes (i maximum-count)
       (unless (< i first-index)
         (setq char (read-char file-stream nil eof))
--- Sparser/code/s/interface/files/read-switch.lisp
+++ Sparser/code/s/interface/files/read-switch.lisp
@@ -1,33 +1,43 @@
 ;;; -*- Mode:LISP; Syntax:Common-Lisp; Package:(SPARSER LISP) -*-
-;;; copyright (c) 1990  Content Technologies Inc.
-;;; copyright (c) 1992  David D. McDonald  -- all rights reserved
+;;; copyright (c) 1990 Content Technologies Inc.
+;;; copyright (c) 1992,2022 David D. McDonald  -- all rights reserved
 ;;; 
 ;;;     File:  "read switch"
 ;;;   Module:  "interface;file ops:"
-;;;  Version:   1.0 September 1992
+;;;  Version:  January 2022
 
 ;; put in 12/90 for mwm
 ;; 1.0 (9/26/92 v2.3) dropped the case-switching option
 
 (in-package :sparser)
 
-
-(defun read-chars-into-buffer/maximum-count
-       (buffer file-stream maximum-count)
-  (declare (ignore buffer file-stream maximum-count))
-  (break "The file->buffer reader isn't initialized.~
-          ~%   Check the switch settings."))
+;; 1/29/22 Doesn't make sense to contemplate having another style of
+;;  buffer populator at this point.
 
 
 (defvar *character-translation-protocol* nil)
 
-
-;; (establish-character-translation-protocol :no-changes)
-
 (defun establish-character-translation-protocol (keyword)
-  (ecase keyword
-   (:no-changes
-    (setf (symbol-function 'read-chars-into-buffer/maximum-count)
-          (symbol-function 'read-chars-into-buffer/no-changes))))
+  "Invoked from switch settings. Not point in modifying them
+   now that we have the prescan. Converting this to a no-op"
+  (unless (eq keyword :no-changes)
+    (error "The only valid character translation protocol is :no-changes.~
+          ~%Not ~A" keyword))
   (setq *character-translation-protocol* keyword))
+
+#+ignore
+  (ecase keyword
+    (setf (symbol-function 'read-chars-into-buffer/maximum-count)
+          (symbol-function 'read-chars-into-buffer/no-changes)))
+  
+
+(defun read-chars-into-buffer/maximum-count (first-index buffer
+                                             file-stream maximum-count)
+  "Just a pass-through"
+  (read-chars-into-buffer/no-changes first-index buffer file-stream maximum-count))
+  
+
+
+
+
 
