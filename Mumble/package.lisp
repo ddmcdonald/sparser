76cd0aab913518f617f198add4ae1c3b7b6df4dc
David D. McDonald <davidmcdonald@alum.mit.edu>
2023-08-02 15:12:41 -0400

    Remove debugging code

--- Mumble/package.lisp
+++ Mumble/package.lisp
@@ -7,15 +7,13 @@
 ;;;   reserved. Permission is granted to use and copy
 ;;;   this file of the Mumble-86 system for
 ;;;   non-commercial purposes.
-;;; Copyright (c) 2010-2017 David D. McDonald
+;;; Copyright (c) 2010-2023 David D. McDonald
 
 ;; Modified, 12/15/10 (ddm) to provides a location for packages and
 ;; exports to be defined across the whole system so that they can 
 ;; be available early before anything other than utilities have
 ;; been loaded. 11/19/13 Added lots more exports, done in the correct
 ;; style. 3/27/16 (afp) Simplified, renamed.
-
-(in-package :cl-user)
 
 (defpackage :mumble
   (:use :common-lisp :ddm-util)
